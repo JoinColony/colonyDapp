24203a21e7b836172c33a486e1ce1bb0db9e4329
elenadimitrova <elenavdimitrova@gmail.com>
2017-11-14 12:32:31 +0200

    Creating a colony via ethers

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import App from './App.jsx';
 
 import { abi } from '../colonyNetwork/build/contracts/ColonyNetwork.json';
 import { networks } from '../colonyNetwork/build/contracts/EtherRouter.json';
+import { abi as colonyAbi } from '../colonyNetwork/build/contracts/Colony.json';
+import { private_keys as accounts } from '../colonyNetwork/test-accounts.json';
 
 // We're dynamically loading this as this is not always needed
 const loadWeb3Signer = async () => {
@@ -18,21 +20,26 @@ const loadWeb3Signer = async () => {
 
 // Address is from deployed EtherRouter
 const etherRouterAddress = networks[Object.keys(networks)[0]].address;
+const privKey = `0x${accounts[Object.keys(accounts)[0]]}`;
+
 const provider = new providers.JsonRpcProvider();
 // This is the wallets private key
-const fundedWallet = new Wallet('0x7231a774a538fce22a329729b03087de4cb4a1119494db1c10eae3bb491823e7', provider);
+const fundedWallet = new Wallet(privKey, provider);
 
 (async function init() {
   let colonyNetwork;
-  if (window.web3 && window.web3.currentProvider) {
-    const Web3Signer = await loadWeb3Signer();
-    colonyNetwork = new Contract(etherRouterAddress, abi, new Web3Signer());
-  } else {
+  // if (window.web3 && window.web3.currentProvider) {
+  //   const Web3Signer = await loadWeb3Signer();
+  //   colonyNetwork = new Contract(etherRouterAddress, abi, new Web3Signer());
+  // } else {
     colonyNetwork = new Contract(etherRouterAddress, abi, fundedWallet);
-  }
-  await colonyNetwork.createColony('0x123456');
-  // const address = await colonyNetwork.functions.getColony('coolony');
-  // console.log(address);
+  // }
+
+  await colonyNetwork.createColony('0x123456', { gasLimit: 4300000 });
+  const address = await colonyNetwork.getColony('0x123456');
+  const colony = new Contract(address[0], colonyAbi, fundedWallet);
+  const version = await colony.version();
+  console.log(version[0].toString());
 }());
 
 const rootNode = document.getElementById('root');
