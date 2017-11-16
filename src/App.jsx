0ab0bdba02700c1f0b431894429547c3fcda79e3
Christian Maniewski <chmanie@fastmail.com>
2017-11-16 14:54:28 +1100

    Add redux and react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 /* @flow */
 
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import {
   BrowserRouter as Router,
@@ -8,16 +10,27 @@ import {
   Link,
 } from 'react-router-dom';
 
+// Simple example reducer
+const reducer = (state = {}) => state;
+
+const store = createStore(
+  reducer,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
 const Home = () => <div>Hello World</div>;
 const Other = () => <div>Other route</div>;
 
 export default () => (
-  <Router>
-    <div>
-      <Link to="/" href="/">Home</Link>
-      <Link to="/other" href="/other">Other</Link>
-      <Route exact path="/" component={Home} />
-      <Route path="/other" component={Other} />
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Link to="/" href="/">Home</Link>
+        <Link to="/other" href="/other">Other</Link>
+        <Route exact path="/" component={Home} />
+        <Route path="/other" component={Other} />
+      </div>
+    </Router>
+  </Provider>
 );
