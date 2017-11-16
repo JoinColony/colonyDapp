75787712f3e7cbfaaea26e2ab7df31b05f7b2d3c
Christian Maniewski <chmanie@fastmail.com>
2017-11-16 14:35:09 +1100

    Add simple react-router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 /* @flow */
 
-import * as React from 'react';
+import React from 'react';
 
-export default () => <div>Hello World</div>;
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const Home = () => <div>Hello World</div>;
+const Other = () => <div>Other route</div>;
+
+export default () => (
+  <Router>
+    <div>
+      <Link to="/" href="/">Home</Link>
+      <Link to="/other" href="/other">Other</Link>
+      <Route exact path="/" component={Home} />
+      <Route path="/other" component={Other} />
+    </div>
+  </Router>
+);
