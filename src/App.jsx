5b4af366f8da84f28bbec7f3b149da3bd9c34a42
Christian Maniewski <chmanie@fastmail.com>
2017-11-16 15:15:50 +1100

    Add react-intl

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { addLocaleData, IntlProvider, FormattedMessage } from 'react-intl';
+import en from 'react-intl/locale-data/en';
 
 import {
   BrowserRouter as Router,
@@ -10,7 +12,10 @@ import {
   Link,
 } from 'react-router-dom';
 
+import messages from './i18n/en.json';
 import rootReducer from './reducer';
+
+addLocaleData(en);
 
 const store = createStore(
   rootReducer,
@@ -22,14 +27,18 @@ const Home = () => <div>Hello World</div>;
 const Other = () => <div>Other route</div>;
 
 export default () => (
-  <Provider store={store}>
-    <Router>
-      <div>
-        <Link to="/" href="/">Home</Link>
-        <Link to="/other" href="/other">Other</Link>
-        <Route exact path="/" component={Home} />
-        <Route path="/other" component={Other} />
-      </div>
-    </Router>
-  </Provider>
+  <IntlProvider locale="en" defaultLocale="en" messages={messages}>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Link to="/" href="/">
+            <FormattedMessage id="button.ok" />
+          </Link>
+          <Link to="/other" href="/other">Other</Link>
+          <Route exact path="/" component={Home} />
+          <Route path="/other" component={Other} />
+        </div>
+      </Router>
+    </Provider>
+  </IntlProvider>
 );
