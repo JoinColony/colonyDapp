f2096adf0ed08c4fd2ef3b13f59f52669c503bcb
Christian Maniewski <chmanie@fastmail.com>
2018-01-18 14:56:36 +0100

    Add FieldSet component

--- src/modules/core/components/Fields/index.js
+++ src/modules/core/components/Fields/index.js
@@ -1,4 +1,6 @@
 /* @flow */
 
-export { default as Textarea } from './Textarea';
 export { default, asField, asStandaloneField } from './Field';
+export { default as FieldSet } from './FieldSet';
+export { default as Input } from './Input';
+export { default as Textarea } from './Textarea';
