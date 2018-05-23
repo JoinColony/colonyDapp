63c26a4de89a33da32787c43189a5b18683287ab
Raul Glogovetan <raul@glogovetan.com>
2018-05-23 15:21:40 +0300

    Add: a new local domain integration tests

--- integration-testing/domain.test.js
+++ integration-testing/domain.test.js
@@ -1,11 +1,9 @@
-// import { bigNumberify } from 'ethers/utils';
-// import { isAddress } from 'web3-utils';
 import { getNetworkClient } from './utils/network-client-helpers';
 
 const colonyName = 'Integration Tests Colony';
 
 describe('`ColonyClient` is able to', () => {
-  test('Create a new Domain in the Colony', async () => {
+  test('Create a new Domain within the Colony', async () => {
     /*
      * Get the network client
      */
@@ -16,6 +14,40 @@ describe('`ColonyClient` is able to', () => {
     const colonyClient = await networkClient.getColonyClient({
       key: colonyName,
     });
-    console.log(colonyClient);
+    const domainCountBefore = await colonyClient.getDomainCount.call();
+    /*
+     * We have just the parent local skill that was added by default when the
+     * colony was created
+     */
+    expect(domainCountBefore).toHaveProperty('count', 1);
+    /*
+     * Since this is the first domain, in the first colony, it's Id is `3`
+     *
+     * The first two are reserved for the meta colony's skills
+     */
+    const newDomainTransaction = await colonyClient.addDomain.send({
+      parentSkillId: 3,
+    });
+    expect(newDomainTransaction).toHaveProperty('successful', true);
+    const domainCountAfter = await colonyClient.getDomainCount.call();
+    /*
+     * If all goes to plan we should have another domain
+     */
+    expect(domainCountAfter).toHaveProperty('count', 2);
+    /*
+     * But we shouln't be able to add a new domain to the meta colony's skill
+     */
+    try {
+      await colonyClient.addDomain.send({
+        parentSkillId: 1,
+      });
+    } catch (e) {
+      const domainCountAfterCatching = await colonyClient.getDomainCount.call();
+      /*
+       * Count should still be 2 since we shouldn't have been able to add
+       * a new domain
+       */
+      expect(domainCountAfterCatching).toHaveProperty('count', 2);
+    }
   });
 });
