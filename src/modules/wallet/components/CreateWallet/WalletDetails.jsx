b6206847a425d7da532f10d5819121299ff47dbb
karolinkas <li-nee@gmx.de>
2018-05-31 00:48:57 +0200

    add 4 rows for login methods

--- src/modules/wallet/components/CreateWallet/WalletDetails.jsx
+++ src/modules/wallet/components/CreateWallet/WalletDetails.jsx
@@ -1,13 +1,26 @@
+/* @flow */
+
 import React from 'react';
 
 import layout from '~styles/layout.css';
 import styles from './CreateWallet.css';
 
+const DetailRow = () => <div className={`${styles.row}`}>Row</div>;
+
 const WalletDetails = () => (
-  <section>
+  <section className={`${styles.content}`}>
     <div className={`${styles.title}`}>
       How would you like to access Colony?
     </div>
+    <div className={`${styles.subtitle}`}>
+      Each Colony account is accessed through an associated Ethereum wallet. You
+      can use an existing wallet that you own, or create a new wallet below.
+    </div>
+    <DetailRow />
+    <DetailRow />
+    <DetailRow />
+    <DetailRow />
+    <div className={`${styles.callToAction}`}>Need a wallet? Let us help.</div>
   </section>
 );
 
