bc9aa3fe467b8b98732f4dd05b346435dcf7b7cd
Raul Glogovetan <raul@glogovetan.com>
2018-06-25 16:12:22 +0300

    Add: token integration test to make colony owner

--- integration-testing/token.test.js
+++ integration-testing/token.test.js
@@ -16,4 +16,39 @@ describe('`ColonyNetworkClient` is able to', () => {
      */
     expect(isAddress(token)).toBeTruthy();
   });
+  test('Make the Colony owner of the Token', async () => {
+    /*
+     * Get the network client
+     */
+    const networkClient = await getNetworkClient();
+    /*
+     * Get the number of colonies. This will also represent the last created
+     * colony's Id which we created in the previous step.
+     */
+    const { count: lastColonyId } = await networkClient.getColonyCount.call();
+    /*
+     * Get the existing colony
+     */
+    const colonyClient = await networkClient.getColonyClient(lastColonyId);
+    /*
+     * Get the token's owner
+     */
+    const tokenOwner = await colonyClient.token.contract.owner();
+    /*
+     * Make the colony the owner of the Token
+     *
+     * We only do this if the colony isn't already an owner, otherwise your
+     * trasaction will revert.
+     */
+    if (tokenOwner !== colonyClient.contract.address) {
+      const tokenAuthorityTransaction = await colonyClient.token.setOwner.send({
+        owner: colonyClient.contract.address,
+      });
+      expect(tokenAuthorityTransaction).toHaveProperty('successful', true);
+      expect(tokenAuthorityTransaction.eventData).toHaveProperty(
+        'owner',
+        colonyClient.contract.address,
+      );
+    }
+  });
 });
