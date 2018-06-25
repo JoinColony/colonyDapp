b14cf8644664e1c8711802170d0cd9c1eb7644a6
Raul Glogovetan <raul@glogovetan.com>
2018-06-25 16:35:53 +0300

    Add: token integration test to mint new ones

--- integration-testing/token.test.js
+++ integration-testing/token.test.js
@@ -1,5 +1,8 @@
 import { isAddress } from 'web3-utils';
+import { bigNumberify } from 'ethers/utils';
 import { getNetworkClient } from './utils/network-client-helpers';
+
+const tokensToMint = 1000;
 
 describe('`ColonyNetworkClient` is able to', () => {
   test('Create and deploy a new Token', async () => {
@@ -49,6 +52,40 @@ describe('`ColonyNetworkClient` is able to', () => {
         'owner',
         colonyClient.contract.address,
       );
+    }
+  });
+  test('Mint new tokens', async () => {
+    /*
+     * Get the network client
+     */
+    const networkClient = await getNetworkClient();
+    /*
+     * Get the number of colonies. This will also represent the last created
+     * colony's Id which we created in the previous step.
+     */
+    const { count: lastColonyId } = await networkClient.getColonyCount.call();
+    /*
+     * Get the existing colony
+     */
+    const colonyClient = await networkClient.getColonyClient(lastColonyId);
+    /*
+     * Get the amount of tokens before minting new ones
+     */
+    const {
+      amount: initialTokenAmount,
+    } = await colonyClient.token.getTotalSupply.call();
+    /*
+     * Mint new tokens, but only if there aren't any in the Colony
+     */
+    if (initialTokenAmount.toNumber() === 0) {
+      const mintTokensTransaction = await colonyClient.mintTokens.send({
+        amount: bigNumberify(tokensToMint),
+      });
+      expect(mintTokensTransaction).toHaveProperty('successful', true);
+      const {
+        amount: currentTokenAmount,
+      } = await colonyClient.token.getTotalSupply.call();
+      expect(currentTokenAmount.toNumber()).toEqual(tokensToMint);
     }
   });
 });
