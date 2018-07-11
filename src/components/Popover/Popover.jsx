18334e244f8510e5c7b2d1e28c4de00e6b343525
Christian Maniewski <chmanie@fastmail.com>
2018-07-11 16:26:26 +0200

    Add proper popover styles

--- src/components/Popover/Popover.jsx
+++ src/components/Popover/Popover.jsx
@@ -1,10 +1,39 @@
 /* @flow */
 
+import type { Node } from 'react';
+
 import React, { Component } from 'react';
 import { Manager, Reference, Popper } from 'react-popper';
+import nanoid from 'nanoid';
+
+import PopoverContent from './PopoverContent.jsx';
+
+export type Placement =
+  | 'auto'
+  | 'top'
+  | 'right'
+  | 'bottom'
+  | 'left'
+  | 'top-start'
+  | 'right-start'
+  | 'top-start'
+  | 'left-start'
+  | 'top-end'
+  | 'right-end'
+  | 'top-end'
+  | 'left-end';
+
+type Appearance = {
+  theme: 'dark',
+  placement: Placement,
+};
 
 type Props = {
-  children: React$Element<*> | Function,
+  appearance: Appearance,
+  /** Children to trigger the popover */
+  children: React$Element<*> | (({ ref: (?HTMLElement) => void }) => Node),
+  /** Popover placement */
+  placement?: Placement,
 };
 
 type State = {
@@ -12,30 +41,56 @@ type State = {
 };
 
 // Convention: we are using innerRef for all of our components that can be wrapped in popovers
-const getRefProps = (children, ref) =>
-  typeof children.type == 'function' ? { innerRef: ref } : { ref };
-
-const renderReference = children => {
-  if (typeof children == 'function') {
-    return children;
+const getChildProps = (children, id, ref) => {
+  const props = {};
+  if (typeof children.type == 'function') {
+    props.innerRef = ref;
+  } else {
+    props.ref = ref;
   }
-  return ({ ref }) => React.cloneElement(children, getRefProps(children, ref));
+  props['aria-describedby'] = id;
+  return props;
+};
+
+const renderReference = (children, id) => {
+  if (typeof children == 'function') {
+    return ({ ref }) => children({ ref, id });
+  }
+  return ({ ref }) =>
+    React.cloneElement(children, getChildProps(children, id, ref));
 };
 
 class Popover extends Component<Props, State> {
+  id: string;
+
+  static defaultProps = {
+    placement: 'top',
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this.id = nanoid();
+  }
+
   // state = { isOpen: false };
 
   render() {
-    const { children } = this.props;
+    const { appearance, children, placement: origPlacement } = this.props;
     return (
       <Manager>
-        <Reference>{renderReference(children)}</Reference>
-        <Popper placement="right">
+        <Reference>{renderReference(children, this.id)}</Reference>
+        <Popper placement={origPlacement}>
           {({ ref, style, placement, arrowProps }) => (
-            <div ref={ref} style={style} data-placement={placement}>
-              Popper element
-              <div ref={arrowProps.ref} style={arrowProps.style} />
-            </div>
+            <PopoverContent
+              appearance={{ ...appearance, placement }}
+              id={this.id}
+              innerRef={ref}
+              style={style}
+              placement={placement}
+              arrowProps={arrowProps}
+            >
+              Popover content!
+            </PopoverContent>
           )}
         </Popper>
       </Manager>
--- src/components/Popover/Popover.md
+++ src/components/Popover/Popover.md
@@ -1,5 +1,51 @@
 ### Simple Popover
 
+The styles of the popover content are deliberately kept simple so that you can style the inner content flexibly
+
 ```js
 <Popover><Button appearance={{ theme: 'primary' }}>Hi</Button></Popover>
+```
+
+### Popover on the right
+
+```js
+<Popover placement="right"><Button appearance={{ theme: 'primary' }}>Hi</Button></Popover>
+```
+
+### Popover on the bottom
+
+```js
+<Popover placement="bottom"><Button appearance={{ theme: 'primary' }}>Hi</Button></Popover>
+```
+
+### Popover on the left
+
+```js
+<Popover placement="left"><Button appearance={{ theme: 'primary' }}>Hi</Button></Popover>
+```
+
+### Popover dark theme
+
+```js
+<Popover appearance={{ theme: 'dark' }}><Button appearance={{ theme: 'primary' }}>Hi</Button></Popover>
+```
+
+### Popover using a render function
+
+Use this method if you have complex elements
+
+**Heads up!** Don't forget to add `aria-describedby` and `innerRef` / `ref` to the button yourself! This will happen automatically when using the simple method.
+
+```js
+<Popover>
+  {({ ref, id }) =>
+    <Button
+      appearance={{ theme: 'primary' }}
+      innerRef={ref}
+      aria-describedby={id}
+    >
+      Hi
+    </Button>
+  }
+</Popover>
 ```
