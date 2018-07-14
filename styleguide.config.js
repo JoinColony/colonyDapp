d34089f650856daa2c5ab7aabe335ec1281cc23d
Christian Maniewski <chmanie@fastmail.com>
2018-07-14 14:50:48 +0200

    Prepare webpack-config for dApp code

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 module.exports = {
   title: 'Colony UI Style Guide',
-  // components: '**/[A-Z]*.jsx',
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/styleguide/Wrapper.jsx'),
   },
@@ -11,17 +10,15 @@ module.exports = {
     {
       name: 'Forms',
       components: [
-        './src/components/Fields/Input/Input.jsx',
-        './src/components/Fields/InputLabel/InputLabel.jsx',
-        './src/components/Button/Button.jsx',
+        './src/modules/core/components/Fields/Input/Input.jsx',
+        './src/modules/core/components/Fields/InputLabel/InputLabel.jsx',
+        './src/modules/core/components/Button/Button.jsx',
       ],
       content: 'docs/Forms.md',
     },
     {
       name: 'Popovers & Modals',
-      components: [
-        './src/components/Popover/Popover.jsx'
-      ],
+      components: ['./src/modules/core/components/Popover/Popover.jsx'],
     },
   ],
 };
