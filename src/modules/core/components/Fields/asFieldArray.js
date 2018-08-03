415d2b991e6960e20f504884df8c101d4495bfb7
Curtis Olson <curtis@colony.io>
2018-08-03 11:19:28 -0400

    Fix component re-mounting

--- src/modules/core/components/Fields/asFieldArray.js
+++ src/modules/core/components/Fields/asFieldArray.js
@@ -1,11 +1,12 @@
 /* @flow */
 import { createElement } from 'react';
-import { withProps } from 'recompose';
 import { FieldArray } from 'formik';
 
-const asFieldArray = () => (Component: any) => ({ name, ...props }: Object) => {
-  const ComponentWithProps = withProps({ name, ...props })(Component);
-  return createElement(FieldArray, { name, component: ComponentWithProps });
-};
+const asFieldArray = () => (Component: any) => ({ name, ...props }: Object) =>
+  createElement(FieldArray, {
+    name,
+    render: formikProps =>
+      createElement(Component, { ...props, ...formikProps }),
+  });
 
 export default asFieldArray;
