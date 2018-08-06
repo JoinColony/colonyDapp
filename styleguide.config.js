7f1bc10e27b6465aae005702c5ca7c209edfbef6
Richard Hoffmann <rhoffmann@fastmail.com>
2018-08-06 21:02:41 +0200

    corrected path naming in textarea

--- styleguide.config.js
+++ styleguide.config.js
@@ -20,7 +20,7 @@ module.exports = {
       components: [
         './src/modules/core/components/Fields/Input/Input.jsx',
         './src/modules/core/components/Fields/InputLabel/InputLabel.jsx',
-        './src/modules/core/components/Fields/TextArea/TextArea.jsx',
+        './src/modules/core/components/Fields/Textarea/Textarea.jsx',
         './src/modules/core/components/Button/Button.jsx',
         './src/modules/core/components/MnemonicGenerator/MnemonicGenerator.jsx',
         './src/modules/core/components/MnemonicDnDSorter/MnemonicDnDSorter.jsx',
