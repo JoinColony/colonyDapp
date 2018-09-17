73a278c9553bd25c1741df52e3f63acc891755d4
James Lefrère <james@colony.io>
2018-09-17 21:15:13 +0200

    Remove lib provisioning
    * Remove colonyJS/colony-wallet submodule library provisioning steps

--- scripts/provision_submodules.sh
+++ scripts/provision_submodules.sh
@@ -3,8 +3,6 @@
 # Paths
 LIB_PATH="src/lib"
 
-CLIENT="colonyJS"
-WALLET="colony-wallet"
 NETWORK="colonyNetwork"
 PINNING="pinningService"
 
@@ -22,23 +20,6 @@ log() {
 # Update / re-pull submodules
 log "Initialize submodule libs"
 git submodule update --init --recursive
-
-# Build client
-log "Building '${CLIENT}' submodule"
-# We need global lerna to build the client's packages, make sure we have installed globally
-#
-yarn global add lerna
-cd "${LIB_PATH}/${CLIENT}"
-yarn --ignore-engines
-lerna run build
-cd ${ROOT_PATH}
-
-# Build wallet
-log "Building '${WALLET}' submodule"
-cd "${LIB_PATH}/${WALLET}"
-yarn
-yarn build:dev
-cd ${ROOT_PATH}
 
 # Build network
 log "Building '${NETWORK}' submodule"
