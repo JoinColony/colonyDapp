24104d2bc50e2b9910b8e56a3fc3a559a01c6d53
Christian Maniewski <chmanie@fastmail.com>
2018-09-17 22:04:34 +0200

    Add alternative theme to ProfileTemplate

--- src/modules/pages/ProfileTemplate/ProfileTemplate.jsx
+++ src/modules/pages/ProfileTemplate/ProfileTemplate.jsx
@@ -3,17 +3,24 @@ import type { Node } from 'react';
 
 import React from 'react';
 
+import { getMainClasses } from '~utils/css';
+
 import styles from './ProfileTemplate.css';
 
+type Appearance = {
+  theme: 'alt',
+};
+
 type Props = {
+  appearance?: Appearance,
   children: Node,
   asideContent: Node,
 };
 
 const displayName = 'pages.ProfileTemplate';
 
-const ProfileTemplate = ({ children, asideContent }: Props) => (
-  <div className={styles.profileTemplate}>
+const ProfileTemplate = ({ appearance, children, asideContent }: Props) => (
+  <div className={getMainClasses(appearance, styles)}>
     <aside className={styles.sidebar}>{asideContent}</aside>
     <div className={styles.mainContainer}>
       {/* header will go here */}
