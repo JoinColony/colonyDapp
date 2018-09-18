d3f5b908b2707ef824c33fdfd34eeb774953845e
Christian Maniewski <chmanie@fastmail.com>
2018-09-18 20:47:04 +0200

    Add InputStatus component

---
+++ src/modules/core/components/Fields/InputStatus/InputStatus.jsx
@@ -0,0 +1,63 @@
+/* @flow */
+
+import type { IntlShape, MessageDescriptor } from 'react-intl';
+
+import React, { Fragment } from 'react';
+import { injectIntl } from 'react-intl';
+
+import { getMainClasses } from '~utils/css';
+
+import styles from './InputStatus.css';
+
+type Appearance = {
+  // Make it covariant: tell flow we're not changing it in here
+  +theme?: 'fat' | 'underlined' | 'minimal',
+  +direction?: 'horizontal',
+  +colorSchema?: 'dark' | 'transparent',
+};
+
+type Props = {
+  /** Appearance object */
+  appearance?: Appearance,
+  /** Error text (if applicable) */
+  error?: string | MessageDescriptor,
+  /** Status text (if applicable) */
+  status?: string | MessageDescriptor,
+  /** @ignore injected by `react-intl` */
+  intl: IntlShape,
+};
+
+const displayName = 'InputStatus';
+
+const InputStatus = ({
+  appearance = {},
+  error,
+  intl: { formatMessage },
+  status,
+}: Props) => {
+  const errorText = typeof error == 'object' ? formatMessage(error) : error;
+  const statusText = typeof status == 'object' ? formatMessage(status) : status;
+  const text = errorText || statusText;
+  if (!text) {
+    return null;
+  }
+  return (
+    <Fragment>
+      {appearance.direction === 'horizontal' ? (
+        <span
+          className={getMainClasses(appearance, styles, { error: !!error })}
+        >
+          {text}
+        </span>
+      ) : (
+        <p className={getMainClasses(appearance, styles, { error: !!error })}>
+          {text}
+        </p>
+      )}
+    </Fragment>
+  );
+};
+
+InputStatus.displayName = displayName;
+
+export default injectIntl(InputStatus);
---
+++ src/modules/core/components/Fields/InputStatus/InputStatus.md
@@ -0,0 +1,11 @@
+### Positive (neutral) status
+
+```js
+<InputStatus status="Cool" />
+```
+
+### Error status
+
+```js
+<InputStatus error="Oh noes!" />
+```
