97f7388fcfead46ac4b4e80a8b6d61e5b7279074
Raul Glogovetan <raul@glogovetan.com>
2018-09-19 20:46:26 +0300

    Add: wire up correct icons in user `Navigation` component

--- src/modules/users/components/Navigation/Navigation.jsx
+++ src/modules/users/components/Navigation/Navigation.jsx
@@ -1,23 +1,40 @@
 /* @flow */
 import React from 'react';
 
-import Icon from '../../../core/components/Icon';
-import Link from '../../../core/components/Link';
+import { defineMessages } from 'react-intl';
+
+import Icon from '~core/Icon';
+import Link from '~core/Link';
 
 import styles from './Navigation.css';
 
 const displayName = 'users.Navigation';
 
+const MSG = defineMessages({
+  dashboardTitle: {
+    id: 'Navigation.dashboardTitle',
+    defaultMessage: 'Go to your Dashboard',
+  },
+  walletTitle: {
+    id: 'Navigation.walletTitle',
+    defaultMessage: 'Go to your Wallet',
+  },
+  inboxTitle: {
+    id: 'Navigation.inboxTitle',
+    defaultMessage: 'Go to your Inbox',
+  },
+});
+
 const Navigation = () => (
   <nav className={styles.main}>
     <Link to="/" className={styles.navigationItem}>
-      <Icon name="forbidden-signal" title="file" />
+      <Icon name="home" title={MSG.dashboardTitle} />
     </Link>
     <Link to="/" className={styles.navigationItem}>
-      <Icon name="forbidden-signal" title="file" />
+      <Icon name="wallet" title={MSG.walletTitle} />
     </Link>
     <Link to="/" className={styles.navigationItem}>
-      <Icon name="forbidden-signal" title="file" />
+      <Icon name="envelope" title={MSG.inboxTitle} />
     </Link>
   </nav>
 );
