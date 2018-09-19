b03fe46a673d82f5dc5deee5749d9b6631a0399e
Christian Maniewski <chmanie@fastmail.com>
2018-09-20 00:23:32 +0200

    🤫

--- src/modules/core/components/ExternalLink/ExternalLink.jsx
+++ src/modules/core/components/ExternalLink/ExternalLink.jsx
@@ -12,7 +12,7 @@ type Props = {
   href: string,
   /** A string or a `messageDescriptor` that make up the link's text */
   text: MessageDescriptor | string,
-  /** Values for loading text (react-intl interpolation) */
+  /** Values for text (react-intl interpolation) */
   textValues?: { [string]: any },
   /** @ignore injected by `react-intl` */
   intl: IntlShape,
