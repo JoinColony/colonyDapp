913be538ae73614f98ec79f157c2519787cc4a2f
James Lefrère <james@colony.io>
2018-09-20 14:50:38 +0200

    Add provisioning options

--- scripts/provision_submodules.sh
+++ scripts/provision_submodules.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --skip-colony-network-build)
+      SKIP_COLONY_NETWORK_BUILD=true
+      ;;
+    --skip-pinning-service-build)
+      SKIP_PINNING_SERVICE_BUILD=true
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      exit 1
+  esac
+  shift
+done
+
 # Paths
 LIB_PATH="src/lib"
 
@@ -21,15 +36,21 @@ log() {
 log "Initialize submodule libs"
 git submodule update --init --recursive
 
-# Build network
-log "Building '${NETWORK}' submodule"
-cd "${LIB_PATH}/${NETWORK}"
-git submodule update --init
-yarn
-cd ${ROOT_PATH}
+if [ "$SKIP_COLONY_NETWORK_BUILD" != true ]
+then
+    # Build network
+    log "Building '${NETWORK}' submodule"
+    cd "${LIB_PATH}/${NETWORK}"
+    git submodule update --init
+    yarn
+    cd ${ROOT_PATH}
+fi
 
-# Build pinning service
-log "Building '${PINNING}' submodule"
-cd "${LIB_PATH}/${PINNING}"
-yarn
-cd ${ROOT_PATH}
+if [ "$SKIP_PINNING_SERVICE_BUILD" != true ]
+then
+    # Build pinning service
+    log "Building '${PINNING}' submodule"
+    cd "${LIB_PATH}/${PINNING}"
+    yarn
+    cd ${ROOT_PATH}
+fi
