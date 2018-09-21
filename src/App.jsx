6391a27b82deb4bf7a600f3ff2c3819ceb359a20
Raul Glogovetan <raul@glogovetan.com>
2018-09-21 12:44:07 +0300

    Refactor: move redux store setup to own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,58 @@
 /* @flow */
 
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
 import { addLocaleData, IntlProvider } from 'react-intl';
 import en from 'react-intl/locale-data/en';
-
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import layout from '~styles/layout.css';
 
 import messages from './i18n/en.json';
-import rootReducer from './reducers';
-import rootSaga from './sagas';
 
-import ConnectWallet from './modules/wallet/components/ConnectWallet';
+import CreateColonyWizard from '~dashboard/CreateColonyWizard';
+import Dashboard from '~dashboard/Dashboard';
 
-/* eslint-disable-next-line max-len */
-import AsyncComponentLoader from './modules/core/components/AsyncComponentLoader';
-/* eslint-disable-next-line max-len */
-import CreateColonyWizard from './modules/dashboard/components/CreateColonyWizard';
-import Dashboard from './modules/dashboard/components/Dashboard';
-import WalletStart from './modules/wallet/components/WalletStart';
-import UserProfile from './modules/users/components/UserProfile';
-import UserProfileEdit from './modules/users/components/UserProfileEdit';
-import ProfileCreate from './modules/wallet/components/ProfileCreate';
-import CreateWalletWizard from './modules/wallet/components/CreateWalletWizard';
-import { SpinnerLoader } from './modules/core/components/Preloaders';
+import UserProfile from '~users/UserProfile';
+import UserProfileEdit from '~users/UserProfileEdit';
+
+import ConnectWallet from '~wallet/ConnectWallet';
+import WalletStart from '~wallet/WalletStart';
+import ProfileCreate from '~wallet/ProfileCreate';
+import CreateWalletWizard from '~wallet/CreateWalletWizard';
 
 addLocaleData(en);
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(sagaMiddleware),
-);
-sagaMiddleware.run(rootSaga);
-
 const Home = () => (
-  <div>
-    <ul>
-      <li>
-        <NavLink style={{ color: 'blue' }} to="/dynamic-import-route">
-          Dynamic Import Route
-        </NavLink>
-      </li>
-      <li>
-        <NavLink style={{ color: 'blue' }} to="/createcolony">
-          Create Colony Wizard
-        </NavLink>
-      </li>
-      <li>
-        <NavLink style={{ color: 'blue' }} to="/dashboard">
-          Dashboard
-        </NavLink>
-      </li>
-      <li>
-        <NavLink style={{ color: 'blue' }} to="/start">
-          Start
-        </NavLink>
-      </li>
-      <li>
-        <NavLink style={{ color: 'blue' }} to="/profile">
-          User Profile
-        </NavLink>
-      </li>
-    </ul>
-    <p>Hello World</p>
-    {/*
-     * This component is used only as a test reference please remove when setting
-     * this up properly. Thanks.
-     */}
-    <AsyncComponentLoader
-      loaderFn={() => import('./DynamicComponent.jsx')}
-      preloader={<SpinnerLoader appearance={{ size: 'medium' }} />}
-    />
-  </div>
+  <ul>
+    <li>
+      <NavLink style={{ color: 'blue' }} to="/createcolony">
+        Create Colony Wizard
+      </NavLink>
+    </li>
+    <li>
+      <NavLink style={{ color: 'blue' }} to="/dashboard">
+        Dashboard
+      </NavLink>
+    </li>
+    <li>
+      <NavLink style={{ color: 'blue' }} to="/start">
+        Start
+      </NavLink>
+    </li>
+    <li>
+      <NavLink style={{ color: 'blue' }} to="/profile">
+        User Profile
+      </NavLink>
+    </li>
+  </ul>
 );
 
-/*
- * This component is used only as a test reference please remove when setting
- * this up properly. Thanks.
- */
-const DynamicRoute = () => (
-  <AsyncComponentLoader
-    loaderFn={() => import('./DynamicRoute.jsx')}
-    preloader={<SpinnerLoader appearance={{ size: 'medium' }} />}
-  />
-);
+type Props = {
+  store: Object,
+};
 
-export default function App() {
+export default function App({ store }: Props) {
   return (
     <IntlProvider locale="en" defaultLocale="en" messages={messages}>
       <Provider store={store}>
@@ -101,7 +61,6 @@ export default function App() {
             <Route exact path="/" component={Home} />
             <Route path="/createcolony" component={CreateColonyWizard} />
             <Route path="/dashboard" component={Dashboard} />
-            <Route path="/dynamic-import-route" component={DynamicRoute} />
             <Route path="/start" component={WalletStart} />
             <Route path="/createwallet" component={CreateWalletWizard} />
             <Route path="/connectwallet/:provider" component={ConnectWallet} />
