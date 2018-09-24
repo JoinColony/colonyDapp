6612d02e56a360b6acd77334968756b6eb6b3902
karolinkas <li-nee@gmx.de>
2018-09-24 13:54:04 +0200

    pass values through to next step on confirm

--- src/modules/dashboard/components/CreateColonyWizard/StepCreateColony.jsx
+++ src/modules/dashboard/components/CreateColonyWizard/StepCreateColony.jsx
@@ -2,8 +2,7 @@
 import type { FormikProps } from 'formik';
 
 import React from 'react';
-import { defineMessages, FormattedMessage } from 'react-intl';
-import * as yup from 'yup';
+import { defineMessages } from 'react-intl';
 
 import type { SubmitFn } from '../../../core/components/Wizard';
 
@@ -21,6 +20,13 @@ type Props = {
   previousStep: () => void,
 } & FormikProps<FormValues>;
 
+type CardProps = {
+  options: Array<{
+    title: string,
+    valueKey: string,
+  }>,
+};
+
 const MSG = defineMessages({
   title: {
     id: 'CreateColony.StepCreateColony.title',
@@ -30,14 +36,77 @@ const MSG = defineMessages({
     id: 'CreateColony.StepCreateColony.subtitle',
     defaultMessage: `and create your new Colony.`,
   },
+  confirm: {
+    id: 'CreateColony.StepCreateColony.confirmButton',
+    defaultMessage: `Create Colony`,
+  },
+  back: {
+    id: 'CreateColony.StepCreateColony.backButton',
+    defaultMessage: `Back`,
+  },
+  colonyName: {
+    id: 'CreateColony.StepCreateColony.colonyName',
+    defaultMessage: `Colony Name`,
+  },
+  tokenName: {
+    id: 'CreateColony.StepCreateColony.tokenName',
+    defaultMessage: `Token Name`,
+  },
+  tokenSymbol: {
+    id: 'CreateColony.StepCreateColony.tokenSymbol',
+    defaultMessage: `Token Symbol`,
+  },
 });
 
 const displayName = 'dashboard.CreateColonyWizard.StepCreateColony';
 
+const options = [
+  {
+    title: MSG.colonyName,
+    valueKey: 'colonyName',
+  },
+  {
+    title: MSG.tokenName,
+    valueKey: 'tokenName',
+  },
+  {
+    title: MSG.tokenSymbol,
+    valueKey: 'tokenSymbol',
+  },
+];
+
+// TODO: Once we wire this step up with the previous
+// steps all we need to do is getting the "values"
+// prop from Formik and pass it throught to the CardRow component
+const values = {
+  colonyName: 'Encecladus',
+  tokenName: 'Starfleet Token',
+  tokenSymbol: 'STRF',
+};
+
+// To allow us to pass these through to the next step easily using Formik
+// we make values that have to be confirmed in this screen the input fields
+const CardRow = ({ options }: CardsProps) =>
+  options.map(option => (
+    <div className={styles.cardRow} key={`option ${option.valueKey}`}>
+      <Heading
+        appearance={{ size: 'normal', weight: 'medium', margin: 'none' }}
+        text={option.title}
+      />
+      <Input
+        elementOnly
+        readOnly
+        className={styles.fakeInput}
+        value={values[option.valueKey]}
+        name={option.valueKey}
+      />
+    </div>
+  ));
+
 const StepCreateColony = ({ handleSubmit }: Props) => (
   <section className={styles.content}>
-    <div className={styles.title}>
-      <form className={styles.finalForm} onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
+      <div className={styles.finalForm}>
         <Heading
           appearance={{ size: 'medium', weight: 'bold', margin: 'none' }}
           text={MSG.title}
@@ -46,21 +115,21 @@ const StepCreateColony = ({ handleSubmit }: Props) => (
           appearance={{ size: 'medium', weight: 'bold', margin: 'none' }}
           text={MSG.subtitle}
         />
-
-        <div className={styles.buttons}>
-          <Button
-            appearance={{ theme: 'secondary' }}
-            type="cancel"
-            text={MSG.cancel}
-          />
-          <Button
-            appearance={{ theme: 'primary' }}
-            type="submit"
-            text={MSG.next}
-          />
-        </div>
-      </form>
-    </div>
+        <CardRow options={options} />
+      </div>
+      <div className={styles.buttons}>
+        <Button
+          appearance={{ theme: 'secondary' }}
+          type="cancel"
+          text={MSG.back}
+        />
+        <Button
+          appearance={{ theme: 'primary' }}
+          type="submit"
+          text={MSG.confirm}
+        />
+      </div>
+    </form>
   </section>
 );
 
@@ -70,5 +139,3 @@ export const Step = StepCreateColony;
 
 export const onSubmit: SubmitFn<FormValues> = (values, { nextStep }) =>
   nextStep();
-
-export const sidebarChild = <FormattedMessage {...MSG.helpText} />;
