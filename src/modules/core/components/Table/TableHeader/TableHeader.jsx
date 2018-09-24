83abcd41d28e3249841ab1c4c3afd8917c1f4992
Christian Maniewski <chmanie@fastmail.com>
2018-09-24 11:57:31 +0200

    Apply some table fixes

--- src/modules/core/components/Table/TableHeader/TableHeader.jsx
+++ src/modules/core/components/Table/TableHeader/TableHeader.jsx
@@ -5,10 +5,8 @@ import React from 'react';
 
 import styles from './TableHeader.css';
 
-import TableRow from '../TableRow';
-
 type Props = {
-  children: ChildrenArray<ElementType<typeof TableRow>>,
+  children: ChildrenArray<ElementType<*>>,
 };
 
 const displayName = 'TableHeader';
