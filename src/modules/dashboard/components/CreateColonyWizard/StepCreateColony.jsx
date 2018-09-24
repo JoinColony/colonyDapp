37c45439cc4f7d9ba4283d9042fbd2bdd01a7770
karolinkas <li-nee@gmx.de>
2018-09-24 16:36:37 +0200

    add activity bar for gas station

--- src/modules/dashboard/components/CreateColonyWizard/StepCreateColony.jsx
+++ src/modules/dashboard/components/CreateColonyWizard/StepCreateColony.jsx
@@ -1,8 +1,11 @@
 // @flow
 import type { FormikProps } from 'formik';
+import type { MessageDescriptor } from 'react-intl';
 
-import React from 'react';
+import React, { Component } from 'react';
 import { defineMessages } from 'react-intl';
+import { compose, nest, withProps } from 'recompose';
+//import withProps from 'recompose/withProps';
 
 import type { SubmitFn } from '../../../core/components/Wizard';
 
@@ -11,6 +14,9 @@ import styles from './StepCreateColony.css';
 import Input from '../../../core/components/Fields/Input';
 import Heading from '../../../core/components/Heading';
 import Button from '../../../core/components/Button';
+import ActivityBar from '../../../core/components/ActivityBar/ActivityBar.jsx';
+import withDialog from '../../../core/components/Dialog/withDialog.js';
+import DialogProvider from '../../../core/components/Dialog/DialogProvider.jsx';
 
 type FormValues = {
   nextStep: () => void,
@@ -18,13 +24,16 @@ type FormValues = {
 
 type Props = {
   previousStep: () => void,
+  handleSubmit: () => void,
 } & FormikProps<FormValues>;
 
 type CardProps = {
-  options: Array<{
-    title: string,
-    valueKey: string,
-  }>,
+  cardOptions: Array<Row>,
+};
+
+type Row = {
+  title: MessageDescriptor,
+  valueKey: string,
 };
 
 const MSG = defineMessages({
@@ -78,7 +87,7 @@ const options = [
 // TODO: Once we wire this step up with the previous
 // steps all we need to do is getting the "values"
 // prop from Formik and pass it throught to the CardRow component
-const values = {
+const mockFormikValues = {
   colonyName: 'Encecladus',
   tokenName: 'Starfleet Token',
   tokenSymbol: 'STRF',
@@ -86,8 +95,8 @@ const values = {
 
 // To allow us to pass these through to the next step easily using Formik
 // we make values that have to be confirmed in this screen the input fields
-const CardRow = ({ options }: CardsProps) =>
-  options.map(option => (
+const CardRow = ({ cardOptions }: CardProps) =>
+  cardOptions.map(option => (
     <div className={styles.cardRow} key={`option ${option.valueKey}`}>
       <Heading
         appearance={{ size: 'normal', weight: 'medium', margin: 'none' }}
@@ -97,45 +106,58 @@ const CardRow = ({ options }: CardsProps) =>
         elementOnly
         readOnly
         className={styles.fakeInput}
-        value={values[option.valueKey]}
+        value={mockFormikValues[option.valueKey]}
         name={option.valueKey}
       />
     </div>
   ));
 
-const StepCreateColony = ({ handleSubmit }: Props) => (
-  <section className={styles.content}>
-    <form onSubmit={handleSubmit}>
-      <div className={styles.finalForm}>
-        <Heading
-          appearance={{ size: 'medium', weight: 'bold', margin: 'none' }}
-          text={MSG.title}
-        />
-        <Heading
-          appearance={{ size: 'medium', weight: 'bold', margin: 'none' }}
-          text={MSG.subtitle}
-        />
-        <CardRow options={options} />
-      </div>
-      <div className={styles.buttons}>
-        <Button
-          appearance={{ theme: 'secondary' }}
-          type="cancel"
-          text={MSG.back}
-        />
-        <Button
-          appearance={{ theme: 'primary' }}
-          type="submit"
-          text={MSG.confirm}
-        />
-      </div>
-    </form>
-  </section>
-);
+class StepCreateColony extends Component<Props> {
+  openGasDialog = () => {};
+
+  render() {
+    console.log(this.props);
+    const { handleSubmit, openDialog } = this.props;
+    return (
+      <section className={styles.content}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.finalForm}>
+            <Heading
+              appearance={{ size: 'medium', weight: 'bold', margin: 'none' }}
+              text={MSG.title}
+            />
+            <Heading
+              appearance={{ size: 'medium', weight: 'bold', margin: 'none' }}
+              text={MSG.subtitle}
+            />
+            <CardRow cardOptions={options} />
+          </div>
+          <div className={styles.buttons}>
+            <Button
+              appearance={{ theme: 'secondary' }}
+              type="cancel"
+              text={MSG.back}
+            />
+            <Button
+              onClick={openDialog('ActivityBar')}
+              appearance={{ theme: 'primary' }}
+              type="submit"
+              text={MSG.confirm}
+            />
+          </div>
+        </form>
+      </section>
+    );
+  }
+}
 
 StepCreateColony.displayName = displayName;
 
-export const Step = StepCreateColony;
+const StepWithDialog = withDialog()(StepCreateColony);
+
+const enhance = compose(withProps(() => ({ dialogComponents: ActivityBar })));
+
+export const Step = nest(DialogProvider, enhance(StepWithDialog));
 
 export const onSubmit: SubmitFn<FormValues> = (values, { nextStep }) =>
   nextStep();
