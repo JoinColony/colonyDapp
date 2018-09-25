50c6d09570938f02987285aa6556cc226aca08f8
Christian Maniewski <chmanie@fastmail.com>
2018-09-25 14:00:20 +0200

    Add Number core component

---
+++ src/modules/core/components/Number/Number.jsx
@@ -0,0 +1,71 @@
+/* @flow */
+
+import React from 'react';
+import BN from 'bn.js';
+import formatNumber from 'format-number';
+import { fromWei } from 'ethjs-unit';
+
+import { getMainClasses } from '~utils/css';
+
+import styles from './Number.css';
+
+type Appearance = {
+  theme: 'primary' | 'grey' | 'dark',
+  size: 'medium' | 'large',
+};
+
+type Props = {
+  /** Appearance object */
+  appearance?: Appearance,
+  /** Optional custom className, will overwrite appearance */
+  className?: string,
+  /** Number of decimals to show after comma */
+  decimals?: number,
+  /** Prefix the value with this string */
+  prefix?: string,
+  /** Suffix the value with this string */
+  suffix?: string,
+  /** Ether unit the number is notated in (e.g. 'ether' = 10^18 wei) */
+  unit?: string,
+  /** Actual value */
+  value: number | string | BN,
+  /** Separator for thousands (e.g. ',') */
+  integerSeparator?: string,
+};
+
+const Number = ({
+  appearance,
+  className,
+  decimals: truncate,
+  integerSeparator = ',',
+  prefix,
+  suffix,
+  unit,
+  value,
+  ...props
+}: Props) => {
+  let convertedNum;
+  if (value instanceof BN) {
+    convertedNum = unit ? fromWei(value, unit) : value.toString();
+  } else {
+    convertedNum = unit ? fromWei(parseInt(value, 10), unit) : value;
+  }
+
+  const formattedNumber = formatNumber({
+    prefix,
+    suffix,
+    integerSeparator,
+    truncate,
+  })(convertedNum);
+
+  return (
+    <span
+      className={className || getMainClasses(appearance, styles)}
+      {...props}
+    >
+      {formattedNumber}
+    </span>
+  );
+};
+
+export default Number;
--- styleguide.config.js
+++ styleguide.config.js
@@ -15,6 +15,7 @@ module.exports = {
         './src/modules/core/components/Link/Link.jsx',
         './src/modules/core/components/NavLink/NavLink.jsx',
         './src/modules/core/components/ExternalLink/ExternalLink.jsx',
+        './src/modules/core/components/Number/Number.jsx',
       ],
     },
     {
