129ce1ea35fffb615479151db7a1a06d7fb6d21e
Raul Glogovetan <raul@glogovetan.com>
2018-09-25 19:05:01 +0300

    Add: wallet context basic update methods

--- src/context/walletContext.js
+++ src/context/walletContext.js
@@ -1,5 +1,20 @@
 /* @flow */
 
-const walletContext: Object = {};
+let walletInstance: Object | void;
+
+const clearWallet = () => {
+  walletInstance = undefined;
+};
+
+const setNewWallet = (newWalletInstance: Object) => {
+  clearWallet();
+  walletInstance = newWalletInstance;
+};
+
+const walletContext: Object = {
+  instance: walletInstance,
+  clearWallet,
+  setNewWallet,
+};
 
 export default walletContext;
