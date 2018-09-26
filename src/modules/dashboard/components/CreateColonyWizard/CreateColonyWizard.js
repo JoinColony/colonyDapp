a5832fa7a14ce4353c5675c0dc9ca4a7e6b5f932
Christian Maniewski <chmanie@fastmail.com>
2018-09-26 12:24:47 +0200

    Fix steps function typing in CreateColonyWizard

--- src/modules/dashboard/components/CreateColonyWizard/CreateColonyWizard.js
+++ src/modules/dashboard/components/CreateColonyWizard/CreateColonyWizard.js
@@ -12,34 +12,26 @@ import * as StepTokenChoice from './StepTokenChoice.jsx';
 import * as StepSelectToken from './StepSelectToken.jsx';
 import * as StepCreateToken from './StepCreateToken.jsx';
 
-const stepArray = [
-  StepColonyName,
-  StepTokenChoice,
-  {
-    select: StepSelectToken,
-    create: StepCreateToken,
-  },
-];
+const stepArray = [StepColonyName, StepTokenChoice];
 
 type StepValues = {
   tokenChoice: 'create' | 'select',
 };
 
-// this is an example step function it allow the wizard flow to branch
+// This is a step function to allow the wizard flow to branch
 // off into two instead of just stepping through an array in a linear manner
 const stepFunction = (step: number, values: StepValues) => {
-  if (step === 2) {
-    return stepArray[2][values.tokenChoice];
+  if (step === 2 && values.tokenChoice === 'create') {
+    return StepCreateToken;
+  }
+  if (step === 2 && values.tokenChoice === 'select') {
+    return StepSelectToken;
   }
   return stepArray[step];
 };
 
 const CreateColonyContainer = compose(
   withWizard({
-    // This is a bit tricky to type for some reason
-    // since it's a function or an array which we have specified but flow
-    // is still complaining
-    // $FlowFixMe
     steps: stepFunction,
   }),
   withProps(() => ({ external: true })),
