a6ae4cfabb5ce3cce64a472caa9c285b29f792a8
Raul Glogovetan <raul@glogovetan.com>
2018-09-26 12:53:28 +0300

    Add: link up `CreateNewToken` with the create colony wizard

--- src/modules/dashboard/components/CreateColonyWizard/CreateColonyWizard.js
+++ src/modules/dashboard/components/CreateColonyWizard/CreateColonyWizard.js
@@ -6,8 +6,9 @@ import withProps from 'recompose/withProps';
 import { withWizard } from '../../../core/components/Wizard';
 import CreateColony from './CreateColonyWizard.jsx';
 import * as StepColonyDetails from './StepColonyDetails.jsx';
+import * as StepCreateNewToken from './CreateNewToken.jsx';
 
-const steps = [StepColonyDetails];
+const steps = [StepColonyDetails, StepCreateNewToken];
 
 const CreateColonyContainer = compose(
   withWizard({
