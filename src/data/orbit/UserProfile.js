179b92192f440eeaf8e49e790a31d39ede6f2085
coyotespike <timothy.roy@protonmail.ch>
2018-09-27 15:25:57 +0300

    Updates schema for UserProfile

--- src/data/orbit/UserProfile.js
+++ src/data/orbit/UserProfile.js
@@ -1,5 +1,11 @@
 // @flow
-import type { OrbitKVStore } from '../types';
+import type {
+  IPFSHash,
+  OrbitKVStore,
+  profileColony,
+  profileTask,
+  UserProfile,
+} from '../types';
 
 class UserProfile {
   _store: OrbitKVStore;
@@ -14,21 +20,59 @@ class UserProfile {
   }
 
   async setName(name: string) {
-    if (this.isEmpty()) {
-      await this._store.put('created', new Date().toUTCString());
-    }
-    await this._store.put('name', name);
-  }
-
-  async joinColony(colonyHash: string) {
     await this.initialize();
-    const colonies = this._store.get('colonies');
-    colonies.push(colonyHash);
-    await this._store.put('colonies', colonies);
+    await this._store.put('name', name);
   }
 
   getName() {
     return this._store.get('name');
+  }
+
+  async setBio(bio: string) {
+    await this.initialize();
+    await this._store.put('bio', bio);
+  }
+
+  getBio() {
+    return this._store.get('bio');
+  }
+
+  async setAvatar(avatar: IPFSHash) {
+    await this.initialize();
+    await this._store.put('avatar', avatar);
+  }
+
+  getAvatar(): IPFSHash {
+    return this._store.get('avatar');
+  }
+
+  async joinColony(colony: profileColony) {
+    await this.initialize();
+    const colonies = this._store.get('colonies');
+    colonies.push(colony);
+    await this._store.put('colonies', colonies);
+  }
+
+  async addTask(task: profileTask) {
+    await this.initialize();
+    const tasks = await this._store.get('tasks');
+    tasks.push(task);
+    await this._store.put('tasks', tasks);
+  }
+
+  async getTasks(): profileTask[] {
+    await this.initialize();
+    return this._store.get('tasks');
+  }
+
+  async getWholeProfile(): UserProfile {
+    await this.initialize();
+    const name = this._store.get('name');
+    const bio = this._store.get('bio');
+    const avatarHash = this._store.get('avatar');
+    const tasks = this._store.get('tasks');
+    const colonies = this._store.get('colonies');
+    return { name, bio, avatarHash, colonies, tasks };
   }
 
   address() {
@@ -50,6 +94,7 @@ class UserProfile {
     if (this.isEmpty()) {
       await this._store.put('created', new Date().toUTCString());
       await this._store.put('colonies', []);
+      await this._store.put('tasks', []);
     }
     this.initialized = true;
   }
