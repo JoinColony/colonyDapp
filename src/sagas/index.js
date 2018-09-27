981219b43f02077e2f3cd620a0568a387979191c
Raul Glogovetan <raul@glogovetan.com>
2018-09-27 22:18:06 +0300

    Refactor: move old colony sagas to better structure

--- src/sagas/index.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import { all } from 'redux-saga/effects';
-import type { Saga } from 'redux-saga';
-import colony from './colony';
-
-export default function* rootSaga(): Saga<*> {
-  yield all(colony());
-}
