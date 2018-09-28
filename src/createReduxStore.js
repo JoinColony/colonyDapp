d109517f2d203f62bda4b67f68410df2d677e921
coyotespike <timothy.roy@protonmail.ch>
2018-09-28 17:44:16 +0300

    Add: createReduxStore start dataSagas, and runs initializeData

--- src/createReduxStore.js
+++ src/createReduxStore.js
@@ -7,13 +7,16 @@ import walletReducer from './modules/wallet/reducers';
 
 import walletSagas from './modules/wallet/sagas';
 import colonySagas from './modules/dashboard/sagas';
+import { dataReducer, dataSagas } from './data';
+import { initializeData } from './actions';
 
 const rootReducer = combineReducers({
   wallet: walletReducer,
+  data: dataReducer,
 });
 
 function* rootSaga(): any {
-  yield [walletSagas(), colonySagas()];
+  yield [walletSagas(), colonySagas(), dataSagas()];
 }
 
 const sagaMiddleware = createSagaMiddleware();
@@ -26,5 +29,8 @@ const store = createStore(
 );
 
 sagaMiddleware.run(rootSaga);
+
+const rootRepo = '/tmp/dataTests';
+initializeData(store.dispatch, rootRepo).then(result => console.log(result));
 
 export default store;
