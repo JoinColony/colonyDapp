8d390e593f15a8ec4986322927a515a86ea341bd
coyotespike <timothy.roy@protonmail.ch>
2018-10-01 14:19:52 +0300

    Adds constants and actions for UserProfile

---
+++ src/data/actions/userActions.js
@@ -0,0 +1,82 @@
+// @flow
+
+import {
+  STATE_READY,
+  EDIT_PROFILE,
+  LOAD_PROFILE,
+  UPDATE_PROFILE,
+} from './constants';
+
+import type { Action } from '../types';
+
+// Sagas use this action to update Redux
+export function setUserProfileContent({
+  property,
+  value,
+}: {
+  property: string,
+  value: any,
+}): Action {
+  return {
+    type: UPDATE_PROFILE,
+    payload: { update: { property, value } },
+  };
+}
+
+/*
+   Dapp uses the following actions to call Sagas to edit profile and update Redux
+*/
+export function editWholeProfile(profile: {}): Action {
+  return {
+    type: EDIT_PROFILE,
+    payload: { update: { property: 'profile', value: profile } },
+  };
+}
+
+export function fetchWholeProfile(key: string): Action {
+  return {
+    type: LOAD_PROFILE,
+    payload: { key },
+  };
+}
+
+export function addColonyToUserProfile(colonyId: string): Action {
+  return {
+    type: EDIT_PROFILE,
+    payload: { update: { property: 'colonies', value: colonyId } },
+  };
+}
+
+export function addTaskToUserProfile(task: {}): Action {
+  return {
+    type: EDIT_PROFILE,
+    payload: { update: { property: 'tasks', value: task } },
+  };
+}
+
+export function setUserAvatar(avatarHash: string): Action {
+  return {
+    type: EDIT_PROFILE,
+    payload: { update: { property: 'avatar', value: avatarHash } },
+  };
+}
+
+export function setUserBio(bio: string): Action {
+  return {
+    type: EDIT_PROFILE,
+    payload: { update: { property: 'bio', value: bio } },
+  };
+}
+
+export function setUserName(name: string): Action {
+  return {
+    type: EDIT_PROFILE,
+    payload: { update: { property: 'name', value: name } },
+  };
+}
+
+// updates user's notifications database
+export function notifyUser() {}
+
+// user updates own events aka recent actions
+export function updateUserEvents() {}
