75522cb7802a529b6a287a20bd694b006efbf9d6
coyotespike <timothy.roy@protonmail.ch>
2018-10-02 12:57:36 +0300

    Refactor DataAPI: rename, move constructor to top, change method defs
    The method definitions avoid a Flow error now.

--- src/data/Data.js
+++ src/data/Data.js
@@ -11,60 +11,7 @@ import type {
   UserProfileType,
 } from './types';
 
-export default class Data {
-  /*
-    Returns UserProfile store class for the given user.
-  */
-  async _getUserProfile(key: PublicKey): any {
-    const store = await this._orbitNode.kvstore(key);
-    await store.load();
-
-    if (this._pinner) {
-      await this._pinner.pin(store);
-    }
-    return new UserProfile(store);
-  }
-
-  /*
-    Returns metadata for the logged-in user's profile.
-  */
-  async _getMyUserProfile(
-    key: PublicKey = 'user-profile',
-  ): Promise<OrbitKVStore> {
-    const store = await this._getUserProfile(key);
-    return store;
-  }
-
-  /*
-    Adds to or edits the UserProfile
-    Also used to set all properties at once
-  */
-  async editUserProfile(property: string, value: any, profileKey: string) {
-    const store = await this._getMyUserProfile(profileKey);
-    const result = await store.setProperty(property, value);
-    return result;
-  }
-
-  /*
-  Returns value of given profile property
-  */
-  async getUserProfileProperty(
-    key: PublicKey = 'user-profile',
-    property: string,
-  ) {
-    const store = await this._getUserProfile(key);
-    const result = await store.getProperty(property);
-    return result;
-  }
-
-  /*
-  Returns all profile data as an object
-  */
-  async getUserProfileData(key: PublicKey = 'user-profile'): any {
-    const store = await this._getUserProfile(key);
-    const profile = await store.getWholeProfile();
-    return profile;
-  }
+export default class DataAPI {
   /*
    Setup
    */
@@ -89,28 +36,85 @@ export default class Data {
     this.getUserProfileData = this.getUserProfileData.bind(this);
   }
 
-  // @TODO This design is time-dependent and relies
-  // on lots of mutations, refactor to work with a builder
-  // pattern that is immutable.
-  async ready(): Promise<boolean> {
+  /*
+    Returns UserProfile store class for the given user.
+  */
+  _getUserProfile = async (key: PublicKey): any => {
+    const store = await this._orbitNode.kvstore(key);
+    await store.load();
+
+    if (this._pinner) {
+      await this._pinner.pin(store);
+    }
+    return new UserProfile(store);
+  };
+
+  /*
+    Returns metadata for the logged-in user's profile.
+  */
+  _getMyUserProfile = async (
+    key: PublicKey = 'user-profile',
+  ): Promise<OrbitKVStore> => {
+    const store = await this._getUserProfile(key);
+    return store;
+  };
+
+  /*
+    Adds to or edits the UserProfile
+    Also used to set all properties at once
+  */
+  editUserProfile = async (
+    property: string,
+    value: any,
+    profileKey: string,
+  ) => {
+    const store = await this._getMyUserProfile(profileKey);
+    const result = await store.setProperty(property, value);
+    return result;
+  };
+
+  /*
+  Returns value of given profile property
+  */
+  getUserProfileProperty = async (
+    key: PublicKey = 'user-profile',
+    property: string,
+  ) => {
+    const store = await this._getUserProfile(key);
+    const result = await store.getProperty(property);
+    return result;
+  };
+
+  /*
+  Returns all profile data as an object
+  */
+  getUserProfileData = async (
+    key: PublicKey = 'user-profile',
+  ): UserProfileType => {
+    const store = await this._getUserProfile(key);
+    const profile = await store.getWholeProfile();
+    return profile;
+  };
+
+  ready = async (): Promise<boolean> => {
     await this._ipfsNode.ready();
     return true;
-  }
+  };
 
-  async stop(): Promise<void> {
+  stop = async (): Promise<void> => {
     await this._orbitNode.stop();
     return this._ipfsNode.stop();
-  }
+  };
 
   static async fromDefaultConfig(
     pinner: ?Pinner,
     opts: DataOptions = { ipfs: {}, orbit: {} },
-  ): Promise<Data> {
+  ): Promise<DataAPI> {
     const ipfsConf = ipfs.makeOptions(opts.ipfs);
     const ipfsNode = ipfs.getIPFS(ipfsConf);
     const orbitConf = orbitSetup.makeOptions(opts.orbit);
     const orbitNode = await orbitSetup.getOrbitDB(ipfsNode, orbitConf);
 
-    return new Data(pinner, ipfsNode, orbitNode);
+    return new DataAPI(pinner, ipfsNode, orbitNode);
   }
 }
