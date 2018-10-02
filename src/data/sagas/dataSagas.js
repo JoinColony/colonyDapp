3da9bd89548922e14c429fd78e8e07dafdca37d5
coyotespike <timothy.roy@protonmail.ch>
2018-10-02 12:56:49 +0300

    Refactor: change return type in dataSagas to Saga<void>
    cf. https://github.com/redux-saga/redux-saga/issues/864

--- src/data/sagas/dataSagas.js
+++ src/data/sagas/dataSagas.js
@@ -1,5 +1,6 @@
 // @flow
 import { call, put, select, takeEvery } from 'redux-saga/effects';
+import type { Saga } from 'redux-saga';
 import Data from '../Data';
 
 import {
@@ -10,7 +11,7 @@ import {
   setUserProfileContent,
 } from '../actions';
 
-function* editProfile(action) {
+function* editProfile(action): Saga<void> {
   const { update: { profileKey, property, value } } = action.payload;
   const dataAPI = yield select(state => state.data.Data);
   const result = yield call(
@@ -22,7 +23,7 @@ function* editProfile(action) {
   yield put(setUserProfileContent({ profileKey, property, value: result }));
 }
 
-function* getWholeProfile(action) {
+function* getWholeProfile(action): Saga<void> {
   const { profileKey } = action.payload;
   const dataAPI = yield select(state => state.data.Data);
   const result = yield call(dataAPI.getUserProfileData, profileKey);
@@ -31,7 +32,7 @@ function* getWholeProfile(action) {
   );
 }
 
-function* initializeData(action) {
+function* initializeData(action): Saga<void> {
   const { resolve, rootRepo } = action;
   const data = yield call(Data.fromDefaultConfig, null, {
     ipfs: {
@@ -49,7 +50,7 @@ function* initializeData(action) {
   yield put(init);
 }
 
-export function* dataSagas(): Generator<any, any, any> {
+export function* dataSagas(): Saga<void> {
   yield takeEvery(INITIALIZE_DATA, initializeData);
   yield takeEvery(EDIT_PROFILE, editProfile);
   yield takeEvery(LOAD_PROFILE, getWholeProfile);
