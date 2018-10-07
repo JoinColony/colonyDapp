519232fe53f6a60f8d84adfc20854bfdaa20b907
Christian Maniewski <chmanie@fastmail.com>
2018-10-07 22:04:04 +0200

    Use ipfsNode in data

--- src/data/index.js
+++ src/data/index.js
@@ -2,22 +2,11 @@
 
 import { open as openWallet } from '@colony/purser-software';
 
-import * as ipfs from './ipfs';
-import * as orbit from './orbit';
+import OrbitDB from 'orbit-db';
 
 import PurserIdentityProvider from '../lib/database/PurserIdentityProvider';
 import PurserAccessController from '../lib/database/PurserAccessController';
-
-import type {
-  B58String,
-  ColonyIPFSNode,
-  DataOptions,
-  OrbitKVStore,
-  OrbitNode,
-  Pinner,
-} from './types';
-
-type PublicKey = string;
+import ipfsNode from '../lib/ipfsNode';
 
 (async function() {
   const wallet = await openWallet({
@@ -28,12 +17,11 @@ type PublicKey = string;
   const identityProvider = new PurserIdentityProvider(wallet);
   const identity = await identityProvider.createIdentity();
 
-  const ipfsConf = ipfs.makeOptions();
-  const ipfsNode = ipfs.getIPFS(ipfsConf);
-  const orbitConf = orbit.makeOptions();
-  await ipfsNode.ready();
+  await ipfsNode.ready;
 
-  const orbitNode = await orbit.getOrbitDB(ipfsNode, identity, orbitConf);
+  const orbitNode = new OrbitDB(ipfsNode.getIpfs(), identity, {
+    path: 'colonyOrbitdb',
+  });
 
   const accessController = new PurserAccessController(wallet);
   // Uncomment to create a store
@@ -54,113 +42,113 @@ type PublicKey = string;
   console.log(foo);
 })();
 
-class UserProfile {
-  _store: OrbitKVStore;
+// class UserProfile {
+//   _store: OrbitKVStore;
 
-  constructor(store) {
-    this._store = store;
-  }
+//   constructor(store) {
+//     this._store = store;
+//   }
 
-  isEmpty(): boolean {
-    return !this._store.get('created');
-  }
+//   isEmpty(): boolean {
+//     return !this._store.get('created');
+//   }
 
-  async setName(name: string) {
-    if (this.isEmpty()) {
-      await this._store.put('created', new Date().toUTCString());
-    }
-    await this._store.put('name', name);
-  }
+//   async setName(name: string) {
+//     if (this.isEmpty()) {
+//       await this._store.put('created', new Date().toUTCString());
+//     }
+//     await this._store.put('name', name);
+//   }
 
-  getName() {
-    return this._store.get('name');
-  }
+//   getName() {
+//     return this._store.get('name');
+//   }
 
-  address() {
-    return this._store.address;
-  }
+//   address() {
+//     return this._store.address;
+//   }
 
-  subscribe(f) {
-    this._store.events.on('replicated', () => {
-      f({ name: this.getName() });
-    });
-  }
-}
+//   subscribe(f) {
+//     this._store.events.on('replicated', () => {
+//       f({ name: this.getName() });
+//     });
+//   }
+// }
 
-export default class Data {
-  _pinner: ?Pinner;
+// export default class Data {
+//   _pinner: ?Pinner;
 
-  _ipfsNode: ColonyIPFSNode;
+//   _ipfsNode: ColonyIPFSNode;
 
-  _orbitNode: OrbitNode;
+//   _orbitNode: OrbitNode;
 
-  _key: string;
+//   _key: string;
 
-  constructor(pinner: ?Pinner, ipfsNode: ColonyIPFSNode, orbitNode: OrbitNode) {
-    this._pinner = pinner;
-    this._ipfsNode = ipfsNode;
-    this._orbitNode = orbitNode;
-    this._key = 'helloworld';
-  }
+//   constructor(pinner: ?Pinner, ipfsNode: ColonyIPFSNode, orbitNode: OrbitNode) {
+//     this._pinner = pinner;
+//     this._ipfsNode = ipfsNode;
+//     this._orbitNode = orbitNode;
+//     this._key = 'helloworld';
+//   }
 
-  // @TODO This design is time-dependant and relies
-  // on lots of mutations, refactor to work with a builder
-  // pattern that is immutable.
-  async ready(): Promise<boolean> {
-    await this._ipfsNode.ready();
-    return true;
-  }
+//   // @TODO This design is time-dependant and relies
+//   // on lots of mutations, refactor to work with a builder
+//   // pattern that is immutable.
+//   async ready(): Promise<boolean> {
+//     await this._ipfsNode.ready();
+//     return true;
+//   }
 
-  async waitForPeer(peerID: B58String): Promise<boolean> {
-    return this._ipfsNode.waitForPeer(peerID);
-  }
+//   async waitForPeer(peerID: B58String): Promise<boolean> {
+//     return this._ipfsNode.waitForPeer(peerID);
+//   }
 
-  async peerID(): Promise<B58String> {
-    return ipfs.getNodeID(this._ipfsNode);
-  }
+//   async peerID(): Promise<B58String> {
+//     return ipfs.getNodeID(this._ipfsNode);
+//   }
 
-  async stop(): Promise<void> {
-    await this._orbitNode.stop();
-    return this._ipfsNode.stop();
-  }
+//   async stop(): Promise<void> {
+//     await this._orbitNode.stop();
+//     return this._ipfsNode.stop();
+//   }
 
-  static async fromDefaultConfig(
-    pinner: ?Pinner,
-    opts: DataOptions = { ipfs: {}, orbit: {} },
-  ): Promise<Data> {
-    const ipfsConf = ipfs.makeOptions(opts.ipfs);
-    const ipfsNode = ipfs.getIPFS(ipfsConf);
+//   static async fromDefaultConfig(
+//     pinner: ?Pinner,
+//     opts: DataOptions = { ipfs: {}, orbit: {} },
+//   ): Promise<Data> {
+//     const ipfsConf = ipfs.makeOptions(opts.ipfs);
+//     const ipfsNode = ipfs.getIPFS(ipfsConf);
 
-    await ipfsNode.ready();
+//     await ipfsNode.ready();
 
-    // If we passed some bootstrap nodes,
-    // wait for some of them to be available.
-    if (opts.ipfs.bootstrap) {
-      await ipfsNode.waitForSomePeers();
-    }
+//     // If we passed some bootstrap nodes,
+//     // wait for some of them to be available.
+//     if (opts.ipfs.bootstrap) {
+//       await ipfsNode.waitForSomePeers();
+//     }
 
-    const orbitConf = orbit.makeOptions(opts.orbit);
-    const orbitNode = await orbit.getOrbitDB(ipfsNode, orbitConf);
+//     const orbitConf = orbit.makeOptions(opts.orbit);
+//     const orbitNode = await orbit.getOrbitDB(ipfsNode, orbitConf);
 
-    return new Data(pinner, ipfsNode, orbitNode);
-  }
+//     return new Data(pinner, ipfsNode, orbitNode);
+//   }
 
-  async getUserProfile(key: PublicKey): Promise<UserProfile> {
-    const store = await this._orbitNode.kvstore(key);
-    await store.load();
+//   async getUserProfile(key: PublicKey): Promise<UserProfile> {
+//     const store = await this._orbitNode.kvstore(key);
+//     await store.load();
 
-    if (this._pinner) {
-      await this._pinner.pin(store);
-    }
-    return new UserProfile(store);
-  }
+//     if (this._pinner) {
+//       await this._pinner.pin(store);
+//     }
+//     return new UserProfile(store);
+//   }
 
-  async getMyUserProfile(): Promise<UserProfile> {
-    return this.getUserProfile('user-profile');
-  }
+//   async getMyUserProfile(): Promise<UserProfile> {
+//     return this.getUserProfile('user-profile');
+//   }
 
-  async listPeers(): Promise<B58String[]> {
-    const peers = await this._ipfsNode.swarm.peers();
-    return peers.map(x => x.peer.toB58String());
-  }
-}
+//   async listPeers(): Promise<B58String[]> {
+//     const peers = await this._ipfsNode.swarm.peers();
+//     return peers.map(x => x.peer.toB58String());
+//   }
+// }
