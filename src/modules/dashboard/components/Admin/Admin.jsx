f8fc9c55ec085befbe88881f95fc98cb7c3b4253
karolinkas <li-nee@gmx.de>
2018-10-08 12:56:45 +0200

    Add back button to admin screen

--- src/modules/dashboard/components/Admin/Admin.jsx
+++ src/modules/dashboard/components/Admin/Admin.jsx
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
 import { Tab, Tabs, TabList, TabPanel } from '~core/Tabs';
+import NavLink from '~core/NavLink';
+import Icon from '~core/Icon';
 
 import AdminNavigation from './AdminNavigation.jsx';
 
@@ -11,12 +13,16 @@ import styles from './Admin.css';
 
 const MSG = defineMessages({
   tabColonyProfile: {
-    id: 'dashboard.Dashboard.tabColonyProfile',
+    id: 'dashboard.Admin.tabColonyProfile',
     defaultMessage: 'Colony Profile',
   },
   tabAdvanced: {
-    id: 'dashboard.Dashboard.tabAdvanced',
+    id: 'dashboard.Admin.tabAdvanced',
     defaultMessage: 'Advanced',
+  },
+  backButton: {
+    id: 'dashboard.Admin.backButton',
+    defaultMessage: 'Go to {colonyName}',
   },
 });
 
@@ -25,14 +31,32 @@ type Props = {};
 type State = {};
 
 export default class Admin extends Component<Props, State> {
+  colonyName: 'Colony';
+
   static displayName = 'dashboard.Admin';
 
   state = {};
+
+  componentWillMount() {
+    // TODO: get current Colony from Redux
+    this.colonyName = 'Colony';
+  }
 
   render() {
     return (
       <div className={styles.main}>
         <aside className={styles.colonyInfo}>
+          <header className={styles.header}>
+            <Icon name="back" title="back" appearance={{ size: 'medium' }} />
+            <NavLink to="/colony">
+              <FormattedMessage
+                {...MSG.backButton}
+                values={{
+                  colonyName: this.colonyName,
+                }}
+              />
+            </NavLink>
+          </header>
           <AdminNavigation />
         </aside>
         <main className={styles.content}>
