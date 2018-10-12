54c0152a5ef81c6b1b7354eba6b8a02a483c5343
coyotespike <timothy.roy@protonmail.ch>
2018-10-12 16:16:32 +0200

    Add tests for ENSResolver class

---
+++ integration-testing/ENSResolver.test.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+import ENSResolver from '../src/lib/database/ENSResolver'
+
+let resolver, colonyResolver, userResolver;
+describe('ENSResolver', () => {
+  test('Get a resolver', () => {
+    resolver = new ENSResolver();
+    expect(resolver).toBeTruthy();
+    expect(typeof resolver.lookupUsernameFromAddress).toBe('function')
+  });
+  test('User and colony resolvers inherit from ENSResolver', () => {
+    userResolver = resolver.getResolver('user')
+    colonyResolver = resolver.getResolver('colony')
+
+    expect(userResolver instanceof ENSResolver).toBeTruthy();
+    expect(colonyResolver instanceof ENSResolver).toBeTruthy();
+  });
+  test('User resolver contains a resolve method, colony resolver not implemented', async () => {
+    expect(typeof userResolver.resolve).toBe('function');
+    expect(colonyResolver.resolve).toThrow();
+  });
+});
