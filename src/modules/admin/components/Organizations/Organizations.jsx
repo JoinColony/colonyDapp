1fdadcd684a62b8ef192d36bbe8d0549adc1c494
Raul Glogovetan <raul@glogovetan.com>
2018-10-12 15:48:38 +0300

    Add: admin `Organizations` show list of admins

--- src/modules/admin/components/Organizations/Organizations.jsx
+++ src/modules/admin/components/Organizations/Organizations.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
 import { Tab, Tabs, TabList, TabPanel } from '~core/Tabs';
-
+import UserList from '../UserList';
 import OrganizationAddAdmins from './OrganizationAddAdmins.jsx';
 
 import styles from './Organizations.css';
@@ -24,6 +24,10 @@ const MSG = defineMessages({
     id: 'admin.Organizations.tabDomains',
     defaultMessage: 'Domains',
   },
+  labelAdminList: {
+    id: 'admin.Organizations.labelAdminList',
+    defaultMessage: 'Name',
+  },
 });
 
 const displayName: string = 'admin.Organizations';
@@ -40,6 +44,22 @@ const Organizations = () => (
         </TabList>
         <TabPanel>
           <OrganizationAddAdmins availableAdmins={usersMocks} />
+          <div className={styles.userListWrapper}>
+            {/*
+              * UserList follows the design principles from TaskList in dashboard,
+              * but if it turns out we're going to use this in multiple places,
+              * we should consider moving it to core
+              */}
+            <UserList
+              users={usersMocks}
+              label={MSG.labelAdminList}
+              showDisplayName
+              showUsername
+              showMaskedAddress
+              viewOnly={false}
+              onRemove={console.log}
+            />
+          </div>
         </TabPanel>
       </Tabs>
     </div>
