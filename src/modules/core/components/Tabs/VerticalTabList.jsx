87257b42e698b55356d51a172c58e652b03abdbb
Raul Glogovetan <raul@glogovetan.com>
2018-10-15 16:59:13 +0300

    Fix: `core` `VerticalTabList` proper `tabsRole` name

--- src/modules/core/components/Tabs/VerticalTabList.jsx
+++ src/modules/core/components/Tabs/VerticalTabList.jsx
@@ -7,19 +7,14 @@ import styles from './VerticalTabList.css';
 
 type Props = {};
 
-/*
- * @NOTE This is breaking convention and might get confusing
- *
- * But if we don't name the component `TabList`, `react-tabs` won't detect it and will
- * break it's functionality (because it goes by name and not by rendered component)
- */
-const displayName: string = 'TabList';
+const displayName: string = 'VerticalTabList';
+const tabsRole: string = 'TabList';
 
 const VerticalTabList = ({ ...props }: Props) => (
   <ReactTabList className={styles.main} {...props} />
 );
 
-VerticalTabList.tabsRole = displayName;
+VerticalTabList.tabsRole = tabsRole;
 
 VerticalTabList.displayName = displayName;
 
