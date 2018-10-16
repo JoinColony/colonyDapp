408aa83ae774a8b77fcf6c086691c12eabb6f16d
Curtis Olson <curt@webfinitytc.com>
2018-10-16 18:39:01 -0400

    Add util to sort objects by multiple properties, use in `Tokens`

--- src/utils/arrays/index.js
+++ src/utils/arrays/index.js
@@ -21,3 +21,88 @@ export const shuffle = (array: Array<string>) => {
   }
   return array;
 };
+
+/**
+ * Sort an array of objects by multiple properties.
+ */
+type ValidSortByType = boolean | number | string;
+
+type SortByPropertyConfig = {
+  compareFn?: (prevVal: any, nextVal: any) => number,
+  name: string,
+  reverse?: boolean,
+};
+
+export const sortObjectsBy = (
+  ...sortByPropertyNames: Array<string | SortByPropertyConfig>
+): ((prev: Object, next: Object) => number) => {
+  const defaultCompare = (
+    valA: ValidSortByType,
+    valB: ValidSortByType,
+  ): number => {
+    if (!!valA && valB === undefined) {
+      return -1;
+    }
+    if (!!valB && valA === undefined) {
+      return 1;
+    }
+
+    if (typeof valA !== typeof valB) {
+      return 0;
+    }
+
+    if (typeof valA === 'string' && typeof valB === 'string') {
+      // eslint-disable-next-line no-param-reassign
+      valA = valA.toLowerCase();
+      // eslint-disable-next-line no-param-reassign
+      valB = valB.toLowerCase();
+    }
+
+    if (valA === valB) {
+      return 0;
+    }
+
+    if (typeof valA === 'boolean' && typeof valB === 'boolean') {
+      if (valA || valB) {
+        return valA ? -1 : 1;
+      }
+    }
+
+    // $FlowFixMe: Flow is confused by mismatched types. Mismatched types will return 0 above.
+    return valA < valB ? -1 : 1;
+  };
+
+  const sortByConfigs: Array<SortByPropertyConfig> = sortByPropertyNames.map(
+    sortKey => {
+      let sortItem = {};
+      if (typeof sortKey === 'string') {
+        sortItem.name = sortKey;
+      } else {
+        sortItem = { ...sortKey };
+      }
+      return sortItem;
+    },
+  );
+
+  return (prev: Object, next: Object): number => {
+    let result = 0;
+
+    for (let i = 0; i < sortByConfigs.length; i += 1) {
+      const {
+        compareFn = defaultCompare,
+        name,
+        reverse = false,
+      }: SortByPropertyConfig = sortByConfigs[i];
+
+      const directionMultiplier = reverse ? -1 : 1;
+      const prevVal = prev[name];
+      const nextVal = next[name];
+
+      result = compareFn(prevVal, nextVal) * directionMultiplier;
+      if (result !== 0) {
+        break;
+      }
+    }
+    return result;
+  };
+};
