845a6c9a434649b2f0722685c3d16c9c2087153b
Raul Glogovetan <raul@glogovetan.com>
2018-10-16 18:16:47 +0300

    Add: admin `TransactionList` initial component

--- src/modules/admin/components/Transactions/Transactions.jsx
+++ src/modules/admin/components/Transactions/Transactions.jsx
@@ -2,12 +2,17 @@
 
 import React from 'react';
 
+import TransactionList from '../TransactionList';
+
 import styles from './Transactions.css';
 
 const displayName: string = 'admin.Transactions';
 
 const Transactions = () => (
-  <div className={styles.main}>Transactions content</div>
+  <div className={styles.main}>
+    Transactions content
+    <TransactionList />
+  </div>
 );
 
 Transactions.displayName = displayName;
