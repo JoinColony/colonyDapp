9cfa0721a4d86e353c38894aa26322b2d54ad5bb
Raul Glogovetan <raul@glogovetan.com>
2018-10-18 20:50:54 +0300

    Refactor: `ConnectWalletWizard` inject different hardware
    Into the hardware step based on what option was choosen

--- src/modules/users/components/ConnectWalletWizard/ConnectWalletWizard.js
+++ src/modules/users/components/ConnectWalletWizard/ConnectWalletWizard.js
@@ -1,6 +1,8 @@
 /* @flow */
 
-import { compose } from 'recompose';
+import { compose, withProps } from 'recompose';
+import ledgerWallet from '@colony/purser-ledger';
+import trezorWallet from '@colony/purser-trezor';
 
 import withWizard from '~core/Wizard/withWizard';
 import ConnectWalletWizard from './ConnectWalletWizard.jsx';
@@ -14,16 +16,29 @@ import * as StepJSONUpload from './StepJSONUpload';
 const stepArray = [StepStart];
 
 type StepValues = {
-  method: 'metamask' | 'hardware' | 'mnemonic' | 'json',
+  method: 'metamask' | 'trezor' | 'ledger' | 'mnemonic' | 'json',
 };
+
+/*
+ * Retruns a new step export so we can enhance it with the different hardware
+ * wallet types
+ */
+const enhancedHardwareStep = (
+  hardwareWalletType: ledgerWallet | trezorWallet,
+) =>
+  Object.assign({}, StepHardware, {
+    Step: withProps({ hardwareWalletType })(StepHardware.Step),
+  });
 
 // This is a step function to allow the wizard flow to branch
 // off into two instead of just stepping through an array in a linear manner
 const stepFunction = (step: number, { method }: StepValues) => {
   if (step === 1) {
     switch (method) {
-      case 'hardware':
-        return StepHardware;
+      case 'trezor':
+        return enhancedHardwareStep(trezorWallet);
+      case 'ledger':
+        return enhancedHardwareStep(ledgerWallet);
       case 'metamask':
         return StepMetaMask;
       case 'mnemonic':
