dc41cbfc4975db0f0f966ea67683aba2a608439b
Raul Glogovetan <raul@glogovetan.com>
2018-10-18 19:29:45 +0300

    Add: wire up admin `Transaction` component

--- src/modules/admin/components/Transactions/Transactions.jsx
+++ src/modules/admin/components/Transactions/Transactions.jsx
@@ -1,17 +1,122 @@
 /* @flow */
 
 import React from 'react';
+import { defineMessages } from 'react-intl';
+
+import Heading from '~core/Heading';
 
 import TransactionList from '../TransactionList';
 
 import styles from './Transactions.css';
 
+/*
+ * Mock transaction data
+ */
+import {
+  mockTransactions,
+  mockedColoniesAddressBook,
+  mockedUsersAddressBook,
+  mockedTasksList,
+} from './__datamocks__/transactionMocks';
+
+import type { TransactionType } from '~types/transaction';
+
+const MSG = defineMessages({
+  transactionsTitle: {
+    id: 'admin.Transactions.transactionsTitle',
+    defaultMessage: 'Transactions',
+  },
+  transactionHistoryTitle: {
+    id: 'admin.Transactions.transactionHistoryTitle',
+    defaultMessage: 'Transaction History',
+  },
+  pendingTransactionsTitle: {
+    id: 'admin.Transactions.pendingTransactionsTitle',
+    defaultMessage: 'Pending Transactions',
+  },
+});
+
 const displayName: string = 'admin.Transactions';
+
+/*
+ * @NOTE Mock values.
+ * These we most likely come from the redux store.
+ */
+const colonyAddress: string = '0x344FD3EaDF01E9BF077f4a3208439A3A4A428507';
+/*
+ * @NOTE Mock data augmentation
+ * Augment the original transaction list with user's display and username
+ * Augment the original transaction list with the tasks details
+ *
+ * This should be done somewhere in a `reselect` maybe?
+ */
+const augmentedTransactions: Array<TransactionType> = mockTransactions.map(
+  (transaction: TransactionType): TransactionType => {
+    const { to, from, nonce } = transaction;
+    return Object.assign({}, transaction, {
+      userDetails: mockedUsersAddressBook[to] || mockedUsersAddressBook[from],
+      colonyDetails:
+        mockedColoniesAddressBook[to] || mockedColoniesAddressBook[from],
+      task: mockedTasksList[nonce],
+    });
+  },
+);
+const augmentedPendingTransactions = augmentedTransactions.slice(0, 10);
+/*
+* @NOTE Mock methods
+* These are also mocks basically, since the actual ones might change
+ */
+/*
+ * Method to call when claiming the pot
+ */
+const handleClaim = (transaction: TransactionType) => {
+  console.log(`[${displayName}] Claimed transaction`, transaction);
+};
+/*
+ * Method to call when clicking the *Etherscan* button
+ */
+const handleEtherscan = (transaction: TransactionType) => {
+  console.log(`[${displayName}] Redirecting to Etherscan`, transaction);
+  if (transaction && transaction.hash) {
+    return global.location.assign(
+      `https://rinkeby.etherscan.io/tx/${transaction.hash}`,
+    );
+  }
+  return false;
+};
 
 const Transactions = () => (
   <div className={styles.main}>
-    Transactions content
-    <TransactionList />
+    <div className={styles.titleContainer}>
+      <Heading
+        appearance={{ size: 'medium', margin: 'none' }}
+        text={MSG.transactionsTitle}
+      />
+    </div>
+    <div className={styles.transactionsWrapper}>
+      {augmentedPendingTransactions &&
+        /*
+         * Needs to be typecast, otherwise React will just print the value (eg: 0)
+         */
+        !!augmentedPendingTransactions.length && (
+          <div className={styles.pendingTransactionsWrapper}>
+            <TransactionList
+              label={MSG.pendingTransactionsTitle}
+              currentColonyAddress={colonyAddress}
+              transactions={augmentedPendingTransactions}
+              onClaim={handleClaim}
+            />
+          </div>
+        )}
+      <div className={styles.historyTransactionsWrapper}>
+        <TransactionList
+          label={MSG.transactionHistoryTitle}
+          currentColonyAddress={colonyAddress}
+          transactions={augmentedTransactions}
+          onEtherscan={handleEtherscan}
+        />
+      </div>
+    </div>
   </div>
 );
 
