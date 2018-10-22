afe8955d209eebd9d7a7bc31599e3a8f748044c5
coyotespike <timothy.roy@protonmail.ch>
2018-10-22 17:04:14 +0200

    Remove UPDATE_USER_PROFILE reducer in favor of setting whole profile

--- src/modules/users/reducers/userProfilesReducer.js
+++ src/modules/users/reducers/userProfilesReducer.js
@@ -2,7 +2,7 @@
 
 import update from 'immutability-helper';
 
-import { SET_USER_PROFILE, UPDATE_USER_PROFILE } from '../actionTypes';
+import { SET_USER_PROFILE } from '../actionTypes';
 
 import type { Action } from '~types/';
 import type { UserType } from '~types/user';
@@ -18,32 +18,6 @@ const userProfilesReducer = (state: State = INITIAL_STATE, action: Action) => {
     case SET_USER_PROFILE: {
       const { walletAddress, set } = action.payload;
       return update(state, { [walletAddress]: { $set: set } });
-    }
-
-    case UPDATE_USER_PROFILE: {
-      // TODO: THIS IS NOT DONE/CORRECT - Please use the following payload (and implement it like this):
-      //  payload: { walletAddress: string, update: { name: string, bio: string /* ... */ }}
-
-      const {
-        profileKey,
-        property: profileProperty,
-        value: profileValue,
-      } = action.payload;
-
-      return update(state, {
-        data: {
-          profiles: {
-            [profileKey]: {
-              [profileProperty]: {
-                $apply: prop =>
-                  Array.isArray(prop)
-                    ? prop.concat(profileValue)
-                    : profileValue,
-              },
-            },
-          },
-        },
-      });
     }
 
     default:
