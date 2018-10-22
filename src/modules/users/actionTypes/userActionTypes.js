2c4b7fe98fc3878ce429055d0f02bf472ba6af99
coyotespike <timothy.roy@protonmail.ch>
2018-10-22 17:03:13 +0200

    Add to and remove from userActionTypes

---
+++ src/modules/users/actionTypes/userActionTypes.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+/*
+ * User Action Types
+ */
+
+export const EDIT_USER_PROFILE = 'user/EDIT_USER_PROFILE';
+export const SET_USER_PROFILE = 'user/SET_PROFILE';
+export const SET_USER_PROFILE_ERROR = 'user/SET_PROFILE_ERROR';
+export const SET_CURRENT_USER = 'user/SET_CURRENT_USER';
+export const SET_CURRENT_USER_ERROR = 'user/SET_CURRENT_USER_ERROR';
+
+export const FETCH_USER_PROFILE = 'user/FETCH_PROFILE';
