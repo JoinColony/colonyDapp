24036550012c94d861847b09c30cb4014b23ecbe
Raul Glogovetan <raul@glogovetan.com>
2018-10-22 20:36:03 +0300

    Move: `AvatarDropdown` from `dashboard` to `user` module

--- src/modules/pages/components/ProfileTemplate/ProfileTemplate.jsx
+++ src/modules/pages/components/ProfileTemplate/ProfileTemplate.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 import { getMainClasses } from '~utils/css';
 
-import AvatarDropdown from '~dashboard/AvatarDropdown';
+import AvatarDropdown from '~user/AvatarDropdown';
 
 import NavigationBar from '~pages/NavigationBar';
 
