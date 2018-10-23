094c8dc3125cde3c22b5b3ff23c16d563d78286c
coyotespike <timothy.roy@protonmail.ch>
2018-10-23 18:04:57 +0200

    Change exports from lib/database

--- src/lib/database/index.js
+++ src/lib/database/index.js
@@ -1,9 +1,8 @@
 /* @flow */
 
-import * as schemas from './schemas';
-
+import * as Commands from './commands';
 import * as Resolvers from './resolvers';
+import * as SCHEMAS from './schemas';
 
 export { default as DDB } from './DDB';
-export const SCHEMAS = schemas;
-export { Resolvers };
+export { Commands, Resolvers, SCHEMAS };
