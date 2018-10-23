16db08c22883ea046d153ae2b5c4e62dfa2217dc
Raul Glogovetan <raul@glogovetan.com>
2018-10-23 20:25:35 +0300

    Fix: `NavigationBar` better back routes props naming

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -90,7 +90,7 @@ const Routes = ({
         isConnected={isConnected}
         path={COLONY_HOME_ROUTE}
         component={ColonyHome}
-        overwriteBackRoute={DASHBOARD_ROUTE}
+        backRoute={DASHBOARD_ROUTE}
       />
       <ConnectedOnlyRoute
         isConnected={isConnected}
