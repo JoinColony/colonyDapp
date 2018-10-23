02ffd76345a7779ad124f4c528d3ada7da936fb6
Raul Glogovetan <raul@glogovetan.com>
2018-10-23 13:32:52 +0300

    Fix: add custom yup validators to jest's setup

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -3,6 +3,15 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+/*
+ * Custom validations need to be imported, otherwise all files that rely internally
+ * on them will fail tests
+ *
+ * Eg: `ProfileCreate` who is relying on `StepSelectToken` which
+ * uses a custom validator internally
+ */
+import '../modules/validations';
+
 Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('../i18n/en.json', () => ({}));
