a0a683e45380abb8d4ad140e74590db89e788468
coyotespike <timothy.roy@protonmail.ch>
2018-10-23 18:05:09 +0200

    Add integration test for setting multiple attributes in a store

--- integration-testing/DDB.test.js
+++ integration-testing/DDB.test.js
@@ -3,21 +3,35 @@
 import { create as createWallet } from '@colony/purser-software';
 import OrbitDB from 'orbit-db';
 import ipfsNode from '../src/lib/ipfsNode';
-import DDB from '../src/lib/database/DDB';
+import { Commands, DDB, SCHEMAS } from '../src/lib/database';
 import PurserIdentity from '../src/lib/database/PurserIdentity';
 import PurserIdentityProvider from '../src/lib/database/PurserIdentityProvider';
 
+let wallet;
+let identityProvider;
+let ddb;
+
+beforeAll(async () => {
+  DDB.registerSchema('userProfile', SCHEMAS.UserProfile);
+
+  wallet = await createWallet();
+
+  identityProvider = new PurserIdentityProvider(wallet);
+
+  ddb = await DDB.createDatabase(ipfsNode, identityProvider);
+});
+
 describe('Database setup', () => {
   test('Using purser', async () => {
-    const wallet = await createWallet();
-
-    const identityProvider = new PurserIdentityProvider(wallet);
-
-    const ddb = await DDB.createDatabase(ipfsNode, identityProvider);
-
     /* eslint-disable no-underscore-dangle */
     expect(ddb._orbitNode).toBeInstanceOf(OrbitDB);
     expect(ddb._orbitNode.identity).toBeInstanceOf(PurserIdentity);
     /* eslint-enable no-underscore-dangle */
+  });
+  test('Can edit multiple attributes', async () => {
+    const store = await ddb.createStore('keyvalue', 'userProfile');
+    await store.set({ username: 'hello', bio: 'born in Warsaw' });
+    const state = Commands.all(store);
+    expect(Object.keys(state).length).toBe(2);
   });
 });
