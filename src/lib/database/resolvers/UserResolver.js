ce7b3bd54ce48af64bbeb9c7587439df73dba572
coyotespike <timothy.roy@protonmail.ch>
2018-10-24 19:53:30 +0200

    Refactor UserResolver to use static ensHash method

--- src/lib/database/resolvers/UserResolver.js
+++ src/lib/database/resolvers/UserResolver.js
@@ -1,12 +1,11 @@
 /* @flow */
 
-import namehash from 'eth-ens-namehash';
 import ENSResolver from './ENSResolver';
 import type { OrbitDBAddress } from '../types';
 
 class UserResolver extends ENSResolver {
   async resolve(identifier: string): Promise<OrbitDBAddress> {
-    const nameHash = namehash.hash(identifier);
+    const nameHash = ENSResolver.ensHash(identifier);
     const {
       orbitDBAddress,
     } = await this._networkClient.getProfileDBAddress.call({
