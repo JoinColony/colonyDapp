1a0e06dea37360dbfb127227be9b81f73cbb100f
coyotespike <timothy.roy@protonmail.ch>
2018-10-24 15:20:22 +0200

    Change sagas to use objects instead of actionCreators

--- src/modules/users/actionCreators/userActionCreators.js
+++ src/modules/users/actionCreators/userActionCreators.js
@@ -1,27 +1,8 @@
 // @flow
-import {
-  SET_CURRENT_USER,
-  SET_CURRENT_USER_ERROR,
-  SET_USER_PROFILE,
-  SET_USER_PROFILE_ERROR,
-} from '../actionTypes';
+import { EDIT_USER_PROFILE } from '../actionTypes';
 
-export const setCurrentUserError = error => ({
-  type: SET_CURRENT_USER_ERROR,
-  payload: { error },
-});
-
-export const setCurrentUser = (user, walletAddress) => ({
-  type: SET_CURRENT_USER,
-  payload: { set: user, walletAddress },
-});
-
-export const updateUserProfileError = error => ({
-  type: SET_USER_PROFILE_ERROR,
-  payload: { error },
-});
-
-export const updateUserProfile = (user, walletAddress) => ({
-  type: SET_USER_PROFILE,
-  payload: { set: user, walletAddress },
+// eslint-disable-next-line import/prefer-default-export
+export const updateUserProfile = (currentAddress: String, update: Object) => ({
+  type: EDIT_USER_PROFILE,
+  payload: { currentAddress, update },
 });
