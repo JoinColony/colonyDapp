4f7fd617006a7ea041bec59a13c50a9388cfdaee
curtis <curtis@colony.io>
2018-10-24 12:28:36 -0400

    Remove `withHandlers` composition, make `Tokens.jsx` a class component

--- src/modules/admin/components/Tokens/Tokens.jsx
+++ src/modules/admin/components/Tokens/Tokens.jsx
@@ -1,8 +1,9 @@
 /* @flow */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
+import type { DialogType } from '~core/Dialog';
 import type { TokenType } from '~types/token';
 
 import Button from '~core/Button';
@@ -33,69 +34,74 @@ const MSG = defineMessages({
 });
 
 type Props = {
-  handleOpenTokenEditDialog: () => void,
-  handleSumbitTokenEditForm: (colonyTokens: Array<string>) => void,
+  openDialog: (dialogName: string, dialogProps: Object) => DialogType,
   tokens: Array<TokenType>,
 };
 
-const displayName = 'admin.Tokens';
+class Tokens extends Component<Props> {
+  static displayName = 'admin.Tokens';
 
-const Tokens = ({ handleOpenTokenEditDialog, tokens = [] }: Props) => {
-  const nativeToken = tokens.find(token => token.isNative);
-  const isColonyAdmin = true; // TODO determine this value. Will all users visiting this route be admins?
-  const isUserColonyOwner = true; // TODO determine this value.
-  const canMintNewTokens = true; // TODO determine this value. token generated at colony launch ? true : false;
-  return (
-    <div className={styles.main}>
-      <main>
-        <div className={styles.titleContainer}>
-          <Heading
-            text={MSG.title}
-            appearance={{ size: 'medium', theme: 'dark' }}
-          />
-          {nativeToken && (
-            <Heading appearance={{ size: 'normal' }}>
-              <FormattedMessage
-                {...MSG.nativeTokenText}
-                values={{ nativeToken: nativeToken.tokenSymbol }}
-              />
-            </Heading>
-          )}
-        </div>
-        <div className={styles.tokenCardContainer}>
-          <CardList>
-            {tokens.filter(token => token.isEnabled).map(token => (
-              <TokenCard key={token.id} token={token} />
-            ))}
-          </CardList>
-        </div>
-      </main>
-      {isColonyAdmin && (
-        <aside className={styles.sidebar}>
-          <ul>
-            {isUserColonyOwner &&
-              canMintNewTokens && (
-                <li>
-                  <Button
-                    text={MSG.navItemMintNewTokens}
-                    appearance={{ theme: 'blue' }}
-                  />
-                </li>
-              )}
-            <li>
-              <Button
-                text={MSG.navItemEditTokens}
-                appearance={{ theme: 'blue' }}
-                onClick={handleOpenTokenEditDialog}
-              />
-            </li>
-          </ul>
-        </aside>
-      )}
-    </div>
-  );
-};
+  handleOpenTokenEditDialog = (): void => {
+    const { openDialog, tokens } = this.props;
+    openDialog('TokenEditDialog', { tokens });
+  };
 
-Tokens.displayName = displayName;
+  render() {
+    const { tokens = [] } = this.props;
+    const nativeToken = tokens.find(token => token.isNative);
+    const isColonyAdmin = true; // TODO determine this value. Will all users visiting this route be admins?
+    const isUserColonyOwner = true; // TODO determine this value.
+    const canMintNewTokens = true; // TODO determine this value. token generated at colony launch ? true : false;
+    return (
+      <div className={styles.main}>
+        <main>
+          <div className={styles.titleContainer}>
+            <Heading
+              text={MSG.title}
+              appearance={{ size: 'medium', theme: 'dark' }}
+            />
+            {nativeToken && (
+              <Heading appearance={{ size: 'normal' }}>
+                <FormattedMessage
+                  {...MSG.nativeTokenText}
+                  values={{ nativeToken: nativeToken.tokenSymbol }}
+                />
+              </Heading>
+            )}
+          </div>
+          <div className={styles.tokenCardContainer}>
+            <CardList>
+              {tokens.filter(token => token.isEnabled).map(token => (
+                <TokenCard key={token.id} token={token} />
+              ))}
+            </CardList>
+          </div>
+        </main>
+        {isColonyAdmin && (
+          <aside className={styles.sidebar}>
+            <ul>
+              {isUserColonyOwner &&
+                canMintNewTokens && (
+                  <li>
+                    <Button
+                      text={MSG.navItemMintNewTokens}
+                      appearance={{ theme: 'blue' }}
+                    />
+                  </li>
+                )}
+              <li>
+                <Button
+                  text={MSG.navItemEditTokens}
+                  appearance={{ theme: 'blue' }}
+                  onClick={this.handleOpenTokenEditDialog}
+                />
+              </li>
+            </ul>
+          </aside>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Tokens;
