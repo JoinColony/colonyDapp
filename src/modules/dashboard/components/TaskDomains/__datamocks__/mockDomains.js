8935511820f6b3804802d5cac7e26317091f4263
Raul Glogovetan <raul@glogovetan.com>
2018-10-24 23:24:19 +0300

    Add: `TaskDomains` domain data mocks

---
+++ src/modules/dashboard/components/TaskDomains/__datamocks__/mockDomains.js
@@ -0,0 +1,34 @@
+/* @flow */
+
+export const domainMocks = [
+  {
+    id: 0,
+    name: 'Daily Standup',
+  },
+  {
+    id: 1,
+    name: 'Design',
+  },
+  {
+    id: 2,
+    name: 'Dev',
+  },
+  {
+    id: 3,
+    name: 'Hackathon',
+  },
+  {
+    id: 4,
+    name: 'Random',
+  },
+  {
+    id: 5,
+    name: 'Time Off',
+  },
+  {
+    id: 6,
+    name: 'Website',
+  },
+];
+
+export const selectedDomainMock = 2;
