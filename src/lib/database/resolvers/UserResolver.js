539c445389872aee4170981b6ab720e4415b7efc
coyotespike <timothy.roy@protonmail.ch>
2018-10-27 18:26:32 +0200

    Make UserResolver handle wallet addresses and short usernames, add static suffix

--- src/lib/database/resolvers/UserResolver.js
+++ src/lib/database/resolvers/UserResolver.js
@@ -4,8 +4,16 @@ import ENSResolver from './ENSResolver';
 import type { OrbitDBAddress } from '../types';
 
 class UserResolver extends ENSResolver {
+  static suffix = 'user';
+
   async resolve(identifier: string): Promise<OrbitDBAddress> {
-    const nameHash = ENSResolver.ensHash(identifier);
+    let domain;
+    if (ENSResolver.isAddress(identifier)) {
+      domain = await this.lookupDomainNameFromAddress(identifier);
+    } else {
+      domain = this.getDomain(identifier);
+    }
+    const nameHash = ENSResolver.ensHash(domain);
     const {
       orbitDBAddress,
     } = await this._networkClient.getProfileDBAddress.call({
