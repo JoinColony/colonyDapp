0828d45b92acbce80d2ea71a13be0e7cd73bb214
coyotespike <timothy.roy@protonmail.ch>
2018-10-28 18:19:05 +0100

    Change action types location, add action types

--- src/modules/users/actionTypes/currentUserActionTypes.js
+++ src/modules/users/actionTypes/currentUserActionTypes.js
@@ -1,6 +1,8 @@
 /* @flow */
 
-export const SET_USER_PROFILE = 'users/SET_PROFILE';
-export const SET_USER_PROFILE_ERROR = 'user/SET_PROFILE_ERROR';
 export const SET_CURRENT_USER = 'users/SET_CURRENT_USER';
 export const SET_CURRENT_USER_ERROR = 'users/SET_CURRENT_USER_ERROR';
+
+export const EDIT_USER_PROFILE = 'users/EDIT_USER_PROFILE';
+export const EDIT_USER_PROFILE_ERROR = 'users/EDIT_USER_PROFILE_ERROR';
+export const EDIT_USER_PROFILE_SUCCESS = 'users/EDIT_USER_PROFILE_SUCCESS';
