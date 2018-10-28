5bc16299d7539135d540bd19fac266949b58f5cc
coyotespike <timothy.roy@protonmail.ch>
2018-10-28 18:20:54 +0100

    Refactor reducers to use immutable records, accept multiple actions

--- src/modules/users/reducers/currentUserReducer.js
+++ src/modules/users/reducers/currentUserReducer.js
@@ -1,27 +1,36 @@
 /* @flow */
 
-import { SET_CURRENT_USER } from '../actionTypes';
+import { Map as ImmutableMap } from 'immutable';
+
+import {
+  EDIT_USER_PROFILE_ERROR,
+  EDIT_USER_PROFILE_SUCCESS,
+  SET_CURRENT_USER,
+} from '../actionTypes';
 
 import type { Action } from '~types/index';
 
 import { User } from '../records';
 
-type State = User | null;
-
-// TODO pair with James to make look like userProfilesReducer
-
-const INITIAL_STATE = null;
+type CurrentUserProfileState = ImmutableMap<string, User>;
+const INITIAL_STATE: CurrentUserProfileState = new ImmutableMap();
 
 // TODO better types for action payloads
-const currentUserReducer = (state: State = INITIAL_STATE, action: Action) => {
+const currentUserReducer = (
+  state: CurrentUserProfileState = INITIAL_STATE,
+  action: Action,
+) => {
   switch (action.type) {
-    case SET_CURRENT_USER: {
+    case SET_CURRENT_USER:
+    case EDIT_USER_PROFILE_SUCCESS: {
       const { walletAddress } = action.payload;
       // TODO username is a required property, but we don't have it at this
       // stage; what can we do to improve this?
       return new User({ walletAddress, username: '' });
     }
 
+    case EDIT_USER_PROFILE_ERROR: {
+    }
     default:
       return state;
   }
