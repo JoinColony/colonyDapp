878a70f931555e413fd536aceedfeb0e18ac8e98
coyotespike <timothy.roy@protonmail.ch>
2018-10-31 22:53:30 +0100

    Change UserProfile component to class, define smart wrapper

--- src/modules/users/components/UserProfile/UserProfile.js
+++ src/modules/users/components/UserProfile/UserProfile.js
@@ -1,67 +1,25 @@
 /* @flow */
 
-import React from 'react';
-import {
-  compose,
-  lifecycle,
-  branch,
-  renderComponent,
-  withHandlers,
-  withProps,
-} from 'recompose';
-
 import { connect } from 'react-redux';
-import { defineMessages } from 'react-intl';
 
 import UserProfile from './UserProfile.jsx';
-import LoadingTemplate from '~pages/LoadingTemplate';
 
-const FETCH_USER_PROFILE = 'FETCH_USER_PROFILE';
+import { USER_PROFILE_FETCH } from '../../actionTypes';
 
-const MSG = defineMessages({
-  loadingText: {
-    id: 'ViewUserProfile.UserProfile.loadingText',
-    defaultMessage: 'Fetching a user profile',
-  },
-  loaderDescription: {
-    id: 'ViewUserProfile.UserProfile.loaderDescription',
-    defaultMessage: 'Please wait while this user profile is being fetched.',
-  },
+const mapStateToProps = (state, ownProps) => ({
+  isLoading: state.user.userProfiles.isLoading,
+  isError: state.user.userProfiles.isError,
+  targetUserId: ownProps.match.params.userId,
+  targetProfile: state.user.userProfiles[ownProps.match.params.userId],
 });
 
-const Spinner = () => <LoadingTemplate loadingText={MSG.loadingText} />;
-const withUserData = lifecycle({
-  componentDidMount() {
-    if (!this.props.targetProfile) {
-      this.props.fetchUserProfile(this.props.targetUserId);
-    }
+const mapDispatchToProps = dispatch => ({
+  fetchUserProfile: username => {
+    dispatch({ type: USER_PROFILE_FETCH, payload: username });
   },
 });
 
-// TODO make this placeholder real
-const isError = ({ targetProfile }) => typeof targetProfile === 'Error';
-const isLoading = ({ targetProfile }) => !targetProfile;
-
-const mapStateToProps = state => ({
-  userProfiles: state.user.userProfiles,
-});
-
-const enhance = compose(
-  connect(mapStateToProps),
-  withProps(props => ({
-    targeProfile: !!props.userProfiles[props.match.params.userId],
-    targetUserId: props.match.params.userId,
-  })),
-  withHandlers({
-    fetchUserProfile: ({ dispatch }) => username => {
-      dispatch({ type: FETCH_USER_PROFILE, payload: username });
-    },
-  }),
-  withUserData,
-  branch(isError, renderComponent(Spinner)),
-  branch(isLoading, renderComponent(Spinner)),
-);
-
-const UserProfileContainer = enhance(UserProfile);
-
-export default UserProfileContainer;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(UserProfile);
--- src/modules/users/components/UserProfile/UserProfile.jsx
+++ src/modules/users/components/UserProfile/UserProfile.jsx
@@ -1,9 +1,13 @@
 /* @flow */
-import React from 'react';
+
+import React, { Component } from 'react';
+
+import { defineMessages } from 'react-intl';
 
 import ColonyGrid from '~core/ColonyGrid';
 import ActivityFeed from '~core/ActivityFeed';
 
+import LoadingTemplate from '~pages/LoadingTemplate';
 import ProfileTemplate from '~pages/ProfileTemplate';
 import UserMeta from './UserMeta.jsx';
 
@@ -13,16 +17,50 @@ import mockUser from './__datamocks__/mockUser';
 
 import styles from './UserProfile.css';
 
-// @NOTE userId can be accessed from the props: `{ match: { userId } }`
-const UserProfile = () => (
-  <ProfileTemplate asideContent={<UserMeta user={mockUser} />}>
-    <section className={styles.sectionContainer}>
-      <ColonyGrid colonies={mockColonies} />
-    </section>
-    <section className={styles.sectionContainer}>
-      <ActivityFeed activities={mockActivities} />
-    </section>
-  </ProfileTemplate>
-);
+type Props = {
+  fetchUserProfile: func,
+  targetProfile: Object,
+  targetUserId: string,
+  isLoading: boolean,
+};
 
+type State = {};
+
+const MSG = defineMessages({
+  loadingText: {
+    id: 'ViewUserProfile.UserProfile.loadingText',
+    defaultMessage: 'Fetching a user profile',
+  },
+  loaderDescription: {
+    id: 'ViewUserProfile.UserProfile.loaderDescription',
+    defaultMessage: 'Please wait while this user profile is being fetched.',
+  },
+});
+
+const Spinner = () => <LoadingTemplate loadingText={MSG.loadingText} />;
+
+class UserProfile extends Component<Props, State> {
+  componentDidMount() {
+    const { fetchUserProfile, targetProfile, targetUserId } = this.props;
+    if (!targetProfile) {
+      fetchUserProfile(targetUserId);
+    }
+  }
+
+  render() {
+    const { isLoading } = this.props;
+    return isLoading ? (
+      <Spinner />
+    ) : (
+      <ProfileTemplate asideContent={<UserMeta user={mockUser} />}>
+        <section className={styles.sectionContainer}>
+          <ColonyGrid colonies={mockColonies} />
+        </section>
+        <section className={styles.sectionContainer}>
+          <ActivityFeed activities={mockActivities} />
+        </section>
+      </ProfileTemplate>
+    );
+  }
+}
 export default UserProfile;
