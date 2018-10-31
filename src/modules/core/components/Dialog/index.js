73b1fba92be1d0df96394afbbf7ea555a87135b7
Raul Glogovetan <raul@glogovetan.com>
2018-10-31 15:57:32 +0200

    Fix: core `Dialog` export all components from index

--- src/modules/core/components/Dialog/index.js
+++ src/modules/core/components/Dialog/index.js
@@ -2,4 +2,8 @@
 
 export { default } from './Dialog.jsx';
 
+export { default as DialogProvider } from './DialogProvider.jsx';
+export { default as ConfirmDialog } from './ConfirmDialog.jsx';
+export { default as DialogSection } from './DialogSection.jsx';
+
 export type { Cancel, Close, DialogComponent, DialogType } from './types';
