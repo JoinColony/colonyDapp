5a41ff4b764290456731a0e2ab5094387e38062f
coyotespike <timothy.roy@protonmail.ch>
2018-11-06 14:50:01 +0100

    Fix type ActionCreator's type argument

--- src/types/index.js
+++ src/types/index.js
@@ -11,4 +11,4 @@ export * from './WalletRecord';
 // TODO consider making this accept generics so that we can better test
 // reducers: https://github.com/facebook/flow/issues/4737
 export type Action = { type: string, payload: any };
-export type ActionCreator = (payload: any) => Action;
+export type ActionCreator = (...args: Array<any>) => Action;
