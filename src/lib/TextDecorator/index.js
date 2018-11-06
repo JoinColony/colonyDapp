8ee772d585352498c0dd40406f3eb46b41837d56
sprusr <scott@spru.sr>
2018-11-06 15:53:19 +0000

    Add TextDecorator from colonyBeta
    Used for formatting strings in a modular way. For example, extracting links and
    transforming them to <a> tags.

---
+++ src/lib/TextDecorator/index.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import TextDecorator from './TextDecorator';
+
+import { USERNAME_SCHEMA } from './schemas';
+
+TextDecorator.loadSchema(USERNAME_SCHEMA);
+
+export default TextDecorator;
---
+++ src/lib/TextDecorator/schemas.js
@@ -0,0 +1,26 @@
+/* @flow */
+/* eslint import/prefer-default-export: 0 */
+
+const USER_PREFIX = '@';
+
+export const USERNAME_SCHEMA = {
+  prefix: USER_PREFIX,
+  name: 'username',
+  schema: {
+    validate(text: *, pos: *, self: *) {
+      const tail = text.slice(pos);
+      const usernameRegex =
+        self.re.username ||
+        new RegExp(`^([a-z0-9_.-]*)(?=$|${self.re.src_ZPCc})`);
+      if (usernameRegex.test(tail)) {
+        // We additionally disable `@` ("@@mention" is invalid)
+        if (tail[0] === USER_PREFIX) {
+          return false;
+        }
+        // $FlowFixMe
+        return tail.match(usernameRegex)[0].length;
+      }
+      return 0;
+    },
+  },
+};
