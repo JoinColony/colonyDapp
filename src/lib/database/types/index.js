d64efd8347552b3eb269b0c9c2579527dc2c21e0
James Lefrère <james@colony.io>
2018-11-07 12:25:42 +0100

    Improve DDB types
    * Add comments
    * Add `isAuthorized`, `setup`, `grant` and `revoke` methods to `AccessController` interface

--- src/lib/database/types/index.js
+++ src/lib/database/types/index.js
@@ -8,12 +8,9 @@ export type { OrbitDBStore } from './OrbitDBStore';
 export type { OrbitDBKVStore } from './OrbitDBKVStore';
 export type { ENSResolverType } from './ENSResolver';
 
-export type Schema = {
-  [string]: any,
-};
-
 export type IPFSHash = string;
 
+// IPFS store types
 export type StoreType =
   | 'counter'
   | 'eventlog'
