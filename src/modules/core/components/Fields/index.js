95434452a593419560a2ff89f29c165c2e638a00
Raul Glogovetan <raul@glogovetan.com>
2018-11-07 21:35:05 +0200

    Fix: core `Fields` export `Radio` as well

--- src/modules/core/components/Fields/index.js
+++ src/modules/core/components/Fields/index.js
@@ -8,6 +8,8 @@ export { default as Input } from './Input';
 export { default as InputLabel } from './InputLabel';
 export { default as Select } from './Select';
 export { default as Textarea, TextareaAutoresize } from './Textarea';
+export { default as Radio } from './Radio';
+
 export { default as asField } from './asField';
 export { default as asFieldArray } from './asFieldArray';
 
