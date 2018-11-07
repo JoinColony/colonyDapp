5a47d21563373e154603066df72b50e3c567d9a9
James Lefrère <james@colony.io>
2018-11-07 12:31:06 +0100

    Extend command types

--- src/lib/database/commands.js
+++ src/lib/database/commands.js
@@ -1,18 +1,12 @@
 /* @flow */
 
-// This file is in prepartion to handle multiple store types with one command
+import type { Store, KVStore } from './stores/index';
+
+// This file is in preparation to handle multiple store types with one command
 // and to add more complex functionality
 
-export const get = (store: Store, key: string): any => {
-  if (store.get) {
-    return store.get(key);
-  }
-  return null;
-};
+export const get = (store: Store | KVStore, key: string) =>
+  typeof store.get === 'function' ? store.get(key) : null;
 
-export const getAll = (store: Store): any => {
-  if (store.all) {
-    return store.all();
-  }
-  return null;
-};
+export const getAll = (store: Store | KVStore) =>
+  typeof store.all === 'function' ? store.all() : null;
