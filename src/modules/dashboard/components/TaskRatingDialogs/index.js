b99f997fff34ee3056b270d05996818e483da323
Raul Glogovetan <raul@glogovetan.com>
2018-11-08 00:15:50 +0200

    Add: `WorkerRatingDialog` component

--- src/modules/dashboard/components/TaskRatingDialogs/index.js
+++ src/modules/dashboard/components/TaskRatingDialogs/index.js
@@ -1,6 +1,4 @@
 /* @flow */
 
-export {
-  default,
-  default as ManagerRatingDialog,
-} from './ManagerRatingDialog.jsx';
+export { default as ManagerRatingDialog } from './ManagerRatingDialog.jsx';
+export { default as WorkerRatingDialog } from './WorkerRatingDialog.jsx';
