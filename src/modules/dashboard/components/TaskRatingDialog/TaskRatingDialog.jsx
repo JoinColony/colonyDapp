442a025f47a82206277bbabe4aabfc3b498329fc
Raul Glogovetan <raul@glogovetan.com>
2018-11-07 22:59:25 +0200

    Add: `TaskRatingDialog` star rating radio fields

--- src/modules/dashboard/components/TaskRatingDialog/TaskRatingDialog.jsx
+++ src/modules/dashboard/components/TaskRatingDialog/TaskRatingDialog.jsx
@@ -9,8 +9,9 @@ import * as yup from 'yup';
 import Button from '~core/Button';
 import Dialog from '~core/Dialog';
 import DialogSection from '~core/Dialog/DialogSection.jsx';
-import { Checkbox, Form, InputLabel } from '~core/Fields';
+import { Form, Radio } from '~core/Fields';
 import Heading from '~core/Heading';
+import Icon from '~core/Icon';
 
 import styles from './TaskRatingDialog.css';
 
@@ -24,10 +25,39 @@ const MSG = defineMessages({
     defaultMessage:
       'Please rate the manager of this task based on the criteria below.',
   },
+  ratingStar: {
+    id: 'dashboard.TaskRatingDialog.ratingStar',
+    defaultMessage: 'Rating Star',
+  },
+  managerRating3Title: {
+    id: 'dashboard.TaskRatingDialog.managerRating3Title',
+    defaultMessage: 'Above and beyond',
+  },
+  managerRating3Description: {
+    id: 'dashboard.TaskRatingDialog.managerRating3Description',
+    defaultMessage: 'The manager went above and beyond in their role.',
+  },
+  managerRating2Title: {
+    id: 'dashboard.TaskRatingDialog.managerRating2Title',
+    defaultMessage: 'Good',
+  },
+  managerRating2Description: {
+    id: 'dashboard.TaskRatingDialog.managerRating2Description',
+    defaultMessage: 'The manager performed their role well and as expected.',
+  },
+  managerRating1Title: {
+    id: 'dashboard.TaskRatingDialog.managerRating1Title',
+    defaultMessage: 'Unacceptable',
+  },
+  managerRating1Description: {
+    id: 'dashboard.TaskRatingDialog.managerRating1Description',
+    defaultMessage:
+      'Warning: this may result in a reputation penalty to the manager.',
+  },
 });
 
 type FormValues = {
-  colonyTokens: Array<string>,
+  rating: number,
 };
 
 type Props = {
@@ -37,18 +67,25 @@ type Props = {
 
 const displayName = 'dashboard.TaskRatingDialog';
 
-const validationSchema = yup.object().shape({});
+const validationSchema = yup.object().shape({
+  rating: yup.string().required('ARg!!!!'),
+});
 
-const TaskRatingDialog = ({ cancel, close }: Props) => (
+const TaskRatingDialog = ({ cancel }: Props) => (
   <Dialog cancel={cancel} className={styles.main}>
     <Form
       initialValues={{
         rating: '',
       }}
-      onSubmit={console.log}
-      // validationSchema={validationSchema}
+      /* eslint-disable-next-line no-console */
+      onSubmit={(values: FormValues) => console.log(`[${displayName}]`, values)}
+      validationSchema={validationSchema}
     >
-      {({ isSubmitting }: FormikProps<FormValues>) => (
+      {({
+        isSubmitting,
+        isValid,
+        values: { rating },
+      }: FormikProps<FormValues>) => (
         <Fragment>
           <DialogSection>
             <div className={styles.contentWrapper}>
@@ -56,6 +93,119 @@ const TaskRatingDialog = ({ cancel, close }: Props) => (
               <p className={styles.descriptionText}>
                 <FormattedMessage {...MSG.endTaskRateManagerDescription} />
               </p>
+              <section className={styles.ratingSection}>
+                {/*
+                 * Three star rating
+                 */}
+                <Radio
+                  checked={parseInt(rating, 10) === 3}
+                  name="rating"
+                  value={3}
+                >
+                  <div className={styles.ratingItem}>
+                    <div className={styles.ratingText}>
+                      <Heading
+                        appearance={{ size: 'normal', margin: 'none' }}
+                        text={MSG.managerRating3Title}
+                      />
+                      <p className={styles.ratingItemDescription}>
+                        <FormattedMessage {...MSG.managerRating3Description} />
+                      </p>
+                    </div>
+                    <div className={styles.ratingStars}>
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        appearance={{ size: 'tiny', theme: 'primary' }}
+                      />
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        appearance={{ size: 'tiny', theme: 'primary' }}
+                      />
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        appearance={{ size: 'tiny', theme: 'primary' }}
+                      />
+                    </div>
+                  </div>
+                </Radio>
+                {/*
+                 * Two star rating
+                 */}
+                <Radio
+                  checked={parseInt(rating, 10) === 2}
+                  name="rating"
+                  value={2}
+                >
+                  <div className={styles.ratingItem}>
+                    <div className={styles.ratingText}>
+                      <Heading
+                        appearance={{ size: 'normal', margin: 'none' }}
+                        text={MSG.managerRating2Title}
+                      />
+                      <p className={styles.ratingItemDescription}>
+                        <FormattedMessage {...MSG.managerRating2Description} />
+                      </p>
+                    </div>
+                    <div className={styles.ratingStars}>
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        appearance={{ size: 'tiny', theme: 'primary' }}
+                      />
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        appearance={{ size: 'tiny', theme: 'primary' }}
+                      />
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        className={styles.ratingStarUnselected}
+                      />
+                    </div>
+                  </div>
+                </Radio>
+                {/*
+                 * One star rating
+                 */}
+                <Radio
+                  checked={parseInt(rating, 10) === 1}
+                  name="rating"
+                  value={1}
+                >
+                  <div className={styles.ratingItem}>
+                    <div className={styles.ratingText}>
+                      <Heading
+                        appearance={{ size: 'normal', margin: 'none' }}
+                        text={MSG.managerRating1Title}
+                      />
+                      <p className={styles.ratingItemWarning}>
+                        <FormattedMessage {...MSG.managerRating1Description} />
+                      </p>
+                    </div>
+                    <div className={styles.ratingStars}>
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        appearance={{ size: 'tiny', theme: 'primary' }}
+                      />
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        className={styles.ratingStarUnselected}
+                      />
+                      <Icon
+                        name="star"
+                        title={MSG.ratingStar}
+                        className={styles.ratingStarUnselected}
+                      />
+                    </div>
+                  </div>
+                </Radio>
+              </section>
             </div>
           </DialogSection>
           <DialogSection appearance={{ align: 'right' }}>
@@ -66,6 +216,7 @@ const TaskRatingDialog = ({ cancel, close }: Props) => (
             />
             <Button
               appearance={{ theme: 'primary', size: 'large' }}
+              disabled={!isValid}
               loading={isSubmitting}
               text={{ id: 'button.confirm' }}
               type="submit"
