465eab60882e93314a35a326b29a38061b579a19
coyotespike <timothy.roy@protonmail.ch>
2018-11-08 15:41:40 -0600

    Improve OrbitDBFeedStore typing

--- src/lib/database/types/OrbitDBFeedStore.js
+++ src/lib/database/types/OrbitDBFeedStore.js
@@ -2,9 +2,18 @@
 
 import type { OrbitDBStore } from './OrbitDBStore';
 
+type FeedStoreOptions = {
+  gt?: string,
+  gte?: string,
+  lt?: string,
+  lte?: string,
+  limit?: number,
+  reverse?: boolean,
+};
+
 export interface OrbitDBFeedStore extends OrbitDBStore {
-    add(value: any): Promise<string>;
-    remove(key: string): Promise<string>;
-    get(key: string): Object;
-    iterator(options: Object): Array<Object>;
+  add(value: any): Promise<string>;
+  remove(key: string): Promise<string>;
+  get(key: string): Object;
+  iterator(options?: FeedStoreOptions): { collect: () => Array<Object> };
 }
