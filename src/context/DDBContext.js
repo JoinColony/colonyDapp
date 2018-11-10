6342f9e2980afdfdcaec4841a92826e2596be2d1
coyotespike <timothy.roy@protonmail.ch>
2018-11-09 18:56:42 -0600

    Register userActivity schema in DDB

--- src/context/DDBContext.js
+++ src/context/DDBContext.js
@@ -1,5 +1,8 @@
 /* @flow */
 
-import { DDB } from '../lib/database';
+import { DDB, SCHEMAS } from '../lib/database';
+
+DDB.registerSchema('userProfile', SCHEMAS.UserProfile);
+DDB.registerSchema('userActivity', SCHEMAS.UserActivity);
 
 export default DDB;
