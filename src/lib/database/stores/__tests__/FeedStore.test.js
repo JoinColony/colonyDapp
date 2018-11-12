68902e9688eea8d98881b717ed2aa6ba206efd77
coyotespike <timothy.roy@protonmail.ch>
2018-11-12 17:34:38 -0600

    Add test for recent events with all() method, and use updated schema

--- src/lib/database/stores/__tests__/FeedStore.test.js
+++ src/lib/database/stores/__tests__/FeedStore.test.js
@@ -53,7 +53,7 @@ describe('FeedStore', () => {
     sandbox.spyOn(store._schema, 'validate');
     const validProps = {
       colonyName: 'Zombies',
-      userAction: { joinedColony: 'joinedColony' },
+      userAction: 'joinedColony',
     };
     const validated = await store.validate(validProps);
     expect(validated).toEqual(validProps);
@@ -73,26 +73,62 @@ describe('FeedStore', () => {
       );
       expect(error.toString()).toMatch('userAction is a required field');
     }
+
+    const wrongUserActionKeyProps = {
+      colonyName: 'bar',
+      userAction: 'noColony',
+    };
+    try {
+      await store.validate(wrongUserActionKeyProps);
+      expect(false).toBe(true); // unreachable
+    } catch (error) {
+      expect(store._schema.validate).toHaveBeenCalledWith(
+        wrongUserActionKeyProps,
+        expect.any(Object),
+      );
+      // Split the error string so if we add more fields we do not need to update the test
+      expect(error.toString().split(':')[1]).toMatch(
+        'userAction must be one of the following values',
+      );
+    }
   });
   test('The all() method returns events in the order added', async () => {
     const store = await ddb.createStore('feed', 'userActivity');
     const firstActivity = {
       colonyName: 'Zombies',
-      userAction: { joinedColony: 'joinedColony' },
+      userAction: 'joinedColony',
     };
     const secondActivity = {
       colonyName: 'Zombies2',
-      userAction: {
-        acceptedTask: 'I will help you bear this burden, Bilbo Baggins',
-      },
+      userAction: 'acceptedTask',
     };
 
     await store.add(firstActivity);
     await store.add(secondActivity);
 
     const events = store.all();
+
     expect(events.length).toBe(2);
     expect(events[0].colonyName).toBe('Zombies');
     expect(events[1].colonyName).toBe('Zombies2');
+  });
+  test('The all() method can limit to most recent events', async () => {
+    const store = await ddb.createStore('feed', 'userActivity');
+    const firstActivity = {
+      colonyName: 'Zombies',
+      userAction: 'joinedColony',
+    };
+    const secondActivity = {
+      colonyName: 'Zombies2',
+      userAction: 'acceptedTask',
+    };
+
+    await store.add(firstActivity);
+    await store.add(secondActivity);
+
+    const recent = store.all({ limit: 1 });
+
+    expect(recent.length).toBe(1);
+    expect(recent[0].colonyName).toBe('Zombies2');
   });
 });
