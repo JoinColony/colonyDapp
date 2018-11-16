2017f3643154219bbc71a34d7d21d15ead997bdc
Raul Glogovetan <raul@glogovetan.com>
2018-11-16 14:13:32 +0200

    Refactor: `UnfinishedProfileDialog` to use `ConfirmDialog`

--- src/modules/users/components/UnfinishedProfileDialog/UnfinishedProfileDialog.jsx
+++ src/modules/users/components/UnfinishedProfileDialog/UnfinishedProfileDialog.jsx
@@ -1,15 +1,24 @@
 /* @flow */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
-import Dialog from '~core/Dialog';
-import DialogSection from '~core/Dialog/DialogSection.jsx';
+import { ConfirmDialog } from '~core/Dialog';
 
 const MSG = defineMessages({
   title: {
     id: 'user.UnfinishedProfileDialog.title',
-    defaultMessage: 'Unfinished Profile',
+    defaultMessage: 'Finish setting up your Colony account',
+  },
+  description: {
+    id: 'user.UnfinishedProfileDialog.description',
+    defaultMessage: `
+      Before working on a task, you'll need to finish setting up your account.
+      As soon as you're done, you can begin earning tokens and reputation in
+      colonies.
+
+      You're almost there: click Continue below to create your unique username.
+    `,
   },
 });
 
@@ -18,16 +27,30 @@ type Props = {
   close: () => void,
 };
 
-const displayName = 'user.UnfinishedProfileDialog';
+class UnfinishedProfileDialog extends Component<Props> {
+  static displayName = 'dashboard.UnfinishedProfileDialog';
 
-const ManagerRatingDialog = ({ cancel }: Props) => (
-  <Dialog cancel={cancel}>
-    <DialogSection>
-      <FormattedMessage {...MSG.title} />
-    </DialogSection>
-  </Dialog>
-);
+  handleContinue = () => {
+    const { close } = this.props;
+    close();
+  };
 
-ManagerRatingDialog.displayName = displayName;
+  render() {
+    const {
+      props: { cancel },
+      handleContinue,
+    } = this;
+    return (
+      <ConfirmDialog
+        cancel={cancel}
+        close={handleContinue}
+        heading={MSG.title}
+        confirmButtonText={{ id: 'button.continue' }}
+      >
+        <FormattedMessage {...MSG.description} />
+      </ConfirmDialog>
+    );
+  }
+}
 
-export default ManagerRatingDialog;
+export default UnfinishedProfileDialog;
