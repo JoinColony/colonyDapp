ecadb73aeaa79e712ed137a2a2bb39777bc7b4e2
James Lefrère <james@colony.io>
2018-11-18 14:43:37 +0100

    Remove `actionType` tx property

--- src/types/TransactionRecord.js
+++ src/types/TransactionRecord.js
@@ -18,7 +18,6 @@ export type TransactionParams = Object;
 export type TransactionEventData = Object;
 
 export type TransactionProps<P: TransactionParams, E: TransactionEventData> = {
-  actionType?: string,
   contextName: string,
   createdAt: Date,
   errors: List<TransactionError>,
