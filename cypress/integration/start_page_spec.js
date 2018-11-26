cdb1de80f4c9afcae6448dbc40ea9beff619801e
coyotespike <timothy.roy@protonmail.ch>
2018-11-26 13:40:34 -0600

    Add basic Cypress config and first test for /connect page

---
+++ cypress/integration/start_page_spec.js
@@ -0,0 +1,11 @@
+describe('The Home Page', function() {
+  it('successfully loads, and redirects to /connect', function() {
+    cy.visit('/')
+    cy.url().should('include', '/connect')
+  })
+  it('contains a button to create a wallet, which changes the page', function() {
+    cy.get('[data-test="createWalletLink"]').click()
+    cy.url().should('include', '/create-wallet')
+  })
+
+})
