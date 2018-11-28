63b6cd6f6af3943f991ff0e56d208c9ca6dbda9c
Raul Glogovetan <raul@glogovetan.com>
2018-11-29 01:11:25 +0200

    Add: `GasStation` switch between expanded and summary

--- src/modules/dashboard/components/GasStation/GasStation.jsx
+++ src/modules/dashboard/components/GasStation/GasStation.jsx
@@ -1,6 +1,7 @@
 /* @flow */
-import React from 'react';
-import { defineMessages } from 'react-intl';
+
+import React, { Fragment, Component } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
 
 import type { InProps } from './GasStation';
 import type { TransactionType } from '~types/transaction';
@@ -28,9 +29,11 @@ const MSG = defineMessages({
     id: 'dashboard.GasStation.goToWalletLinkTitle',
     defaultMessage: 'Go to Wallet',
   },
+  returnToSummary: {
+    id: 'dashboard.GasStation.returnToSummary',
+    defaultMessage: 'See all pending actions',
+  },
 });
-
-const displayName = 'dashboard.GasStation';
 
 type Props = InProps & {
   balance: number,
@@ -38,76 +41,137 @@ type Props = InProps & {
   walletAddress: string,
 };
 
-const GasStation = ({ balance, close, transactions, walletAddress }: Props) => (
-  <div
-    className={getMainClasses({}, styles, {
-      isEmpty: transactions.length === 0,
-    })}
-  >
-    <div className={styles.gasStationHeader}>
-      <div className={styles.walletDetails}>
-        <div className={styles.walletHeading}>
-          <Heading
-            appearance={{ margin: 'none', size: 'normal' }}
-            text={{ id: 'wallet' }}
-          />
-        </div>
-        <div className={styles.walletAddress}>
-          <CopyableAddress>{walletAddress}</CopyableAddress>
-        </div>
-        <div>
-          <Numeral value={balance} suffix="ETH" />
-        </div>
-      </div>
-      <div className={styles.actionsContainer}>
-        <Link to={WALLET_ROUTE}>
-          <div className={styles.goToWalletIcon}>
-            <Icon
-              appearance={{ size: 'medium' }}
-              name="arrow-wallet"
-              title={MSG.goToWalletLinkTitle}
-            />
-          </div>
-        </Link>
-        <button className={styles.closeButton} onClick={close} type="button">
-          <Icon
-            appearance={{ size: 'normal' }}
-            name="close"
-            title={{ id: 'button.close' }}
-          />
-        </button>
-      </div>
-    </div>
-    <div className={styles.transactionsContainer}>
-      {transactions && transactions.length > 0 ? (
-        <CardList appearance={{ numCols: '1' }}>
-          {transactions.map((transaction: TransactionType) => (
-            <GasStationCard
-              /*
-               * @NOTE Nonces are unique, but our mock data might add duplicates.
-               * In case you see duplicate key errors in the console, don't panic.
-               */
-              key={transaction.nonce}
-              transaction={transaction}
-              onClick={
-                transaction.set && transaction.set.length
-                  ? event => console.log(event)
-                  : undefined
-              }
-              expanded
-            />
-          ))}
-        </CardList>
-      ) : (
-        <Heading
-          appearance={{ margin: 'none', size: 'normal' }}
-          text={MSG.transactionsEmptyStateText}
-        />
-      )}
-    </div>
-  </div>
-);
+type State = {
+  expandedTransactionId: number,
+};
 
-GasStation.displayName = displayName;
+class GasStation extends Component<Props, State> {
+  static displayName = 'dashboard.GasStation';
+
+  state = {
+    expandedTransactionId: -1,
+  };
+
+  clearExpandedTrasaction() {
+    return this.setState({ expandedTransactionId: -1 });
+  }
+
+  handleExpandTransaction(transactionIndex: number) {
+    return this.setState({ expandedTransactionId: transactionIndex });
+  }
+
+  renderTransactionsSummary(
+    transactions: Array<TransactionType>,
+  ): Array<React$Element<*>> {
+    return transactions.map(
+      (transaction: TransactionType, transactionIndex: number) => (
+        <GasStationCard
+          /*
+           * @NOTE Nonces are unique, but our mock data might add duplicates.
+           * In case you see duplicate key errors in the console, don't panic.
+           */
+          key={transaction.nonce}
+          transaction={transaction}
+          onClick={() => this.handleExpandTransaction(transactionIndex)}
+        />
+      ),
+    );
+  }
+
+  renderExpandedTransaction(transaction: TransactionType) {
+    return (
+      <Fragment>
+        <button
+          type="button"
+          className={styles.returnToSummary}
+          onClick={() => this.clearExpandedTrasaction()}
+        >
+          <Icon
+            appearance={{ size: 'small' }}
+            name="caret-left"
+            title={MSG.returnToSummary}
+          />
+          <FormattedMessage {...MSG.returnToSummary} />
+        </button>
+        <GasStationCard
+          /*
+           * @NOTE Nonces are unique, but our mock data might add duplicates.
+           * In case you see duplicate key errors in the console, don't panic.
+           */
+          key={transaction.nonce}
+          transaction={transaction}
+          expanded
+        />
+      </Fragment>
+    );
+  }
+
+  render() {
+    const { balance, close, transactions, walletAddress } = this.props;
+    const { expandedTransactionId } = this.state;
+    return (
+      <div
+        className={getMainClasses({}, styles, {
+          isEmpty: transactions.length === 0,
+        })}
+      >
+        <div className={styles.gasStationHeader}>
+          <div className={styles.walletDetails}>
+            <div className={styles.walletHeading}>
+              <Heading
+                appearance={{ margin: 'none', size: 'normal' }}
+                text={{ id: 'wallet' }}
+              />
+            </div>
+            <div className={styles.walletAddress}>
+              <CopyableAddress>{walletAddress}</CopyableAddress>
+            </div>
+            <div>
+              <Numeral value={balance} suffix="ETH" />
+            </div>
+          </div>
+          <div className={styles.actionsContainer}>
+            <Link to={WALLET_ROUTE}>
+              <div className={styles.goToWalletIcon}>
+                <Icon
+                  appearance={{ size: 'medium' }}
+                  name="arrow-wallet"
+                  title={MSG.goToWalletLinkTitle}
+                />
+              </div>
+            </Link>
+            <button
+              className={styles.closeButton}
+              onClick={close}
+              type="button"
+            >
+              <Icon
+                appearance={{ size: 'normal' }}
+                name="close"
+                title={{ id: 'button.close' }}
+              />
+            </button>
+          </div>
+        </div>
+        <div className={styles.transactionsContainer}>
+          {transactions && transactions.length > 0 ? (
+            <CardList appearance={{ numCols: '1' }}>
+              {expandedTransactionId >= 0
+                ? this.renderExpandedTransaction(
+                    transactions[expandedTransactionId],
+                  )
+                : this.renderTransactionsSummary(transactions)}
+            </CardList>
+          ) : (
+            <Heading
+              appearance={{ margin: 'none', size: 'normal' }}
+              text={MSG.transactionsEmptyStateText}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default GasStation;
