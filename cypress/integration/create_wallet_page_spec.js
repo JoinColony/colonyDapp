a08ebb4596e7f76a4b9d3f8381b0b1edb4a5c74c
coyotespike <timothy.roy@protonmail.ch>
2018-11-28 14:35:14 -0600

    Refactor cypress wallet page test into separate test blocks

--- cypress/integration/create_wallet_page_spec.js
+++ cypress/integration/create_wallet_page_spec.js
@@ -1,15 +1,26 @@
 describe('The create wallet page', function() {
-  it('contains a mnemonic phrase', function() {
+  before(function(){
     cy.visit('/create-wallet')
     cy.get('[data-test="mnemonicPhrase"]').invoke('text').then(text => {
-    const phraseArray = text.split(' ')
-      expect(phraseArray.length).to.eq(12)
-      cy.get('[data-test="confirmStoredPhraseButton"]').click()
-      cy.get('[data-test="confirmCreatedPhraseBackupButton"]').click()
-      cy.get("#proofWord1").type(phraseArray[0])
-      cy.get("#proofWord2").type(phraseArray[4])
-      cy.get("#proofWord3").type(phraseArray[11])
-      cy.get('[data-test="proveBackupPhraseButton"]').click()
+      const phraseArray = text.split(' ')
+      beforeEach(function() {
+        cy.wrap(phraseArray).as('phraseArray')
+      })
     })
+  })
+  it('contains a mnemonic phrase', function() {
+    expect(this.phraseArray.length).to.eq(12)
+  })
+   it('contains a button to confirm you stored the phrase', function() {
+     cy.get('[data-test="confirmStoredPhraseButton"]').click()
+   })
+  it('then contains a button to confirm you backed up the phrase', function() {
+     cy.get('[data-test="confirmCreatedPhraseBackupButton"]').click()
+   })
+  it('then asks you to prove the phrase', function() {
+    cy.get("#proofWord1").type(this.phraseArray[0])
+    cy.get("#proofWord2").type(this.phraseArray[4])
+    cy.get("#proofWord3").type(this.phraseArray[11])
+    cy.get('[data-test="proveBackupPhraseButton"]').click()
   })
 })
