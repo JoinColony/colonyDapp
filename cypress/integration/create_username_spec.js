deee3a45d5808c31c0e69d97668e191f57acb918
coyotespike <timothy.roy@protonmail.ch>
2018-11-29 15:18:38 -0600

    Add Cypress test for colony creation wizard

--- cypress/integration/create_username_spec.js
+++ cypress/integration/create_username_spec.js
@@ -9,13 +9,26 @@ describe('Can create a username in dev mode', () => {
     cy.url({ timeout: 60000 }).should('include', '/dashboard');
   });
 
-  it('The dashboard contains an avatar dropdown where you can enter a username', () => {
+  it('The dashboard contains an avatar dropdown', () => {
     cy.get('[data-test="avatarDropdown"]').click();
   });
 
-  it('The avatar dropdown lets you get started and enter a username', () => {
+  it('The avatar dropdown and username wizard let you register a username', () => {
     cy.contains('Get started').click();
     cy.get('[name="username"]').type('littlePiggie');
     cy.contains('Confirm').click();
+  });
+  it('The avatar dropdown and colony creation wizard create a colony', () => {
+    cy.get('[data-test="avatarDropdown"]').click();
+    cy.get('[href="/create-colony"]').click();
+    cy.get('[name="colonyName"]').type('littlePiggieHouse');
+    cy.contains('Next').click();
+    cy.contains('Create a new token').click();
+    cy.get('[name="tokenName"]').type('PigsAreNotBacon');
+    cy.get('[name="tokenSymbol"]').type('PIG');
+    cy.contains('Create Token').click();
+    cy.contains('Create Colony').click();
+    cy.get('[name="ensName"]').type('Piglet');
+    cy.contains('Done').click();
   });
 });
