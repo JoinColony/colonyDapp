03ae5be1b284ab28b08d1fed0f1e5f9c02f6fdea
karolinkas <li-nee@gmx.de>
2018-11-30 16:46:25 +0100

    Wrap styleguide in a fake redux store

--- src/styleguide/Wrapper.jsx
+++ src/styleguide/Wrapper.jsx
@@ -3,6 +3,8 @@
 import type { Node } from 'react';
 
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { addLocaleData, IntlProvider } from 'react-intl';
 import { BrowserRouter } from 'react-router-dom';
 import en from 'react-intl/locale-data/en';
@@ -17,11 +19,34 @@ type Props = {
   children: Node,
 };
 
+const initialState = {
+  users: {
+    currentUser: null,
+    wallet: {
+      availableAddresses: [],
+      isLoading: false,
+    },
+    allUsers: {
+      isLoading: false,
+      users: {},
+      avatars: {},
+    },
+  },
+};
+
+const configureStore = () => {
+  const reducer = (state = initialState) => state;
+  return createStore(reducer);
+};
+
+const store = configureStore();
 // We're injecting ReactIntl into all of our components, even though it might not be needed everywhere
 const Wrapper = ({ children }: Props) => (
-  <IntlProvider locale="en" defaultLocale="en" messages={messages}>
-    <BrowserRouter>{children}</BrowserRouter>
-  </IntlProvider>
+  <Provider store={store}>
+    <IntlProvider locale="en" defaultLocale="en" messages={messages}>
+      <BrowserRouter>{children}</BrowserRouter>
+    </IntlProvider>
+  </Provider>
 );
 
 export default Wrapper;
