f0a760caf02bc275098e5d0efed41f3abd85c2a9
Raul Glogovetan <raul@glogovetan.com>
2018-12-05 17:50:53 +0200

    Add: `withFeatureFlags` dependent selector and state

--- src/utils/hoc/index.js
+++ src/utils/hoc/index.js
@@ -4,11 +4,19 @@ import type { ComponentType, Node } from 'react';
 
 import { createElement } from 'react';
 
+import store from '../../createReduxStore';
+
 type ConsumerType<T> = ComponentType<{
   children: (value: T) => ?Node,
 }>;
 
 type Selector<T> = (reduxState: Object, props: Object) => T;
+
+type DependantSelector = (
+  selector: Selector<any> | any,
+  reduxState: Object,
+  props: Object,
+) => boolean;
 
 // eslint-disable-next-line import/prefer-default-export
 export const withConsumerFactory = (Consumer: ConsumerType<*>) => () => (
@@ -24,6 +32,19 @@ export const withConsumerFactory = (Consumer: ConsumerType<*>) => () => (
 export const withFeatureFlags = (
   Component: ComponentType<{ [string]: any }>,
 ) => (props: Object) => {
-  const given = (cond: Selector<boolean>) => cond;
+  const given = (
+    potentialSelector: Selector<any> | any,
+    dependantSelector: DependantSelector,
+  ) => {
+    /*
+     * @NOTE I have mixed feelings about getting the state like this
+     */
+    const reduxState = store.getState();
+    const potentialSelectorValue = potentialSelector(reduxState, props);
+    if (dependantSelector && typeof dependantSelector === 'function') {
+      return dependantSelector(potentialSelectorValue, reduxState, props);
+    }
+    return potentialSelectorValue;
+  };
   return createElement(Component, { ...props, given });
 };
