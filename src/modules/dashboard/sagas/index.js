fd4803a8ec1e9b9a6b5c3ff302ff8b2e15736075
coyotespike <timothy.roy@protonmail.ch>
2018-12-05 11:16:16 -0600

    Add getRootDomain saga, include domainSagas with other sagas

--- src/modules/dashboard/sagas/index.js
+++ src/modules/dashboard/sagas/index.js
@@ -3,9 +3,10 @@
 import { all } from 'redux-saga/effects';
 
 import colonySagas from './colony';
+import domainSagas from './domains';
 import tokenSagas from './token';
 import taskSagas from './task';
 
 export default function* dashboardSagas(): any {
-  yield all([colonySagas(), taskSagas(), tokenSagas()]);
+  yield all([colonySagas(), domainSagas(), taskSagas(), tokenSagas()]);
 }
