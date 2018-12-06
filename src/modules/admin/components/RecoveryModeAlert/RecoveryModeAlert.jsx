ebb73c875af442f861cacba00a510825ad2a8fc2
Raul Glogovetan <raul@glogovetan.com>
2018-12-06 19:38:43 +0200

    Add: admin `RecoveryModeAlert` bar component

---
+++ src/modules/admin/components/RecoveryModeAlert/RecoveryModeAlert.jsx
@@ -0,0 +1,25 @@
+/* @flow */
+
+import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import styles from './RecoveryModeAlert.css';
+
+const MSG = defineMessages({
+  message: {
+    id: 'admin.RecoveryModeAlert.message',
+    defaultMessage: 'This colony is in recovery mode!',
+  },
+});
+
+const displayName = 'admin.RecoveryModeAlert';
+
+const RecoveryModeAlert = () => (
+  <div className={styles.main}>
+    <FormattedMessage {...MSG.message} />
+  </div>
+);
+
+RecoveryModeAlert.displayName = displayName;
+
+export default RecoveryModeAlert;
