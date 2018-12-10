e35e71d971c0fbf0b05509788b9f6ba4a3e0c0ca
coyotespike <timothy.roy@protonmail.ch>
2018-12-10 15:02:45 -0600

    Add timeouts to several tests

--- cypress/integration/create_username_and_colony_spec.js
+++ cypress/integration/create_username_and_colony_spec.js
@@ -5,28 +5,28 @@ describe('Can create a username and colony in dev mode', () => {
   });
 
   it('TrufflePig button shows a go to colony button which loads dashboard', () => {
-    cy.contains('Go to Colony').click();
+    cy.contains('Go to Colony', { timeout: 20000 }).click();
     cy.url({ timeout: 60000 }).should('include', '/dashboard');
   });
 
   it('The dashboard contains an avatar dropdown', () => {
-    cy.get('[data-test="avatarDropdown"]').click();
+    cy.get('[data-test="avatarDropdown"]', { timeout: 20000 }).click();
   });
 
   it('The avatar dropdown and username wizard let you register a username', () => {
-    cy.contains('Get started').click();
+    cy.contains('Get started', { timeout: 20000 }).click();
     cy.get('[name="username"]').type('littlePiggie');
     cy.contains('Confirm').click();
   });
   it('The avatar dropdown and colony creation wizard create a colony', () => {
-    cy.get('[data-test="avatarDropdown"]').click();
+    cy.get('[data-test="avatarDropdown"]', { timeout: 20000 }).click();
     cy.get('[href="/create-colony"]').click();
     cy.get('[name="colonyName"]').type('littlePiggieHouse');
     cy.contains('Next').click();
     cy.contains('Create a new token').click();
     cy.get('[name="tokenName"]').type('PigsAreNotBacon');
     cy.get('[name="tokenSymbol"]').type('PIG');
-    cy.contains('Create Token', { timeout: 200000 }).click();
+    cy.contains('Create Token', { timeout: 20000 }).click();
     cy.contains('Create Colony', { timeout: 20000 }).click();
     cy.get('[name="ensName"]', { timeout: 20000 }).type('Piglet');
     cy.contains('Done').click();
