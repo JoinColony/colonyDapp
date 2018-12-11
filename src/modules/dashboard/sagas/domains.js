18eb0cc13ebce12536fbce59392e171ad379eb6c
coyotespike <timothy.roy@protonmail.ch>
2018-12-10 18:18:32 -0600

    Refactors getRootDomain into getOrFetchDomain saga

--- src/modules/dashboard/sagas/domains.js
+++ src/modules/dashboard/sagas/domains.js
@@ -1,33 +1,30 @@
 /* @flow */
 
 import type { Saga } from 'redux-saga';
-
-import { put, takeEvery } from 'redux-saga/effects';
+import { call, getContext, takeEvery } from 'redux-saga/effects';
 
 import type { Action } from '~types';
+import { DDB } from '../../../lib/database';
+import { KVStore } from '../../../lib/database/stores';
 
-import { callCaller, putError } from '~utils/saga/effects';
+import { domainStore } from '../stores';
 
-import {
-  ROOT_DOMAIN_FETCH,
-  ROOT_DOMAIN_FETCH_ERROR,
-  ROOT_DOMAIN_FETCH_SUCCESS,
-} from '../actionTypes';
+import { DOMAIN_FETCH } from '../actionTypes';
 
-function* getRootDomain(action: Action): Saga<void> {
-  const { colonyIdentifier } = action.payload;
-  try {
-    const result = yield callCaller({
-      methodName: 'getDomain',
-      colonyIdentifier,
-      params: { domainId: 1 },
-    });
-    yield put({ type: ROOT_DOMAIN_FETCH_SUCCESS, payload: result });
-  } catch (error) {
-    yield putError(ROOT_DOMAIN_FETCH_ERROR, error);
+function* fetchOrCreateDomainDDB(action: Action): Saga<KVStore> {
+  const ddb: DDB = yield getContext('ddb');
+  const { domainIdentifier } = action.payload;
+  let store;
+
+  if (domainIdentifier) {
+    store = yield call([ddb, ddb.getStore], domainStore, domainIdentifier);
+    yield call([store, store.load]);
+  } else {
+    store = yield call([ddb, ddb.createStore], domainStore);
   }
+  return store;
 }
 
 export default function* domainSagas(): any {
-  yield takeEvery(ROOT_DOMAIN_FETCH, getRootDomain);
+  yield takeEvery(DOMAIN_FETCH, fetchOrCreateDomainDDB);
 }
