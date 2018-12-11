cd04c19d33890da352c7f855a306e15f0ad06a6e
sprusr <scott@spru.sr>
2018-12-11 16:53:59 +0000

    Add saga for fetching Colony transactions from events

---
+++ src/modules/admin/actionTypes/index.js
@@ -0,0 +1,8 @@
+/* @flow */
+/* eslint-disable max-len */
+
+import ns from '../namespace';
+
+export const COLONY_FETCH_TRANSACTIONS = `${ns}/COLONY_FETCH_TRANSACTIONS`;
+export const COLONY_FETCH_TRANSACTIONS_SUCCESS = `${ns}/COLONY_FETCH_TRANSACTIONS_SUCCESS`;
+export const COLONY_FETCH_TRANSACTIONS_ERROR = `${ns}/COLONY_FETCH_TRANSACTIONS_ERROR`;
