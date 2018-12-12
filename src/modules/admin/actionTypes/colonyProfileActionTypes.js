76d0a24e2ecf40cbdf5db542c8540c8648586028
Raul Glogovetan <raul@glogovetan.com>
2018-12-12 21:15:17 +0200

    Add: admin Colony Profile action types

---
+++ src/modules/admin/actionTypes/colonyProfileActionTypes.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import ns from '../namespace';
+
+/*
+ * Colony Action Types
+ */
+
+ export const COLONY_PROFILE_UPDATE = `${ns}/COLONY_PROFILE_UPDATE`;
+ export const COLONY_PROFILE_UPDATE_ERROR = `${ns}/COLONY_PROFILE_UPDATE_ERROR`;
+ export const COLONY_PROFILE_UPDATE_SUCCESS = `${ns}/COLONY_PROFILE_UPDATE_SUCCESS`;
