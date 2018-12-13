ca743982573d89e85c94624dc037298d951c09c1
coyotespike <timothy.roy@protonmail.ch>
2018-12-13 13:38:43 -0600

    Add domains reducer

--- src/modules/dashboard/reducers/index.js
+++ src/modules/dashboard/reducers/index.js
@@ -3,9 +3,11 @@
 import { combineReducers } from 'redux';
 
 import allColonies from './allColonies';
+import domainsReducer from './domains';
 
 const dashboardReducer = combineReducers({
   allColonies,
+  domains: domainsReducer,
 });
 
 export default dashboardReducer;
