df1dfc1a0d658902a32fd5a2641c812a3310c300
coyotespike <timothy.roy@protonmail.ch>
2018-12-13 13:42:37 -0600

    Add tasksDatabase field to immutable Domain

--- src/immutable/Domain.js
+++ src/immutable/Domain.js
@@ -7,6 +7,7 @@ import { Record } from 'immutable';
 export type DomainProps = {
   id: number,
   name: string,
+  tasksDatabase: string,
 };
 
 export type DomainRecord = RecordOf<DomainProps>;
@@ -14,6 +15,7 @@ export type DomainRecord = RecordOf<DomainProps>;
 const defaultValues: $Shape<DomainProps> = {
   id: undefined,
   name: undefined,
+  tasksDatabase: undefined,
 };
 
 const Domain: RecordFactory<DomainProps> = Record(defaultValues);
