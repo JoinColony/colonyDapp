1ffeaa5ac3bd6f3432d5b7819d9b62354acba25c
coyotespike <timothy.roy@protonmail.ch>
2018-12-14 11:41:59 -0600

    Add load() method to FeedStore

--- src/lib/database/stores/FeedStore.js
+++ src/lib/database/stores/FeedStore.js
@@ -39,5 +39,24 @@ class FeedStore extends Store {
   remove(key: string) {
     return this._orbitStore.remove(key);
   }
+
+  async load() {
+    const readyPromise = new Promise(resolve => {
+      this._orbitStore.events.once('ready', resolve);
+    });
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(
+          new Error(
+            `Timeout while waiting on replication for store "${this._name}"`,
+          ),
+        );
+      }, 15 * 1000); // 15 seconds
+    });
+
+    await super.load();
+
+    return Promise.race([readyPromise, timeoutPromise]);
+  }
 }
 export default FeedStore;
