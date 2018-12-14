da013778b24160fa13e39391dfc29ba62f94a4c1
coyotespike <timothy.roy@protonmail.ch>
2018-12-14 11:39:21 -0600

    Add more actionTypes to draft.js

---
+++ src/modules/dashboard/actionTypes/draft.js
@@ -0,0 +1,19 @@
+/* @flow */
+
+import ns from '../namespace';
+
+export const DRAFT_CREATE = `${ns}/DRAFT_CREATE`;
+export const DRAFT_CREATE_ERROR = `${ns}/DRAFT_CREATE_ERROR`;
+export const DRAFT_CREATE_SUCCESS = `${ns}/DRAFT_CREATE_SUCCESS`;
+
+export const DRAFT_DELETE = `${ns}/DRAFT_DELETE`;
+export const DRAFT_DELETE_ERROR = `${ns}/DRAFT_DELETE_ERROR`;
+export const DRAFT_DELETE_SUCCESS = `${ns}/DRAFT_DELETE_SUCCESS`;
+
+export const DRAFT_EDIT = `${ns}/DRAFT_EDIT`;
+export const DRAFT_EDIT_ERROR = `${ns}/DRAFT_EDIT_ERROR`;
+export const DRAFT_EDIT_SUCCESS = `${ns}/DRAFT_EDIT_SUCCESS`;
+
+export const DRAFTS_FETCH = `${ns}/DRAFTS_FETCH`;
+export const DRAFTS_FETCH_SUCCESS = `${ns}/DRAFTS_FETCH_SUCCESS`;
+export const DRAFTS_FETCH_ERROR = `${ns}/DRAFTS_FETCH_ERROR`;
