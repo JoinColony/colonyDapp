106d09184f97ea2fbad6f887dc02c0fb0b48fffb
Thiago Delgado <thiagodelgado111@gmail.com>
2018-12-17 17:39:19 -0200

    Create user inbox store blueprint

--- src/modules/users/stores/index.js
+++ src/modules/users/stores/index.js
@@ -1,4 +1,5 @@
 /* @flow */
 
 export { default as userProfileStore } from './userProfileStore';
+export { default as userInboxStore } from './userInboxStore';
 export { default as userActivitiesStore } from './userActivitiesStore';
