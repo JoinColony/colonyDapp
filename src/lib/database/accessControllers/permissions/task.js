0933a34808493a6ea5f00a644347773839712d7e
Thiago Delgado <thiagodelgado111@gmail.com>
2018-12-18 17:09:40 -0200

    Fix task permission manifest

--- src/lib/database/accessControllers/permissions/task.js
+++ src/lib/database/accessControllers/permissions/task.js
@@ -4,22 +4,24 @@ import type { PermissionsManifest } from './types';
 
 export default function loadModule(): PermissionsManifest {
   return {
+    'cancel-task': { inherits: 'is-task-manager-or-worker' },
     'create-task': { inherits: 'is-colony-founder-or-admin' },
-    'cancel-task': { inherits: 'is-task-manager-or-evaluator' },
-    'set-task-skill': { inherits: 'is-task-manager-or-evaluator' },
-    'set-task-domain': { inherits: 'is-task-manager-or-evaluator' },
-    'set-task-due-date': { inherits: 'is-task-manager-or-evaluator' },
-    'set-task-worker': { inherits: 'is-task-manager-or-evaluator' },
-    'change-task-title-or-brief': { inherits: 'is-task-manager-or-evaluator' },
-    'send-task-work-request': async () => true,
     'send-task-work-invite': { inherits: 'is-task-manager-or-evaluator' },
-    'close-task': { inherits: 'is-task-manager-or-evaluator' },
-    'set-task-manager-payout': { inherits: 'is-task-manager' },
+    'send-task-work-request': async () => true,
+    'set-task-brief': { inherits: 'is-task-manager-or-worker' },
+    'set-task-domain': { inherits: 'is-task-manager-or-worker' },
+    'set-task-due-date': { inherits: 'is-task-manager-or-worker' },
+    'set-task-skill': { inherits: 'is-task-manager-or-worker' },
     'set-task-evaluator-payout': { inherits: 'is-task-manager-or-evaluator' },
+    'set-task-manager-payout': { inherits: 'is-task-manager' },
     'set-task-worker-payout': { inherits: 'is-task-manager-or-worker' },
+    // @TODO: We might want make this validate signatures
+    'set-task-worker-role': { inherits: 'is-task-manager' },
+    'submit-task-deliverable': { inherits: 'is-task-worker' },
+    'claim-task-payout': { inherits: 'is-assigned-to-task' },
+    'submit-task-work-rating': { inherits: 'is-assigned-to-task' },
+    'reveal-task-work-rating': { inherits: 'is-assigned-to-task' },
     'remove-task-evaluator-role': { inherits: 'is-task-manager-or-evaluator' },
     'remove-task-worker-role': { inherits: 'is-task-manager-or-worker' },
-    // @TODO: We might want make this validate signatures
-    'set-task-worker-role': { inherits: 'is-task-manager-or-evaluator' },
   };
 }
