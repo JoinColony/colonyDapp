105e0e923505c94a2968aa53dc7061ac74fa7517
coyotespike <timothy.roy@protonmail.ch>
2018-12-18 11:28:44 -0600

    Start draft sagas

--- src/modules/dashboard/sagas/index.js
+++ src/modules/dashboard/sagas/index.js
@@ -6,7 +6,14 @@ import colonySagas from './colony';
 import domainSagas from './domain';
 import tokenSagas from './token';
 import taskSagas from './task';
+import draftSagas from './draft';
 
 export default function* dashboardSagas(): any {
-  yield all([colonySagas(), domainSagas(), taskSagas(), tokenSagas()]);
+  yield all([
+    colonySagas(),
+    domainSagas(),
+    draftSagas(),
+    taskSagas(),
+    tokenSagas(),
+  ]);
 }
