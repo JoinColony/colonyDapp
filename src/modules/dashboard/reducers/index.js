be8ef86bcbcef8fac642adc4520d5da36a376987
coyotespike <timothy.roy@protonmail.ch>
2018-12-18 11:57:44 -0600

    Begin tasks reducer

--- src/modules/dashboard/reducers/index.js
+++ src/modules/dashboard/reducers/index.js
@@ -4,10 +4,12 @@ import { combineReducers } from 'redux';
 
 import coloniesReducer from './colonies';
 import domainsReducer from './domains';
+import tasksReducer from './tasks';
 
 const coreReducer = combineReducers({
   colonies: coloniesReducer,
   domains: domainsReducer,
+  tasks: tasksReducer,
 });
 
 export default coreReducer;
