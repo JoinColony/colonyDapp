a731b68315062752877ae52082a9ad2adec52c12
karolinkas <li-nee@gmx.de>
2018-12-18 08:59:30 +0100

    Use lifecycle method in task.js
    - connect to redux store to get task data

--- src/modules/dashboard/components/Task/Task.js
+++ src/modules/dashboard/components/Task/Task.js
@@ -1,16 +1,28 @@
 /* @flow */
 
-import { compose, withProps } from 'recompose';
+import { compose, withProps, lifecycle } from 'recompose';
+import { connect } from 'react-redux';
 
 import withDialog from '~core/Dialog/withDialog';
 
 import Task from './Task.jsx';
+
+import promiseListener from '../../../../createPromiseListener';
 
 import userMock from '~users/AvatarDropdown/__datamocks__/mockUser';
 import { mockTask } from './__datamocks__/mockTask';
 
 const enhance = compose(
   withDialog(),
+  lifecycle({
+    componentDidMount() {
+      this.asyncFunc = promiseListener.createAsyncFunction({
+        start: 'TASK_FETCH_DETAILS',
+        resolve: 'TASK_FETCH_DETAILS_SUCCESS',
+        reject: 'TASK_FETCH_DETAILS_ERROR',
+      });
+    },
+  }),
   withProps(() => {
     const task = mockTask;
     const user = userMock;
@@ -22,9 +34,17 @@ const enhance = compose(
       user,
       isTaskCreator,
       preventEdit:
-        !!task && !task.currentState === 'finalized' && isTaskCreator,
+        !!task &&
+        !task.currentState === 'TASK_STATE.FINALIZED' &&
+        isTaskCreator,
     };
   }),
+  connect(
+    state => ({
+      task: state.task,
+    }),
+    {},
+  ),
 );
 
 export default enhance(Task);
