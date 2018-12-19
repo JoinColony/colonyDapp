a48aa2c561754a99cc02ed572666dec331cb0845
coyotespike <timothy.roy@protonmail.ch>
2018-12-19 12:51:12 -0600

    Move tasks reducer cases inside domains, use Domains type for state

--- src/modules/dashboard/reducers/domains.js
+++ src/modules/dashboard/reducers/domains.js
@@ -2,12 +2,19 @@
 
 import { Map as ImmutableMap } from 'immutable';
 
-import { Domain } from '~immutable';
+import { Domain, Domains, Draft, Task } from '~immutable';
 
 import type { Action } from '~types';
-import { DOMAIN_FETCH_SUCCESS } from '../actionTypes';
 
-type State = ImmutableMap<string, Domain>;
+import {
+  DOMAIN_FETCH_SUCCESS,
+  DRAFT_CREATE_SUCCESS,
+  DRAFT_FETCH_SUCCESS,
+  TASK_CREATE_SUCCESS,
+  TASK_FETCH_SUCCESS,
+} from '../actionTypes';
+
+type State = ImmutableMap<string, Domains>;
 
 const INITIAL_STATE: State = new ImmutableMap();
 
@@ -27,6 +34,26 @@ const domainsReducer = (state: State = INITIAL_STATE, action: Action) => {
           id,
         }),
       );
+    }
+    case DRAFT_FETCH_SUCCESS:
+    case DRAFT_CREATE_SUCCESS: {
+      const {
+        ensName,
+        domainId,
+        draft: { id },
+        draft,
+      } = action.payload;
+      return state.setIn([ensName, domainId, 'tasks', id], Draft(draft));
+    }
+    case TASK_FETCH_SUCCESS:
+    case TASK_CREATE_SUCCESS: {
+      const {
+        ensName,
+        domainId,
+        task: { id },
+        task,
+      } = action.payload;
+      return state.setIn([ensName, domainId, 'tasks', id], Task(task));
     }
     default:
       return state;
--- src/modules/dashboard/reducers/index.js
+++ src/modules/dashboard/reducers/index.js
@@ -4,12 +4,10 @@ import { combineReducers } from 'redux';
 
 import allColonies from './allColonies';
 import domainsReducer from './domains';
-import tasksReducer from './tasks';
 
 const dashboardReducer = combineReducers({
   allColonies,
   domains: domainsReducer,
-  tasks: tasksReducer,
 });
 
 export default dashboardReducer;
