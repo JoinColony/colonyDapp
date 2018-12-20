19ee3c7e77e2265f2eedf3659c940f05f6de220f
Raul Glogovetan <raul@glogovetan.com>
2018-12-20 18:24:58 +0200

    Add: `admin` get colony admins selector

--- src/modules/admin/selectors/colonies.js
+++ src/modules/admin/selectors/colonies.js
@@ -5,7 +5,7 @@ import type { Map as ImmutableMap } from 'immutable';
 import { createSelector } from 'reselect';
 
 import type { ENSName } from '~types';
-import type { ColonyRecord } from '~immutable';
+import type { ColonyRecord, ColonyAdminRecord } from '~immutable';
 
 import { allColonies } from '../../dashboard/selectors';
 
@@ -17,8 +17,29 @@ type RootState = {
 
 type CurrentColonySelector = (state: RootState, props: Object) => ColonyRecord;
 
+type ColonyAdminStpreSelector = (
+  state: RootState,
+  props: Object,
+) => Map<string, ColonyAdminRecord>;
+
+type ColonyAdminsSelector = (
+  state: RootState,
+  props: Object,
+) => Array<ColonyAdminRecord>;
+
 export const getCurrentColony: CurrentColonySelector = createSelector(
   allColonies,
-  props => props.colony.ensName,
-  (colonies, ensName) => colonies.get(ensName),
+  (state, props) => (props.colony ? props.colony.ensName : undefined),
+  (colonies, ensName) => colonies.get(ensName, {}),
+);
+
+export const getColonyAdminStore: ColonyAdminStpreSelector = createSelector(
+  getCurrentColony,
+  currentColony => currentColony.get('admins', {}),
+);
+
+export const getColonyAdmins: ColonyAdminsSelector = createSelector(
+  getColonyAdminStore,
+  colonyAdmins =>
+    Object.keys(colonyAdmins).map(username => colonyAdmins[username]),
 );
