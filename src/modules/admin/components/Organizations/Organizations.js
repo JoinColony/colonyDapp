39dcf91685c9f96ea80f116bb3c848549f641a37
Raul Glogovetan <raul@glogovetan.com>
2018-12-20 18:25:56 +0200

    Add: admin `Organizations` display store admins

--- src/modules/admin/components/Organizations/Organizations.js
+++ src/modules/admin/components/Organizations/Organizations.js
@@ -1,9 +1,16 @@
 /* @flow */
 
 import { compose } from 'recompose';
+import { connect } from 'react-redux';
+
+import { getColonyAdmins } from '../../selectors';
 
 import Organizations from './Organizations.jsx';
 
-const enhance = compose();
+const enhance = compose(
+  connect((state, props) => ({
+    colonyAdmins: getColonyAdmins(state, props),
+  })),
+);
 
 export default enhance(Organizations);
