88207f5f87b29e610de28bb5523eb7fed8171932
coyotespike <timothy.roy@protonmail.ch>
2018-12-20 10:49:58 -0600

    Add delete method to KVStore and type, and test for it

--- src/lib/database/types/OrbitDBKVStore.js
+++ src/lib/database/types/OrbitDBKVStore.js
@@ -5,6 +5,7 @@ import type { OrbitDBStore } from './OrbitDBStore';
 export interface OrbitDBKVStore extends OrbitDBStore {
   put(key: string, value: any): Promise<void>;
   set(key: string, value: any): Promise<void>;
+  del(key: string): Promise<void>;
 
   get(key: string): any;
 }
