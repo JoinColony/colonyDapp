5fb7fd502bcb6023f8d53861dd632aed3cbf8a82
Raul Glogovetan <raul@glogovetan.com>
2018-12-21 17:17:09 +0200

    Refactor: admin `UserList` remove user method

--- src/modules/admin/components/UserList/UserList.jsx
+++ src/modules/admin/components/UserList/UserList.jsx
@@ -2,16 +2,24 @@
 
 import type { MessageDescriptor } from 'react-intl';
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Table, TableBody } from '~core/Table';
 import Heading from '~core/Heading';
 
+import type { AsyncFunction } from '../../../../createPromiseListener';
+import type { ColonyAdminRecord } from '~immutable';
+
+import promiseListener from '../../../../createPromiseListener';
 import UserListItem from './UserListItem.jsx';
 
-import styles from './UserList.css';
+import {
+  COLONY_ADMIN_REMOVE,
+  COLONY_ADMIN_REMOVE_SUCCESS,
+  COLONY_ADMIN_REMOVE_ERROR,
+} from '../../../dashboard/actionTypes';
 
-import type { ColonyAdminRecord } from '~immutable';
+import styles from './UserList.css';
 
 type Props = {
   /*
@@ -43,54 +51,83 @@ type Props = {
    * Title to show before the list
    */
   label?: string | MessageDescriptor,
-  /*
-   * Method to call when removing the user
-   * Gets passed down to `UserListItem`
-   */
-  onRemove: ColonyAdminRecord => any,
+  /** Redux action to dispatch on submit (e.g. CREATE_XXX) */
+  remove: string,
+  /** Redux action listener for successful action (e.g. CREATE_XXX_SUCCESS) */
+  removeSuccess: string,
+  /** Redux action listener for unsuccessful action (e.g. CREATE_XXX_ERROR) */
+  removeError: string,
 };
 
 const displayName: string = 'admin.UserList';
 
-const UserList = ({
-  users,
-  showDisplayName,
-  showUsername,
-  showMaskedAddress,
-  viewOnly = true,
-  label,
-  onRemove,
-}: Props) => (
-  <div className={styles.main}>
-    {label && (
-      <Heading
-        appearance={{ size: 'small', weight: 'bold', margin: 'small' }}
-        text={label}
-      />
-    )}
-    <div className={styles.listWrapper}>
-      <Table scrollable>
-        <TableBody>
-          {users.map((user, currentIndex) => (
-            <UserListItem
-              /*
-               * This is just so we can have duplicate data inside datamocks
-               * Might as well remove it when the *real* data comes in
-               */
-              key={`${user.walletAddress}${currentIndex + 1}`}
-              user={user}
-              showDisplayName={showDisplayName}
-              showUsername={showUsername}
-              showMaskedAddress={showMaskedAddress}
-              viewOnly={viewOnly}
-              onRemove={() => onRemove(user)}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </div>
-  </div>
-);
+class UserList extends Component<Props> {
+  remove: AsyncFunction<Object, empty>;
+
+  static displayName = 'admin.UserList';
+
+  static defaultProps = {
+    remove: COLONY_ADMIN_REMOVE,
+    removeSuccess: COLONY_ADMIN_REMOVE_SUCCESS,
+    removeError: COLONY_ADMIN_REMOVE_ERROR,
+  };
+
+  constructor(props: Props) {
+    super(props);
+    const { remove, removeSuccess, removeError } = this.props;
+    this.remove = promiseListener.createAsyncFunction({
+      start: remove,
+      resolve: removeSuccess,
+      reject: removeError,
+    });
+  }
+
+  componentWillUnmount() {
+    this.remove.unsubscribe();
+  }
+
+  render() {
+    const {
+      users,
+      showDisplayName,
+      showUsername,
+      showMaskedAddress,
+      viewOnly = true,
+      label,
+    } = this.props;
+    return (
+      <div className={styles.main}>
+        {label && (
+          <Heading
+            appearance={{ size: 'small', weight: 'bold', margin: 'small' }}
+            text={label}
+          />
+        )}
+        <div className={styles.listWrapper}>
+          <Table scrollable>
+            <TableBody>
+              {users.map((user, currentIndex) => (
+                <UserListItem
+                  /*
+                   * This is just so we can have duplicate data inside datamocks
+                   * Might as well remove it when the *real* data comes in
+                   */
+                  key={`${user.walletAddress}${currentIndex + 1}`}
+                  user={user}
+                  showDisplayName={showDisplayName}
+                  showUsername={showUsername}
+                  showMaskedAddress={showMaskedAddress}
+                  viewOnly={viewOnly}
+                  onRemove={() => this.remove.asyncFunction({ admin: user })}
+                />
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+}
 
 UserList.displayName = displayName;
 
