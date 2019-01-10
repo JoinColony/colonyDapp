3e61e5ab5e9d7d86ed7e1cbd7ae1ba863f04c33d
James Lefrère <james@colony.io>
2019-01-10 16:41:40 +0100

    Add `allDrafts` reducer

--- src/modules/dashboard/reducers/index.js
+++ src/modules/dashboard/reducers/index.js
@@ -4,10 +4,12 @@ import { combineReducers } from 'redux';
 
 import allColonies from './allColonies';
 import allDomains from './allDomains';
+import allDrafts from './allDrafts';
 
 const dashboardReducer = combineReducers({
   allColonies,
   allDomains,
+  allDrafts,
 });
 
 export default dashboardReducer;
