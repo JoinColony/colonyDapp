a97d36d3857e7a3b43c43dfcc1ec123b9889b37b
Raul Glogovetan <raul@glogovetan.com>
2019-01-15 14:28:50 +0200

    Move: `GasStation` to `users` module

--- src/modules/core/sagas/transactions/index.js
+++ src/modules/core/sagas/transactions/index.js
@@ -6,5 +6,60 @@ import { takeEvery, select, put } from 'redux-saga/effects';
 
 import type { SendTransactionAction } from '../../types';
 
-export { default as sendMethodTransaction } from './sendMethodTransaction';
-export { default as suggestMethodTransactionGas } from './suggestMethodTransactionGas';
+import sendMethodTransaction from './sendMethodTransaction';
+import suggestMethodTransactionGas from './suggestMethodTransactionGas';
+import {
+  METHOD_TRANSACTION_SENT,
+  TRANSACTION_CREATED,
+  TRANSACTION_GAS_SUGGESTED,
+} from '../../actionTypes';
+
+// TODO replace me with the real gas station/wallet!
+function* jimmysDiscountGas({
+  payload: { id },
+}: SendTransactionAction): Saga<void> {
+  const {
+    context,
+    methodName,
+    suggestedGasLimit,
+    suggestedGasPrice,
+  } = yield select(state => state.core.transactions.get(id));
+//   if (
+//     process.env.SKIP_GAS_STATION_CONFIRM === 'true' ||
+//     // eslint-disable-next-line no-alert
+//     window.confirm(
+//       `Welcome to Jimmy’s Discount Gas
+// ------------------------------------
+// Send ${context}/${methodName} transaction?
+// Gas limit: ${
+//         suggestedGasLimit ? suggestedGasLimit.toNumber() : 'not set'
+//       }, gas price: ${
+//         suggestedGasPrice ? suggestedGasPrice.toNumber() : 'not set'
+//       }`,
+//     )
+//   )
+  // yield put({ type: METHOD_TRANSACTION_SENT, payload: { id } });
+  yield put({ type: 'USERNAME_CREATE_PENDING' });
+}
+
+export default function* transactionsSagas(): any {
+  /*
+   * 1. TRANSACTION_CREATED
+   * When new outgoing transactions are created, a saga is run to get
+   * suggested gas values for the transaction.
+   */
+  yield takeEvery(TRANSACTION_CREATED, suggestMethodTransactionGas);
+  /*
+   * 2. TRANSACTION_GAS_SUGGESTED
+   * TODO replace me with the real gas station/wallet!
+   * When the suggested gas values have been set, this saga prompts the user
+   * to send the transaction (then the next action is dispatched, to send it).
+   */
+  yield takeEvery(TRANSACTION_GAS_SUGGESTED, jimmysDiscountGas);
+  /*
+   * 3. METHOD_TRANSACTION_SENT
+   * is responsible for sending transactions which have been created with
+   * a specific method, for a specific client.
+   */
+  yield takeEvery(METHOD_TRANSACTION_SENT, sendMethodTransaction);
+}
