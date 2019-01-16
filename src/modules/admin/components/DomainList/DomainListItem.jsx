4240be6ca5abdb1334c0317a7d92025c91100a33
Curtis Olson <curtis@colony.io>
2019-01-16 13:04:54 -0600

    Flow typing for domains data records

--- src/modules/admin/components/DomainList/DomainListItem.jsx
+++ src/modules/admin/components/DomainList/DomainListItem.jsx
@@ -1,9 +1,8 @@
 /* @flow */
-
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
-import type { DomainRecord } from '~immutable';
+import type { DataRecord, DomainRecord } from '~immutable';
 
 import { TableRow, TableCell } from '~core/Table';
 import Button from '~core/Button';
@@ -28,46 +27,50 @@ type Props = {
   /*
    * User data Object, follows the same format as UserPicker
    */
-  domain: DomainRecord,
+  domain: DataRecord<DomainRecord>,
   viewOnly: boolean,
   /*
    * Method to call when clicking the remove button
    * Gets passed down to `DomainListItem`
    */
-  onRemove: DomainRecord => any,
+  onRemove: (DataRecord<DomainRecord>) => any,
 };
 
 const DomainListItem = ({
-  domain: {
-    record: { name },
-  },
+  domain,
   contributions,
   viewOnly = true,
   onRemove,
-}: Props) => (
-  <TableRow className={styles.main}>
-    <TableCell className={styles.domainDetails}>
-      <span className={styles.domainName} title={name}>
-        #{name}
-      </span>
-      {contributions && (
-        <span className={styles.contributions}>
-          <FormattedMessage values={{ contributions }} {...MSG.contributions} />
+}: Props) => {
+  const name = domain.getIn(['record', 'name']);
+  return (
+    <TableRow className={styles.main}>
+      <TableCell className={styles.domainDetails}>
+        <span className={styles.domainName} title={name}>
+          #{name}
         </span>
-      )}
-    </TableCell>
-    <TableCell className={styles.userRemove}>
-      {!viewOnly && (
-        <Button
-          className={styles.customRemoveButton}
-          appearance={{ theme: 'primary' }}
-          text={MSG.buttonRemove}
-          onClick={onRemove}
-        />
-      )}
-    </TableCell>
-  </TableRow>
-);
+        {contributions && (
+          <span className={styles.contributions}>
+            <FormattedMessage
+              values={{ contributions }}
+              {...MSG.contributions}
+            />
+          </span>
+        )}
+      </TableCell>
+      <TableCell className={styles.userRemove}>
+        {!viewOnly && (
+          <Button
+            className={styles.customRemoveButton}
+            appearance={{ theme: 'primary' }}
+            text={MSG.buttonRemove}
+            onClick={onRemove}
+          />
+        )}
+      </TableCell>
+    </TableRow>
+  );
+};
 
 DomainListItem.displayName = displayName;
 
--- src/modules/dashboard/selectors/domains.js
+++ src/modules/dashboard/selectors/domains.js
@@ -1,5 +1,4 @@
 /* @flow */
-
 import { createSelector } from 'reselect';
 
 import { Map as ImmutableMap } from 'immutable';
