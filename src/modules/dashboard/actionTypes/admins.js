4dba8b6f0fb46b9dc8218269f40fd81a4e69a62f
Raul Glogovetan <raul@glogovetan.com>
2019-01-16 16:08:32 +0200

    Add: Colony admin intermediate transaction action types

---
+++ src/modules/dashboard/actionTypes/admins.js
@@ -0,0 +1,8 @@
+/* @flow */
+/* eslint-disable max-len */
+
+import ns from '../namespace';
+
+export const COLONY_TRANSACTION_ADMIN_ADD = `${ns}/COLONY_TRANSACTION_ADMIN_ADD`;
+export const COLONY_TRANSACTION_ADMIN_ADD_SUCCESS = `${ns}/COLONY_TRANSACTION_ADMIN_ADD_SUCCESS`;
+export const COLONY_TRANSACTION_ADMIN_ADD_ERROR = `${ns}/COLONY_TRANSACTION_ADMIN_ADD_ERROR`;
