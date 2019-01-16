868f4db91e68c0197276e7df2e80deb0b0920a90
Raul Glogovetan <raul@glogovetan.com>
2019-01-16 11:26:46 +0200

    Add: automatically sign create colony related transactions

--- src/modules/core/sagas/transactions/index.js
+++ src/modules/core/sagas/transactions/index.js
@@ -4,8 +4,10 @@ import { takeEvery } from 'redux-saga/effects';
 
 import sendMethodTransaction from './sendMethodTransaction';
 import suggestMethodTransactionGas from './suggestMethodTransactionGas';
+import readyMethodTransaction from './readyMethodTransaction';
 import {
   METHOD_TRANSACTION_SENT,
+  TRANSACTION_GAS_SUGGESTED,
   TRANSACTION_CREATED,
 } from '../../actionTypes';
 
@@ -16,6 +18,18 @@ export default function* transactionsSagas(): any {
    * suggested gas values for the transaction.
    */
   yield takeEvery(TRANSACTION_CREATED, suggestMethodTransactionGas);
+  /*
+   * 2. TRANSACTION_GAS_SUGGESTED -> TRANSACTION_READY
+   *
+   * After gas sa been suggested we consider the transaction to be "ready" to sign
+   * and we dispatch that action.
+   *
+   * This action's role is to inform the `ActionForm` in the various components
+   * that the trnsaction is ready to sign, so that it can act upon it. Eg: close the current modal
+   *
+   * This will be hooked into the `success` prop of the `ActionForm`
+   */
+  yield takeEvery(TRANSACTION_GAS_SUGGESTED, readyMethodTransaction);
   /*
    * 3. METHOD_TRANSACTION_SENT
    * is responsible for sending transactions which have been created with
