9af144824468c6681a0ea9c01841dbfe7d4a8cb6
Raul Glogovetan <raul@glogovetan.com>
2019-01-17 15:08:27 +0200

    Fix: separate `dashboard` colony admin sagas

---
+++ src/modules/dashboard/sagas/admins.js
@@ -0,0 +1,130 @@
+/* @flow */
+
+import type { Saga } from 'redux-saga';
+
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { push } from 'connected-react-router';
+
+import type { Action } from '~types';
+
+import { putError } from '~utils/saga/effects';
+import { ensureColonyIsInState, fetchColonyStore } from './shared';
+
+import {
+  COLONY_ADMIN_ADD,
+  COLONY_ADMIN_ADD_SUCCESS,
+  COLONY_ADMIN_ADD_ERROR,
+  COLONY_ADMIN_REMOVE,
+  COLONY_ADMIN_REMOVE_SUCCESS,
+  COLONY_ADMIN_REMOVE_ERROR,
+} from '../actionTypes';
+
+import {
+  addColonyAdminTransaction as addColonyAdminAction,
+  removeColonyAdminTransaction as removeColonyAdminAction,
+} from '../actionCreators';
+
+function* addColonyAdmin({
+  payload: { newAdmin, ensName },
+}: Action): Saga<void> {
+  try {
+    const { walletAddress, username } = newAdmin.profile;
+    /*
+     * Get the colony store
+     */
+    const store = yield call(fetchColonyStore, ensName);
+    const colonyAddress = store.get('address');
+    const colonyAdmins = store.get('admins');
+    /*
+     * Dispatch the action to the admin in th redux store
+     */
+    yield put({
+      type: COLONY_ADMIN_ADD_SUCCESS,
+      payload: {
+        ensName,
+        adminData: newAdmin.profile,
+      },
+    });
+    /*
+     * Set the new value on the colony's store
+     */
+    yield call([store, store.set], 'admins', {
+      ...colonyAdmins,
+      [username]: newAdmin.profile,
+    });
+    /*
+     * Dispatch the action to set the admin on the contract level (transaction)
+     */
+    const action = yield call(addColonyAdminAction, colonyAddress, {
+      user: walletAddress,
+    });
+    yield put(action);
+    /*
+     * Redirect the user back to the admins tab
+     */
+    yield put(
+      push({
+        state: { initialTab: 3 },
+      }),
+    );
+  } catch (error) {
+    yield putError(COLONY_ADMIN_ADD_ERROR, error);
+  }
+}
+
+function* removeColonyAdmin({
+  payload: { admin, ensName },
+}: Action): Saga<void> {
+  try {
+    const { walletAddress, username } = admin;
+
+    /*
+     * Ensure the colony is in the state.
+     */
+    yield call(ensureColonyIsInState, ensName);
+
+    /*
+     * Get the colony store
+     */
+    const store = yield call(fetchColonyStore, ensName);
+    const colonyAddress = store.get('address');
+    const colonyAdmins = store.get('admins');
+    /*
+     * Dispatch the action to the admin in th redux store
+     */
+    yield put({
+      type: COLONY_ADMIN_REMOVE_SUCCESS,
+      payload: {
+        ensName,
+        username,
+      },
+    });
+    /*
+     * Remove the colony admin and set the new value on the colony's store
+     */
+    delete colonyAdmins[username];
+    yield call([store, store.set], 'admins', colonyAdmins);
+    /*
+     * Dispatch the action to set the admin on the contract level (transaction)
+     */
+    const action = yield call(removeColonyAdminAction, colonyAddress, {
+      user: walletAddress,
+    });
+    yield put(action);
+    /*
+     * Redirect the user back to the admins tab
+     */
+    yield put(
+      push({
+        state: { initialTab: 3 },
+      }),
+    );
+  } catch (error) {
+    yield putError(COLONY_ADMIN_REMOVE_ERROR, error);
+  }
+}
+
+export default function* adminsSagas(): any {
+  yield takeEvery(COLONY_ADMIN_ADD, addColonyAdmin);
+  yield takeEvery(COLONY_ADMIN_REMOVE, removeColonyAdmin);
+}
