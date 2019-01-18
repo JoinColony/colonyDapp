aa9eee63a00dc24255cb7797587dd16d64278446
Curtis Olson <curtis@colony.io>
2019-01-18 16:59:06 -0600

    Fix `HistoryNavigation` export

--- src/modules/pages/components/NavigationWrapper/index.js
+++ src/modules/pages/components/NavigationWrapper/index.js
@@ -3,4 +3,4 @@
 export { default } from './NavigationWrapper.jsx';
 
 export { default as UserNavigation } from './UserNavigation.jsx';
-export { default as HistoryNavigation } from './UserNavigation.jsx';
+export { default as HistoryNavigation } from './HistoryNavigation.jsx';
