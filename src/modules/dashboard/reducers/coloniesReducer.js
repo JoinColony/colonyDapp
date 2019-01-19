1150f74bf69e151eff2836a5acf0ae952a1c1c82
Raul Glogovetan <raul@glogovetan.com>
2019-01-19 15:32:56 +0200

    Add: separate colony `admin` reducers

--- src/modules/dashboard/reducers/coloniesReducer.js
+++ src/modules/dashboard/reducers/coloniesReducer.js
@@ -8,8 +8,6 @@ import {
   COLONY_FETCH,
   COLONY_FETCH_SUCCESS,
   COLONY_PROFILE_UPDATE_SUCCESS,
-  COLONY_ADMIN_ADD_SUCCESS,
-  COLONY_ADMIN_REMOVE_SUCCESS,
 } from '../actionTypes';
 
 import { Colony, Data, Token } from '~immutable';
@@ -48,21 +46,6 @@ const coloniesReducer = (
     case COLONY_AVATAR_REMOVE_SUCCESS: {
       const { ensName } = action.payload;
       return state.setIn([ensName, 'record', 'avatar'], undefined);
-    }
-    case COLONY_ADMIN_ADD_SUCCESS: {
-      const { ensName, adminData } = action.payload;
-      return state
-        ? state.setIn(
-            [ensName, 'record', 'admins', adminData.username],
-            adminData.toObject(),
-          )
-        : state;
-    }
-    case COLONY_ADMIN_REMOVE_SUCCESS: {
-      const { ensName, username } = action.payload;
-      return state
-        ? state.deleteIn([ensName, 'record', 'admins', username])
-        : state;
     }
     default:
       return state;
