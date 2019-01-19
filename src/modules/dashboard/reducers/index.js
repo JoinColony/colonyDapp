0084c5f6a873319a49a1c5774e560eebe32ba7db
Raul Glogovetan <raul@glogovetan.com>
2019-01-19 15:32:56 +0200

    Add: separate colony `admin` reducers

--- src/modules/dashboard/reducers/index.js
+++ src/modules/dashboard/reducers/index.js
@@ -4,12 +4,14 @@ import { combineReducers } from 'redux';
 
 import allColonies from './allColonies';
 import allTasks from './allTasks';
+import allAdmins from './allAdmins';
 import allDomains from './allDomains';
 import allDrafts from './allDrafts';
 
 const dashboardReducer = combineReducers({
   allColonies,
   allTasks,
+  allAdmins,
   allDomains,
   allDrafts,
 });
