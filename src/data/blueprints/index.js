58eb7e826b333d9bfcf04a60f2f90a72df450d73
Thiago Delgado <thiagodelgado111@gmail.com>
2019-01-20 21:24:39 -0200

    Move store blueprints to data

---
+++ src/data/blueprints/index.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+export * from './meta';
+export { default as colony } from './colony';
+export { default as task } from './task';
+export { default as userProfile } from './userProfile';
+export { default as userInbox } from './userInbox';
+export { default as userActivities } from './userActivities';
---
+++ src/data/blueprints/userInbox.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import * as yup from 'yup';
+
+import type { StoreBlueprint } from '~types/index';
+
+import { FeedStore } from '../../lib/database/stores';
+
+const userInboxStore: StoreBlueprint = {
+  name: 'userInbox',
+  schema: yup.object({
+    userAction: yup
+      .string()
+      // @TODO: Add inbox event type message validation
+      // .oneOf(Object.keys(activityMessages))
+      .required(),
+    colonyName: yup.string(),
+    domainName: yup.string(),
+    createdAt: yup.date().default(() => new Date()),
+  }),
+  type: FeedStore,
+};
+
+export default userInboxStore;
