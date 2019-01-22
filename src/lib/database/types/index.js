0b9f961caad0fdb36b66e25e1760f0886bbb05e1
Thiago Delgado <thiagodelgado111@gmail.com>
2019-01-22 17:37:02 -0200

    Use conditional storeProps
    * Cherry-picked and amended from 974cc55f617afbb7a17732fd8943f0b7f56f81d9

--- src/lib/database/types/index.js
+++ src/lib/database/types/index.js
@@ -48,7 +48,7 @@ export type StoreBlueprint = {
   name: string,
   schema?: ObjectSchema,
   getAccessController?: (
-    storeProps?: Object,
+    storeProps: Object,
   ) => AccessController<PurserIdentity, PurserIdentityProvider<PurserIdentity>>,
   type: typeof Store,
 };
