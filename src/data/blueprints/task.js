2d0e8ec44a0d00f764fcc5b0aea432dc23d6a224
Thiago Delgado <thiagodelgado111@gmail.com>
2019-01-22 18:00:29 -0200

    Set up task store access controller

--- src/data/blueprints/task.js
+++ src/data/blueprints/task.js
@@ -1,12 +1,27 @@
 /* @flow */
 
-import { EventStore } from '../../lib/database/stores';
-// import { colonyMeta } from './meta';
+import type { ColonyClient as ColonyClientType } from '@colony/colony-js-client';
+import type { WalletObjectType } from '@colony/purser-core/flowtypes';
 
-import type { StoreBlueprint } from '~types';
+import type { Address, StoreBlueprint } from '~types';
+
+import { EventStore } from '../../lib/database/stores';
+import { ColonyAccessController } from '../../lib/database/accessControllers';
+import loadPermissionManifest from '../../lib/database/accessControllers/permissions';
 
 const taskStoreBlueprint: StoreBlueprint = {
-  // TODO add access controller
+  getAccessController({
+    colonyAddress,
+    colonyClient,
+    wallet,
+  }: {
+    colonyAddress: Address,
+    wallet: WalletObjectType,
+    colonyClient: ColonyClientType,
+  }) {
+    const manifest = loadPermissionManifest(colonyClient);
+    return new ColonyAccessController(colonyAddress, wallet, manifest);
+  },
   name: 'task',
   type: EventStore,
 };
