5d7ac23a1042792eaf5add4276a4b9f6a9c01427
Raul Glogovetan <raul@glogovetan.com>
2019-01-25 15:24:39 +0200

    Add: dashboard `draftsComments` selector

--- src/modules/dashboard/selectors/comments.js
+++ src/modules/dashboard/selectors/comments.js
@@ -1,5 +1,8 @@
 /* @flow */
 
+import { createSelector } from 'reselect';
+import { Map as ImmutableMap } from 'immutable';
+
 import type { RootState } from '~types';
 
 import ns from '../namespace';
@@ -8,3 +11,9 @@ import ns from '../namespace';
  * Drafts selectors
  */
 export const allCommentsSelector = (state: RootState) => state[ns].allComments;
+
+export const draftsCommentsSelector = createSelector(
+  allCommentsSelector,
+  (state, props) => props.draftId,
+  (allComments, draftId) => allComments.get(draftId, new ImmutableMap()),
+);
