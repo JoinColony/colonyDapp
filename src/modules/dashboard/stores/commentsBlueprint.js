22289b9ffa6e83e8f7227b27849943334072c6a2
Raul Glogovetan <raul@glogovetan.com>
2019-01-26 23:24:58 +0200

    Fix: comments store blueprint spacing

--- src/modules/dashboard/stores/commentsBlueprint.js
+++ src/modules/dashboard/stores/commentsBlueprint.js
@@ -7,26 +7,26 @@ import { FeedStore } from '../../../lib/database/stores';
 import type { StoreBlueprint } from '~types';
 
 const commentsBlueprint: StoreBlueprint = {
-   name: 'comments',
-   schema: yup.object({
-     signature: yup.string().required(),
-     content: yup.object({
-       id: yup.string().required(),
-       author: yup
-         .string()
-         .address()
-         .required(),
-       timestamp: yup.date().default(() => Date.now()),
-       body: yup.string().required(),
-       metadata: yup.object({
-         /*
-          * @TODO When the time is right, add attachments
-          */
-         mentions: yup.array().of(yup.string().required()),
-       }),
-     }),
-   }),
-   type: FeedStore,
+  name: 'comments',
+  schema: yup.object({
+    signature: yup.string().required(),
+    content: yup.object({
+      id: yup.string().required(),
+      author: yup
+        .string()
+        .address()
+        .required(),
+      timestamp: yup.date().default(() => Date.now()),
+      body: yup.string().required(),
+      metadata: yup.object({
+        /*
+         * @TODO When the time is right, add attachments
+         */
+        mentions: yup.array().of(yup.string().required()),
+      }),
+    }),
+  }),
+  type: FeedStore,
 };
 
 export default commentsBlueprint;
