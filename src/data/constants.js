65f6aa8ec127b8b04935a243b974bfe99ce10023
Thiago Delgado <thiagodelgado111@gmail.com>
2019-01-29 15:58:20 -0200

    Add event types

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,32 @@
 /* @flow */
 
 // @TODO add flow typing here
-// eslint-disable-next-line import/prefer-default-export
-export const TASK_EVENT_TYPES = {
-  COMMENT_STORE_CREATED: 'COMMENT_STORE_CREATED',
+
+type EventTypeId = string;
+type EventTypeDictionary = {
+  [eventType: EventTypeId]: string,
 };
+
+export const TASK_EVENT_TYPES = ({
+  COMMENT_STORE_CREATED: 'COMMENT_STORE_CREATED',
+  DRAFT_UPDATED: 'DRAFT_UPDATED',
+  DUE_DATE_SET: 'DUE_DATE_SET',
+  SKILL_SET: 'SKILL_SET',
+}: EventTypeDictionary);
+
+export const USER_EVENT_TYPES = ({
+  // @TODO: Add inbox event types
+  READ_UNTIL: 'READ_UNTIL',
+}: EventTypeDictionary);
+
+export const COLONY_EVENT_TYPES = ({
+  COLONY_STORE_INITIALIZED: 'INITIALIZE',
+  ADMIN_ADDED: 'ADMIN_ADDED',
+  ADMIN_REMOVED: 'ADMIN_REMOVED',
+  AVATAR_UPLOADED: 'AVATAR_UPLOADED',
+  AVATAR_REMOVED: 'AVATAR_REMOVED',
+  DOMAIN_CREATED: 'DOMAIN_CREATED',
+  PROFILE_CREATED: 'PROFILE_CREATED',
+  PROFILE_UPDATED: 'PROFILE_UPDATED',
+  TOKEN_INFO_ADDED: 'TOKEN_INFO_ADDED',
+}: EventTypeDictionary);
