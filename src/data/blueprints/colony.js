35a8bb8096de848afe100ea485866fddefd3adfc
Thiago Delgado <thiagodelgado111@gmail.com>
2019-01-29 17:12:18 -0200

    Remove old comment from colony blueprint

--- src/data/blueprints/colony.js
+++ src/data/blueprints/colony.js
@@ -9,27 +9,6 @@ const colonyStoreBlueprint: StoreBlueprint = {
   getAccessController: getAttributesBasedStoreAccessController,
   name: 'colony',
   type: EventStore,
-  // schema: yup.object({
-  //   id: yup.number(),
-  //   address: yup.string().address(),
-  //   ensName: yup.string(),
-  //   name: yup.string(),
-  //   description: yup.string(),
-  //   website: yup.string().url(),
-  //   guideline: yup.string().url(),
-  //   // TODO: IPFS hash add yup validation for IPFS hash
-  //   avatar: yup.string(),
-  //   token: yup.object({
-  //     address: yup.string().address(),
-  //     icon: yup.string(),
-  //     name: yup.string(),
-  //     symbol: yup.string(),
-  //   }),
-  //   admins: yup.object(),
-  //   databases: yup.object({
-  //     domainsIndex: yup.string(),
-  //   }),
-  // }),
 };
 
 export default colonyStoreBlueprint;
