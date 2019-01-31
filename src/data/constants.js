9a6f4203659ffc290dad6b6f2519246d669623fc
Thiago Delgado <thiagodelgado111@gmail.com>
2019-01-31 14:55:12 -0200

    Freeze!
    * Freeze colony, task and user type dictionary

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,26 +1,24 @@
 /* @flow */
 
-// @TODO add flow typing here
-
 type EventTypeId = string;
 type EventTypeDictionary = {
   [eventType: EventTypeId]: string,
 };
 
-export const TASK_EVENT_TYPES = ({
+export const TASK_EVENT_TYPES: EventTypeDictionary = Object.freeze({
   COMMENT_STORE_CREATED: 'COMMENT_STORE_CREATED',
   DRAFT_CREATED: 'DRAFT_CREATED',
   DRAFT_UPDATED: 'DRAFT_UPDATED',
   DUE_DATE_SET: 'DUE_DATE_SET',
   SKILL_SET: 'SKILL_SET',
-}: EventTypeDictionary);
+});
 
-export const USER_EVENT_TYPES = ({
+export const USER_EVENT_TYPES: EventTypeDictionary = Object.freeze({
   // @TODO: Add inbox event types
   READ_UNTIL: 'READ_UNTIL',
-}: EventTypeDictionary);
+});
 
-export const COLONY_EVENT_TYPES = ({
+export const COLONY_EVENT_TYPES: EventTypeDictionary = Object.freeze({
   ADMIN_ADDED: 'ADMIN_ADDED',
   ADMIN_REMOVED: 'ADMIN_REMOVED',
   AVATAR_UPLOADED: 'AVATAR_UPLOADED',
@@ -30,4 +28,4 @@ export const COLONY_EVENT_TYPES = ({
   PROFILE_UPDATED: 'PROFILE_UPDATED',
   TASK_STORE_CREATED: 'TASK_STORE_CREATED',
   TOKEN_INFO_ADDED: 'TOKEN_INFO_ADDED',
-}: EventTypeDictionary);
+});
