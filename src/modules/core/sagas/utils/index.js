90052e7b89aea9be0c2b9abbc0b16ee83ecc13b5
Raul Glogovetan <raul@glogovetan.com>
2019-02-05 17:44:25 +0200

    Fix: export `getProvider` from core saga utils

--- src/modules/core/sagas/utils/index.js
+++ src/modules/core/sagas/utils/index.js
@@ -12,6 +12,7 @@ export { default as getDDB } from './getDDB';
 export { default as getGasPrices } from './getGasPrices';
 export { default as getNetworkClient } from './getNetworkClient';
 export { default as getMethod } from './getMethod';
+export { getProvider, defaultNetwork } from './getProvider';
 
 export const getNetworkMethod = getMethod.bind(null, NETWORK_CONTEXT);
 export const getColonyMethod = getMethod.bind(null, COLONY_CONTEXT);
