4e683fbd09cc6ec40b42d486dfb14c7edf704e29
Thiago Delgado <thiagodelgado111@gmail.com>
2019-02-05 17:57:38 -0200

    Add command and query saga effect utils

--- src/utils/saga/effects.js
+++ src/utils/saga/effects.js
@@ -2,12 +2,13 @@
 
 import type { Saga } from 'redux-saga';
 
-import { call, put, race, take, getContext } from 'redux-saga/effects';
+import { call, put, race, take } from 'redux-saga/effects';
 
 import type { ENSName, TakeFilter } from '~types';
+import type { Command, Query } from '../../data/types';
 
 import { isDev, log } from '~utils/debug';
-import { CONTEXT } from '~context';
+import { getContext, CONTEXT } from '~context';
 
 /*
  * Effect to create a new class instance of Class (use instead of "new Class")
@@ -60,7 +61,7 @@ export const raceError = (
  * Gets the caller from the colonyManager and calls it with the given
  * parameters. If no colonyENSName, network context is assumed.
  */
-export const callCaller = ({
+export function* callCaller({
   context,
   identifier,
   methodName,
@@ -70,16 +71,42 @@ export const callCaller = ({
   identifier?: ENSName,
   methodName: string,
   params?: Object,
-}) => {
-  function* callCallerGenerator(): Saga<Object> {
-    const colonyManager = yield getContext(CONTEXT.COLONY_MANAGER);
-    const caller = yield call(
-      [colonyManager, colonyManager.getMethod],
-      context,
-      methodName,
-      identifier,
-    );
-    return yield call([caller, caller.call], params);
-  }
-  return call(callCallerGenerator);
-};
+}): Saga<*> {
+  const colonyManager = yield* getContext(CONTEXT.COLONY_MANAGER);
+  // TODO typing could be better here
+  const caller = yield call(
+    [colonyManager, colonyManager.getMethod],
+    context,
+    methodName,
+    identifier,
+  );
+  return yield call([caller, caller.call], params);
+}
+
+export function executeQuery<C: *, Q: Query, I: *>(
+  context: C,
+  query: Q<I>,
+  args: I,
+): Saga<Object> {
+  const { execute } = query(context);
+  return call(execute, args);
+}
+
+export function executeCommand<C: *, CO: Command, I: *>(
+  context: C,
+  command: CO<I>,
+  args: I,
+): Saga<Object> {
+  const { execute } = command(context);
+  return call(execute, args);
+}
+
+export function* validateAndExecuteCommand<C: *, CO: Command, I: *>(
+  context: C,
+  command: CO<I>,
+  args: I,
+): Saga<Object> {
+  const { execute, validate } = command(context);
+  const sanitizedArgs = validate ? yield call(validate, args) : args;
+  return call(execute, sanitizedArgs);
+}
