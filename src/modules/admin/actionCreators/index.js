0d48f9b35fed874fc5bc4a3f8d65e2bbb81d7661
Christian Maniewski <chmanie@fastmail.com>
2019-02-15 20:49:43 +0630

    Refactor admin sagas to use new tx creation

--- src/modules/admin/actionCreators/index.js
+++ src/modules/admin/actionCreators/index.js
@@ -6,14 +6,7 @@ import {
   COLONY_FETCH_TRANSACTIONS,
   COLONY_FETCH_UNCLAIMED_TRANSACTIONS,
   COLONY_CLAIM_TOKEN,
-  COLONY_CLAIM_TOKEN_ERROR,
-  COLONY_CLAIM_TOKEN_SUCCESS,
 } from '../actionTypes';
-
-import {
-  createTxActionCreator,
-  COLONY_CONTEXT,
-} from '../../core/actionCreators';
 
 export const fetchColonyTransactions = (colonyENSName: ENSName) => ({
   type: COLONY_FETCH_TRANSACTIONS,
@@ -28,15 +21,4 @@ export const fetchColonyUnclaimedTransactions = (colonyENSName: ENSName) => ({
 export const claimColonyToken = (ensName: ENSName, tokenAddress: Address) => ({
   type: COLONY_CLAIM_TOKEN,
   payload: { ensName, tokenAddress },
-});
-
-export const claimColonyTokenTransaction = createTxActionCreator<{
-  token: string,
-}>({
-  context: COLONY_CONTEXT,
-  methodName: 'claimColonyFunds',
-  lifecycle: {
-    error: COLONY_CLAIM_TOKEN_ERROR,
-    eventDataReceived: COLONY_CLAIM_TOKEN_SUCCESS,
-  },
 });
