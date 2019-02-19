b480ac5d41d975cba0c6a224c29b6399c8e21f67
Thiago Delgado <thiagodelgado111@gmail.com>
2019-02-19 18:00:39 -0300

    Create aliases for action ids
    * For now, those aliases come from event types
      and we map them to action ids using the inherits
      feature from PermissionManager. But there are
      better ways to do that

--- src/lib/database/accessControllers/permissions/colony.js
+++ src/lib/database/accessControllers/permissions/colony.js
@@ -4,9 +4,18 @@ import type { PermissionsManifest } from './types';
 
 export default function loadModule(): PermissionsManifest {
   return {
+    AVATAR_UPLOADED: { inherits: 'set-colony-avatar' },
+    AVATAR_REMOVED: { inherits: 'set-colony-avatar' },
+    DOMAIN_CREATED: { inherits: 'add-domain' },
+    PROFILE_CREATED: { inherits: 'create-colony-profile' },
+    PROFILE_UPDATED: { inherits: 'update-colony-profile' },
+    TASK_STORE_CREATED: { inherits: 'register-task-store' },
+    TOKEN_INFO_ADDED: { inherits: 'add-token' },
+    'create-colony-profile': { inherits: 'is-colony-founder' },
+    'update-colony-profile': { inherits: 'is-colony-founder-or-admin' },
+    'register-task-store': { inherits: 'is-colony-founder-or-admin' },
+    'add-token': { inherits: 'is-colony-founder' },
     'set-colony-avatar': { inherits: 'is-colony-founder' },
-    'add-global-skill': { inherits: 'is-colony-founder' },
-    'set-admin-role': { inherits: 'is-colony-founder-or-admin' },
     'add-domain': { inherits: 'is-colony-founder-or-admin' },
   };
 }
