7fcdcc8009cd31dd9590eecb15b0630057c538f3
Thiago Delgado <thiagodelgado111@gmail.com>
2019-02-26 16:07:49 -0300

    Set task store permissions

--- src/lib/database/accessControllers/permissions/common.js
+++ src/lib/database/accessControllers/permissions/common.js
@@ -5,17 +5,12 @@ import type { ColonyClient as ColonyClientType } from '@colony/colony-js-client'
 import {
   ADMIN_ROLE,
   AUTHORITY_ROLES,
-  EVALUATOR_ROLE,
   FOUNDER_ROLE,
-  MANAGER_ROLE,
-  TASK_ROLES,
-  WORKER_ROLE,
 } from '@colony/colony-js-client';
 
 import type { PermissionsManifest } from './types';
 
 type AuthorityRole = $Keys<typeof AUTHORITY_ROLES>;
-type Role = $Keys<typeof TASK_ROLES>;
 
 const isAny = (...promises): Promise<boolean> =>
   Promise.all(promises).then(values => values.some(value => !!value));
@@ -28,47 +23,16 @@ const makeUserHasRoleFn = (
   return hasRole;
 };
 
-const makeUserTaskAssignedRoleFn = (
-  colonyClient: ColonyClientType,
-  role: Role,
-) => async (user: string, { taskId }: { taskId: number }) => {
-  const { address } = await colonyClient.getTaskRole.call({
-    taskId,
-    role,
-  });
-  return address === user;
-};
-
 export default function loadModule(
   colonyClient: ColonyClientType,
 ): PermissionsManifest {
   const isColonyAdmin = makeUserHasRoleFn(colonyClient, ADMIN_ROLE);
   const isColonyFounder = makeUserHasRoleFn(colonyClient, FOUNDER_ROLE);
 
-  const isTaskManager = makeUserTaskAssignedRoleFn(colonyClient, MANAGER_ROLE);
-  const isTaskEvaluator = makeUserTaskAssignedRoleFn(
-    colonyClient,
-    EVALUATOR_ROLE,
-  );
-  const isTaskWorker = makeUserTaskAssignedRoleFn(colonyClient, WORKER_ROLE);
-
   return {
     'is-colony-admin': isColonyAdmin,
     'is-colony-founder': isColonyFounder,
     'is-colony-founder-or-admin': user =>
       isAny(isColonyAdmin(user), isColonyFounder(user)),
-    'is-task-manager': isTaskManager,
-    'is-task-evaluator': isTaskEvaluator,
-    'is-task-worker': isTaskWorker,
-    'is-assigned-to-task': (user, context) =>
-      isAny(
-        isTaskWorker(user, context),
-        isTaskManager(user, context),
-        isTaskEvaluator(user, context),
-      ),
-    'is-task-manager-or-evaluator': (user, context) =>
-      isAny(isTaskManager(user, context), isTaskEvaluator(user, context)),
-    'is-task-manager-or-worker': (user, context) =>
-      isAny(isTaskManager(user, context), isTaskWorker(user, context)),
   };
 }
