65ffc19ac2001a27957243592dec4e05ea85bfba
Christian Maniewski <chmanie@fastmail.com>
2019-03-03 15:47:29 +0630

    Add useDataFetcher hook

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,13 +1,54 @@
 /* @flow */
 
-// $FlowFixMe (not possible until we upgrade flow to 0.87)
-import { useEffect, useRef } from 'react';
+import type { Action } from '~redux';
+import type { RootStateRecord } from '~immutable';
 
-// eslint-disable-next-line import/prefer-default-export
+// $FlowFixMe (not possible until we upgrade flow to 0.87)
+import { useEffect, useCallback, useRef } from 'react';
+import { useDispatch, useMappedState } from 'redux-react-hook';
+
+import { isFetchingData } from '~immutable/utils';
+
+type DataFetcher = {
+  select: (rootState: RootStateRecord, ...selectArgs: any[]) => any,
+  fetch: (...fetchArgs: any[]) => Action<*>,
+};
+
 export const usePrevious = (value: any) => {
   const ref = useRef();
   useEffect(() => {
     ref.current = value;
   });
   return ref.current;
+};
+
+const transformFetchedData = data => {
+  if (!data) return null;
+  if (data.record && typeof data.record.toJS == 'function') {
+    return data.record.toJS();
+  }
+  return data.record;
+};
+
+export const useDataFetcher = (
+  fetcher: DataFetcher,
+  selectArgs: any[],
+  fetchArgs: any[],
+) => {
+  const dispatch = useDispatch();
+  const mapState = useCallback(
+    state => ({
+      data: fetcher.select(state, ...selectArgs),
+    }),
+    selectArgs,
+  );
+  const { data } = useMappedState(mapState);
+  useEffect(() => {
+    dispatch(fetcher.fetch(...fetchArgs), fetchArgs);
+  }, fetchArgs);
+  return {
+    data: transformFetchedData(data),
+    isFetching: isFetchingData(data),
+    error: data ? data.error : null,
+  };
 };
