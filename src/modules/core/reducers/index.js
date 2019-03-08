4adae9f8fd64a4058e16127f9f9065f1c4132d5e
Raul Glogovetan <raul@glogovetan.com>
2019-03-08 12:08:31 +0200

    Add: core network reducer

--- src/modules/core/reducers/index.js
+++ src/modules/core/reducers/index.js
@@ -4,11 +4,14 @@ import { combineReducers } from 'redux-immutable';
 
 import coreTransactionsReducer from './transactions';
 import coreGasPricesReducer from './gasPrices';
-import { CORE_GAS_PRICES, CORE_TRANSACTIONS } from '../constants';
+import coreNetworkReducer from './network';
+
+import { CORE_GAS_PRICES, CORE_TRANSACTIONS, CORE_NETWORK } from '../constants';
 
 const coreReducer = combineReducers({
   [CORE_TRANSACTIONS]: coreTransactionsReducer,
   [CORE_GAS_PRICES]: coreGasPricesReducer,
+  [CORE_NETWORK]: coreNetworkReducer,
 });
 
 export default coreReducer;
