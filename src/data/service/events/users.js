f4a93d921f5ea181ceb1f5ac06b933d9f44fbb28
Thiago Delgado <thiagodelgado111@gmail.com>
2019-03-08 19:59:39 -0300

    Add user metadata events

--- src/data/service/events/users.js
+++ src/data/service/events/users.js
@@ -7,7 +7,11 @@ import { createEventCreator } from '../../utils';
 import { USER_EVENT_TYPES } from '../../constants';
 import { CreateNotificationsReadUntilEventSchema } from './schemas';
 
-const { READ_UNTIL } = USER_EVENT_TYPES;
+const {
+  READ_UNTIL,
+  SUBSCRIBED_TO_TASK,
+  UNSUBSCRIBED_FROM_TASK,
+} = USER_EVENT_TYPES;
 
 export type NotificationsReadUntilEventArgs = {|
   readUntil: string,
@@ -19,8 +23,38 @@ export type NotificationsReadUntilEvent = Event<
   NotificationsReadUntilEventPayload,
 >;
 
+export type SubscribedToTaskEventArgs = {|
+  taskId: string,
+|};
+export type SubscribedToTaskEventPayload = SubscribedToTaskEventArgs;
+export type SubscribedToTaskEvent = Event<
+  typeof SUBSCRIBED_TO_TASK,
+  SubscribedToTaskEventPayload,
+>;
+
+export type UnsubscribedFromTaskEventArgs = {|
+  taskId: string,
+|};
+export type UnsubscribedFromTaskEventPayload = UnsubscribedFromTaskEventArgs;
+export type UnsubscribedFromTaskEvent = Event<
+  typeof UNSUBSCRIBED_FROM_TASK,
+  UnsubscribedFromTaskEventPayload,
+>;
+
 export const createNotificationsReadEvent = createEventCreator<
   typeof READ_UNTIL,
   NotificationsReadUntilEventArgs,
   NotificationsReadUntilEvent,
 >(READ_UNTIL, CreateNotificationsReadUntilEventSchema);
+
+export const createSubscribeToTaskEvent = createEventCreator<
+  typeof SUBSCRIBED_TO_TASK,
+  SubscribedToTaskEventArgs,
+  SubscribedToTaskEvent,
+>(SUBSCRIBED_TO_TASK);
+
+export const createUnsubscribeToTaskEvent = createEventCreator<
+  typeof UNSUBSCRIBED_FROM_TASK,
+  UnsubscribedFromTaskEventArgs,
+  UnsubscribedFromTaskEvent,
+>(UNSUBSCRIBED_FROM_TASK);
