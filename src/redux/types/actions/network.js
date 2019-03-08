1b442f1cf2d613719a042260acef129e4545e181
Raul Glogovetan <raul@glogovetan.com>
2019-03-08 13:57:01 +0200

    Fix: core network redux action types

--- src/redux/types/actions/network.js
+++ src/redux/types/actions/network.js
@@ -1,12 +1,13 @@
 /* @flow */
 
 import type { ActionTypeWithPayload } from '~redux';
+import type { NetworkProps } from '~immutable';
 
 import { ACTIONS } from '../../index';
 
 export type NetworkActionTypes = {|
   NETWORK_VERSION_UPDATE: ActionTypeWithPayload<
     typeof ACTIONS.NETWORK_VERSION_UPDATE,
-    string,
+    NetworkProps,
   >,
 |};
