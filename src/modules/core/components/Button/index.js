ac56dd34fb72f4b2f05d4e4e0d74e36627eb23be
sprusr <scott@spru.sr>
2019-03-12 19:21:29 +0000

    Add ConfirmButton component

--- src/modules/core/components/Button/index.js
+++ src/modules/core/components/Button/index.js
@@ -2,4 +2,5 @@
 
 export { default } from './Button.jsx';
 export { default as ActionButton } from './ActionButton.jsx';
+export { default as ConfirmButton } from './ConfirmButton.jsx';
 export { default as DialogActionButton } from './DialogActionButton.jsx';
