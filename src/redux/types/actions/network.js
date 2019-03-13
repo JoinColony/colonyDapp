a6a2a121739126b9cf12fb6e0d608ec79e14031a
Raul Glogovetan <raul@glogovetan.com>
2019-03-13 16:18:43 +0200

    Add: `NETWORK_VERSION_UPDATE` action and types

--- src/redux/types/actions/network.js
+++ src/redux/types/actions/network.js
@@ -6,7 +6,8 @@ import type { NetworkProps } from '~immutable';
 import { ACTIONS } from '../../index';
 
 export type NetworkActionTypes = {|
-  NETWORK_VERSION_UPDATE: ActionTypeWithPayload<
+  NETWORK_VERSION_UPDATE: ActionType<typeof ACTIONS.NETWORK_VERSION_UPDATE>,
+  NETWORK_FETCH_VERSION: ActionTypeWithPayload<
     typeof ACTIONS.NETWORK_VERSION_UPDATE,
     NetworkProps,
   >,
