23829ac34bbe48d29d6eb46bea2eeedd77b67021
Raul Glogovetan <raul@glogovetan.com>
2019-03-13 16:58:32 +0200

    Add: `NETWORK_FETCH_VERSION` error/success states

--- src/redux/types/actions/network.js
+++ src/redux/types/actions/network.js
@@ -1,14 +1,19 @@
 /* @flow */
 
-import type { Action, ActionTypeWithPayload } from '~redux';
+import type { Action, ActionTypeWithPayload, ErrorActionType } from '~redux';
 import type { NetworkProps } from '~immutable';
+import type { WithKeyPathDepth1 } from '~types';
 
 import { ACTIONS } from '../../index';
 
 export type NetworkActionTypes = {|
   NETWORK_FETCH_VERSION: Action<typeof ACTIONS.NETWORK_FETCH_VERSION>,
-  NETWORK_VERSION_UPDATE: ActionTypeWithPayload<
-    typeof ACTIONS.NETWORK_VERSION_UPDATE,
+  NETWORK_FETCH_VERSION_SUCCESS: ActionTypeWithPayload<
+    typeof ACTIONS.NETWORK_FETCH_VERSION_SUCCESS,
     NetworkProps,
+  >,
+  NETWORK_FETCH_VERSION_ERROR: ErrorActionType<
+    typeof ACTIONS.NETWORK_FETCH_VERSION_ERROR,
+    WithKeyPathDepth1,
   >,
 |};
