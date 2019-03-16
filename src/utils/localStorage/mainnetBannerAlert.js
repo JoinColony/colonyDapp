7fd3a41503ffacbe0bfaa185cfd0245e283211a0
Raul Glogovetan <raul@glogovetan.com>
2019-03-16 14:57:58 +0200

    Remove: un-needed `localStorage` banner utils

--- src/utils/localStorage/mainnetBannerAlert.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import { PAGES_MAINNET_BANNER_DISMISSED } from '../../modules/pages/constants';
-
-export const isBannerDismissed = () =>
-  localStorage.getItem(PAGES_MAINNET_BANNER_DISMISSED) === 'true' || false;
-
-export const dismissBanner = () =>
-  localStorage.setItem(PAGES_MAINNET_BANNER_DISMISSED, 'true');
