595c507797f11371257e9d56b5cdae8215f48c0c
Raul Glogovetan <raul@glogovetan.com>
2019-03-27 21:10:58 +0200

    Add: data service comment mention event schema

--- src/data/blueprints/userInbox.js
+++ src/data/blueprints/userInbox.js
@@ -1,23 +1,22 @@
 /* @flow */
 
+import * as yup from 'yup';
+
 import type { StoreBlueprint } from '~types/index';
 
-import * as yup from 'yup';
 import { FeedStore } from '../../lib/database/stores';
 import { getPermissiveStoreAccessController } from '../accessControllers';
+import inboxMessages from '~users/Inbox/messages';
 
 const userInboxStore: StoreBlueprint = {
   getAccessController: getPermissiveStoreAccessController,
   name: 'userInbox',
+  /*
+   * @TODO Add the relevant props, only as they become necessary, otherwise
+   * this will become even more confusing
+   */
   schema: yup.object({
-    userAction: yup
-      .string()
-      // TODO: Add inbox event type message validation
-      // .oneOf(Object.keys(activityMessages))
-      .required(),
-    colonyName: yup.string(),
-    domainName: yup.string(),
-    createdAt: yup.date().default(() => new Date()),
+    event: yup.string().oneOf(Object.keys(inboxMessages)),
   }),
   type: FeedStore,
 };
