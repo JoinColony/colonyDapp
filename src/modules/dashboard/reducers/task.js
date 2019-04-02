8d812a7b40e89d809f888f5f0d6826dcc59b9c45
James Lefrère <james@colony.io>
2019-04-02 13:45:29 +0200

    Update task reducers
    * Update the tasks/task reducers such that more task actions are handled

--- src/modules/dashboard/reducers/task.js
+++ src/modules/dashboard/reducers/task.js
@@ -1,29 +1,61 @@
 /* @flow */
 
+import { fromJS } from 'immutable';
+
 import type { ReducerType } from '~redux';
 import type { DataRecordType, TaskRecordType } from '~immutable';
 
 import { ACTIONS } from '~redux';
-import { DataRecord, TaskRecord } from '~immutable';
+import {
+  DataRecord,
+  TaskPayoutRecord,
+  TokenRecord,
+  TaskRecord,
+} from '~immutable';
 
 const taskReducer: ReducerType<
   DataRecordType<TaskRecordType>,
   {|
     TASK_CREATE_SUCCESS: *,
     TASK_FETCH_SUCCESS: *,
+    TASK_SET_DESCRIPTION_SUCCESS: *,
+    TASK_SET_DOMAIN_SUCCESS: *,
+    TASK_SET_DUE_DATE_SUCCESS: *,
+    TASK_SET_SKILL_SUCCESS: *,
+    TASK_SET_PAYOUT_SUCCESS: *,
+    TASK_SET_TITLE_SUCCESS: *,
   |},
 > = (state = DataRecord(), action) => {
   switch (action.type) {
-    // TODO in #939 handle all task state!
+    case ACTIONS.TASK_SET_DESCRIPTION_SUCCESS:
+      // $FlowFixMe https://www.youtube.com/embed/mrtya1apTtM?start=13&end=18&autoplay=1
+      return state.setIn(['record', 'description'], action.payload.description);
+    case ACTIONS.TASK_SET_DOMAIN_SUCCESS:
+      return state.setIn(['record', 'domainId'], action.payload.domainId);
+    case ACTIONS.TASK_SET_DUE_DATE_SUCCESS:
+      return state.setIn(['record', 'dueDate'], action.payload.dueDate);
+    case ACTIONS.TASK_SET_PAYOUT_SUCCESS: {
+      const { token, amount } = action.payload;
+      return state.updateIn(['record', 'payouts'], payouts =>
+        payouts.push(
+          TaskPayoutRecord({
+            amount: parseInt(amount, 10),
+            token: TokenRecord({ address: token }),
+          }),
+        ),
+      );
+    }
+    case ACTIONS.TASK_SET_SKILL_SUCCESS:
+      return state.setIn(['record', 'skillId'], action.payload.skillId);
+    case ACTIONS.TASK_SET_TITLE_SUCCESS:
+      return state.setIn(['record', 'title'], action.payload.title);
     case ACTIONS.TASK_CREATE_SUCCESS:
     case ACTIONS.TASK_FETCH_SUCCESS: {
-      // TODO in #939 handle these things properly
-      // $FlowFixMe
-      const { feedItems, manager, worker, payouts, ...task } = action.payload;
+      const { task } = action.payload;
       return DataRecord({
         isFetching: false,
         error: undefined,
-        record: TaskRecord(task),
+        record: TaskRecord(fromJS(task)),
       });
     }
     default:
