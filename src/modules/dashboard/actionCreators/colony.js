15dfcc2b87954388d24ec2f2a80c8ef05274e4fd
James Lefrère <james@colony.io>
2019-04-05 19:04:52 +0200

    Add action types

--- src/modules/dashboard/actionCreators/colony.js
+++ src/modules/dashboard/actionCreators/colony.js
@@ -1,18 +1,22 @@
 /* @flow */
 
 import type { ENSName } from '~types';
+import type { Action } from '~redux';
 
 import { ACTIONS } from '~redux';
 
-export const fetchColony = (ensName: ENSName) => ({
+export const fetchColony = (
+  colonyENSName: ENSName,
+): Action<typeof ACTIONS.COLONY_FETCH> => ({
   type: ACTIONS.COLONY_FETCH,
-  payload: {
-    ensName,
-  },
-  meta: { keyPath: [ensName] },
+  payload: { colonyENSName },
+  meta: { keyPath: [colonyENSName] },
 });
 
-export const fetchColonyENSName = (colonyAddress: string) => ({
+export const fetchColonyENSName = (
+  colonyAddress: string,
+): Action<typeof ACTIONS.COLONY_ENS_NAME_FETCH> => ({
   type: ACTIONS.COLONY_ENS_NAME_FETCH,
+  payload: { colonyAddress },
   meta: { keyPath: [colonyAddress] },
 });
