9b3f7093d546724b03db95dc4f10fcbd7e537f57
Raul Glogovetan <raul@glogovetan.com>
2019-04-09 13:38:41 +0300

    Update: `createColony` dashboard data command

--- src/modules/dashboard/data/commands/notification.js
+++ src/modules/dashboard/data/commands/notification.js
@@ -1,8 +1,19 @@
 /* @flow */
+import type { FeedStore } from '~lib/database/stores';
+import type { UserInboxCommand } from '~data/types';
 
-export const createColony = () => ({
-  async execute() {
-    return false;
+import { getUserInboxStore } from '~data/stores';
+
+import { createColonyCreatedEvent } from '../events';
+
+export const createColony: UserInboxCommand<*, FeedStore> = ({
+  ddb,
+  metadata,
+}) => ({
+  async execute(args) {
+    const userInboxStore = await getUserInboxStore(ddb)(metadata);
+    await userInboxStore.add(createColonyCreatedEvent(args));
+    return userInboxStore;
   },
 });
 
