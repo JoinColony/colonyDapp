25c919207c287e2da33842619116c809c163aadf
Curtis Olson <curtis@colony.io>
2019-04-10 12:07:12 -0500

    Accept props

--- src/modules/core/components/ColonyFee/ColonyFee.jsx
+++ src/modules/core/components/ColonyFee/ColonyFee.jsx
@@ -1,10 +1,24 @@
 /* @flow */
 
 import React from 'react';
+import BN from 'bn.js';
+
+import Numeral from '~core/Numeral';
+
+import styles from './ColonyFee.css';
+
+type Props = {|
+  amount: BN | string,
+  symbol: string,
+|};
 
 const displayName = 'ColonyFee';
 
-const ColonyFee = () => <div />;
+const ColonyFee = ({ amount, symbol }: Props) => (
+  <div className={styles.main}>
+    <Numeral value={amount} suffix={` ${symbol}`} />
+  </div>
+);
 
 ColonyFee.displayName = displayName;
 
