9656e153143a70e292e1cc3a7d713194cd397d5e
Raul Glogovetan <raul@glogovetan.com>
2019-04-10 20:39:43 +0300

    Add: token created notification event

--- src/modules/dashboard/data/events/notification.js
+++ src/modules/dashboard/data/events/notification.js
@@ -3,9 +3,12 @@
 import { createEventCreator } from '~data/utils';
 import { COLONY_EVENT_TYPES } from '~data/constants';
 
-const { COLONY_CREATED } = COLONY_EVENT_TYPES;
+const { COLONY_CREATED, TOKEN_CREATED } = COLONY_EVENT_TYPES;
 
-// eslint-disable-next-line import/prefer-default-export
 export const createColonyCreatedEvent = createEventCreator<
   typeof COLONY_CREATED,
 >(COLONY_CREATED);
+
+export const createTokenCreatedEvent = createEventCreator<
+  typeof TOKEN_CREATED,
+>(TOKEN_CREATED);
