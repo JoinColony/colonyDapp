9a2aab8ea9c3403a4b892da38c88900e76c9e66c
Curtis Olson <curtis@colony.io>
2019-04-10 14:53:48 -0500

    Add icon and basic fee text

--- src/modules/core/components/ColonyFee/ColonyFee.jsx
+++ src/modules/core/components/ColonyFee/ColonyFee.jsx
@@ -1,11 +1,24 @@
 /* @flow */
 
 import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
 import BN from 'bn.js';
 
+import Icon from '~core/Icon';
 import Numeral from '~core/Numeral';
 
 import styles from './ColonyFee.css';
+
+const MSG = defineMessages({
+  colonyFeeText: {
+    id: 'ColonyFee.colonyFeeText',
+    defaultMessage: 'Colony Fee: {amount}',
+  },
+  helpIconTitle: {
+    id: 'ColonyFee.helpIconTitle',
+    defaultMessage: 'Help',
+  },
+});
 
 type Props = {|
   amount: BN | string,
@@ -15,9 +28,21 @@ type Props = {|
 const displayName = 'ColonyFee';
 
 const ColonyFee = ({ amount, symbol }: Props) => (
-  <div className={styles.main}>
-    <Numeral value={amount} suffix={` ${symbol}`} />
-  </div>
+  <>
+    <div className={styles.amount}>
+      <FormattedMessage
+        {...MSG.colonyFeeText}
+        values={{ amount: <Numeral value={amount} suffix={` ${symbol}`} /> }}
+      />
+    </div>
+    <div className={styles.help}>
+      <Icon
+        appearance={{ size: 'small', theme: 'invert' }}
+        name="question-mark"
+        title={MSG.helpIconTitle}
+      />
+    </div>
+  </>
 );
 
 ColonyFee.displayName = displayName;
