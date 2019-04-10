d3b276fdf86321dde66eb522db92ca07124c510a
Curtis Olson <curtis@colony.io>
2019-04-10 12:03:03 -0500

    Add base `ColonyFee` core component

---
+++ src/modules/core/components/ColonyFee/ColonyFee.jsx
@@ -0,0 +1,11 @@
+/* @flow */
+
+import React from 'react';
+
+const displayName = 'ColonyFee';
+
+const ColonyFee = () => <div />;
+
+ColonyFee.displayName = displayName;
+
+export default ColonyFee;
