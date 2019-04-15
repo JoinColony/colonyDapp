947fd1638ac9f9c5e978e98a04258aa41320f231
Curtis Olson <curtis@colony.io>
2019-04-15 16:48:12 -0500

    Add `feeInverse` to network props

--- src/immutable/state/core/Network.js
+++ src/immutable/state/core/Network.js
@@ -1,14 +1,17 @@
 /* @flow */
 
 import type { RecordOf, RecordFactory } from 'immutable';
+import type BigNumber from 'bn.js';
 
 import { Record } from 'immutable';
 
 export type NetworkProps = {
+  feeInverse?: BigNumber,
   version?: number,
 };
 
 const defaultValues: $Shape<NetworkProps> = {
+  feeInverse: undefined,
   version: undefined,
 };
 
