73f7e1c40c3e507774d0b8d37b04dc3080ff29a1
Curtis Olson <curtis@colony.io>
2019-04-15 16:47:47 -0500

    Add network fee fetch saga

--- src/modules/core/sagas/network.js
+++ src/modules/core/sagas/network.js
@@ -10,6 +10,24 @@ import { CONTEXT, getContext } from '~context';
 import { ACTIONS } from '~redux';
 import { putError } from '~utils/saga/effects';
 
+function* networkFeeFetch(): Saga<void> {
+  try {
+    const colonyManager = yield* getContext(CONTEXT.COLONY_MANAGER);
+
+    const { feeInverse } = yield call([
+      colonyManager.networkClient.getFeeInverse,
+      colonyManager.networkClient.getFeeInverse.call,
+    ]);
+
+    yield put<Action<typeof ACTIONS.NETWORK_FETCH_FEE_SUCCESS>>({
+      type: ACTIONS.NETWORK_FETCH_FEE_SUCCESS,
+      payload: { feeInverse },
+    });
+  } catch (error) {
+    yield putError(ACTIONS.NETWORK_FETCH_FEE_ERROR, error);
+  }
+}
+
 function* networkVersionFetch(): Saga<void> {
   try {
     const colonyManager = yield* getContext(CONTEXT.COLONY_MANAGER);
@@ -29,5 +47,6 @@ function* networkVersionFetch(): Saga<void> {
 }
 
 export default function* networkSagas(): Saga<void> {
+  yield takeLatest(ACTIONS.NETWORK_FETCH_FEE, networkFeeFetch);
   yield takeLatest(ACTIONS.NETWORK_FETCH_VERSION, networkVersionFetch);
 }
