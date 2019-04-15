0194e7f947902ad75c5b6369dfbd0d40b6553eab
Curtis Olson <curtis@colony.io>
2019-04-15 16:47:19 -0500

    Add network fee redux action types

--- src/redux/types/actions/network.js
+++ src/redux/types/actions/network.js
@@ -7,6 +7,15 @@ import type { WithKeyPathDepth1 } from '~types';
 import { ACTIONS } from '../../index';
 
 export type NetworkActionTypes = {|
+  NETWORK_FETCH_FEE: Action<typeof ACTIONS.NETWORK_FETCH_FEE>,
+  NETWORK_FETCH_FEE_SUCCESS: ActionTypeWithPayload<
+    typeof ACTIONS.NETWORK_FETCH_FEE_SUCCESS,
+    NetworkProps,
+  >,
+  NETWORK_FETCH_FEE_ERROR: ErrorActionType<
+    typeof ACTIONS.NETWORK_FETCH_FEE_ERROR,
+    WithKeyPathDepth1,
+  >,
   NETWORK_FETCH_VERSION: Action<typeof ACTIONS.NETWORK_FETCH_VERSION>,
   NETWORK_FETCH_VERSION_SUCCESS: ActionTypeWithPayload<
     typeof ACTIONS.NETWORK_FETCH_VERSION_SUCCESS,
