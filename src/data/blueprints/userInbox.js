e2a1281ad845346e62b6b2f8e69160df6278d0e4
Raul Glogovetan <raul@glogovetan.com>
2019-04-16 18:29:59 +0300

    Refactor: `userInbox` store as `EventStore` type

--- src/data/blueprints/userInbox.js
+++ src/data/blueprints/userInbox.js
@@ -1,28 +1,14 @@
 /* @flow */
 
-import * as yup from 'yup';
-
 import type { StoreBlueprint } from '~types/index';
 
-import { FeedStore } from '../../lib/database/stores';
+import { EventStore } from '../../lib/database/stores';
 import { getPermissiveStoreAccessController } from '../accessControllers';
-import inboxMessages from '~users/Inbox/messages';
 
 const userInboxStore: StoreBlueprint = {
   getAccessController: getPermissiveStoreAccessController,
   name: 'userInbox',
-  /*
-   * @TODO Add the relevant props, only as they become necessary, otherwise
-   * this will become even more confusing
-   */
-  schema: yup.object({
-    event: yup.string().oneOf(Object.keys(inboxMessages)),
-    user: yup.string(),
-    task: yup.string(),
-    comment: yup.string(),
-    colony: yup.string(),
-  }),
-  type: FeedStore,
+  type: EventStore,
 };
 
 export default userInboxStore;
