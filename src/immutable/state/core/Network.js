0a47be3e1bbb65b8a62b6b682db5ab6afea36566
Curtis Olson <curtis@colony.io>
2019-04-19 10:34:43 -0500

    Add comments for `fee` & `feeInverse`

--- src/immutable/state/core/Network.js
+++ src/immutable/state/core/Network.js
@@ -5,7 +5,15 @@ import type { RecordOf, RecordFactory } from 'immutable';
 import { Record } from 'immutable';
 
 export type NetworkProps = {
+  /*
+   * Current network fee, calculated using `feeInverse`.
+   * If the current fee is 1%, this will be `.01`.
+   */
   fee?: number,
+  /*
+   * Network fee inverse as defined by the ColonyNetwork contract.
+   * If the current fee is 1%, this will be `100`.
+   */
   feeInverse?: number,
   version?: number,
 };
