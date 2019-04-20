03a8dbb165a626cfd950f45e4619d5d5a5652148
Curtis Olson <curtis@colony.io>
2019-04-19 19:49:12 -0500

    Add fee & version constants

--- src/modules/core/constants.js
+++ src/modules/core/constants.js
@@ -5,6 +5,8 @@ export const CORE_TRANSACTIONS = 'transactions';
 export const CORE_TRANSACTIONS_LIST = 'list';
 export const CORE_GAS_PRICES = 'gasPrices';
 export const CORE_NETWORK = 'network';
+export const CORE_NETWORK_FEE = 'fee';
+export const CORE_NETWORK_VERSION = 'version';
 export const CORE_IPFS_DATA = 'ipfsData';
 
 export {
