d8fcd6e8224c83a8b4dad7b796f97885ce51c358
James Lefrère <james@colony.io>
2019-04-23 13:37:14 +0200

    Fix ENS prop name

--- src/modules/dashboard/sagas/shared.js
+++ src/modules/dashboard/sagas/shared.js
@@ -9,25 +9,21 @@ import { CONTEXT, getContext } from '~context';
 import type { Address, ENSName } from '~types';
 
 export function* getColonyAddress(colonyName: ENSName): Saga<?string> {
-  const ensCache = yield* getContext(CONTEXT.ENS_INSTANCE);
+  const ens = yield* getContext(CONTEXT.ENS_INSTANCE);
   const { networkClient } = yield* getContext(CONTEXT.COLONY_MANAGER);
 
   return yield call(
-    [ensCache, ensCache.getAddress],
-    ensCache.constructor.getFullDomain('colony', colonyName),
+    [ens, ens.getAddress],
+    ens.constructor.getFullDomain('colony', colonyName),
     networkClient,
   );
 }
 
 export function* getColonyName(colonyAddress: Address): Saga<?string> {
-  const ensCache = yield* getContext(CONTEXT.ENS_INSTANCE);
+  const ens = yield* getContext(CONTEXT.ENS_INSTANCE);
   const { networkClient } = yield* getContext(CONTEXT.COLONY_MANAGER);
 
-  const domain = yield call(
-    [ensCache, ensCache.getDomain],
-    colonyAddress,
-    networkClient,
-  );
+  const domain = yield call([ens, ens.getDomain], colonyAddress, networkClient);
   const [colonyName] = domain.split('.');
   return colonyName;
 }
