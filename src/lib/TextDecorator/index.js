e731149b71aa7c6e49b45e18c8c7c7fbda38ebb8
Christian Maniewski <chmanie@fastmail.com>
2019-04-23 22:26:58 +0800

    Add usernameMatcher to get usernames out of text blobs

--- src/lib/TextDecorator/index.js
+++ src/lib/TextDecorator/index.js
@@ -4,6 +4,8 @@ import TextDecorator from './TextDecorator';
 
 import { USERNAME_SCHEMA } from './schemas';
 
+export { default as usernameMatcher } from './usernameMatcher';
+
 TextDecorator.loadSchema(USERNAME_SCHEMA);
 
 export default TextDecorator;
