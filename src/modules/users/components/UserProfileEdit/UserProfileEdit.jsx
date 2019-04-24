b9493c23d83e2447a5e97e20f6eea3c7dca5a9d4
James Lefrère <james@colony.io>
2019-04-24 13:34:57 +0200

    Use user selector
    * Replace `connect` usage with the `useSelector` effect in `UserProfileEdit`

--- src/modules/users/components/UserProfileEdit/UserProfileEdit.jsx
+++ src/modules/users/components/UserProfileEdit/UserProfileEdit.jsx
@@ -2,12 +2,10 @@
 
 import React from 'react';
 import { defineMessages } from 'react-intl';
-import { connect } from 'react-redux';
-import compose from 'recompose/compose';
 
-import type { UserType } from '~immutable';
+import { ACTIONS } from '~redux';
+import { useSelector } from '~utils/hooks';
 
-import { withImmutablePropsToJS } from '~utils/hoc';
 import CopyableAddress from '~core/CopyableAddress';
 import UserMention from '~core/UserMention';
 import Heading from '~core/Heading';
@@ -21,10 +19,8 @@ import {
 } from '~core/Fields';
 import Button from '~core/Button';
 import ProfileTemplate from '~pages/ProfileTemplate';
-import { ACTIONS } from '~redux';
 
 import { userProfile as userProfileStoreBlueprint } from '../../../../data/blueprints';
-
 import { currentUserSelector } from '../../selectors';
 
 import styles from './UserProfileEdit.css';
@@ -62,92 +58,86 @@ const MSG = defineMessages({
   },
 });
 
-type Props = {|
-  user: UserType,
-|};
-
 const displayName = 'users.UserProfileEdit';
 
-const UserProfileEdit = ({ user }: Props) => (
-  <ProfileTemplate
-    appearance={{ theme: 'alt' }}
-    asideContent={<Sidebar user={user} />}
-  >
-    <Heading
-      appearance={{ theme: 'dark', size: 'medium' }}
-      text={MSG.heading}
-    />
-    <ActionForm
-      submit={ACTIONS.USER_PROFILE_UPDATE}
-      success={ACTIONS.USER_PROFILE_UPDATE_SUCCESS}
-      error={ACTIONS.USER_PROFILE_UPDATE_ERROR}
-      initialValues={{
-        displayName: user.profile.displayName,
-        bio: user.profile.bio,
-        website: user.profile.website,
-        location: user.profile.location,
-      }}
-      validationSchema={userProfileStoreBlueprint.schema}
+const UserProfileEdit = () => {
+  const user = useSelector(currentUserSelector);
+  return (
+    <ProfileTemplate
+      appearance={{ theme: 'alt' }}
+      asideContent={<Sidebar user={user} />}
     >
-      {({ status, isSubmitting }) => (
-        <div>
-          <FieldSet>
-            <InputLabel label={MSG.labelWallet} />
-            <CopyableAddress appearance={{ theme: 'big' }} full>
-              {user.profile.walletAddress}
-            </CopyableAddress>
-          </FieldSet>
-          <FieldSet>
-            <InputLabel label={MSG.labelUsername} />
-            <UserMention
-              username={user.profile.username || user.profile.walletAddress}
-              hasLink={false}
-              data-test="userProfileUsername"
-            />
-          </FieldSet>
-          <FieldSet className={styles.inputFieldSet}>
-            <Input
-              label={MSG.labelName}
-              name="displayName"
-              data-test="userSettingsName"
-            />
-            <Textarea
-              label={MSG.labelBio}
-              name="bio"
-              maxLength={160}
-              data-test="userSettingsBio"
-            />
-            <Input
-              label={MSG.labelWebsite}
-              name="website"
-              data-test="userSettingsWebsite"
-            />
-            <Input
-              label={MSG.labelLocation}
-              name="location"
-              data-test="userSettingsLocation"
-            />
-          </FieldSet>
-          <FieldSet>
-            <Button
-              type="submit"
-              text={{ id: 'button.save' }}
-              loading={isSubmitting}
-              data-test="userSettingsSubmit"
-            />
-          </FieldSet>
-          <FormStatus status={status} />
-        </div>
-      )}
-    </ActionForm>
-  </ProfileTemplate>
-);
+      <Heading
+        appearance={{ theme: 'dark', size: 'medium' }}
+        text={MSG.heading}
+      />
+      <ActionForm
+        submit={ACTIONS.USER_PROFILE_UPDATE}
+        success={ACTIONS.USER_PROFILE_UPDATE_SUCCESS}
+        error={ACTIONS.USER_PROFILE_UPDATE_ERROR}
+        initialValues={{
+          displayName: user.profile.displayName,
+          bio: user.profile.bio,
+          website: user.profile.website,
+          location: user.profile.location,
+        }}
+        validationSchema={userProfileStoreBlueprint.schema}
+      >
+        {({ status, isSubmitting }) => (
+          <div>
+            <FieldSet>
+              <InputLabel label={MSG.labelWallet} />
+              <CopyableAddress appearance={{ theme: 'big' }} full>
+                {user.profile.walletAddress}
+              </CopyableAddress>
+            </FieldSet>
+            <FieldSet>
+              <InputLabel label={MSG.labelUsername} />
+              <UserMention
+                username={user.profile.username || user.profile.walletAddress}
+                hasLink={false}
+                data-test="userProfileUsername"
+              />
+            </FieldSet>
+            <FieldSet className={styles.inputFieldSet}>
+              <Input
+                label={MSG.labelName}
+                name="displayName"
+                data-test="userSettingsName"
+              />
+              <Textarea
+                label={MSG.labelBio}
+                name="bio"
+                maxLength={160}
+                data-test="userSettingsBio"
+              />
+              <Input
+                label={MSG.labelWebsite}
+                name="website"
+                data-test="userSettingsWebsite"
+              />
+              <Input
+                label={MSG.labelLocation}
+                name="location"
+                data-test="userSettingsLocation"
+              />
+            </FieldSet>
+            <FieldSet>
+              <Button
+                type="submit"
+                text={{ id: 'button.save' }}
+                loading={isSubmitting}
+                data-test="userSettingsSubmit"
+              />
+            </FieldSet>
+            <FormStatus status={status} />
+          </div>
+        )}
+      </ActionForm>
+    </ProfileTemplate>
+  );
+};
 
 UserProfileEdit.displayName = displayName;
 
-export default compose(
-  connect(state => ({
-    user: currentUserSelector(state),
-  })),
-  withImmutablePropsToJS,
-)(UserProfileEdit);
+export default UserProfileEdit;
