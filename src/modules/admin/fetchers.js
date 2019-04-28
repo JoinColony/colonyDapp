0d3261c6d194fbb112e5af30897821ad4ccdc547
James Lefrère <james@colony.io>
2019-04-28 15:01:40 +0200

    Freeze fetchers

--- src/modules/admin/fetchers.js
+++ src/modules/admin/fetchers.js
@@ -9,14 +9,14 @@ import {
   fetchColonyUnclaimedTransactions,
 } from './actionCreators';
 
-export const colonyTransactionsFetcher = {
+export const colonyTransactionsFetcher = Object.freeze({
   select: colonyTransactionsSelector,
   fetch: fetchColonyTransactions,
   ttl: 1000 * 60, // 1 minute
-};
+});
 
-export const colonyUnclaimedTransactionsFetcher = {
+export const colonyUnclaimedTransactionsFetcher = Object.freeze({
   select: colonyUnclaimedTransactionsSelector,
   fetch: fetchColonyUnclaimedTransactions,
   ttl: 1000 * 60, // 1 minute
-};
+});
