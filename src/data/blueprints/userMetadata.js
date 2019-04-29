8a430fe5f493780d30d09bc3eaaf88626e6de32e
Thiago Delgado <thiagodelgado111@gmail.com>
2019-04-29 15:24:09 -0300

    Freeze!
    *  Freeze store blueprints

--- src/data/blueprints/userMetadata.js
+++ src/data/blueprints/userMetadata.js
@@ -4,10 +4,10 @@ import type { StoreBlueprint } from '~types/index';
 import { EventStore } from '../../lib/database/stores';
 import { getEthereumWalletStoreAccessController } from '../accessControllers';
 
-const userMetadataStore: StoreBlueprint = {
+const userMetadataStore: StoreBlueprint = Object.freeze({
   getAccessController: getEthereumWalletStoreAccessController,
   defaultName: 'userMetadata',
   type: EventStore,
-};
+});
 
 export default userMetadataStore;
