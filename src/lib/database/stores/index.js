3ecdc066d705d04857b7a6d1c07869365cd8a97a
Thiago Delgado <thiagodelgado111@gmail.com>
2019-05-01 14:22:49 -0300

    Remove unused orbit store wrappers

--- src/lib/database/stores/index.js
+++ src/lib/database/stores/index.js
@@ -1,6 +1,4 @@
 /* @flow */
 
-export { default as DocStore } from './DocStore';
 export { default as EventStore } from './EventStore';
-export { default as FeedStore } from './FeedStore';
 export { default as Store } from './Store';
--- src/lib/database/types/index.js
+++ src/lib/database/types/index.js
@@ -10,9 +10,6 @@ export type { AccessController, Entry };
 export type { Identity, IdentityObject } from './Identity';
 export type { IdentityProvider } from './IdentityProvider';
 export type { OrbitDBStore } from './OrbitDBStore';
-export type { OrbitDBKVStore } from './OrbitDBKVStore';
-export type { OrbitDBDocStore, QueryFunction } from './OrbitDBDocStore';
-export type { FeedIteratorOptions, OrbitDBFeedStore } from './OrbitDBFeedStore';
 export type {
   EventIteratorOptions,
   OrbitDBEventStore,
