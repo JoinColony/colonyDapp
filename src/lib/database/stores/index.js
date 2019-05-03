8177f9c2b4bfd0c9d5bf558bf6d71a7e97542b35
James Lefrère <james@colony.io>
2019-05-03 10:32:06 +0200

    Remove KVStores
    * Remove `KVStore` and `ValidatedKVStore`
    * Remove `commands` (now redundant)

--- src/lib/database/stores/index.js
+++ src/lib/database/stores/index.js
@@ -3,6 +3,4 @@
 export { default as DocStore } from './DocStore';
 export { default as EventStore } from './EventStore';
 export { default as FeedStore } from './FeedStore';
-export { default as KVStore } from './KVStore';
 export { default as Store } from './Store';
-export { default as ValidatedKVStore } from './ValidatedKVStore';
