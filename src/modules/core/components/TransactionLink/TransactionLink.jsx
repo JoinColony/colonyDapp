98aa692f70df012547b5d120b54104a56d6491db
Curtis Olson <curtis@colony.io>
2019-05-07 14:47:39 -0500

    Add `TransactionLink` component

---
+++ src/modules/core/components/TransactionLink/TransactionLink.jsx
@@ -0,0 +1,50 @@
+/* @flow */
+
+import type { MessageDescriptor, MessageValues } from 'react-intl';
+
+import React from 'react';
+
+import ExternalLink from '~core/ExternalLink';
+
+type Props = {|
+  /*
+   * Allows for link style customization (Eg: we need to disquise the link as a button)
+   * Don't abuse it!
+   */
+  className?: string,
+  /** Transaction hash */
+  hash: string,
+  /** Optionally override current network */
+  network?: string,
+  /** A string or a `messageDescriptor` that make up the link's text. Defaults to `hash`. */
+  text?: MessageDescriptor | string,
+  /** Values for text (react-intl interpolation) */
+  textValues?: MessageValues,
+|};
+
+const displayName = 'TransactionLink';
+
+const TransactionLink = ({
+  className,
+  hash,
+  network = process.env.NETWORK || 'homestead',
+  text,
+  textValues,
+}: Props) => {
+  const linkText = text || hash;
+  const tld = network === 'tobalaba' ? 'com' : 'io';
+  const networkSubdomain = network === 'homestead' ? '' : `${network}.`;
+  const href = `https://${networkSubdomain}etherscan.${tld}/tx/${hash}`;
+  return (
+    <ExternalLink
+      className={className}
+      href={href}
+      text={linkText}
+      textValues={textValues}
+    />
+  );
+};
+
+TransactionLink.displayName = displayName;
+
+export default TransactionLink;
---
+++ src/modules/core/components/TransactionLink/TransactionLink.md
@@ -0,0 +1,16 @@
+Link to an external resource to provide extra information about a particular transaction.
+
+### Normal transaction link
+
+```jsx
+<TransactionLink hash="0xc28c829e279eeff017b3618cc593cddc2478c9288f9450052abe8b2ff901be63" />
+```
+
+### Transaction link with custom text
+
+```jsx
+<TransactionLink
+  hash="0xc28c829e279eeff017b3618cc593cddc2478c9288f9450052abe8b2ff901be63"
+  text="This is the link text"
+/>
+```
