ffb80a8db2f8778705d3f071b07c8540d87167da
Raul Glogovetan <raul@glogovetan.com>
2019-05-13 17:25:02 +0300

    Fix: notifications transform now uses event constants

--- src/modules/users/data/utils.js
+++ src/modules/users/data/utils.js
@@ -3,6 +3,12 @@
 import type { UserMetadataStore } from '~data/types';
 import { USER_EVENT_TYPES } from '~data/constants';
 import { getUserTokensReducer } from './reducers';
+import {
+  NOTIFICATION_EVENT_ADMIN_ADDED,
+  NOTIFICATION_EVENT_ADMIN_REMOVED,
+  NOTIFICATION_EVENT_COLONY_ENS_CREATED,
+  NOTIFICATION_EVENT_DOMAIN_ADDED,
+} from '~users/Inbox/events';
 
 const { TOKEN_ADDED, TOKEN_REMOVED } = USER_EVENT_TYPES;
 
@@ -14,10 +20,10 @@ export const getUserTokenAddresses = (metadataStore: UserMetadataStore) =>
 
 export const transformNotificationEventNames = (eventName: string): string => {
   const notificationsToEventsMapping = {
-    ColonyAdminRoleSet: 'notificationAdminOtherAdded',
-    ColonyAdminRoleRemoved: 'notificationAdminOtherRemoved',
-    ColonyLabelRegistered: 'notificationAdminENSCreated',
-    DomainAdded: 'notificationAdminColonyLabelAdded',
+    ColonyAdminRoleSet: NOTIFICATION_EVENT_ADMIN_ADDED,
+    ColonyAdminRoleRemoved: NOTIFICATION_EVENT_ADMIN_REMOVED,
+    ColonyLabelRegistered: NOTIFICATION_EVENT_COLONY_ENS_CREATED,
+    DomainAdded: NOTIFICATION_EVENT_DOMAIN_ADDED,
   };
   return notificationsToEventsMapping[eventName];
 };
