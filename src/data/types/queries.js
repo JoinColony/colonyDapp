943ff40451dd5762bcd0ffd349c413fdbc4db3d0
James Lefrère <james@colony.io>
2019-05-23 16:52:46 +0200

    Define `Subscription`
    * Define a `Subscription` type, akin to `Query` and `Command`

--- src/data/types/queries.js
+++ src/data/types/queries.js
@@ -26,6 +26,31 @@ export type Query<D, M, A, R> = {|
 |};
 
 /*
+ * The specification for a data subscription.
+ *
+ * D: Dependencies the query depend on to be executed. Those will be
+ * passed to execute via prepare, which uses the context to set them up.
+ *
+ * M: Metadata needed to locate the right store, colony contract or dependency
+ * in general
+ *
+ * A: Optional type for the arguments the subscribe function will
+ * will be called with.
+ *
+ * R: Argument for the given event emitter, usually Event type(s).
+ */
+export type Subscription<D, M, A, R> = {|
+  context: Array<ContextName>,
+  subscribe: (
+    deps: D,
+    args: ?A,
+    emitter: (R) => void,
+  ) => {| close: () => void |}[],
+  name: string,
+  prepare: (context: *, metadata: M) => Promise<D>,
+|};
+
+/*
  * This type, when assigned to a reducer, ensures that the actions specified
  * exist in `ActionsType`, and that the action objects are fully typed.
  *
