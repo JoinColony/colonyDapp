fd1a6a10faa2dadcec4c5eefafaccca6ef500bb3
Raul Glogovetan <raul@glogovetan.com>
2019-05-29 16:13:59 +0300

    Add: initial `messages` selectors

---
+++ src/modules/core/selectors/messages.js
@@ -0,0 +1,19 @@
+/* @flow */
+
+import { Map as ImmutableMap } from 'immutable';
+
+// import { createSelector } from 'reselect';
+
+import type { RootStateRecord } from '~immutable';
+
+import {
+  CORE_NAMESPACE as ns,
+  CORE_MESSAGES,
+  CORE_MESSAGES_LIST,
+} from '../constants';
+
+export const messageById = (state: RootStateRecord, id: string) =>
+  state.getIn([ns, CORE_MESSAGES, CORE_MESSAGES_LIST, id]);
+
+export const getAllMessages = (state: RootStateRecord) =>
+  state.getIn([ns, CORE_MESSAGES, CORE_MESSAGES_LIST], ImmutableMap());
