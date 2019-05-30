0c414804974ecb2e6432ac89dea9861ffee58834
Raul Glogovetan <raul@glogovetan.com>
2019-05-30 19:12:31 +0300

    Add: initial `MessageCard` subcomponent to gas station

---
+++ src/modules/users/components/GasStation/MessageCard/MessageCard.jsx
@@ -0,0 +1,33 @@
+/* @flow */
+
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import Heading from '~core/Heading';
+import Card from '~core/Card';
+
+import styles from './MessageCard.css';
+
+const MessageCard = () => (
+  <Card className={styles.main}>
+    <button type="button" className={styles.button}>
+      <div className={styles.summary}>
+        <div className={styles.description}>
+          <Heading
+            appearance={{ theme: 'dark', size: 'normal', margin: 'none' }}
+            text={{ id: 'message.generic.title' }}
+          />
+          <FormattedMessage id="message.generic.description" />
+        </div>
+        {/*
+         * @TODO Implement the message status UI
+         */}
+        <div className={styles.statusPlaceholder}>
+          <span />
+        </div>
+      </div>
+    </button>
+  </Card>
+);
+
+export default MessageCard;
