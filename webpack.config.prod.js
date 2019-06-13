621899ff9754d5d180090422e2c1d62f82fc678a
Raul Glogovetan <raul@glogovetan.com>
2019-06-13 23:35:44 +0300

    Add: rough webpack production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,31 @@
+const webpackBaseConfig = require('./webpack.config');
+
+/*
+ * Get the base config object, so we can modify it.
+ */
+const webpackProdConfig = webpackBaseConfig();
+/*
+ * Set mode to `production` to minify the codebase
+ */
+webpackProdConfig.mode = 'production';
+/*
+ * Remove the HMR plugins since we won't be needing it
+ */
+webpackProdConfig.plugins.pop();
+/*
+ * Remove the dev server since we won't be needing it
+ */
+delete webpackProdConfig.devServer;
+/*
+ * Add chunk splitting optimization
+ * This is not employing any code splitting or tree shaking ...yet
+ * This was necessary to avoid a `node` memmory leak
+ * See: https://github.com/webpack/webpack-dev-server/issues/1433#issuecomment-473342612
+ */
+webpackProdConfig.optimization = {
+  splitChunks: {
+    chunks: 'all',
+  },
+};
+
+module.exports = () => webpackProdConfig;
