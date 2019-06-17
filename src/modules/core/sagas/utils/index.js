151f36fb21de61bda3b69394d7bebcf1f01855ad
Raul Glogovetan <raul@glogovetan.com>
2019-06-17 20:25:10 +0300

    Remove: unused `getProvider` saga util

--- src/modules/core/sagas/utils/index.js
+++ src/modules/core/sagas/utils/index.js
@@ -4,5 +4,4 @@ export { default as getColonyManager } from './getColonyManager';
 export { default as getDDB } from './getDDB';
 export { default as getGasPrices } from './getGasPrices';
 export { default as getMethod } from './getMethod';
-export { getProvider, defaultNetwork } from './getProvider';
 export { default as getWalletType } from './getWalletType';
