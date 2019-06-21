e70f8a704103d05cbfe78e77cc8576470d4fdcb4
James Lefrère <james@colony.io>
2019-06-21 14:34:45 +0200

    Split `colonyCreate` saga
    * Split off the `colonyCreate` saga into a separate file, because it's huge

--- src/modules/dashboard/sagas/index.js
+++ src/modules/dashboard/sagas/index.js
@@ -6,6 +6,7 @@ import { all, call } from 'redux-saga/effects';
 
 import rolesSagas from './roles';
 import colonySagas from './colony';
+import colonyCreateSaga from './colonyCreate';
 import domainsSagas from './domains';
 import taskSagas from './task';
 import tokenSagas from './token';
@@ -14,6 +15,7 @@ export default function* setupDashboardSagas(): Saga<void> {
   yield all([
     call(rolesSagas),
     call(colonySagas),
+    call(colonyCreateSaga),
     call(domainsSagas),
     call(taskSagas),
     call(tokenSagas),
