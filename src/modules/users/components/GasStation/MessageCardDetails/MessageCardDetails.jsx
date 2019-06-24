37712b8b1c2ea5131d970b1be2afd639e88a7777
Raul Glogovetan <raul@glogovetan.com>
2019-06-24 11:31:02 +0300

    Recfactor: `MessageCardDetails` as stateless, with hooks

--- src/modules/users/components/GasStation/MessageCardDetails/MessageCardDetails.jsx
+++ src/modules/users/components/GasStation/MessageCardDetails/MessageCardDetails.jsx
@@ -1,10 +1,14 @@
 /* @flow */
 
-import React, { Component, Fragment } from 'react';
-import { defineMessages, FormattedMessage } from 'react-intl';
-
 import type { MessageProps } from '~immutable';
 import type { Action } from '~redux';
+
+/*
+ * Due to our version of Flow no knowing about the existence of React Hooks
+ */
+// $FlowFixMe
+import React, { Fragment, useCallback, useState } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
 
 import CardList from '~core/CardList';
 import Heading from '~core/Heading';
@@ -30,114 +34,94 @@ const MSG = defineMessages({
 });
 
 type Props = {|
-  message: $ReadOnly<MessageProps>,
+  message: MessageProps,
   onClose: (event: SyntheticMouseEvent<HTMLButtonElement>) => void,
   cancelMessage: (id: string) => Action<typeof ACTIONS.MESSAGE_CANCEL>,
 |};
 
-type State = {|
-  isShowingCancelConfirmation: boolean,
-|};
+const displayName = 'users.GasStation.MessageCardDetails';
 
-class MessageCardDetails extends Component<Props, State> {
-  static displayName = 'users.GasStation.MessageCardDetails';
-
-  state = {
-    isShowingCancelConfirmation: false,
-  };
-
-  cancelMessage = () => {
-    const {
-      message: { id },
-      cancelMessage,
-    } = this.props;
-    cancelMessage(id);
-  };
-
-  toggleCancelConfirmation = () =>
-    this.setState(({ isShowingCancelConfirmation }) => ({
-      isShowingCancelConfirmation: !isShowingCancelConfirmation,
-    }));
-
-  renderCancel() {
-    const { isShowingCancelConfirmation } = this.state;
-    return (
-      <div className={styles.confirmationButtonsWrapper}>
-        {isShowingCancelConfirmation ? (
-          <Fragment>
-            <button
-              type="button"
-              className={styles.confirmationButton}
-              onClick={this.cancelMessage}
-            >
-              <FormattedMessage {...{ id: 'button.yes' }} />
-            </button>
-            <span className={styles.cancelDecision}>/</span>
-            <button
-              type="button"
-              className={styles.confirmationButton}
-              onClick={this.toggleCancelConfirmation}
-            >
-              <FormattedMessage {...{ id: 'button.no' }} />
-            </button>
-          </Fragment>
-        ) : (
-          <button
-            type="button"
-            className={styles.cancelButton}
-            onClick={this.toggleCancelConfirmation}
+const MessageCardDetails = ({
+  message: { status, purpose, message: messageContent, id },
+  message,
+  cancelMessage,
+  onClose,
+}: Props) => {
+  const [isShowingCancelConfirmation, setShowCancelConfirmation] = useState(
+    false,
+  );
+  const cancelMessageSigning = useCallback(() => cancelMessage(id), [
+    cancelMessage,
+    id,
+  ]);
+  const canBeCancelled = status === 'created';
+  return (
+    <div>
+      <TransactionBackToList onClose={onClose} />
+      <CardList appearance={{ numCols: '1' }}>
+        <Card className={styles.main}>
+          <div className={styles.summary}>
+            <div className={styles.description}>
+              <Heading
+                appearance={{ theme: 'dark', size: 'normal', margin: 'none' }}
+                text={{ id: `message.${purpose}.title` }}
+              />
+              <FormattedMessage id={`message.${purpose}.description`} />
+            </div>
+            <MessageCardStatus status={status} />
+          </div>
+          <div
+            className={getMainClasses({ theme: 'message' }, styles, {
+              failed: status === 'failed',
+              succeeded: status === 'succeeded',
+              isShowingCancelConfirmation,
+            })}
           >
-            <FormattedMessage {...{ id: 'button.cancel' }} />
-          </button>
-        )}
-      </div>
-    );
-  }
+            <span className={styles.title}>
+              <FormattedMessage {...MSG.messageHeading} />
+              {canBeCancelled && (
+                <div className={styles.confirmationButtonsWrapper}>
+                  {isShowingCancelConfirmation ? (
+                    <Fragment>
+                      <button
+                        type="button"
+                        className={styles.confirmationButton}
+                        onClick={cancelMessageSigning}
+                      >
+                        <FormattedMessage {...{ id: 'button.yes' }} />
+                      </button>
+                      <span className={styles.cancelDecision}>/</span>
+                      <button
+                        type="button"
+                        className={styles.confirmationButton}
+                        onClick={() => setShowCancelConfirmation(false)}
+                      >
+                        <FormattedMessage {...{ id: 'button.no' }} />
+                      </button>
+                    </Fragment>
+                  ) : (
+                    <button
+                      type="button"
+                      className={styles.cancelButton}
+                      onClick={() => setShowCancelConfirmation(true)}
+                    >
+                      <FormattedMessage {...{ id: 'button.cancel' }} />
+                    </button>
+                  )}
+                </div>
+              )}
+            </span>
+            {messageContent}
+          </div>
+        </Card>
+      </CardList>
+      {(status === 'created' || status === 'pending') && (
+        <MessageCardControls message={message} />
+      )}
+    </div>
+  );
+};
 
-  render() {
-    const {
-      message: { status, purpose, message: messageContent },
-      message,
-      onClose,
-    } = this.props;
-    const { isShowingCancelConfirmation } = this.state;
-    const canBeCancelled = status === 'created';
-    return (
-      <div>
-        <TransactionBackToList onClose={onClose} />
-        <CardList appearance={{ numCols: '1' }}>
-          <Card className={styles.main}>
-            <div className={styles.summary}>
-              <div className={styles.description}>
-                <Heading
-                  appearance={{ theme: 'dark', size: 'normal', margin: 'none' }}
-                  text={{ id: `message.${purpose}.title` }}
-                />
-                <FormattedMessage id={`message.${purpose}.description`} />
-              </div>
-              <MessageCardStatus status={status} />
-            </div>
-            <div
-              className={getMainClasses({ theme: 'message' }, styles, {
-                failed: status === 'failed',
-                succeeded: status === 'succeeded',
-                isShowingCancelConfirmation,
-              })}
-            >
-              <span className={styles.title}>
-                <FormattedMessage {...MSG.messageHeading} />
-                {canBeCancelled && this.renderCancel()}
-              </span>
-              {messageContent}
-            </div>
-          </Card>
-        </CardList>
-        {(status === 'created' || status === 'pending') && (
-          <MessageCardControls message={message} />
-        )}
-      </div>
-    );
-  }
-}
+MessageCardDetails.displayName = displayName;
 
 export default MessageCardDetails;
