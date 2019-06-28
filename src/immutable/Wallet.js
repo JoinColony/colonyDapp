b5a55e6a6d4b62ceb7924f18dbd797a090e0972f
Raul Glogovetan <raul@glogovetan.com>
2019-06-28 15:24:34 +0300

    Add: immutable `Wallet` wallet types object

--- src/immutable/Wallet.js
+++ src/immutable/Wallet.js
@@ -6,11 +6,22 @@ import { Record } from 'immutable';
 
 import type { Address } from '~types';
 
+export const WALLET_TYPES = Object.freeze({
+  SOFTWARE: 'software',
+  METAMASK: 'metamask',
+  HARDWARE: 'hardware',
+});
+
+/*
+ * Yeah... I suck at naming things apparently
+ */
+export type WalletTypesType = $Values<typeof WALLET_TYPES>;
+
 export type WalletProps = {|
   availableAddresses?: Address[],
   currentAddress?: Address,
   isLoading: boolean,
-  walletType: 'software' | 'metamask' | 'hardware',
+  walletType: WalletTypesType,
 |};
 
 export type WalletPropsJS = $ReadOnly<WalletProps>;
@@ -21,7 +32,7 @@ const defaultValues: $Shape<WalletProps> = {
   availableAddresses: [],
   currentAddress: undefined,
   isLoading: undefined,
-  walletType: 'software',
+  walletType: WALLET_TYPES.SOFTWARE,
 };
 
 const WalletRecord: RecordFactory<WalletProps> = Record(defaultValues);
