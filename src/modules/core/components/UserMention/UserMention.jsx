4c8fdb5ab0c4fcaffa964fd4657fa026021e094d
Raul Glogovetan <raul@glogovetan.com>
2019-07-02 16:17:04 +0300

    Refactor: core `UserMention` to with better default route

--- src/modules/core/components/UserMention/UserMention.jsx
+++ src/modules/core/components/UserMention/UserMention.jsx
@@ -9,24 +9,29 @@ type Props = {
   /** A user's username (ENS) */
   username: string,
   /** Alternate place to link to. Defaults to user profile */
-  to: string,
+  to?: string,
   /** Either just display mention or link to profile or so  */
   hasLink?: boolean,
 };
 
-const UserMention = ({ username, to, hasLink, ...props }: Props) =>
-  hasLink ? (
-    <Link to={to} text={`@${username}`} className={styles.mention} {...props} />
-  ) : (
+const UserMention = ({ username, to, hasLink, ...props }: Props) => {
+  const fallbackTo = to || `/user/${username}`;
+  if (hasLink) {
+    return (
+      <Link
+        to={fallbackTo}
+        text={`@${username}`}
+        className={styles.mention}
+        {...props}
+      />
+    );
+  }
+  return (
     <span className={styles.mention} {...props}>
       {' '}
       {`@${username}`}
     </span>
   );
-
-UserMention.defaultProps = {
-  to: '/',
-  hasLink: true,
 };
 
 export default UserMention;
