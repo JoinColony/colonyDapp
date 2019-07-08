455354c8bcd781a4581c6a577c6c859f0a8ecbe1
James Lefrère <james@colony.io>
2019-07-08 18:09:48 +0100

    Fix profile opener
    * Fix the redirect for the unfinished profile opener

--- src/modules/users/components/UnfinishedProfile/unfinishedProfileOpener.js
+++ src/modules/users/components/UnfinishedProfile/unfinishedProfileOpener.js
@@ -2,17 +2,17 @@
 
 import type { IBrowserHistory } from 'history';
 
-import { CREATE_COLONY_ROUTE } from '~routes';
+import { CREATE_USER_ROUTE } from '~routes';
 
 /**
- * Simple helper function to navigate
- * to create colony route which we would like to have since there's multiple
+ * Simple helper function to navigate to the create user route
+ * which we would like to have since there's multiple
  * different occasions where we would like to do the same thing.
  *
  * @method unfinishedProfileOpener
  *
  */
 const unfinishedProfileOpener = (history: IBrowserHistory) =>
-  history.push(CREATE_COLONY_ROUTE);
+  history.push(CREATE_USER_ROUTE);
 
 export default unfinishedProfileOpener;
