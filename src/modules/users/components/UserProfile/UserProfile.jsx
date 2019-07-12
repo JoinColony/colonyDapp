0260adf51468c49e3f543d2eda993006f24d683c
Raul Glogovetan <raul@glogovetan.com>
2019-07-12 10:37:10 +0300

    Fix: recently created profile show ENS fetch user data

--- src/modules/users/components/UserProfile/UserProfile.jsx
+++ src/modules/users/components/UserProfile/UserProfile.jsx
@@ -9,11 +9,11 @@ import type { Address } from '~types';
 import type { UserType } from '~immutable';
 
 import { NOT_FOUND_ROUTE } from '~routes';
-import { useDataFetcher } from '~utils/hooks';
-
+import { useDataFetcher, useSelector } from '~utils/hooks';
 import { userFetcher } from '../../fetchers';
 import { currentUserColoniesFetcher } from '../../../dashboard/fetchers';
 import { useUserAddressFetcher } from '../../hooks';
+import { walletAddressSelector } from '../../selectors';
 
 import ColonyGrid from '~dashboard/ColonyGrid';
 import ActivityFeed from '~core/ActivityFeed';
@@ -38,11 +38,29 @@ const UserProfile = ({
   const { userAddress, error: userAddressError } = useUserAddressFetcher(
     username,
   );
+  const currentUserWalletAddress = useSelector(walletAddressSelector);
+  /*
+   * We need to overwrite the TTL, at least for the current user.
+   *
+   * This is what's happening in the background:
+   * - User is created (w/o ens name), added to both current and all users
+   * - Ens name is created, added only to current user
+   * - Since the all users values were updated, they will not be refetched upon entering this page (lastFetchedAt)
+   *
+   * If you're realtively fast (under 1 min), if you just created your profile, you won't actually see your ens name, although created.
+   * This is because we fetch data from all users, not current user (this page is general, not private)
+   * Since the all users data was recently updated, the fetcher won't be triggered.
+   *
+   * To solve this, at least for the current user, we'll force it to re-fetch by overridding the TTL with 1
+   */
+  const ttlOverride =
+    currentUserWalletAddress === userAddress ? { ttl: 1 } : {};
 
   const { error: userError, data: user, isFetching } = useDataFetcher<UserType>(
     userFetcher,
     [userAddress],
     [userAddress],
+    ttlOverride,
   );
 
   // Tracked in colonyDapp#1472
@@ -57,8 +75,6 @@ const UserProfile = ({
   }
 
   if (!isFetching && (userError || userAddressError)) {
-    console.log(userError);
-    console.log(userAddressError);
     return <Redirect to={NOT_FOUND_ROUTE} />;
   }
 
