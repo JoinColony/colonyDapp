e80d9c080a307a2e216f56a396eca0bbc2ab85a8
James Lefrère <james@colony.io>
2019-07-17 18:01:37 +0200

    Adjust `getGasPrices`
    * Adjust `getGasPrices` such that the error handling/flow is clearer, and that the saga is as high-level as possible
    * Ensure gas prices are returned when there is an error (at any point)

--- src/modules/core/sagas/utils/getGasPrices.js
+++ src/modules/core/sagas/utils/getGasPrices.js
@@ -1,6 +1,8 @@
 /* @flow */
 
+import type { ColonyNetworkClient } from '@colony/colony-js-client';
 import type { Saga } from 'redux-saga';
+
 import BigNumber from 'bn.js';
 
 import { call, put, select } from 'redux-saga/effects';
@@ -8,6 +10,7 @@ import { call, put, select } from 'redux-saga/effects';
 import type { GasPricesProps } from '~immutable';
 
 import { CONTEXT, getContext } from '~context';
+import { log } from '~utils/debug';
 import { gasPrices as gasPricesSelector } from '../../selectors';
 import { updateGasPrices } from '../../actionCreators';
 
@@ -27,56 +30,69 @@ type EthGasStationAPIResponse = {
 
 const ETH_GAS_STATION_ENDPOINT =
   'https://ethgasstation.info/json/ethgasAPI.json';
+const DEFAULT_GAS_PRICE = '1';
+
+const getNetworkGasPrice = async (
+  networkClient: ColonyNetworkClient,
+): Promise<string> => {
+  const price: BigNumber = await networkClient.adapter.provider.getGasPrice();
+  // Handling the weird ethers BN implementation. Remove when web3
+  return price.toString();
+};
+
+const fetchGasPrices = async (
+  networkClient: ColonyNetworkClient,
+): Promise<GasPricesProps> => {
+  let networkGasPrice = DEFAULT_GAS_PRICE;
+
+  try {
+    networkGasPrice = await getNetworkGasPrice(networkClient);
+
+    const response = await fetch(ETH_GAS_STATION_ENDPOINT);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data: EthGasStationAPIResponse = await response.json();
+
+    // API prices are in 10Gwei, so they need to be normalised
+    const pointOneGwei = new BigNumber(10 ** 8);
+
+    return {
+      timestamp: Date.now(),
+      network: new BigNumber(networkGasPrice),
+
+      suggested: new BigNumber(data.average).mul(pointOneGwei),
+      cheaper: new BigNumber(data.safeLow).mul(pointOneGwei),
+      faster: new BigNumber(data.fast).mul(pointOneGwei),
+
+      suggestedWait: data.avgWait * 60,
+      cheaperWait: data.safeLowWait * 60,
+      fasterWait: data.fastWait * 60,
+    };
+  } catch (caughtError) {
+    log.warn(`Could not get ETH gas prices: ${caughtError.message}`);
+    // Default values
+    return {
+      timestamp: -Infinity, // Do not cache this
+      suggested: new BigNumber(networkGasPrice),
+    };
+  }
+};
 
 export default function* getGasPrices(): Saga<GasPricesProps> {
-  const colonyManager = yield* getContext(CONTEXT.COLONY_MANAGER);
-
   const cachedPrices = yield select(gasPricesSelector);
 
   if (Date.now() - cachedPrices.timestamp < 15 * 60 * 1000) {
     return cachedPrices;
   }
 
-  const networkGasPrice = yield call([
-    colonyManager.networkClient.adapter.provider,
-    colonyManager.networkClient.adapter.provider.getGasPrice,
-  ]);
-  let gasPrices;
+  const { networkClient } = yield* getContext(CONTEXT.COLONY_MANAGER);
 
-  try {
-    const response = yield call(fetch, ETH_GAS_STATION_ENDPOINT);
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    const data: EthGasStationAPIResponse = yield response.json();
+  const gasPrices = yield call(fetchGasPrices, networkClient);
 
-    const pointOneGwei = new BigNumber(10 ** 8);
+  yield put(updateGasPrices(gasPrices));
 
-    gasPrices = {
-      timestamp: Date.now(),
-
-      // Handling the weird ethers BN implementation. Remove when web3
-      network: new BigNumber(networkGasPrice.toString()),
-      // API prices are in 10Gwei
-      suggested: new BigNumber(data.average).mul(pointOneGwei),
-      suggestedWait: data.avgWait * 60,
-      cheaper: new BigNumber(data.safeLow).mul(pointOneGwei),
-      cheaperWait: data.safeLowWait * 60,
-      faster: new BigNumber(data.fast).mul(pointOneGwei),
-      fasterWait: data.fastWait * 60,
-    };
-
-    yield put(updateGasPrices(gasPrices));
-  } catch (e) {
-    console.warn(`Could not get ETH gas prices: ${e.message}`);
-    gasPrices = {
-      // Do not cache this
-      timestamp: -Infinity,
-
-      // Handling the weird ethers BN implementation. Remove when web3
-      suggested: new BigNumber(networkGasPrice.toString()),
-    };
-    yield put(updateGasPrices(gasPrices));
-  }
   return gasPrices;
 }
