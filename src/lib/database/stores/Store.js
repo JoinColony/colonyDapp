32b7a85267af0665c9c0540330942489cf7a8547
Christian Maniewski <chris@colony.io>
2019-07-23 22:32:45 +0200

    Improve weird replication API

--- src/lib/database/stores/Store.js
+++ src/lib/database/stores/Store.js
@@ -45,7 +45,7 @@ class Store {
     );
     // After a write to a store, wait for some time to give replication a chance to be done
     this._orbitStore.events.on('write', () => {
-      this.replicate(true).catch(log.warn);
+      this.deferReplicate();
       if (this._writeTimeout) clearTimeout(this._writeTimeout);
       this._writeTimeout = setTimeout(() => {
         this._writeTimeout = null;
@@ -108,13 +108,14 @@ class Store {
     return heads;
   }
 
-  async replicate(fireAndForget?: boolean = false) {
+  async deferReplicate() {
+    // We're probably "just" sending data _to_ the pinner. No need to wait for its response.
     const address = this.address.toString();
-    if (fireAndForget) {
-      // We're probably "just" sending data _to_ the pinner. No need to wait for its response.
-      this._pinner.requestReplication(address).catch(log.warn);
-      return;
-    }
+    this._pinner.requestReplication(address).catch(log.warn);
+  }
+
+  async replicate() {
+    const address = this.address.toString();
     const headCount = await this._pinner.requestReplication(address);
     log.verbose(
       `Pinner has ${headCount} heads, we have ${
