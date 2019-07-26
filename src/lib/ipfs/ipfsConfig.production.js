f2c8c62a29567f82234862c7f4b81b6b44907d4b
Christian Maniewski <chris@colony.io>
2019-07-26 16:11:04 +0200

    Add webrtc-star swarm address

--- src/lib/ipfs/ipfsConfig.production.js
+++ src/lib/ipfs/ipfsConfig.production.js
@@ -9,7 +9,10 @@ const config = () => ({
   // config gets merged with the IPFS default config
   config: {
     Addresses: {
-      Swarm: ['/dns4/qa.colony.io/tcp/9090/wss/p2p-websocket-star/'],
+      Swarm: [
+        '/dns4/qa.colony.io/tcp/9090/wss/p2p-websocket-star/',
+        '/dns4/qa.colony.io/tcp/9091/wss/p2p-webrtc-star/',
+      ],
     },
     Bootstrap: [],
     Discovery: {
