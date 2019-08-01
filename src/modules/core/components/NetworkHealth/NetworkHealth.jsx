9f52c5cf546540b982705018cc4a63451b862357
Raul Glogovetan <raul@glogovetan.com>
2019-08-01 20:43:02 +0300

    Add: initial core `NetworkHealth` component

---
+++ src/modules/core/components/NetworkHealth/NetworkHealth.jsx
@@ -0,0 +1,9 @@
+/* @flow */
+
+import React from 'react';
+
+import styles from './NetworkHealth.css';
+
+const NetworkHealth = () => <div className={styles.main} />;
+
+export default NetworkHealth;
