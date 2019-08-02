85cd712e110be14b71bbe522115716ea18f55b48
Raul Glogovetan <raul@glogovetan.com>
2019-08-02 12:51:40 +0300

    Add: core `NetworkHealthIcon` subcomponent

---
+++ src/modules/core/components/NetworkHealth/NetworkHealthIcon/NetworkHealthIcon.jsx
@@ -0,0 +1,24 @@
+/* @flow */
+
+import React from 'react';
+
+import { getMainClasses } from '~utils/css';
+
+import styles from './NetworkHealthIcon.css';
+
+type Appearance = {|
+  /** Size of the main network health icon that's being wrapped in the popover. */
+  size?: 'tiny' | 'small' | 'normal' | 'medium' | 'large' | 'huge',
+|};
+
+type Props = {|
+  health: 'good' | 'mean' | 'critical',
+  appearance?: Appearance,
+|};
+
+const NetworkHealthIcon = ({
+  health,
+  appearance: { size } = { size: 'normal' },
+}: Props) => <span className={getMainClasses({ health, size }, styles)} />;
+
+export default NetworkHealthIcon;
