5864091cd16b775c52483aa521dbd0ff13864be2
Raul Glogovetan <raul@glogovetan.com>
2019-08-02 12:52:13 +0300

    Add: `NetworkHealth` render as a button

--- src/modules/core/components/NetworkHealth/NetworkHealth.jsx
+++ src/modules/core/components/NetworkHealth/NetworkHealth.jsx
@@ -2,8 +2,26 @@
 
 import React from 'react';
 
+import NetworkHealthIcon from './NetworkHealthIcon';
+
 import styles from './NetworkHealth.css';
 
-const NetworkHealth = () => <div className={styles.main} />;
+type Appearance = {|
+  /** Size of the main network health icon that's being wrapped in the popover. */
+  size?: 'tiny' | 'small' | 'normal' | 'medium' | 'large' | 'huge',
+|};
+
+type Props = {|
+  appearance?: Appearance,
+  className?: string,
+|};
+
+const NetworkHealth = ({ appearance, className }: Props) => (
+  <div className={className}>
+    <button type="button" className={styles.main}>
+      <NetworkHealthIcon health="mean" appearance={appearance} />
+    </button>
+  </div>
+);
 
 export default NetworkHealth;
