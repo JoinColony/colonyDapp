7e7c3515306b2364971b558913c432419d58424b
Raul Glogovetan <raul@glogovetan.com>
2019-08-02 15:56:28 +0300

    Add: `NetworkHealthContentItem` subcomponent

---
+++ src/modules/core/components/NetworkHealth/NetworkHealthContent/NetworkHealthContentItem.jsx
@@ -0,0 +1,72 @@
+/* @flow */
+
+import type { MessageDescriptor, MessageValues } from 'react-intl';
+
+// $FlowFixMe until hooks flow types
+import React, { useMemo } from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import NetworkHealthIcon from '../NetworkHealthIcon';
+
+import styles from './NetworkHealthContentItem.css';
+
+type Props = {|
+  itemHealth: 'good' | 'mean' | 'critical',
+  /** A string or a `MessageDescriptor` that make up the headings's text */
+  itemTitle?: MessageDescriptor | string,
+  /** Values for text (react-intl interpolation) */
+  itemTitleValues?: MessageValues,
+|};
+
+const displayName = 'NetworkHealth.NetworkHealthContentItem';
+
+const NetworkHealthContentItem = ({
+  itemTitle,
+  itemTitleValues,
+  itemHealth,
+}: Props) => {
+  const highlightedItem = useMemo(
+    () => {
+      if (!itemTitle) {
+        return null;
+      }
+      if (typeof itemTitle == 'string') {
+        return <span>{itemTitle}</span>;
+      }
+      if (itemTitle.id && itemTitleValues) {
+        const highlightedValues = Object.keys(itemTitleValues)
+          .map(itemKey =>
+            itemTitleValues && itemTitleValues[itemKey]
+              ? {
+                  [itemKey]: (
+                    <span className={styles.itemTitleValuesHighlight}>
+                      {itemTitleValues[itemKey]}
+                    </span>
+                  ),
+                }
+              : {},
+          )
+          .reduce(
+            (highlightedValuesObject, highlightedValue) =>
+              Object.assign(highlightedValuesObject, highlightedValue),
+            {},
+          );
+        return <FormattedMessage {...itemTitle} values={highlightedValues} />;
+      }
+      return null;
+    },
+    [itemTitle, itemTitleValues],
+  );
+  return (
+    <li className={styles.main}>
+      <span className={styles.healthIconWrapper}>
+        <NetworkHealthIcon health={itemHealth} appearance={{ size: 'pea' }} />
+      </span>
+      {highlightedItem}
+    </li>
+  );
+};
+
+NetworkHealthContentItem.displayName = displayName;
+
+export default NetworkHealthContentItem;
