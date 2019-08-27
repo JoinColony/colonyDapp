73f1517373d77161039ade8c52e768c5742dd932
sprusr <scott@spru.sr>
2019-08-27 18:58:35 +0200

    Add UserList to admin Permissions tab

--- src/modules/admin/components/Permissions/Permissions.tsx
+++ src/modules/admin/components/Permissions/Permissions.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { defineMessages } from 'react-intl';
 
-import { Address } from '~types/index';
+import { Address, createAddress } from '~types/index';
+import { DomainType } from '~immutable/index';
 import Heading from '~core/Heading';
+import { Select } from '~core/Fields';
+import { SpinnerLoader } from '~core/Preloaders';
+import { useDataFetcher, useRoles } from '~utils/hooks';
+
+import UserList from '../UserList';
+import { domainsFetcher } from '../../../dashboard/fetchers';
 
 import styles from './Permissions.css';
 
@@ -10,6 +17,10 @@ const MSG = defineMessages({
   title: {
     id: 'dashboard.Permissions.title',
     defaultMessage: 'Permissions',
+  },
+  labelFilter: {
+    id: 'dashboard.Permissions.labelFilter',
+    defaultMessage: 'Filter',
   },
 });
 
@@ -19,27 +30,64 @@ interface Props {
 
 const displayName = 'admin.Permissions';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-const Permissions = ({ colonyAddress }: Props) => (
-  <div className={styles.main}>
-    <main>
-      <div className={styles.titleContainer}>
-        <Heading
-          text={MSG.title}
-          appearance={{ size: 'medium', theme: 'dark' }}
-        />
-      </div>
-      <p>Placeholder for main</p>
-    </main>
-    <aside className={styles.sidebar}>
-      <ul>
-        <li>
-          <p>Placeholder for sidebar</p>
-        </li>
-      </ul>
-    </aside>
-  </div>
-);
+const Permissions = ({ colonyAddress }: Props) => {
+  const { data: domainsData, isFetching: isFetchingDomains } = useDataFetcher<
+    DomainType[]
+  >(domainsFetcher, [colonyAddress], [colonyAddress]);
+  const domains = useMemo(
+    () => [
+      { value: 1, label: 'root' },
+      ...(domainsData || []).map(({ name, id }) => ({
+        label: name,
+        value: id,
+      })),
+    ],
+    [domainsData],
+  );
+  const [selectedDomain, setSelectedDomain] = useState(1);
+  const setFieldValue = useCallback((e, value) => setSelectedDomain(value), [
+    setSelectedDomain,
+  ]);
+  const { data: roles, isFetching: isFetchingRoles } = useRoles(colonyAddress);
+  const users = useMemo(
+    () => Object.keys((roles || {})[selectedDomain] || {}).map(createAddress),
+    [roles, selectedDomain],
+  );
+  return (
+    <div className={styles.main}>
+      <main>
+        <div className={styles.titleContainer}>
+          <Heading
+            text={MSG.title}
+            appearance={{ size: 'medium', theme: 'dark' }}
+          />
+          <Select
+            appearance={{ alignOptions: 'right', theme: 'alt' }}
+            connect={false}
+            elementOnly
+            label={MSG.labelFilter}
+            name="filter"
+            options={domains}
+            form={{ setFieldValue }}
+            $value={selectedDomain}
+          />
+        </div>
+        {isFetchingRoles || isFetchingDomains ? (
+          <SpinnerLoader />
+        ) : (
+          <UserList colonyAddress={colonyAddress} users={users} />
+        )}
+      </main>
+      <aside className={styles.sidebar}>
+        <ul>
+          <li>
+            <p>Placeholder for sidebar</p>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  );
+};
 
 Permissions.displayName = displayName;
 
