c3a5f729fff09d7a84cb20d83abfdb0b51c978ba
karolinkas <li-nee@gmx.de>
2019-08-29 18:29:53 +0200

    Set payload and finish wiring

--- src/modules/admin/components/Permissions/ColonyPermissionEditDialog.css.d.ts
+++ src/modules/admin/components/Permissions/ColonyPermissionEditDialog.css.d.ts
@@ -1,1 +1,6 @@
-export {};
+export const borderColor: string;
+export const permissionChoice: string;
+export const dialogContainer: string;
+export const permissionChoiceContainer: string;
+export const permissionChoiceDescription: string;
+export const titleContainer: string;
