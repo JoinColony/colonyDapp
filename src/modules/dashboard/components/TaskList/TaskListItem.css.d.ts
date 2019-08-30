2f8bd5bd5fa2bdc9bc45110c8bed2ff97abe3270
Raul Glogovetan <raul@glogovetan.com>
2019-08-30 14:57:33 +0300

    Refactor: `TaskListItem` update styles

--- src/modules/dashboard/components/TaskList/TaskListItem.css.d.ts
+++ src/modules/dashboard/components/TaskList/TaskListItem.css.d.ts
@@ -1,6 +1,7 @@
 export const cellPaddingVertical: string;
 export const cellPaddingHorizontal: string;
 export const globalLink: string;
+export const taskLoading: string;
 export const taskDetails: string;
 export const taskDetailsTitle: string;
 export const taskDetailsReputation: string;
