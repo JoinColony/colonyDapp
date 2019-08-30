0ffc9ac65117d9958f3ce592dfbadfe8f227d4d0
Raul Glogovetan <raul@glogovetan.com>
2019-08-30 11:28:21 +0300

    Add: `Dashboard` section titles

--- src/modules/dashboard/components/Dashboard/ColoniesList.tsx
+++ src/modules/dashboard/components/Dashboard/ColoniesList.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { defineMessages } from 'react-intl';
-
-import Heading from '~core/Heading';
-
-const MSG = defineMessages({
-  myColonies: {
-    id: 'dashboard.Dashboard.ColoniesList.myColonies',
-    defaultMessage: `My Colonies`,
-  },
-});
 
 const displayName = 'dashboard.Dashboard.ColoniesList';
 
 const ColoniesList = () => (
-  <Heading text={MSG.myColonies} appearance={{ size: 'small' }} />
+  <div />
 );
 
 ColoniesList.displayName = displayName;
