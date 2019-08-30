eb2ee3101b1cc58af53a861d1e4ff78a464a049b
James Lefrère <james@colony.io>
2019-08-30 12:17:01 +0200

    Add `disabled` prop
    * Add a `disabled` prop to the `DecisionOption` component, so that options can be functionally and visually disabled
    * Convert the class component to a functional component with hooks

--- src/modules/core/components/DecisionHub/DecisionOption.css.d.ts
+++ src/modules/core/components/DecisionHub/DecisionOption.css.d.ts
@@ -1,5 +1,6 @@
 export const main: string;
 export const themeAlt: string;
+export const stateDisabled: string;
 export const rowContent: string;
 export const rowIcon: string;
 export const tooltip: string;
