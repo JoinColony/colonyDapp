df99ea8ca47e2cb575292477e02cf1c190da79ba
Curtis Olson <curtis@colony.io>
2019-09-02 14:43:37 -0500

    Improve permissions list a11y & scrollability

--- src/modules/admin/components/Permissions/Permissions.css.d.ts
+++ src/modules/admin/components/Permissions/Permissions.css.d.ts
@@ -2,3 +2,5 @@ export const main: string;
 export const titleContainer: string;
 export const tableWrapper: string;
 export const sidebar: string;
+export const userPermissionsCell: string;
+export const tableBody: string;
