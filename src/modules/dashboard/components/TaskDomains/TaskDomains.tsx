74a834092fe094266039425355789817e7a5c568
Curtis Olson <curtis@colony.io>
2019-09-16 19:00:29 -0500

    Prevent selecting domains that do not have enough funds

--- src/modules/dashboard/components/TaskDomains/TaskDomains.tsx
+++ src/modules/dashboard/components/TaskDomains/TaskDomains.tsx
@@ -6,14 +6,18 @@ import {
 } from 'react-intl';
 import React, { useCallback, useMemo, useState } from 'react';
 
-import { DomainType, TaskProps } from '~immutable/index';
-import { ActionTypes } from '~redux/actionTypes';
-import { useAsyncFunction, useDataFetcher } from '~utils/hooks';
-import { log } from '~utils/debug';
 import Heading from '~core/Heading';
 import Button from '~core/Button';
 import ItemsList from '~core/ItemsList';
+import { DomainType, TaskProps } from '~immutable/index';
+import { ActionTypes } from '~redux/actionTypes';
+import { log } from '~utils/debug';
+import { useAsyncFunction, useDataFetcher, useSelector } from '~utils/hooks';
+import { bnLessThan } from '~utils/numbers';
+
 import { domainsFetcher } from '../../fetchers';
+import { colonyTokensSelector, taskPayoutsSelector } from '../../selectors';
+
 import styles from './TaskDomains.css';
 
 const MSG = defineMessages({
@@ -87,14 +91,37 @@ const TaskDomains = ({
     domainsFetcher,
     [colonyAddress],
     [colonyAddress],
-    // eslint-disable-next-line prettier/prettier
   );
 
-  const domainsWithRoot = useMemo(
-    () =>
-      domains && [{ id: 1, name: formatMessage(MSG.rootDomain) }, ...domains],
-    [domains, formatMessage],
+  const payouts = useSelector(taskPayoutsSelector, [draftId]);
+  const tokens = useSelector(colonyTokensSelector, [colonyAddress]);
+
+  const domainHasEnoughFunds = useCallback(
+    (domain: number) =>
+      payouts.every(({ amount, token }) => {
+        const payoutToken = tokens.find(({ address }) => address === token);
+        const tokenBalanceInDomain =
+          payoutToken && payoutToken.balances && payoutToken.balances[domain];
+        return !bnLessThan(tokenBalanceInDomain, amount);
+      }),
+    [payouts, tokens],
   );
+
+  const domainsWithRoot = useMemo(() => {
+    return (
+      domains && [
+        {
+          disabled: !domainHasEnoughFunds(1),
+          id: 1,
+          name: formatMessage(MSG.rootDomain),
+        },
+        ...domains.map(domain => ({
+          disabled: !domainHasEnoughFunds(domain.id),
+          ...domain,
+        })),
+      ]
+    );
+  }, [domainHasEnoughFunds, domains, formatMessage]);
 
   return (
     <div className={styles.main}>
