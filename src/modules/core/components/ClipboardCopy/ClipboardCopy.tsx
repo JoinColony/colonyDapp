591e5df21b6543a4af7abb7957d0b51e48742465
Raul Glogovetan <raul@glogovetan.com>
2019-09-18 13:32:23 +0300

    Add: initial core `ClipboardCopy` component

---
+++ src/modules/core/components/ClipboardCopy/ClipboardCopy.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useCallback } from 'react';
+import { defineMessages, MessageDescriptor } from 'react-intl';
+import copyToClipboard from 'copy-to-clipboard';
+
+import Button from '../Button';
+
+interface Props {
+  text?: MessageDescriptor;
+  value: string;
+};
+
+const MSG = defineMessages({
+  copyLabel: {
+    id: 'ClipboardCopy.copyLabel',
+    defaultMessage: `{valueIsCopied, select,
+      true {Copied}
+      false {Copy}
+    }`,
+  },
+});
+
+const ClipboardCopy = ({
+  value,
+  text = MSG.copyLabel,
+}: Props) => {
+  const [valueIsCopied, setValueIsCopied] = useState(false);
+  const handleClipboardCopy = useCallback(() => {
+    setValueIsCopied(true);
+    copyToClipboard(value);
+    const timer = setTimeout(() => setValueIsCopied(false), 2000);
+    return () => clearTimeout(timer);
+  }, [value, copyToClipboard, setValueIsCopied]);
+  return (
+    <Button
+      appearance={{ size: 'small', theme: 'blue' }}
+      disabled={valueIsCopied}
+      onClick={handleClipboardCopy}
+      text={text}
+      textValues={{ valueIsCopied }}
+    />
+  );
+};
+
+export default ClipboardCopy;
