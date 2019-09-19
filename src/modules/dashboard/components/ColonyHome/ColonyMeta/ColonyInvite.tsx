f15e3e51be7418809807374a2f82ce36051a8e6e
Raul Glogovetan <raul@glogovetan.com>
2019-09-19 15:20:28 +0300

    Add: `ColonyInvite` subcomponent

---
+++ src/modules/dashboard/components/ColonyHome/ColonyMeta/ColonyInvite.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import ClipboardCopy from '~core/ClipboardCopy';
+
+import styles from './ColonyInvite.css';
+
+const MSG = defineMessages({
+  inviteLabel: {
+    id: 'dashboard.ColonyHome.ColonyMeta.inviteLabel',
+    defaultMessage: 'Invite your community to join',
+  },
+  copyLabel: {
+    id: 'ClipboardCopy.copyLabel',
+    defaultMessage: `{valueIsCopied, select,
+      true {copied}
+      false {copy}
+    }`,
+  },
+});
+
+interface Props {
+  colonyName: string;
+}
+
+const displayName = 'dashboard.ColonyHome.ColonyInvite';
+
+const ColonyInvite = ({ colonyName }: Props) => {
+  if (!colonyName) {
+    return null;
+  }
+  const colonyLink = `colony.io/colony/${colonyName}`;
+  return (
+    <section className={styles.main}>
+      <FormattedMessage {...MSG.inviteLabel} tagName="p" />
+      <p className={styles.linkDisplay}>
+        <span>{colonyLink}</span>
+        <ClipboardCopy
+          text={MSG.copyLabel}
+          value={`https://${colonyLink}`}
+        />
+      </p>
+    </section>
+  );
+};
+
+ColonyInvite.displayName = displayName;
+
+export default ColonyInvite;
