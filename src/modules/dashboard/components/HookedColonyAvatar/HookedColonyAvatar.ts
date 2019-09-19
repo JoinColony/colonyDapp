f36da25f5ab4aa098ff620c0384eb0e538a5fc0d
Christian Maniewski <chris@colony.io>
2019-09-19 15:05:53 +0200

    Remove useColony hooks
    The reasoning behind this is that when using that hook it's not immediately clear what's going on behind the scenes. I'm always advocating for hooks that are as general as possible.

--- src/modules/dashboard/components/HookedColonyAvatar/HookedColonyAvatar.ts
+++ src/modules/dashboard/components/HookedColonyAvatar/HookedColonyAvatar.ts
@@ -1,23 +1,28 @@
 import { ColonyType } from '~immutable/index';
 import ColonyAvatar, { Props as ColonyAvatarProps } from '~core/ColonyAvatar';
-import { useDataFetcher } from '~utils/hooks';
+import { useDataFetcher, useDataSubscriber } from '~utils/hooks';
 import { withHooks } from '~utils/hoc';
+
 import { ipfsDataFetcher } from '../../../core/fetchers';
-import { useColonyWithAddress } from '../../hooks/useColony';
+import { colonySubscriber } from '../../subscribers';
 
 export default withHooks<
   { fetchColony: boolean },
   ColonyAvatarProps,
-  { colony?: ColonyType; avatarURL: string | void }
+  { colony?: ColonyType; avatarURL?: string }
 >(({ fetchColony = true }, { colony, colonyAddress }) => {
-  const result: { colony?: ColonyType; avatarURL: string | void } = {
+  const result: { colony?: ColonyType; avatarURL?: string } = {
     colony,
     avatarURL: undefined,
   };
 
   if (fetchColony) {
-    const { data: fetchedColony } = useColonyWithAddress(colonyAddress);
-    result.colony = fetchedColony as ColonyType;
+    const { data: fetchedColony } = useDataSubscriber<ColonyType>(
+      colonySubscriber,
+      [colonyAddress],
+      [colonyAddress],
+    );
+    result.colony = fetchedColony;
   }
   const avatarHash = result.colony ? result.colony.avatarHash : undefined;
   const { data: avatarURL } = useDataFetcher<string>(
