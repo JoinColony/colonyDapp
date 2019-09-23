3ac94dda814ced947f624a35d99ea6bb1b5b0a95
Raul Glogovetan <raul@glogovetan.com>
2019-09-23 10:48:57 +0300

    Fix: re-add `InboxIcon` unread activities functionality

--- src/modules/users/components/Inbox/InboxIcon/InboxIcon.tsx
+++ src/modules/users/components/Inbox/InboxIcon/InboxIcon.tsx
@@ -1,7 +1,12 @@
 import { MessageDescriptor, defineMessages } from 'react-intl';
 import React from 'react';
 
+import { useSelector } from '~utils/hooks';
+import { inboxItemsSelector } from '../../../selectors';
+
 import Icon from '~core/Icon';
+
+import styles from './InboxIcon.css';
 
 const MSG = defineMessages({
   fallbackTitle: {
@@ -17,9 +22,17 @@ interface Props {
 
 const displayName = 'users.Inbox.InboxIcon';
 
-const InboxIcon = ({ title = MSG.fallbackTitle }: Props) => (
-  <Icon name="envelope" title={title} />
-);
+const InboxIcon = ({ title = MSG.fallbackTitle }: Props) => {
+  const { record: activities = [] } = useSelector(inboxItemsSelector);
+  const hasUnreadActivities = activities.some(activity => activity.unread);
+  return (
+    <span
+      className={hasUnreadActivities ? styles.inboxNotification : undefined}
+    >
+      <Icon name="envelope" title={title} />
+    </span>
+  );
+};
 
 InboxIcon.displayName = displayName;
 
