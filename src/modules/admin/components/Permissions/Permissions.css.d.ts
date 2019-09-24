3fd31badb57253c306c46f4173b0c395e30e782f
Curtis Olson <curtis@colony.io>
2019-09-23 20:19:05 -0500

    Fix table hover issue

--- src/modules/admin/components/Permissions/Permissions.css.d.ts
+++ src/modules/admin/components/Permissions/Permissions.css.d.ts
@@ -2,5 +2,4 @@ export const main: string;
 export const titleContainer: string;
 export const tableWrapper: string;
 export const sidebar: string;
-export const userPermissionsCell: string;
 export const tableBody: string;
