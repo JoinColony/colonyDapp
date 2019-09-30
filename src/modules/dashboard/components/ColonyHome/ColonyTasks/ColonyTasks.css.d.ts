4c8fe8b06b9abae23770e8a24f29bbfd506fd9b9
sprusr <scott@spru.sr>
2019-09-30 16:30:34 +0300

    Fix TaskList height on Dashboard and ColonyHome
    The TaskList now scrolls when it would otherwise cause the page to
    scroll. This is done in UserTasks and ColonyTasks.

--- src/modules/dashboard/components/ColonyHome/ColonyTasks/ColonyTasks.css.d.ts
+++ src/modules/dashboard/components/ColonyHome/ColonyTasks/ColonyTasks.css.d.ts
@@ -2,3 +2,4 @@ export const newTaskButtonContainer: string;
 export const newTaskSpinnerContainer: string;
 export const newTaskButton: string;
 export const noTaskAddition: string;
+export const taskList: string;
