72e8874088e2b9c625e97ded1bf87e4f0daa519b
Curtis Olson <curtis@colony.io>
2019-10-03 14:11:12 -0500

    Profile edit heading span entire width

--- src/modules/admin/components/Profile/ProfileEdit.tsx
+++ src/modules/admin/components/Profile/ProfileEdit.tsx
@@ -91,107 +91,109 @@ const ProfileEdit = ({ colony }: Props) => {
 
   return (
     <div className={styles.main}>
-      <main className={styles.content}>
-        <ActionForm
-          submit={ActionTypes.COLONY_PROFILE_UPDATE}
-          success={ActionTypes.COLONY_PROFILE_UPDATE_SUCCESS}
-          error={ActionTypes.COLONY_PROFILE_UPDATE_ERROR}
-          transform={transform}
-          initialValues={{
-            colonyName,
-            description,
-            displayName,
-            guideline,
-            website,
-          }}
-          validationSchema={UpdateColonyProfileCommandArgsSchema}
-        >
-          {({ status, isSubmitting }) => (
-            <>
-              <FieldSet className={styles.section}>
-                <div className={styles.titleContainer}>
+      <div className={styles.titleContainer}>
+        <Heading
+          text={MSG.title}
+          appearance={{ size: 'medium', theme: 'dark' }}
+        />
+      </div>
+      <div className={styles.mainContentContainer}>
+        <main className={styles.content}>
+          <ActionForm
+            submit={ActionTypes.COLONY_PROFILE_UPDATE}
+            success={ActionTypes.COLONY_PROFILE_UPDATE_SUCCESS}
+            error={ActionTypes.COLONY_PROFILE_UPDATE_ERROR}
+            transform={transform}
+            initialValues={{
+              colonyName,
+              description,
+              displayName,
+              guideline,
+              website,
+            }}
+            validationSchema={UpdateColonyProfileCommandArgsSchema}
+          >
+            {({ status, isSubmitting }) => (
+              <>
+                <FieldSet className={styles.section}>
+                  <InputLabel label={MSG.labelAddress} help={MSG.sendTokens} />
+                  <CopyableAddress appearance={{ theme: 'big' }} full>
+                    {colonyAddress}
+                  </CopyableAddress>
+                </FieldSet>
+                <div className={styles.sectionENSName}>
+                  <InputLabel label={MSG.labelEnsName} />
                   <Heading
-                    text={MSG.title}
-                    appearance={{ size: 'medium', theme: 'dark' }}
+                    appearance={{
+                      margin: 'none',
+                      size: 'medium',
+                      weight: 'thin',
+                    }}
+                    /*
+                     * For the next improvement, we'll need to strip out
+                     * the ENS subdomain part, and just truncate the actual
+                     * name, so it'll look something like this:
+                     * aaaaaaaaaaaaaaaa... .colony.joincolony.eth
+                     */
+                    text={ENS.getFullDomain('colony', colonyName)}
                   />
                 </div>
-                <InputLabel label={MSG.labelAddress} help={MSG.sendTokens} />
-                <CopyableAddress appearance={{ theme: 'big' }} full>
-                  {colonyAddress}
-                </CopyableAddress>
-              </FieldSet>
-              <div className={styles.sectionENSName}>
-                <InputLabel label={MSG.labelEnsName} />
-                <Heading
-                  appearance={{
-                    margin: 'none',
-                    size: 'medium',
-                    weight: 'thin',
-                  }}
-                  /*
-                   * For the next improvement, we'll need to strip out
-                   * the ENS subdomain part, and just truncate the actual
-                   * name, so it'll look something like this:
-                   * aaaaaaaaaaaaaaaa... .colony.joincolony.eth
-                   */
-                  text={ENS.getFullDomain('colony', colonyName)}
-                />
-              </div>
-              {nativeTokenAddress && (
-                <div className={styles.section}>
-                  <InputLabel label={MSG.labelTokenAddress} />
-                  <CopyableAddress appearance={{ theme: 'big' }} full>
-                    {nativeTokenAddress}
-                  </CopyableAddress>
-                </div>
-              )}
-              <div className={styles.divider} />
-              <FieldSet className={styles.inputSection}>
-                <Input
-                  appearance={{ theme: 'fat' }}
-                  label={MSG.labelDisplayName}
-                  name="displayName"
-                />
-              </FieldSet>
-              <FieldSet className={styles.inputSection}>
-                <Textarea
-                  appearance={{ theme: 'fat', resizable: 'vertical' }}
-                  label={MSG.labelAbout}
-                  name="description"
-                  maxLength={160}
-                />
-              </FieldSet>
-              <FieldSet className={styles.inputSection}>
-                <Input
-                  appearance={{ theme: 'fat' }}
-                  label={MSG.labelWebsite}
-                  name="website"
-                />
-              </FieldSet>
-              <FieldSet className={styles.inputSection}>
-                <Input
-                  appearance={{ theme: 'fat' }}
-                  label={MSG.labelGuidelines}
-                  name="guideline"
-                />
-              </FieldSet>
-              <FieldSet>
-                <Button
-                  appearance={{ theme: 'primary', size: 'large' }}
-                  style={{ width: styles.wideButton }}
-                  text={{ id: 'button.save' }}
-                  type="submit"
-                  loading={isSubmitting}
-                />
-              </FieldSet>
-              <FormStatus status={status} />
-            </>
-          )}
-        </ActionForm>
-      </main>
-      <aside className={styles.sidebar}>
-        <ColonyAvatarUploader colony={colony} />
-      </aside>
+                {nativeTokenAddress && (
+                  <div className={styles.section}>
+                    <InputLabel label={MSG.labelTokenAddress} />
+                    <CopyableAddress appearance={{ theme: 'big' }} full>
+                      {nativeTokenAddress}
+                    </CopyableAddress>
+                  </div>
+                )}
+                <div className={styles.divider} />
+                <FieldSet className={styles.inputSection}>
+                  <Input
+                    appearance={{ theme: 'fat' }}
+                    label={MSG.labelDisplayName}
+                    name="displayName"
+                  />
+                </FieldSet>
+                <FieldSet className={styles.inputSection}>
+                  <Textarea
+                    appearance={{ theme: 'fat', resizable: 'vertical' }}
+                    label={MSG.labelAbout}
+                    name="description"
+                    maxLength={160}
+                  />
+                </FieldSet>
+                <FieldSet className={styles.inputSection}>
+                  <Input
+                    appearance={{ theme: 'fat' }}
+                    label={MSG.labelWebsite}
+                    name="website"
+                  />
+                </FieldSet>
+                <FieldSet className={styles.inputSection}>
+                  <Input
+                    appearance={{ theme: 'fat' }}
+                    label={MSG.labelGuidelines}
+                    name="guideline"
+                  />
+                </FieldSet>
+                <FieldSet>
+                  <Button
+                    appearance={{ theme: 'primary', size: 'large' }}
+                    style={{ width: styles.wideButton }}
+                    text={{ id: 'button.save' }}
+                    type="submit"
+                    loading={isSubmitting}
+                  />
+                </FieldSet>
+                <FormStatus status={status} />
+              </>
+            )}
+          </ActionForm>
+        </main>
+        <aside className={styles.sidebar}>
+          <ColonyAvatarUploader colony={colony} />
+        </aside>
+      </div>
     </div>
   );
 };
