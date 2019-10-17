806232a7785f16591f3d71b6638d875fdc76bcf2
Christian Maniewski <chris@colony.io>
2019-10-17 17:57:15 +0200

    Move COLONY_TOTAL_BALANCE_DOMAIN_ID

--- src/modules/admin/sagas/index.ts
+++ src/modules/admin/sagas/index.ts
@@ -1,6 +1,6 @@
 import { all, call, fork, put, takeEvery, select } from 'redux-saga/effects';
 
-import { ROOT_DOMAIN } from '~constants';
+import { ROOT_DOMAIN, COLONY_TOTAL_BALANCE_DOMAIN_ID } from '~constants';
 import { AllActions, Action, ActionTypes } from '~redux/index';
 import {
   takeFrom,
@@ -12,7 +12,6 @@ import { ContractContexts } from '~types/index';
 // import { Context, getContext } from '~context/index';
 // import { decorateLog } from '~utils/web3/eventLogs/events';
 // import { normalizeTransactionLog } from '~data/normalizers';
-import { COLONY_TOTAL_BALANCE_DOMAIN_ID } from '../constants';
 import { getColony } from '../../dashboard/data/queries';
 import {
   getColonyTransactions,
