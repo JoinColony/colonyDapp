25cfb4090e371d42fd55070030290c31be8e18c2
Christian Maniewski <chris@colony.io>
2019-10-24 18:26:59 +0200

    Use transformers to select tokens

--- src/modules/dashboard/selectors/tokens.ts
+++ src/modules/dashboard/selectors/tokens.ts
@@ -1,26 +1,8 @@
-import { createSelector } from 'reselect';
-import { Map as ImmutableMap } from 'immutable';
-
-import {
-  ColonyTokenReferenceType,
-  FetchableDataRecord,
-  TokenRecord,
-} from '~immutable/index';
+import { FetchableDataRecord, TokenRecord } from '~immutable/index';
 import { Address } from '~types/index';
 
 import { RootStateRecord } from '../../state';
 import { DASHBOARD_ALL_TOKENS, DASHBOARD_NAMESPACE as ns } from '../constants';
-
-/*
- * Input selectors
- */
-export const tokensByAddressesSelector = (
-  state: RootStateRecord,
-  tokenAddresses: string[],
-) =>
-  (state.getIn([ns, DASHBOARD_ALL_TOKENS]) || ImmutableMap()).filter(
-    (_, address) => tokenAddresses.includes(address),
-  );
 
 export const tokenSelector = (
   state: RootStateRecord,
@@ -28,26 +10,5 @@ export const tokenSelector = (
 ): FetchableDataRecord<TokenRecord> =>
   state.getIn([ns, DASHBOARD_ALL_TOKENS, tokenAddress]);
 
-/*
- * Selectors
- */
-export const allFromColonyTokensSelector = createSelector<
-  RootStateRecord,
-  ColonyTokenReferenceType[],
-  any,
-  any,
-  any
->(
-  state => state,
-  (state, tokens) => tokens.map(({ address }) => address),
-  tokensByAddressesSelector,
-);
-
-Object.defineProperty(allFromColonyTokensSelector, 'transform', {
-  value: input =>
-    input
-      .map(token => token.record)
-      .filter(Boolean)
-      .toList()
-      .toJS(),
-});
+export const allTokensSelector = (state: RootStateRecord) =>
+  state.getIn([ns, DASHBOARD_ALL_TOKENS]);
