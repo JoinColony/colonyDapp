ec64928ca42d34186f1e6b1b1b8ae32b6dc11e9a
Raul Glogovetan <raul@glogovetan.com>
2019-10-28 15:09:58 +0200

    Fix: core `IconButton` main functionality

--- src/modules/core/components/Button/IconButton.tsx
+++ src/modules/core/components/Button/IconButton.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import {
+  MessageDescriptor,
+  MessageValues,
+  FormattedMessage,
+} from 'react-intl';
+
+import Button from './Button';
+import Icon from '~core/Icon';
+
+import styles from './IconButton.css';
 
 const displayName = 'IconButton';
 
-const IconButton = () => <div />;
+interface Props {
+  /** Name of the icon to display */
+  icon?: string;
+  /** A string or a `messageDescriptor` that make up the button's text label */
+  text?: MessageDescriptor;
+  /** Values for loading text (react-intl interpolation) */
+  textValues?: MessageValues;
+}
+
+const IconButton = ({
+  icon = 'wallet',
+  text,
+  textValues,
+  ...props
+}: Props) => (
+  <Button {...props}>
+    <div className={styles.main}>
+      <Icon
+        role="button"
+          name={icon}
+      />
+      <FormattedMessage {...text} values={textValues} />
+    </div>
+  </Button>
+);
 
 IconButton.displayName = displayName;
 
