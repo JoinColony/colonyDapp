b38bedf16cf60787b9ed353839649b310c040156
Raul Glogovetan <raul@glogovetan.com>
2019-10-29 15:00:48 +0200

    Fix: export `IconButton` from core `Button`

--- src/modules/core/components/Button/index.ts
+++ src/modules/core/components/Button/index.ts
@@ -2,3 +2,4 @@ export { default } from './Button';
 export { default as ActionButton } from './ActionButton';
 export { default as ConfirmButton } from './ConfirmButton';
 export { default as DialogActionButton } from './DialogActionButton';
+export { default as IconButton } from './IconButton';
