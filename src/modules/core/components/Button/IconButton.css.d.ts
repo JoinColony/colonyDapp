233492624e5b38e5f37f6e37fdab86d58bea41f5
Raul Glogovetan <raul@glogovetan.com>
2019-10-30 16:08:32 +0200

    Add: `IconButton` color themes

--- src/modules/core/components/Button/IconButton.css.d.ts
+++ src/modules/core/components/Button/IconButton.css.d.ts
@@ -1,1 +1,7 @@
 export const main: string;
+export const themePrimary: string;
+export const themeDanger: string;
+export const themeSecondary: string;
+export const themeGhost: string;
+export const themeUnderlineBold: string;
+export const themeBlue: string;
