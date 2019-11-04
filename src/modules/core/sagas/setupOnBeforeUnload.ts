9647077259356482a63f0b3e8ed6e5912dc5b7fa
Raul Glogovetan <raul@glogovetan.com>
2019-11-04 17:53:11 +0200

    Add: `beforeUnload` check for network health

--- src/modules/core/sagas/setupOnBeforeUnload.ts
+++ src/modules/core/sagas/setupOnBeforeUnload.ts
@@ -1,12 +1,18 @@
 import { getContext } from 'redux-saga/effects';
 
 // Do NOT do this at home! We import the store directly here because we need a sync API (that sagas don't provide)
+import { ConnectionType } from '~immutable/index';
 import store from '~redux/createReduxStore';
 
 import { DDB } from '../../../lib/database';
 import IPFSNode from '../../../lib/ipfs';
 
-import { pendingTransactions } from '../selectors';
+import {
+  pendingTransactions,
+  connection as connectionSelector,
+} from '../selectors';
+
+import getNetworkHealth from '~core/NetworkHealth/getNetworkHealth';
 
 const hasPendingTransactions = () => {
   const transactions = pendingTransactions((store as any).getState());
@@ -18,12 +24,36 @@ const pinnerIsBusy = (ipfsNode: IPFSNode) =>
 
 const ddbIsBusy = (ddb: DDB) => ddb.busy;
 
+const networkHealthIsPoor = () => {
+  const connection: ConnectionType = connectionSelector(
+    (store as any).getState(),
+  );
+  const networkItems = getNetworkHealth(connection);
+  /*
+   * @NOTE This is (currently) exactly the same as the logic inside the `<NetworkHealth />` component
+   * I didn't bother centralizing the source (and reducing code repetition) because my view is that
+   * at some point these two pieces of logic will diverge into they're own separate paths
+   */
+  // Errors are important so we set the whole thing to 1 if there are a lot (> 1)
+  const health =
+    connection.errors.length > 1 || connection.stats.pinners.length < 1
+      ? 1
+      : Math.round(
+          networkItems.reduce((sum, current) => sum + current.itemHealth, 0) /
+            networkItems.length,
+        );
+  return health <= 1;
+};
+
 export default function* setupOnBeforeUnload() {
   const ipfsNode = yield getContext('ipfsNode');
   const ddb = yield getContext('ddb');
   const handleBeforeunload = evt => {
     const disallowUnload =
-      hasPendingTransactions() || pinnerIsBusy(ipfsNode) || ddbIsBusy(ddb);
+      hasPendingTransactions() ||
+      pinnerIsBusy(ipfsNode) ||
+      ddbIsBusy(ddb) ||
+      networkHealthIsPoor();
     if (disallowUnload) {
       evt.preventDefault();
       // eslint-disable-next-line no-param-reassign
