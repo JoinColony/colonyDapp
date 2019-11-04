0ed3642d939c628c3fcde7326e27c7277bde1275
Raul Glogovetan <raul@glogovetan.com>
2019-11-04 16:28:49 +0200

    Remove: `getNetworkHealth` remove busy items

--- src/modules/core/components/NetworkHealth/getNetworkHealth.ts
+++ src/modules/core/components/NetworkHealth/getNetworkHealth.ts
@@ -3,10 +3,6 @@ import { defineMessages } from 'react-intl';
 import { ConnectionType } from '~immutable/index';
 
 const MSG = defineMessages({
-  busyStores: {
-    id: 'core.NetworkHealth.busyStores',
-    defaultMessage: 'Busy stores: {busyStores}',
-  },
   openStores: {
     id: 'core.NetworkHealth.openStores',
     defaultMessage: 'Open stores: {openStores}',
@@ -23,10 +19,6 @@ const MSG = defineMessages({
     id: 'core.NetworkHealth.pinners',
     defaultMessage: 'Pinners connected to: {pinners}',
   },
-  pinnerBusy: {
-    id: 'core.NetworkHealth.pinnerBusy',
-    defaultMessage: 'Pinner connector is busy: {pinnerBusy}',
-  },
   pubsubPeers: {
     id: 'core.NetworkHealth.pubsubPeers',
     defaultMessage: 'Pubsub peers: {pubsubPeers}',
@@ -39,11 +31,6 @@ const MSG = defineMessages({
 
 // 3 = good, 2 = soso, 1 = poor
 
-const busyStoresHealth = busyStores => {
-  if (busyStores.length > 5) return 1;
-  return busyStores.length ? 2 : 3;
-};
-
 const openStoresHealth = openStores => (openStores ? 3 : 1);
 
 const pingHealth = ping => {
@@ -55,8 +42,6 @@ const pinnersHealth = pinners => {
   if (!pinners.length) return 1;
   return pinners.length === 1 ? 2 : 3;
 };
-
-const pinnerBusyHealth = pinnerBusy => (pinnerBusy ? 2 : 3);
 
 const pubsubPeersHealth = pubsubPeers => {
   if (!pubsubPeers.length) return 1;
@@ -75,32 +60,18 @@ const errorsHealth = errors => {
 
 const calculateNetworkHealth = ({
   stats: {
-    busyStores = [],
     openStores = 0,
     ping = 0,
     pinners = [],
-    pinnerBusy = false,
     pubsubPeers = [],
     swarmPeers = [],
   },
   errors,
 }: ConnectionType) => [
   {
-    itemTitle: MSG.busyStores,
-    itemHealth: busyStoresHealth(busyStores),
-    itemTitleValues: { busyStores: busyStores.length || '0' },
-  },
-  {
     itemTitle: MSG.openStores,
     itemHealth: openStoresHealth(openStores),
     itemTitleValues: { openStores: openStores || '0' },
-  },
-  {
-    itemTitle: MSG.pinnerBusy,
-    itemHealth: pinnerBusyHealth(pinnerBusy),
-    // I know this should be translated but I couldn't figure out how to do it because of the span decoration
-    // Also I don't really care anymore
-    itemTitleValues: { pinnerBusy: pinnerBusy ? 'yes' : 'no' },
   },
   {
     itemTitle: MSG.ipfsPing,
