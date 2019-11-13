ffdaa2746f79985fe00004cad134f5244a432119
Christian Maniewski <chris@colony.io>
2019-11-13 16:32:18 +0100

    Check root domain for perms, too in access ctrl

--- src/data/permissions/utils.ts
+++ src/data/permissions/utils.ts
@@ -1,6 +1,6 @@
 import { ColonyClient } from '@colony/colony-js-client';
 
-import { ROLES } from '~constants';
+import { ROOT_DOMAIN, ROLES } from '~constants';
 import { Address } from '~types/index';
 
 import { PermissionModuleLoader } from '../types';
@@ -18,10 +18,18 @@ export const makeUserHasRoleFn = (
   colonyClient: ColonyClient,
   role: ROLES,
 ) => async (address: Address, domainId: number): Promise<boolean> => {
+  // This won't work anymore once we have a domain depth of > 1. This should probably go into ColonyJS then
+  // But everything we built in the dapp (and even in ColonyJS) is making that assumption so I guess we're ok for now
   const { hasRole } = await colonyClient.hasColonyRole.call({
     address,
     role,
     domainId,
   });
-  return hasRole;
+  if (hasRole) return hasRole;
+  const { hasRole: hasRoleInRoot } = await colonyClient.hasColonyRole.call({
+    address,
+    role,
+    ROOT_DOMAIN,
+  });
+  return hasRoleInRoot;
 };
