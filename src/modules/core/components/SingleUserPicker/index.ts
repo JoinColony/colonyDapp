83ec1f05460a9da175272a3306269928680317f5
Christian Maniewski <chris@colony.io>
2019-11-21 13:53:06 +0100

    Fix SingleUserPicker filtering

--- src/modules/core/components/SingleUserPicker/index.ts
+++ src/modules/core/components/SingleUserPicker/index.ts
@@ -1,3 +1,5 @@
 export { default } from './SingleUserPicker';
 
 export { default as ItemDefault } from './ItemDefault';
+
+export * from './utils';
---
+++ src/modules/core/components/SingleUserPicker/utils.ts
@@ -0,0 +1,34 @@
+/*
+Extracts the required values to be used in the SingleUserPicker
+on selection
+*/
+export const filterUserSelection = (data, filterValue) => {
+  if (!filterValue) {
+    return data;
+  }
+
+  const filtered = data.filter(
+    user =>
+      user &&
+      user.profile &&
+      user.profile.username &&
+      filterValue &&
+      (user.profile.username
+        .toLowerCase()
+        .includes(filterValue.toLowerCase()) ||
+        user.profile.walletAddress
+          .toLowerCase()
+          .includes(filterValue.toLowerCase()) ||
+        user.id.toLowerCase().includes(filterValue.toLowerCase())),
+  );
+
+  const customValue = {
+    id: 'filterValue',
+    profile: {
+      walletAddress: filterValue,
+      displayName: filterValue,
+    },
+  };
+
+  return [customValue].concat(filtered);
+};
