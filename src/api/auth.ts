a4412c31cb95e02ac1d8b727aa3835150a471237
Christian Maniewski <chris@colony.io>
2019-11-22 20:16:50 +0100

    PoC: get temp-user from apollo

---
+++ src/api/auth.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+// FIXME it seems we don't need axios
+export const apiRequest = axios.create({
+  // FIXME use env var
+  baseURL: 'http://127.0.0.1:3000',
+});
+
+export const authenticate = async wallet => {
+  // FIXME error handling
+  const {
+    data: { challenge },
+  } = await apiRequest.post('/auth/challenge', {
+    address: wallet.address,
+  });
+  const signature = await wallet.signMessage({ message: challenge });
+  const {
+    data: { token },
+  } = await apiRequest.post('/auth/token', {
+    challenge,
+    signature,
+  });
+  apiRequest.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+  // FIXME abstraction???
+  localStorage.setItem('token', token);
+  console.log(token);
+  return token;
+};
