e14f7387b0952006605186f9064f9c51ff2c43d6
Raul Glogovetan <raul@glogovetan.com>
2019-11-27 16:36:35 +0200

    Add: initial colony edit graph mutation

--- src/modules/dashboard/mutations.ts
+++ src/modules/dashboard/mutations.ts
@@ -2,7 +2,20 @@ import gql from 'graphql-tag';
 
 export const CREATE_COLONY = gql`
   mutation createColony($input: CreateColonyInput!) {
-    createColony(input:$input) {
+    createColony(input: $input) {
+			ok
+      error
+      value {
+        id
+        colonyName
+      }
+    }
+  }
+`;
+
+export const EDIT_COLONY = gql`
+  mutation editColony($address: String!) {
+    editColony(address: $address) {
 			ok
       error
       value {
