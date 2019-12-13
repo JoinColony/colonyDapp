11c394bb6351e4b93beb30f2e6cd6f8fced0620c
Curtis Olson <curtis@colony.io>
2019-12-13 15:07:54 -0600

    Remove task permissions

--- src/data/permissions/index.ts
+++ src/data/permissions/index.ts
@@ -3,14 +3,12 @@ import ColonyNetworkClient from '@colony/colony-js-client';
 import { buildManifest } from './utils';
 import COMMON from './common';
 import COLONY from './colony';
-import TASK from './task';
 
 export { default as PermissionManager } from './PermissionManager';
 
 export const MANIFEST_LOADERS = Object.freeze({
   COMMON,
   COLONY,
-  TASK,
 });
 
 const loadPermissionManifest = (
