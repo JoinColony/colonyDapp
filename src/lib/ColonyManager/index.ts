7c2d9117f70357007f6e5d98b48a6b5fdd3026a1
Christian Maniewski <code@chmanie.com>
2019-12-17 01:53:59 +0100

    Only get Colony client once

--- src/lib/ColonyManager/index.ts
+++ src/lib/ColonyManager/index.ts
@@ -10,7 +10,7 @@ import tokenABILoader from './tokenABILoader';
 import ens from '../../context/ensContext';
 
 export default class ColonyManager {
-  clients: Map<Address, ColonyNetworkClient.ColonyClient>;
+  clients: Map<Address, Promise<ColonyNetworkClient.ColonyClient>>;
 
   _metaColonyClient: ColonyNetworkClient.ColonyClient;
 
@@ -19,6 +19,19 @@ export default class ColonyManager {
   constructor(networkClient: ColonyNetworkClient) {
     this.clients = new Map();
     this.networkClient = networkClient;
+  }
+
+  private async getColonyPromise(address: Address) {
+    const client = await this.networkClient.getColonyClientByAddress(address);
+
+    // Check if the colony exists by calling `getVersion` (in lieu of an
+    // explicit means of checking whether a colony exists at an address).
+    try {
+      await client.getVersion.call();
+    } catch (caughtError) {
+      throw new Error(`Colony with address ${address} not found`);
+    }
+    return client;
   }
 
   async resolveColonyIdentifier(identifier: AddressOrENSName): Promise<any> {
@@ -31,18 +44,10 @@ export default class ColonyManager {
   }
 
   async setColonyClient(address: Address) {
-    const client = await this.networkClient.getColonyClientByAddress(address);
+    const clientPromise = this.getColonyPromise(address);
 
-    // Check if the colony exists by calling `getVersion` (in lieu of an
-    // explicit means of checking whether a colony exists at an address).
-    try {
-      await client.getVersion.call();
-    } catch (caughtError) {
-      throw new Error(`Colony with address ${address} not found`);
-    }
-
-    this.clients.set(address, client);
-    return client;
+    this.clients.set(address, clientPromise);
+    return clientPromise.catch(() => this.clients.delete(address));
   }
 
   async getMetaColonyClient() {
