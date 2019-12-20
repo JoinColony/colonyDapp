0cd489670ad09c39294da4dbeda4fc57223dfe4f
Christian Maniewski <code@chmanie.com>
2019-12-20 15:28:31 +0100

    Remove superfluous comment store

--- src/data/types/stores.ts
+++ src/data/types/stores.ts
@@ -2,12 +2,10 @@ import { ObjectSchema } from 'yup';
 
 import { ColonyEvents } from '~data/types/ColonyEvents';
 import { CurrentEvents } from '~data/types/events';
-import { CommentEvents } from '~data/types/CommentEvents';
 import { AccessController } from './accessControllers/index';
 import { EventStore } from '../../lib/database/stores/index';
 
 export type ColonyStore = EventStore<CurrentEvents<ColonyEvents>>;
-export type CommentsStore = EventStore<CurrentEvents<CommentEvents>>;
 
 type StoreClassWrapper = typeof EventStore;
 
