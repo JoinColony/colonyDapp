abf737e1735fd2247d22bae40b3cb3d491dcc1ca
Raul Glogovetan <raul@glogovetan.com>
2019-12-26 15:11:59 +0200

    Add: rest of the server notification events

--- src/modules/users/components/Inbox/events.ts
+++ src/modules/users/components/Inbox/events.ts
@@ -3,20 +3,8 @@
  * make sure that these match the IDs of the corresponding message descriptors
  * in the `messages.ts` file
  */
-export const NOTIFICATION_EVENT_COLONY_ENS_CREATED =
-  'notificationAdminENSCreated';
-export const NOTIFICATION_EVENT_TOKENS_MINTED =
-  'notificationAdminTokensGenerated';
-export const NOTIFICATION_EVENT_DOMAIN_ADDED =
-  'notificationAdminColonyLabelAdded';
-export const NOTIFICATION_EVENT_COLONY_ROLE_SET = 'notificationColonyRoleSet';
-export const NOTIFICATION_EVENT_ADMIN_REMOVED = 'notificationAdminOtherRemoved';
 export const NOTIFICATION_EVENT_USER_MENTIONED = 'notificationUserMentioned';
 export const NOTIFICATION_EVENT_USER_CLAIMED_PROFILE =
   'notificationUserClaimedProfile';
-export const NOTIFICATION_EVENT_USER_TRANSFER =
-  'notificationUserTransferReceived';
-export const NOTIFICATION_EVENT_ASSIGNED = 'notificationWorkerAssigned';
-export const NOTIFICATION_EVENT_UNASSIGNED = 'notificationWorkerUnassigned';
 export const NOTIFICATION_EVENT_REQUEST_WORK = 'notificationWorkRequested';
-export const NOTIFICATION_EVENT_TASK_FINALIZED = 'notificationTaskFinalized';
+export const NOTIFICATION_EVENT_WORK_INVITE = 'actionWorkerInviteReceived';
