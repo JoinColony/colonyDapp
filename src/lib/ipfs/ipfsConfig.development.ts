d100f82d2fcf25f5847f083e2726fc1dd4215ae4
Christian Maniewski <code@chmanie.com>
2020-01-06 21:32:14 +0100

    Adjust IPFS config

--- src/lib/ipfs/ipfsConfig.development.ts
+++ src/lib/ipfs/ipfsConfig.development.ts
@@ -2,13 +2,7 @@ const config = () => ({
   repo: 'colonyIpfs',
   // config gets merged with the IPFS default config
   config: {
-    Addresses: {
-      Swarm: ['/ip4/127.0.0.1/tcp/9091/ws/p2p-websocket-star'],
-    },
     Bootstrap: [],
-  },
-  EXPERIMENTAL: {
-    pubsub: true,
   },
 });
 
--- src/lib/ipfs/ipfsConfig.production.ts
+++ src/lib/ipfs/ipfsConfig.production.ts
@@ -1,24 +1,7 @@
 const config = () => ({
   repo: 'colonyIpfs',
   // config gets merged with the IPFS default config
-  config: {
-    Addresses: {
-      Swarm: [
-        '/dns4/colony.io/tcp/9090/wss/p2p-websocket-star/',
-        '/dns4/colony.io/tcp/9091/wss/p2p-webrtc-star/',
-      ],
-    },
-    Bootstrap: [],
-    Discovery: {
-      webRTCStar: {
-        Enabled: true,
-      },
-    },
-  },
-  preload: { enabled: false, addresses: [] },
-  EXPERIMENTAL: {
-    pubsub: true,
-  },
+  config: {},
 });
 
 export default config;
