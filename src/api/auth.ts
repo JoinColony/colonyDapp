8552dde797dbcb3d39d470f1699afd388dbd164b
Christian Maniewski <code@chmanie.com>
2020-01-07 13:57:37 +0100

    Simplify authentication, check token expiry

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,29 +1,38 @@
-import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
-// FIXME it seems we don't need axios
-export const apiRequest = axios.create({
-  // FIXME use env var
-  baseURL: 'http://127.0.0.1:3000',
-});
+const TOKEN_STORAGE = 'colony-server-token';
+
+const postRequest = async (path: string, data: object) => {
+  const response = await fetch(`${process.env.SERVER_ENDPOINT}${path}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
+export const setToken = (token: string) =>
+  localStorage.setItem(TOKEN_STORAGE, token);
+export const getToken = () => localStorage.getItem(TOKEN_STORAGE);
+export const clearToken = () => localStorage.removeItem(TOKEN_STORAGE);
 
 export const authenticate = async wallet => {
-  // FIXME if we have a token, check expiry date and maybe just use that
-  // FIXME error handling
-  const {
-    data: { challenge },
-  } = await apiRequest.post('/auth/challenge', {
+  const token = getToken();
+  if (token) {
+    const tokenData = jwtDecode(token);
+    // JWT expiry dates are noted in seconds
+    if (tokenData.exp * 10 ** 3 > Date.now()) return token;
+  }
+  const { challenge } = await postRequest('/auth/challenge', {
     address: wallet.address,
   });
   const signature = await wallet.signMessage({ message: challenge });
-  const {
-    data: { token },
-  } = await apiRequest.post('/auth/token', {
+  const { token: refreshedToken } = await postRequest('/auth/token', {
     challenge,
     signature,
   });
-  // apiRequest.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-  // FIXME abstraction???
-  localStorage.setItem('token', token);
-  return token;
+  setToken(refreshedToken);
+  return refreshedToken;
 };
