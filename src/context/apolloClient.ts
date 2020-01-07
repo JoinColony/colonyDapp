b3a82bfa867863957e2547e7c9336c39ee4db65b
Christian Maniewski <code@chmanie.com>
2020-01-07 13:57:37 +0100

    Simplify authentication, check token expiry

--- src/context/apolloClient.ts
+++ src/context/apolloClient.ts
@@ -4,6 +4,8 @@ import { setContext } from 'apollo-link-context';
 
 import { cache, typeDefs } from '~data/index';
 
+import { getToken } from '../api/auth';
+
 export { ApolloProvider } from '@apollo/react-hooks';
 
 const httpLink = createHttpLink({
@@ -12,7 +14,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
+  const token = getToken();
   // return the headers to the context so httpLink can read them
   return {
     headers: {
