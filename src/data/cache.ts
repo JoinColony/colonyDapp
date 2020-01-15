220519f46d5493a64e0187b7466b6a928303bd28
Curtis Olson <curtis@colony.io>
2020-01-15 12:17:53 -0600

    Improve token domain balances in apollo cache

--- src/data/cache.ts
+++ src/data/cache.ts
@@ -1,6 +1,7 @@
 import {
   InMemoryCache,
   IntrospectionFragmentMatcher,
+  defaultDataIdFromObject,
 } from 'apollo-cache-inmemory';
 
 import introspectionResult from './generated';
@@ -9,6 +10,18 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData: introspectionResult,
 });
 
+const dataIdFromObject = obj => {
+  // eslint-disable-next-line no-underscore-dangle
+  if (obj.__typename === 'DomainBalance') {
+    // Avoid having array arguments inside the cache key
+    // Avoid `${address}.${[...domainIds]}`. Cache creates individual entries for each combination
+    const { address, colonyAddress, domainId } = obj;
+    return `Colony:${colonyAddress}.Token:${address}.Domain:${domainId}`;
+  }
+  return defaultDataIdFromObject(obj);
+};
+
 export default new InMemoryCache({
+  dataIdFromObject,
   fragmentMatcher,
 });
