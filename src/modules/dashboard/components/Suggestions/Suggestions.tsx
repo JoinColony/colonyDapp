5175b4aa8ba99a3b076ffe2651416dbd84c6f0b4
Curtis Olson <curtis@colony.io>
2020-01-15 18:01:51 -0600

    Add `SuggestionCreate` component

--- src/modules/dashboard/components/Suggestions/Suggestions.tsx
+++ src/modules/dashboard/components/Suggestions/Suggestions.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+
+import SuggestionCreate from '~dashboard/SuggestionCreate';
 import { Address } from '~types/index';
+import { Domain } from '~data/index';
 
 interface Props {
   colonyAddress: Address;
+  domainId: Domain['ethDomainId'];
 }
 
 const displayName = 'dashboard.Suggestions';
 
-const Suggestions = ({ colonyAddress }: Props) => <div>{colonyAddress}</div>;
+const Suggestions = ({ colonyAddress, domainId }: Props) => (
+  <div>
+    <SuggestionCreate colonyAddress={colonyAddress} domainId={domainId} />
+  </div>
+);
 
 Suggestions.displayName = displayName;
 
