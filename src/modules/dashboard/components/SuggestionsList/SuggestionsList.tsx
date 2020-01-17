ff44c62bb43410027d76991d2003a25b2bc7fbf8
Curtis Olson <curtis@colony.io>
2020-01-17 08:31:26 -0600

    Add `SuggestionsListItem` component

--- src/modules/dashboard/components/SuggestionsList/SuggestionsList.tsx
+++ src/modules/dashboard/components/SuggestionsList/SuggestionsList.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { defineMessages } from 'react-intl';
 
 import { SpinnerLoader } from '~core/Preloaders';
-import { useColonySuggestionsQuery } from '~data/index';
+import { useColonySuggestionsQuery, Domain } from '~data/index';
 import { Address } from '~types/index';
 import Heading from '~core/Heading';
 import { getMainClasses } from '~utils/css';
 
 import styles from './SuggestionsList.css';
+import ListGroup from '~core/ListGroup';
+import ListGroupItem from '~core/ListGroup/ListGroupItem';
+import SuggestionsListItem from '~dashboard/SuggestionsListItem';
+import { COLONY_TOTAL_BALANCE_DOMAIN_ID } from '~constants';
 
 const MSG = defineMessages({
   emptyStateTitle: {
@@ -23,32 +27,42 @@ const MSG = defineMessages({
 
 interface Props {
   colonyAddress: Address;
+  domainId: Domain['ethDomainId'];
 }
 
 const displayName = 'Dashboard.SuggestionsList';
 
-const SuggestionsList = ({ colonyAddress }: Props) => {
+const SuggestionsList = ({ colonyAddress, domainId }: Props) => {
   const { data, loading } = useColonySuggestionsQuery({
     variables: { colonyAddress },
   });
-  if (loading) {
-    return <SpinnerLoader size="medium" />;
-  }
 
-  const suggestions = (data && data.colony.suggestions) || [];
+  const allSuggestions = (data && data.colony.suggestions) || [];
 
-  return (
+  const suggestions = useMemo(
+    () =>
+      domainId === COLONY_TOTAL_BALANCE_DOMAIN_ID
+        ? allSuggestions
+        : allSuggestions.filter(({ ethDomainId }) => ethDomainId === domainId),
+    [allSuggestions, domainId],
+  );
+
+  return loading ? (
+    <SpinnerLoader size="medium" />
+  ) : (
     <div
       className={getMainClasses({}, styles, {
         isEmpty: suggestions.length === 0,
       })}
     >
       {suggestions.length > 0 ? (
-        <ul>
+        <ListGroup>
           {suggestions.map(suggestion => (
-            <li key={suggestion.id}>{suggestion.title}</li>
+            <ListGroupItem key={suggestion.id}>
+              <SuggestionsListItem suggestion={suggestion} />
+            </ListGroupItem>
           ))}
-        </ul>
+        </ListGroup>
       ) : (
         <div>
           <Heading
