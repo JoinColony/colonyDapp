99bbfcadd10e844a8fd8adb348d2597fdc9d2fb9
Curtis Olson <curtis@colony.io>
2020-01-22 17:09:33 -0600

    Reorder tabs

--- src/modules/dashboard/components/ColonyHome/ColonyHome.tsx
+++ src/modules/dashboard/components/ColonyHome/ColonyHome.tsx
@@ -157,16 +157,23 @@ const ColonyHome = ({
           <TabList
             extra={activeTab === TabName.TransactionsTab ? noFilter : null}
           >
+            <Tab onClick={() => setActiveTab(TabName.SuggestionsTab)}>
+              <FormattedMessage {...MSG.tabSuggestions} />
+            </Tab>
             <Tab onClick={() => setActiveTab(TabName.TasksTab)}>
               <FormattedMessage {...MSG.tabContribute} />
             </Tab>
             <Tab onClick={() => setActiveTab(TabName.TransactionsTab)}>
               <FormattedMessage {...MSG.tabTransactions} />
             </Tab>
-            <Tab onClick={() => setActiveTab(TabName.SuggestionsTab)}>
-              <FormattedMessage {...MSG.tabSuggestions} />
-            </Tab>
           </TabList>
+          <TabPanel>
+            <Suggestions
+              colonyAddress={colony.colonyAddress}
+              colonyName={colony.colonyName}
+              domainId={filteredDomainId}
+            />
+          </TabPanel>
           <TabPanel>
             <TabContribute
               canCreateTask={canCreateTask}
@@ -177,13 +184,6 @@ const ColonyHome = ({
           </TabPanel>
           <TabPanel>
             <Transactions colonyAddress={colony.colonyAddress} />
-          </TabPanel>
-          <TabPanel>
-            <Suggestions
-              colonyAddress={colony.colonyAddress}
-              colonyName={colony.colonyName}
-              domainId={filteredDomainId}
-            />
           </TabPanel>
         </Tabs>
       </main>
