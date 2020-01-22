e2fc4839864963418b7e26ab4a5519f375d4694c
Christian Maniewski <code@chmanie.com>
2020-01-22 18:40:17 +0100

    Add suggestion status mutations

--- src/modules/dashboard/components/SuggestionsList/SuggestionsList.tsx
+++ src/modules/dashboard/components/SuggestionsList/SuggestionsList.tsx
@@ -1,17 +1,24 @@
-import React, { useMemo } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import { defineMessages } from 'react-intl';
+import { useHistory } from 'react-router-dom';
 
 import { COLONY_TOTAL_BALANCE_DOMAIN_ID } from '~constants';
 import Heading from '~core/Heading';
 import ListGroup from '~core/ListGroup';
 import ListGroupItem from '~core/ListGroup/ListGroupItem';
 import { SpinnerLoader } from '~core/Preloaders';
+import { withDialog } from '~core/Dialog';
+import { OpenDialog } from '~core/Dialog/types';
 import SuggestionsListItem from '~dashboard/SuggestionsListItem';
 import {
+  cacheUpdates,
   useColonySuggestionsQuery,
   useLoggedInUser,
+  useSetSuggestionStatusMutation,
+  useCreateTaskFromSuggestionMutation,
   Domain,
   OneSuggestion,
+  SuggestionStatus,
 } from '~data/index';
 import { Address } from '~types/index';
 import { useDataFetcher } from '~utils/hooks';
@@ -33,9 +40,15 @@ const MSG = defineMessages({
   },
 });
 
-interface Props {
+interface InProps {
   colonyAddress: Address;
+  colonyName: string;
   domainId: Domain['ethDomainId'];
+}
+
+interface Props extends InProps {
+  // Injected via `withDialog`
+  openDialog: OpenDialog;
 }
 
 const createdAtDesc = (
@@ -45,7 +58,13 @@ const createdAtDesc = (
 
 const displayName = 'Dashboard.SuggestionsList';
 
-const SuggestionsList = ({ colonyAddress, domainId }: Props) => {
+const SuggestionsList = ({
+  colonyAddress,
+  colonyName,
+  domainId,
+  openDialog,
+}: Props) => {
+  const history = useHistory();
   const { walletAddress } = useLoggedInUser();
   const { data, loading } = useColonySuggestionsQuery({
     variables: { colonyAddress },
@@ -55,6 +74,42 @@ const SuggestionsList = ({ colonyAddress, domainId }: Props) => {
     domainsAndRolesFetcher,
     [colonyAddress],
     [colonyAddress],
+  );
+
+  const [setSuggestionStatus] = useSetSuggestionStatusMutation();
+  const [createTaskFromSuggestion] = useCreateTaskFromSuggestionMutation({
+    update: cacheUpdates.createTask(colonyAddress),
+  });
+  const handleNotPlanned = useCallback(
+    async (id: string) => {
+      await openDialog('ConfirmDialog').afterClosed();
+      return setSuggestionStatus({
+        variables: { input: { id, status: SuggestionStatus.NotPlanned } },
+      });
+    },
+    [openDialog, setSuggestionStatus],
+  );
+  const handleDeleted = useCallback(
+    async (id: string) => {
+      await openDialog('ConfirmDialog').afterClosed();
+      return setSuggestionStatus({
+        variables: { input: { id, status: SuggestionStatus.Deleted } },
+      });
+    },
+    [openDialog, setSuggestionStatus],
+  );
+  const handleCreateTask = useCallback(
+    async (id: string) => {
+      await openDialog('ConfirmDialog').afterClosed();
+      const { data: createTaskData } = await createTaskFromSuggestion({
+        variables: { input: { id } },
+      });
+      if (createTaskData && createTaskData.createTaskFromSuggestion) {
+        const { id: taskId } = createTaskData.createTaskFromSuggestion;
+        history.push(`/colony/${colonyName}/task/${taskId}`);
+      }
+    },
+    [colonyName, createTaskFromSuggestion, history, openDialog],
   );
 
   const allSuggestions = (data && data.colony.suggestions) || [];
@@ -80,6 +135,9 @@ const SuggestionsList = ({ colonyAddress, domainId }: Props) => {
           {suggestions.map(suggestion => (
             <ListGroupItem appearance={{ padding: 'none' }} key={suggestion.id}>
               <SuggestionsListItem
+                onNotPlanned={handleNotPlanned}
+                onDeleted={handleDeleted}
+                onCreateTask={handleCreateTask}
                 suggestion={suggestion}
                 domains={domains}
                 walletAddress={walletAddress}
@@ -105,4 +163,4 @@ const SuggestionsList = ({ colonyAddress, domainId }: Props) => {
 
 SuggestionsList.displayName = displayName;
 
-export default SuggestionsList;
+export default (withDialog() as any)(SuggestionsList) as FC<InProps>;
