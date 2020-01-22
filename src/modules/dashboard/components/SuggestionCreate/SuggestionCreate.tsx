bf124ea8c074df0829e34a01b9fc633c4f6c0b70
Curtis Olson <curtis@colony.io>
2020-01-22 15:43:40 -0600

    Add `Button` to create suggestion form

--- src/modules/dashboard/components/SuggestionCreate/SuggestionCreate.tsx
+++ src/modules/dashboard/components/SuggestionCreate/SuggestionCreate.tsx
@@ -1,22 +1,33 @@
 import React, { useCallback, useRef } from 'react';
 import { FormikHelpers } from 'formik';
 import { defineMessages } from 'react-intl';
+import { useHistory } from 'react-router';
 import * as yup from 'yup';
 
-import { useHistory } from 'react-router';
 import { COLONY_TOTAL_BALANCE_DOMAIN_ID, ROOT_DOMAIN } from '~constants';
+import Button from '~core/Button';
 import { Form, Input } from '~core/Fields';
 import {
-  Domain,
-  useCreateSuggestionMutation,
   ColonySuggestionsDocument,
   ColonySuggestionsQueryVariables,
+  Domain,
+  useCreateSuggestionMutation,
   useLoggedInUser,
 } from '~data/index';
 import { Address } from '~types/index';
 import unfinishedProfileOpener from '~users/UnfinishedProfile';
 
+import styles from './SuggestionCreate.css';
+
 const MSG = defineMessages({
+  buttonCreateSuggestion: {
+    id: 'Dashboard.SuggestionCreate.buttonCreateSuggestion',
+    defaultMessage: 'Create Suggestion',
+  },
+  formErrorText: {
+    id: 'Dashboard.SuggestionCreate.formErrorText',
+    defaultMessage: 'There was an error creating your suggestion.',
+  },
   inputLabelTitle: {
     id: 'Dashboard.SuggestionCreate.inputLabelTitle',
     defaultMessage: 'Suggest features, report bugs, or propose tasks',
@@ -80,14 +91,27 @@ const SuggestionCreate = ({ colonyAddress, domainId }: Props) => {
       onSubmit={handleSubmit}
       validationSchema={validationSchema}
     >
-      <div onFocus={handleUnclaimedProfile}>
-        <Input
-          appearance={{ theme: 'fat' }}
-          innerRef={inputRef}
-          label={MSG.inputLabelTitle}
-          name="title"
-        />
-      </div>
+      {({ isSubmitting, isValid }) => (
+        <div className={styles.horizontalForm} onFocus={handleUnclaimedProfile}>
+          <div className={styles.formItem}>
+            <Input
+              appearance={{ theme: 'fat' }}
+              innerRef={inputRef}
+              label={MSG.inputLabelTitle}
+              name="title"
+            />
+          </div>
+          <div className={styles.formItem}>
+            <Button
+              appearance={{ theme: 'primary', size: 'large' }}
+              text={MSG.buttonCreateSuggestion}
+              type="submit"
+              disabled={!isValid}
+              loading={isSubmitting}
+            />
+          </div>
+        </div>
+      )}
     </Form>
   );
 };
