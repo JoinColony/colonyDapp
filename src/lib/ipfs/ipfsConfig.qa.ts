cb6c841b602427d99d2f2e1aeb257ec76497355f
Christian Maniewski <code@chmanie.com>
2020-01-23 12:30:15 +0100

    Upgrade IPFS to v0.40.0

--- src/lib/ipfs/ipfsConfig.qa.ts
+++ src/lib/ipfs/ipfsConfig.qa.ts
@@ -2,23 +2,9 @@ const config = () => ({
   repo: 'colonyIpfs',
   // config gets merged with the IPFS default config
   config: {
-    Addresses: {
-      Swarm: [
-        '/dns4/qa.colony.io/tcp/9090/wss/p2p-websocket-star/',
-        '/dns4/qa.colony.io/tcp/9091/wss/p2p-webrtc-star/',
-      ],
-    },
     Bootstrap: [],
-    Discovery: {
-      webRTCStar: {
-        Enabled: true,
-      },
-    },
   },
   preload: { enabled: false, addresses: [] },
-  EXPERIMENTAL: {
-    pubsub: true,
-  },
 });
 
 export default config;
