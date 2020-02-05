f84bcd39b165bc492a8841c590f2890928feb565
Raul Glogovetan <raul@glogovetan.com>
2020-02-05 18:53:16 +0200

    Add: `TaskFinalize` calculate enough domain balance

--- src/modules/dashboard/components/TaskFinalize/TaskFinalize.tsx
+++ src/modules/dashboard/components/TaskFinalize/TaskFinalize.tsx
@@ -1,12 +1,20 @@
 import React, { useCallback, useState } from 'react';
 import { defineMessages } from 'react-intl';
+import moveDecimal from 'move-decimal-point';
 
 import Button from '~core/Button';
-import { ActionTypes } from '~redux/index';
-import { mergePayload } from '~utils/actions';
-import { AnyTask } from '~data/index';
+
 import { Address } from '~types/index';
+import {
+  AnyTask,
+  Payouts,
+  TokenWithBalances,
+  useTokenBalancesForDomainsQuery,
+} from '~data/index';
+import { ActionTypes } from '~redux/index';
 import { useAsyncFunction } from '~utils/hooks';
+import { mergePayload } from '~utils/actions';
+import { bnLessThan } from '~utils/numbers';
 
 const MSG = defineMessages({
   finalizeTask: {
@@ -20,10 +28,46 @@ const displayName = 'dashboard.TaskFinalize';
 interface Props {
   draftId: AnyTask['id'];
   colonyAddress: Address;
+  ethDomainId: number,
+  payouts: Payouts,
 }
 
-const TaskFinalize = ({ draftId, colonyAddress }: Props) => {
+const TaskFinalize = ({
+  draftId,
+  colonyAddress,
+  ethDomainId,
+  payouts,
+}: Props) => {
   const [isLoading, setIsLoading] = useState(false);
+
+  const tokenAddresses = payouts.map(({ token }) => token.address);
+  const { data: tokenBalances } = useTokenBalancesForDomainsQuery({
+    variables: {
+      colonyAddress,
+      tokenAddresses,
+      domainIds: [ethDomainId],
+    },
+  });
+  const enoughFundsAvailable = payouts.every(({ amount, tokenAddress }) => {
+    if (!tokenBalances) {
+      return false;
+    }
+    /*
+     * @NOTE About the types ignore
+     *
+     * For some reason I get a TS error about balances prop not existing, even though
+     * it's available on the union type. I must be missing something...
+     */
+    // @ts-ignore
+    const { balances: domainBalances, decimals } = tokenBalances.tokens.find(
+      ({ address: domainTokenAddress }) => domainTokenAddress === tokenAddress,
+    ) as TokenWithBalances;
+    return domainBalances.every(
+      ({ amount: availableDomainAmount }) =>
+        !bnLessThan(availableDomainAmount, moveDecimal(amount, decimals || 18)),
+    );
+  });
+
   const transform = useCallback(
     mergePayload({
       colonyAddress,
@@ -37,6 +81,7 @@ const TaskFinalize = ({ draftId, colonyAddress }: Props) => {
     success: ActionTypes.TASK_FINALIZE_SUCCESS,
     transform,
   });
+
   const handleOnClick = async () => {
     setIsLoading(true);
     try {
@@ -46,11 +91,13 @@ const TaskFinalize = ({ draftId, colonyAddress }: Props) => {
       setIsLoading(false);
     }
   };
+
   return (
     <Button
       text={MSG.finalizeTask}
       onClick={handleOnClick}
       loading={isLoading}
+      disabled={!enoughFundsAvailable}
     />
   );
 };
