1933786b31622309b5c6af11d7412328fa543017
Raul Glogovetan <raul@glogovetan.com>
2020-02-06 15:31:13 +0200

    Add: `TaskFinalizeDialog` component and register it

---
+++ src/modules/dashboard/components/TaskFinalize/TaskFinalizeDialog.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import Button from '~core/Button';
+import Heading from '~core/Heading';
+import Dialog, { DialogSection } from '~core/Dialog';
+
+const MSG = defineMessages({
+  heading: {
+    id: 'dashboard.TaskFinalizeDialog.heading',
+    defaultMessage: 'Insufficient Domain Funds',
+  },
+  description: {
+    id: 'dashboard.TaskFinalizeDialog.description',
+    defaultMessage: `The current domain does not have enough funds in it's pot,
+      in order to cover this task's payout`,
+  },
+});
+
+interface Props {
+  cancel: () => void;
+  close: () => void;
+}
+
+const TaskFinalizeDialog = ({ cancel, close }: Props) => (
+  <Dialog cancel={cancel}>
+    <DialogSection appearance={{ border: 'bottom' }}>
+      <Heading
+        appearance={{ size: 'medium', margin: 'none' }}
+        text={MSG.heading}
+      />
+    </DialogSection>
+    <DialogSection appearance={{ border: 'bottom' }}>
+      <FormattedMessage {...MSG.defaultText} />
+    </DialogSection>
+    <DialogSection appearance={{ align: 'right' }}>
+      <Button
+        appearance={{
+          theme: 'primary',
+          size: 'large',
+        }}
+        autoFocus
+        onClick={() => close()}
+        text={{ id: 'button.ok' }}
+      />
+    </DialogSection>
+  </Dialog>
+);
+
+export default TaskFinalizeDialog as FC<Props>;
