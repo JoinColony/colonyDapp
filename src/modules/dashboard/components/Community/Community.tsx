ce6f4495fa7b9c1ce5cf22e4fdf165655249c003
Raul Glogovetan <raul@glogovetan.com>
2020-02-06 20:43:04 +0200

    Add: `Community` component display list subscribed users

--- src/modules/dashboard/components/Community/Community.tsx
+++ src/modules/dashboard/components/Community/Community.tsx
@@ -1,25 +1,71 @@
 import React, { FC } from 'react';
+import { defineMessages } from 'react-intl';
 
-// import { Address } from '~types/index';
+import { Address } from '~types/index';
+import { useColonySubscribedUsersQuery } from '~data/index';
 
-// import { useDataFetcher } from '~utils/hooks';
-// import {
-//   colonyTransactionsFetcher,
-//   colonyUnclaimedTransactionsFetcher,
-// } from '../../fetchers';
+import { Table, TableBody } from '~core/Table';
+import { SpinnerLoader } from '~core/Preloaders';
+import UserListItem from '~admin/UserListItem';
 
-// import TransactionList from '~core/TransactionList';
+import styles from './Community.css';
 
-// import styles from './Transactions.css';
+const MSG = defineMessages({
+  loading: {
+    id: 'dashboard.Community.loading',
+    defaultMessage: "Loading Colony's users...",
+  },
+});
 
-// interface Props {
-//   colonyAddress: Address;
-// }
+interface Props {
+  colonyAddress: Address;
+}
 
-const Community = () => {
-  return <div />;
+const Community = ({ colonyAddress }: Props) => {
+  const {
+    data: colonySubscribedUsers,
+    loading,
+  } = useColonySubscribedUsersQuery({
+    variables: {
+      colonyAddress,
+    },
+  });
+
+  if (!colonySubscribedUsers || loading) {
+    return (
+      <div className={styles.main}>
+        <SpinnerLoader
+          loadingText={MSG.loading}
+          appearance={{ size: 'massive', theme: 'primary' }}
+        />
+      </div>
+    );
+  }
+
+  const {
+    colony: { subscribedUsers },
+  } = colonySubscribedUsers;
+
+  return (
+    <div className={styles.main}>
+      <Table scrollable>
+        <TableBody className={styles.tableBody}>
+          {subscribedUsers.map(({ id: userAddress }) => (
+            <UserListItem
+              address={userAddress}
+              key={userAddress}
+              showDisplayName
+              showMaskedAddress
+              showUsername
+              showInfo={false}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 Community.displayName = 'admin.Community';
 
-export default Community as FC<any>;
+export default Community as FC<Props>;
