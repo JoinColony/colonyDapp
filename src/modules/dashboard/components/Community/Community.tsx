e853ea39d88889841f9fc8a3c2d1e0a386ad8d07
Raul Glogovetan <raul@glogovetan.com>
2020-02-06 18:25:37 +0200

    Add: initial dashboard `Community` component

---
+++ src/modules/dashboard/components/Community/Community.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+
+// import { Address } from '~types/index';
+
+// import { useDataFetcher } from '~utils/hooks';
+// import {
+//   colonyTransactionsFetcher,
+//   colonyUnclaimedTransactionsFetcher,
+// } from '../../fetchers';
+
+// import TransactionList from '~core/TransactionList';
+
+// import styles from './Transactions.css';
+
+// interface Props {
+//   colonyAddress: Address;
+// }
+
+const Community = () => {
+  return <div />;
+};
+
+Community.displayName = 'admin.Community';
+
+export default Community as FC<any>;
