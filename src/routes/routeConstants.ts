2602435f255ff6d537bfb30ecca614fa109301f5
Curtis Olson <curtis@colony.io>
2020-02-11 17:01:41 -0600

    Add route for programs

--- src/routes/routeConstants.ts
+++ src/routes/routeConstants.ts
@@ -1,5 +1,6 @@
 export const CONNECT_ROUTE = '/connect';
 export const COLONY_HOME_ROUTE = '/colony/:colonyName';
+export const PROGRAM_ROUTE = `${COLONY_HOME_ROUTE}/program/:programId`;
 export const TASK_ROUTE = `${COLONY_HOME_ROUTE}/task/:draftId`;
 export const CREATE_COLONY_ROUTE = '/create-colony';
 export const CREATE_USER_ROUTE = '/create-user';
