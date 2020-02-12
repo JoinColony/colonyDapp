3dee31b4efe04e70e39c3ef0d7e4019c7abdff37
Curtis Olson <curtis@colony.io>
2020-02-12 11:43:13 -0600

    Improve display of long program titles

--- src/modules/dashboard/components/ColonyHome/ColonyMeta/ColonyPrograms.css.d.ts
+++ src/modules/dashboard/components/ColonyHome/ColonyMeta/ColonyPrograms.css.d.ts
@@ -1,4 +1,5 @@
 export const main: string;
 export const programsNav: string;
+export const baseStyles: string;
+export const draft: string;
 export const navLink: string;
-export const navLinkDraft: string;
