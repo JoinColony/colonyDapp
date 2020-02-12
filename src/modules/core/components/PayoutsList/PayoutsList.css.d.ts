e6502b1d7f73455c54a1c76159501e3db98ed607
Raul Glogovetan <raul@glogovetan.com>
2020-02-12 23:53:50 +0200

    Add: `PayoutsList` use info popover for token display

--- src/modules/core/components/PayoutsList/PayoutsList.css.d.ts
+++ src/modules/core/components/PayoutsList/PayoutsList.css.d.ts
@@ -3,3 +3,5 @@ export const payoutNumber: string;
 export const popoverContent: string;
 export const payoutPopover: string;
 export const native: string;
+export const tokenInfo: string;
+export const tokenPayout: string;
