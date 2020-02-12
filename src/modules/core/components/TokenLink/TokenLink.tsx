2f2ca253e260c61d1bef4a3c8b47b43adabdae38
Raul Glogovetan <raul@glogovetan.com>
2020-02-12 23:05:59 +0200

    Add: `TokenLink` core component

---
+++ src/modules/core/components/TokenLink/TokenLink.tsx
@@ -0,0 +1,51 @@
+import { MessageDescriptor, MessageValues } from 'react-intl';
+import React from 'react';
+
+import { DEFAULT_NETWORK } from '~constants';
+import ExternalLink from '~core/ExternalLink';
+import { getEtherscanLink } from '~utils/external';
+
+interface Props {
+  /*
+   * Allows for link style customization (Eg: we need to disquise the link as a button)
+   * Don't abuse it!
+   */
+  className?: string;
+
+  /** Address of the token to link to */
+  tokenAddress: string;
+
+  /** Optionally override current network */
+  network?: string;
+
+  /** A string or a `messageDescriptor` that make up the link's text. Defaults to `tokenAddress`. */
+  text?: MessageDescriptor | string;
+
+  /** Values for text (react-intl interpolation) */
+  textValues?: MessageValues;
+}
+
+const displayName = 'TokenLink';
+
+const TokenLink = ({
+  className,
+  tokenAddress,
+  network = DEFAULT_NETWORK,
+  text,
+  textValues,
+}: Props) => (
+  <ExternalLink
+    className={className}
+    href={getEtherscanLink({
+      network,
+      linkType: 'token',
+      addressOrHash: tokenAddress,
+    })}
+    text={text || tokenAddress}
+    textValues={textValues}
+  />
+);
+
+TokenLink.displayName = displayName;
+
+export default TokenLink;
