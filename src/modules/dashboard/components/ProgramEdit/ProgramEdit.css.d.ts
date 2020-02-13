a1c6d65cb22f3172f415671e3b663ca99f159476
Curtis Olson <curtis@colony.io>
2020-02-12 18:03:25 -0600

    Levels section

--- src/modules/dashboard/components/ProgramEdit/ProgramEdit.css.d.ts
+++ src/modules/dashboard/components/ProgramEdit/ProgramEdit.css.d.ts
@@ -2,3 +2,4 @@ export const formActions: string;
 export const headingContainer: string;
 export const cancelButtonContainer: string;
 export const actionButtons: string;
+export const levelsContainer: string;
