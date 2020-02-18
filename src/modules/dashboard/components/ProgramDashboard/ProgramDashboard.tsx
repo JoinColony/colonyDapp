25fda4934b492f051301d133d5ba9ef3f891a671
Curtis Olson <curtis@colony.io>
2020-02-17 19:32:53 -0600

    Program dashboard for admins & non-admins

--- src/modules/dashboard/components/ProgramDashboard/ProgramDashboard.tsx
+++ src/modules/dashboard/components/ProgramDashboard/ProgramDashboard.tsx
@@ -1,15 +1,56 @@
 import React from 'react';
+import { defineMessages } from 'react-intl';
 
+import Button from '~core/Button';
+import Heading from '~core/Heading';
 import { OneProgram } from '~data/index';
 
+import styles from './ProgramDashboard.css';
+
+const MSG = defineMessages({
+  buttonJoinProgram: {
+    id: 'Dashboard.ProgramDashboard.buttonJoinProgram',
+    defaultMessage: 'Join Program',
+  },
+  linkEdit: {
+    id: 'Dashboard.ProgramDashboard.linkEdit',
+    defaultMessage: 'Edit',
+  },
+});
+
 interface Props {
+  canAdmin: boolean;
   program: OneProgram;
+  toggleEditMode: () => void;
 }
 
 const displayName = 'Dashboard.ProgramDashboard';
 
-const ProgramDashboard = ({ program }: Props) => (
-  <div>{JSON.stringify(program)}</div>
+const ProgramDashboard = ({
+  canAdmin,
+  program: { title, description },
+  toggleEditMode,
+}: Props) => (
+  <div>
+    <div className={styles.titleContainer}>
+      <div className={styles.headingContainer}>
+        <Heading appearance={{ margin: 'none', size: 'medium' }} text={title} />
+        {canAdmin && (
+          <div className={styles.editButtonContainer}>
+            <Button
+              appearance={{ theme: 'blue' }}
+              onClick={toggleEditMode}
+              text={MSG.linkEdit}
+            />
+          </div>
+        )}
+      </div>
+      <div>
+        <Button text={MSG.buttonJoinProgram} />
+      </div>
+    </div>
+    {description && <p>{description}</p>}
+  </div>
 );
 
 ProgramDashboard.displayName = displayName;
