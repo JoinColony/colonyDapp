5296361fd32629b4c7b0eefece5fecd2d9f02d59
Curtis Olson <curtis@colony.io>
2020-02-24 08:19:59 -0600

    Refactor list item into its own component

--- src/modules/dashboard/components/ProgramLevelsEdit/LevelsList.tsx
+++ src/modules/dashboard/components/ProgramLevelsEdit/LevelsList.tsx
@@ -6,37 +6,15 @@ import React, {
   useState,
 } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { defineMessages } from 'react-intl';
 import nanoid from 'nanoid';
 
-import Button from '~core/Button';
-import Heading from '~core/Heading';
-import Icon from '~core/Icon';
 import ListGroup, { ListGroupItem } from '~core/ListGroup';
 import { OneProgram, useReorderProgramLevelsMutation } from '~data/index';
-
-import styles from './LevelsList.css';
-
-const MSG = defineMessages({
-  dragHandleTitle: {
-    id: 'dashboard.LevelsList.dragHandleTitle',
-    defaultMessage: 'Click, hold, and drag to re-order levels.',
-  },
-  linkView: {
-    id: 'dashboard.LevelsList.linkView',
-    defaultMessage: 'View',
-  },
-  untitledLevel: {
-    id: 'dashboard.LevelsList.untitledLevel',
-    defaultMessage: 'Untitled Level',
-  },
-});
+import LevelsListItem from './LevelsListItem';
 
 interface Props {
   colonyName: string;
-  programId: OneProgram['id'];
-  levelIds: OneProgram['levelIds'];
-  levels: OneProgram['levels'];
+  program: OneProgram;
 }
 
 const hasOrderChanged = (arr1: string[], arr2: string[]): boolean => {
@@ -48,15 +26,15 @@ const displayName = 'dashboard.LevelsList';
 
 const LevelsList = ({
   colonyName,
-  programId,
-  levelIds: levelIdsProp,
-  levels: unsortedLevels,
+  program: { id: programId, levelIds: levelIdsProp, levels: unsortedLevels },
 }: Props) => {
   // Use local state to optimize optimistic UI - avoid FOIC `onDragEnd`
-  const [levelIds, setLevelIds] = useState<Props['levelIds']>(levelIdsProp);
+  const [levelIds, setLevelIds] = useState<Props['program']['levelIds']>(
+    levelIdsProp,
+  );
 
   const { current: droppdableId } = useRef<string>(nanoid());
-  const lastLevelIdsRef = useRef<Props['levelIds']>(levelIds);
+  const lastLevelIdsRef = useRef<Props['program']['levelIds']>(levelIds);
 
   const [
     reorderProgramLevels,
@@ -114,49 +92,31 @@ const LevelsList = ({
         {({ innerRef: droppableInnerRef, droppableProps }) => (
           <div ref={droppableInnerRef} {...droppableProps}>
             <ListGroup appearance={{ gaps: 'true' }}>
-              {levels.map(({ id: levelId, title }, idx) => {
-                const levelUrl = `/colony/${colonyName}/program/${programId}/level/${levelId}`;
-                return (
-                  <Draggable
-                    key={levelId}
-                    draggableId={`draggableLevelId${levelId}`}
-                    index={idx}
-                  >
-                    {({
-                      innerRef: draggableInnerRef,
-                      draggableProps,
-                      dragHandleProps,
-                    }) => (
-                      <ListGroupItem
-                        innerRef={draggableInnerRef}
-                        {...draggableProps}
-                      >
-                        <div className={styles.listItemInner}>
-                          <div className={styles.dragHandleContainer}>
-                            <div {...dragHandleProps}>
-                              <Icon
-                                className={styles.dragHandleIcon}
-                                name="drag-handle"
-                                title={MSG.dragHandleTitle}
-                              />
-                            </div>
-                          </div>
-                          {/* @todo Add level achievement here */}
-                          <div className={styles.itemContentContainer}>
-                            <Heading
-                              appearance={{ margin: 'none', size: 'medium' }}
-                              text={title || MSG.untitledLevel}
-                            />
-                          </div>
-                          <div className={styles.itemActionContainer}>
-                            <Button linkTo={levelUrl} text={MSG.linkView} />
-                          </div>
-                        </div>
-                      </ListGroupItem>
-                    )}
-                  </Draggable>
-                );
-              })}
+              {levels.map((level, idx) => (
+                <Draggable
+                  key={level.id}
+                  draggableId={`draggableLevelId${level.id}`}
+                  index={idx}
+                >
+                  {({
+                    innerRef: draggableInnerRef,
+                    draggableProps,
+                    dragHandleProps,
+                  }) => (
+                    <ListGroupItem
+                      innerRef={draggableInnerRef}
+                      {...draggableProps}
+                    >
+                      <LevelsListItem
+                        colonyName={colonyName}
+                        dragHandleProps={dragHandleProps}
+                        level={level}
+                        programId={programId}
+                      />
+                    </ListGroupItem>
+                  )}
+                </Draggable>
+              ))}
             </ListGroup>
           </div>
         )}
