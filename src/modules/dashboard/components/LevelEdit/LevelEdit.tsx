22fa872f7e74fedf221240b3db9543d60ce97961
Christian Maniewski <code@chmanie.com>
2020-02-25 17:20:45 +0100

    Add LevelEdit base layout

--- src/modules/dashboard/components/LevelEdit/LevelEdit.tsx
+++ src/modules/dashboard/components/LevelEdit/LevelEdit.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-/* import { defineMessages } from 'react-intl'; */
+import { defineMessages } from 'react-intl';
 import { useParams } from 'react-router-dom';
 
-/* const MSG = defineMessages({}); */
+import Card from '~core/Card';
+import Heading from '~core/Heading';
+import CenteredTemplate from '~pages/CenteredTemplate';
+
+import styles from './LevelEdit.css';
+
+const MSG = defineMessages({
+  heading: {
+    id: 'dashboard.LevelEdit.heading',
+    defaultMessage: 'Edit Level',
+  },
+});
 
 const LevelEdit = () => {
   const { programId } = useParams();
-  return <>{programId}</>;
+  return (
+    <CenteredTemplate>
+      <div className={styles.main}>
+        <Heading
+          appearance={{ margin: 'small', size: 'medium' }}
+          text={MSG.heading}
+        />
+        <Card>{programId}</Card>
+      </div>
+    </CenteredTemplate>
+  );
 };
 
 export default LevelEdit;
