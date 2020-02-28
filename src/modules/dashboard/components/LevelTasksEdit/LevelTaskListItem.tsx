9d74fe0606c47790338b4d282518140d858ad4d6
Curtis Olson <curtis@colony.io>
2020-02-28 15:44:22 -0600

    Edit level tasks

--- src/modules/dashboard/components/LevelTasksEdit/LevelTaskListItem.tsx
+++ src/modules/dashboard/components/LevelTasksEdit/LevelTaskListItem.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { OnePersistentTask } from '~data/index';
 
+import TaskDisplay from './TaskDisplay';
+import TaskEdit from './TaskEdit';
+
 interface Props {
+  isEditing?: boolean;
   persistentTask: OnePersistentTask;
 }
 
 const displayName = 'dashboard.LevelTasksEdit.LevelTaskListItem';
 
-const LevelTaskListItem = ({ persistentTask }: Props) => (
-  <div>{persistentTask.id}</div>
-);
+const LevelTaskListItem = ({
+  isEditing: isEditingProp = false,
+  persistentTask,
+}: Props) => {
+  const [isEditing, setIsEditing] = useState<boolean>(isEditingProp);
+  return isEditing ? (
+    <TaskEdit persistentTask={persistentTask} setIsEditing={setIsEditing} />
+  ) : (
+    <TaskDisplay persistentTask={persistentTask} setIsEditing={setIsEditing} />
+  );
+};
 
 LevelTaskListItem.displayName = displayName;
 
