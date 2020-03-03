3014c9c731e41f6c4958b35fc6a978b32c71a6a0
Christian Maniewski <code@chmanie.com>
2020-03-03 23:36:16 +0100

    Add all LevelEdit input components

--- src/modules/dashboard/components/LevelEdit/LevelEdit.tsx
+++ src/modules/dashboard/components/LevelEdit/LevelEdit.tsx
@@ -1,30 +1,175 @@
-import React from 'react';
-import { defineMessages } from 'react-intl';
+import React, { useCallback } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
 import { useParams } from 'react-router-dom';
+import * as yup from 'yup';
+import { FormikProps } from 'formik';
 
-import Card from '~core/Card';
+import Button from '~core/Button';
 import Heading from '~core/Heading';
+import { Form, Input, InputLabel, Textarea, FormStatus } from '~core/Fields';
+import { SpinnerLoader } from '~core/Preloaders';
 import CenteredTemplate from '~pages/CenteredTemplate';
+import { useLevelQuery, useEditLevelMutation } from '~data/index';
 
 import styles from './LevelEdit.css';
+
+import BadgePicker from './BadgePicker';
 
 const MSG = defineMessages({
   heading: {
     id: 'dashboard.LevelEdit.heading',
     defaultMessage: 'Edit Level',
   },
+  labelTitle: {
+    id: 'dashboard.LevelEdit.labelTitle',
+    defaultMessage: 'Level Title',
+  },
+  labelDescription: {
+    id: 'dashboard.LevelEdit.labelDescription',
+    defaultMessage: 'Level Description',
+  },
+  labelNumRequiredSteps: {
+    id: 'dashboard.LevelEdit.labelNumRequiredSteps',
+    defaultMessage: 'Set Requirement',
+  },
+  errorValidateNumStepsMax: {
+    id: 'dashboard.LevelEdit.errorValidateNumStepsMax',
+    defaultMessage: 'Can not be more than total steps',
+  },
+  explainerRequirement: {
+    id: 'dashboard.LevelEdit.explainerRequirement',
+    defaultMessage: `To complete the level and earn the achievement, users must complete {numRequiredSteps} of {numTotalSteps} tasks.`,
+  },
+});
+
+interface FormValues {
+  achievement: string;
+  title: string;
+  description: string;
+  numRequiredSteps: string;
+  numTotalSteps: number;
+}
+
+const validationSchema = yup.object({
+  title: yup.string().required(),
+  description: yup.string(),
+  achievement: yup.string(),
+  numRequiredSteps: yup
+    .number()
+    .min(0)
+    .max(yup.ref('numTotalSteps'), () => MSG.errorValidateNumStepsMax),
 });
 
 const LevelEdit = () => {
-  const { programId } = useParams();
+  const { colonyName, levelId, programId } = useParams();
+
+  const { data } = useLevelQuery({ variables: { id: levelId } });
+  const [editLevel] = useEditLevelMutation();
+
+  const handleUpdate = useCallback(
+    ({ achievement, title, description, numRequiredSteps }: FormValues) =>
+      editLevel({
+        variables: {
+          input: {
+            id: levelId,
+            achievement,
+            title,
+            description,
+            numRequiredSteps: parseInt(numRequiredSteps, 10),
+          },
+        },
+      }),
+    [editLevel, levelId],
+  );
+
+  if (!data) return <SpinnerLoader />;
+
+  const {
+    achievement,
+    description,
+    numRequiredSteps,
+    stepIds,
+    title,
+  } = data.level;
+  const numTotalSteps = stepIds.length;
+
   return (
     <CenteredTemplate>
       <div className={styles.main}>
-        <Heading
-          appearance={{ margin: 'small', size: 'medium' }}
-          text={MSG.heading}
-        />
-        <Card>{programId}</Card>
+        <Form
+          enableReinitialize
+          key={levelId}
+          initialValues={
+            {
+              achievement,
+              description: description || '',
+              title: title || '',
+              numRequiredSteps: numRequiredSteps
+                ? numRequiredSteps.toString()
+                : '',
+              numTotalSteps,
+            } as FormValues
+          }
+          onSubmit={handleUpdate}
+          validationSchema={validationSchema}
+        >
+          {({
+            dirty,
+            isSubmitting,
+            isValid,
+            status: formStatus,
+          }: FormikProps<FormValues>) => (
+            <>
+              <div className={styles.formActions}>
+                <div className={styles.headingContainer}>
+                  <Heading appearance={{ size: 'medium' }} text={MSG.heading} />
+                </div>
+                <div>
+                  <Button
+                    appearance={{ theme: 'blue' }}
+                    text={{ id: 'button.cancel' }}
+                    linkTo={`/colony/${colonyName}/program/${programId}`}
+                  />
+                  <Button
+                    disabled={!dirty || !isValid}
+                    loading={isSubmitting}
+                    text={{ id: 'button.save' }}
+                    type="submit"
+                  />
+                </div>
+              </div>
+              <FormStatus status={formStatus} />
+              <Input
+                appearance={{ theme: 'fat' }}
+                label={MSG.labelTitle}
+                name="title"
+              />
+              <Textarea
+                appearance={{ resizable: 'vertical' }}
+                label={MSG.labelDescription}
+                name="description"
+              />
+              <BadgePicker name="achievement" />
+              <div className={styles.numRequiredStepsInput}>
+                <InputLabel label={MSG.labelNumRequiredSteps} />
+                <p>
+                  <FormattedMessage
+                    {...MSG.explainerRequirement}
+                    values={{
+                      numRequiredSteps: numRequiredSteps || 0,
+                      numTotalSteps,
+                    }}
+                  />
+                </p>
+                <Input
+                  elementOnly
+                  appearance={{ theme: 'underlined' }}
+                  name="numRequiredSteps"
+                />
+              </div>
+            </>
+          )}
+        </Form>
       </div>
     </CenteredTemplate>
   );
