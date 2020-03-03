316c175588127f9bac59ac24ae472b2eab75a5bc
Curtis Olson <curtis@colony.io>
2020-03-02 18:24:28 -0600

    Distinct urls for level view vs edit

---
+++ src/modules/dashboard/components/LevelDashboard/LevelDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const displayName = 'dashboard.LevelDashboard';
+
+const LevelDashboard = () => <div />;
+
+LevelDashboard.displayName = displayName;
+
+export default LevelDashboard;
