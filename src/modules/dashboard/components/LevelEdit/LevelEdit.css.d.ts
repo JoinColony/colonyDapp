d6506185bce24e6335ca66898189dc20eab66379
Christian Maniewski <code@chmanie.com>
2020-03-05 00:09:44 +0100

    Fix required steps label mess
    (by introducing a new mess)

--- src/modules/dashboard/components/LevelEdit/LevelEdit.css.d.ts
+++ src/modules/dashboard/components/LevelEdit/LevelEdit.css.d.ts
@@ -1,4 +1,7 @@
 export const main: string;
 export const formActions: string;
 export const headingContainer: string;
+export const numRequiredStepsText: string;
 export const numRequiredStepsInput: string;
+export const requiredStepsInputContainer: string;
+export const requiredStepsExtensionText: string;
