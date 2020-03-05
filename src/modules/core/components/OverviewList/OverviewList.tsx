65d5d6649e5a17ef05b1576c5fd28f4a070cffac
Christian Maniewski <code@chmanie.com>
2020-03-05 17:14:25 +0100

    Add displayName to OverviewList

--- src/modules/core/components/OverviewList/OverviewList.tsx
+++ src/modules/core/components/OverviewList/OverviewList.tsx
@@ -6,10 +6,14 @@ interface Props {
   children: ReactNode;
 }
 
+const displayName = 'core.OverviewList';
+
 const OverviewList = ({ children }: Props) => (
   <div>
     <ul className={styles.list}>{children}</ul>
   </div>
 );
+
+OverviewList.displayName = displayName;
 
 export default OverviewList;
