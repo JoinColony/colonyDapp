ebbda18a644acb0516112bc60a3b3a53a9ce07f9
Christian Maniewski <code@chmanie.com>
2020-03-05 14:19:22 +0100

    Add user badges to UserInfoPopover

--- src/modules/core/components/InfoPopover/InfoPopover.css.d.ts
+++ src/modules/core/components/InfoPopover/InfoPopover.css.d.ts
@@ -2,6 +2,7 @@ export const main: string;
 export const displayName: string;
 export const userName: string;
 export const address: string;
+export const badges: string;
 export const symbol: string;
 export const nativeTokenMessage: string;
 export const etherscanDivider: string;
