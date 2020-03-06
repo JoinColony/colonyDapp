97d86e5eb8db98a42f62217db135d9a09afc1c97
Curtis Olson <curtis@colony.io>
2020-03-06 12:24:56 -0600

    Improve `ProgressBar` a11y

--- src/modules/core/components/ProgressBar/ProgressBar.tsx
+++ src/modules/core/components/ProgressBar/ProgressBar.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
 
 import styles from './ProgressBar.css';
+
+const MSG = defineMessages({
+  titleProgress: {
+    id: 'ProgressBar.titleProgress',
+    defaultMessage: '{value} / {max}',
+  },
+});
 
 interface Props {
   value?: number;
   max?: number;
 }
 
+const displayName = 'ProgressBar';
+
 /* Trying to use an actual ProgressBar here to be semantic and accessible */
-const ProgressBar = ({ value = 0, max = 100 }: Props) => (
-  <progress className={styles.main} value={value} max={max} />
-);
+const ProgressBar = ({ value = 0, max = 100 }: Props) => {
+  const { formatMessage } = useIntl();
+  const titleText = formatMessage(MSG.titleProgress, { value, max });
+  return (
+    <progress
+      className={styles.main}
+      value={value}
+      max={max}
+      title={titleText}
+    />
+  );
+};
+
+ProgressBar.displayName = displayName;
 
 export default ProgressBar;
