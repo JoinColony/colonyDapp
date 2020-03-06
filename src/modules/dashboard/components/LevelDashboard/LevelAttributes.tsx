77c05ffb54054172ace0f64f2787f8ef34fb2333
Curtis Olson <curtis@colony.io>
2020-03-06 12:16:26 -0600

    Add level rewards to attributes panel

--- src/modules/dashboard/components/LevelDashboard/LevelAttributes.tsx
+++ src/modules/dashboard/components/LevelDashboard/LevelAttributes.tsx
@@ -1,10 +1,12 @@
 import React, { useMemo, useEffect } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
+import Badge from '~core/Badge';
 import Heading from '~core/Heading';
 import Icon from '~core/Icon';
 import Panel, { PanelSection } from '~core/Panel';
 import ProgressBar from '~core/ProgressBar';
+import Tag from '~core/Tag';
 import {
   OneLevelWithUnlocked,
   OneProgram,
@@ -41,6 +43,10 @@ const MSG = defineMessages({
     id: 'dashboard.LevelDashboard.LevelAttributes.titleLocked',
     defaultMessage: 'Locked',
   },
+  titleRewards: {
+    id: 'dashboard.LevelDashboard.LevelAttributes.titleRewards',
+    defaultMessage: 'Rewards',
+  },
   untitled: {
     id: 'dashboard.LevelDashboard.LevelAttributes.untitled',
     defaultMessage: 'Untitled',
@@ -51,6 +57,7 @@ interface Props {
   enrolled: OneProgram['enrolled'];
   level: OneLevelWithUnlocked;
   levelIds: OneProgram['levelIds'];
+  levelTotalPayouts: { amount: string; symbol: string }[];
 }
 
 const displayName = 'dashboard.LevelDashboard.LevelAttributes';
@@ -60,12 +67,14 @@ const LevelAttributes = ({
   levelIds,
   level: {
     id: levelId,
+    achievement,
     description,
     stepIds,
     title,
     numRequiredSteps,
     unlocked,
   },
+  levelTotalPayouts,
 }: Props) => {
   const [
     fetchDependentLevel,
@@ -101,6 +110,26 @@ const LevelAttributes = ({
               appearance={{ margin: 'none', size: 'medium', weight: 'thin' }}
               text={title || MSG.untitled}
             />
+            <div className={styles.headingRewardsContainer}>
+              <div className={styles.rewardItem}>
+                <Heading
+                  appearance={{ margin: 'none', size: 'normal' }}
+                  text={MSG.titleRewards}
+                />
+              </div>
+              {levelTotalPayouts.map(({ amount, symbol }) => (
+                // Use symbol for key, since sums will be aggregated by token
+                <div className={styles.rewardItem} key={symbol}>
+                  <Tag
+                    appearance={{ theme: 'golden' }}
+                    text={`${amount} ${symbol}`}
+                  />
+                </div>
+              ))}
+              <div className={styles.rewardItem}>
+                <Badge size="s" title={title || ''} name={achievement || ''} />
+              </div>
+            </div>
           </div>
         </PanelSection>
         {description && (
