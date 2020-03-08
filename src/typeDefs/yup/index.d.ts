882dd8ee07c0c035c696dde66d032aea1ce82133
Curtis Olson <curtis@colony.io>
2020-03-08 13:15:07 -0500

    Require a badge be selected

--- src/typeDefs/yup/index.d.ts
+++ src/typeDefs/yup/index.d.ts
@@ -1,8 +1,8 @@
 import { StringSchema, TestOptionsMessage } from 'yup';
 
 declare module 'yup' {
-  interface StringSchema {
-    address(message?: TestOptionsMessage);
-    ensAddress(message?: TestOptionsMessage);
+  interface StringSchema<T> {
+    address(message?: TestOptionsMessage): StringSchema<T>;
+    ensAddress(message?: TestOptionsMessage): StringSchema<T>;
   }
 }
