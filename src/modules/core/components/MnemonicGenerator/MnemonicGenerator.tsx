d22d577df9e6cdddf2e007ecf62935395349f3a4
Curtis Olson <curtis@colony.io>
2020-05-01 16:06:09 -0500

    Refactor `MnemonicGenerator.tsx` to function component

--- src/modules/core/components/MnemonicGenerator/MnemonicGenerator.tsx
+++ src/modules/core/components/MnemonicGenerator/MnemonicGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { defineMessages } from 'react-intl';
 import copy from 'copy-to-clipboard';
 
@@ -36,111 +36,104 @@ interface Props {
   generateFn: () => string | Promise<string>;
 }
 
-interface State {
-  copied: boolean;
-}
+const displayName = 'MnemonicGenerator';
 
-class MnemonicGenerator extends Component<Props & FieldEnhancedProps, State> {
-  timeout: any;
+const MnemonicGenerator = ({
+  disabled,
+  elementOnly,
+  help,
+  $id,
+  label,
+  $value,
+  $error,
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  connect,
+  formatIntl,
+  generateFn,
+  isSubmitting,
+  name,
+  $touched,
+  onBlur,
+  setError,
+  setValue,
+  /* eslint-enable @typescript-eslint/no-unused-vars */
+  ...props
+}: Props & FieldEnhancedProps) => {
+  const timeout = useRef<number>();
 
-  static displayName = 'MnemonicGenerator';
+  const [isCopied, setIsCopied] = useState<boolean>(false);
 
-  state = { copied: false };
-
-  componentDidMount() {
-    const { $value } = this.props;
-    if (!$value) {
-      this.generateMnemonic();
-    }
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
-  }
-
-  generateMnemonic = (): void => {
-    const { generateFn, setValue } = this.props;
+  const generateMnemonic = useCallback(() => {
     const res = generateFn();
     if (res instanceof Promise && !!res.then && setValue) {
       res.then((phrase) => setValue(phrase));
     } else if (setValue) {
       setValue(res);
     }
-  };
+  }, [generateFn, setValue]);
 
-  copyToClipboard = () => {
-    const { $value } = this.props;
+  const copyToClipboard = useCallback(() => {
     if (!$value) return;
     copy($value);
-    this.setState({ copied: true });
-    this.timeout = setTimeout(() => this.setState({ copied: false }), 4000);
-  };
+    setIsCopied(true);
+    if (window) {
+      timeout.current = window.setTimeout(() => setIsCopied(false), 4000);
+    }
+  }, [$value]);
 
-  render() {
-    const {
-      disabled,
-      elementOnly,
-      help,
-      $id,
-      label,
-      $value,
-      $error,
-      /* eslint-disable @typescript-eslint/no-unused-vars */
-      connect,
-      formatIntl,
-      generateFn,
-      isSubmitting,
-      name,
-      $touched,
-      onBlur,
-      setError,
-      setValue,
-      /* eslint-enable @typescript-eslint/no-unused-vars */
-      ...props
-    } = this.props;
-    const { copied } = this.state;
-    return (
-      <div {...props}>
-        <div className={styles.buttons}>
-          <Heading
-            appearance={{ size: 'small', weight: 'bold' }}
-            text={MSG.titleBox}
-            className={styles.heading}
+  useEffect(() => {
+    if (!$value) {
+      generateMnemonic();
+    }
+
+    return () => {
+      if (window) {
+        window.clearTimeout(timeout.current);
+      }
+    };
+  }, [$value, generateMnemonic]);
+
+  return (
+    <div {...props}>
+      <div className={styles.buttons}>
+        <Heading
+          appearance={{ size: 'small', weight: 'bold' }}
+          text={MSG.titleBox}
+          className={styles.heading}
+        />
+        <div>
+          <Button
+            appearance={{ theme: 'ghost' }}
+            type="button"
+            onClick={generateMnemonic}
+            text={MSG.buttonRefresh}
           />
-          <div>
-            <Button
-              appearance={{ theme: 'ghost' }}
-              type="button"
-              onClick={this.generateMnemonic}
-              text={MSG.buttonRefresh}
-            />
-            <Button
-              appearance={{ theme: 'ghost' }}
-              type="button"
-              disabled={copied}
-              onClick={this.copyToClipboard}
-              text={{ ...MSG.buttonCopy }}
-              textValues={{ copied }}
-            />
-          </div>
-        </div>
-        <div
-          className={styles.main}
-          aria-invalid={!!$error}
-          aria-disabled={disabled}
-        >
-          {!elementOnly && (
-            <InputLabel inputId={$id} label={label} help={help} />
-          )}
-          <div className={styles.generator}>
-            <span className={styles.mnemonic} data-test="mnemonicPhrase">
-              {$value}
-            </span>
-          </div>
+          <Button
+            appearance={{ theme: 'ghost' }}
+            type="button"
+            disabled={isCopied}
+            onClick={copyToClipboard}
+            text={{ ...MSG.buttonCopy }}
+            textValues={{ copied: isCopied }}
+          />
         </div>
       </div>
-    );
-  }
-}
+      <div
+        className={styles.main}
+        aria-invalid={!!$error}
+        aria-disabled={disabled}
+      >
+        {!elementOnly && <InputLabel inputId={$id} label={label} help={help} />}
+        <div className={styles.generator}>
+          <span className={styles.mnemonic} data-test="mnemonicPhrase">
+            {$value}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+MnemonicGenerator.displayName = displayName;
 
 export default asField<Props>()(MnemonicGenerator);
