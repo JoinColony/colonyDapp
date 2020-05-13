869361a75ef1a38ab1014d3385f9378846d8a9c6
Christian Maniewski <code@chmanie.com>
2020-05-13 14:31:36 +0200

    Add local rpc provider

--- src/modules/core/sagas/utils/getNetworkClient.ts
+++ src/modules/core/sagas/utils/getNetworkClient.ts
@@ -1,5 +1,5 @@
 import { call } from 'redux-saga/effects';
-import { InfuraProvider } from 'ethers/providers';
+import { InfuraProvider, JsonRpcProvider } from 'ethers/providers';
 import { getColonyNetworkClient, Network } from '@colony/colony-js';
 import { EthersSigner } from '@purser/signer-ethers';
 
@@ -16,7 +16,11 @@ export default function* getNetworkClient() {
 
   const network = DEFAULT_NETWORK as Network;
 
-  const provider = new InfuraProvider(network, process.env.INFURA_ID);
+  const provider =
+    network === Network.Local
+      ? new JsonRpcProvider()
+      : new InfuraProvider(network, process.env.INFURA_ID);
+
   const signer = new EthersSigner({ purserWallet: wallet, provider });
 
   return yield call(getColonyNetworkClient, network, signer);
