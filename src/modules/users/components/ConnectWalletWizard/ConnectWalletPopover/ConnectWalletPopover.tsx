40871130b70e664b2193b2611ce68143dc583a97
Raul Glogovetan <raul@glogovetan.com>
2020-06-29 13:27:14 +0300

    Remove: `ConnectWalletPopover` leftover commented out code

--- src/modules/users/components/ConnectWalletWizard/ConnectWalletPopover/ConnectWalletPopover.tsx
+++ src/modules/users/components/ConnectWalletWizard/ConnectWalletPopover/ConnectWalletPopover.tsx
@@ -3,35 +3,12 @@ import React, { useState, ReactElement } from 'react';
 import Popover, { PopoverTriggerType } from '~core/Popover';
 import { ConnectWalletContent } from '../ConnectWalletWizard';
 
-// import { usePrevious } from '~utils/hooks';
-
-// import {
-//   TransactionOrMessageGroups,
-//   transactionCount,
-// } from './transactionGroup';
-
-// import GasStationContent from './GasStationContent';
-
 interface Props {
-  // transactionAndMessageGroups: TransactionOrMessageGroups;
   children: ReactElement | PopoverTriggerType;
 }
 
-const ConnectWalletPopover = ({
-  children,
-}: // transactionAndMessageGroups,
-Props) => {
+const ConnectWalletPopover = ({ children }: Props) => {
   const [isOpen, setOpen] = useState(false);
-  // const txCount = useMemo(() => transactionCount(transactionAndMessageGroups), [
-  //   transactionAndMessageGroups,
-  // ]);
-  // const prevTxCount: number | void = usePrevious(txCount);
-  // useEffect(() => {
-  //   if (prevTxCount != null && txCount > prevTxCount) {
-  //     setOpen(true);
-  //   }
-  // }, [txCount, prevTxCount]);
-
   return (
     <Popover
       appearance={{ theme: 'grey' }}
