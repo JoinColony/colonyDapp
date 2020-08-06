9fb39a774973ad34be84c4ae367f4ef2f022e046
Curtis Olson <curtis@colony.io>
2020-08-06 09:51:47 -0500

    Use `typePolicies` for `DomainBalance` cache keys
    Remove `dataIdFromObject` (deprecated)

--- src/data/cache.ts
+++ src/data/cache.ts
@@ -1,4 +1,4 @@
-import { InMemoryCache, defaultDataIdFromObject } from '@apollo/client/cache/';
+import { InMemoryCache } from '@apollo/client/cache/';
 
 // import introspectionResult from './generated';
 
@@ -6,18 +6,11 @@ import { InMemoryCache, defaultDataIdFromObject } from '@apollo/client/cache/';
 //   introspectionQueryResultData: introspectionResult,
 // });
 
-const dataIdFromObject = (obj) => {
-  // eslint-disable-next-line no-underscore-dangle
-  if (obj.__typename === 'DomainBalance') {
-    // Avoid having array arguments inside the cache key
-    // Avoid `${address}.${[...domainIds]}`. Cache creates individual entries for each combination
-    const { address, colonyAddress, domainId } = obj;
-    return `Colony:${colonyAddress}.Token:${address}.Domain:${domainId}`;
-  }
-  return defaultDataIdFromObject(obj);
-};
-
 export default new InMemoryCache({
-  dataIdFromObject,
+  typePolicies: {
+    DomainBalance: {
+      keyFields: ['colonyAddress', 'address', 'domainId'],
+    }
+  },
   // fragmentMatcher,
 });
