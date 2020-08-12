225983a96af42d4cebbdd5d14ee912125e0d157d
Christian Maniewski <code@chmanie.com>
2020-08-12 10:47:20 +0200

    Remove FragmentMatcher comments

--- src/data/cache.ts
+++ src/data/cache.ts
@@ -1,16 +1,9 @@
 import { InMemoryCache } from '@apollo/client/cache/';
 
-// import introspectionResult from './generated';
-
-// const fragmentMatcher = new IntrospectionFragmentMatcher({
-//   introspectionQueryResultData: introspectionResult,
-// });
-
 export default new InMemoryCache({
   typePolicies: {
     DomainBalance: {
       keyFields: ['colonyAddress', 'address', 'domainId'],
-    }
+    },
   },
-  // fragmentMatcher,
 });
