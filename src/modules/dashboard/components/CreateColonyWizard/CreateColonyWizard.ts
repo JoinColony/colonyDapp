126ec4cb196fb8de3d08d65920be0aad5ee5ea17
Raul Glogovetan <raul@glogovetan.com>
2020-09-03 13:38:58 +0300

    Fix: `CreateColonyWizard` correctly fetch user from props

--- src/modules/dashboard/components/CreateColonyWizard/CreateColonyWizard.ts
+++ src/modules/dashboard/components/CreateColonyWizard/CreateColonyWizard.ts
@@ -41,7 +41,7 @@ const stepFunction: StepsFn<any> = (
   props?: any,
 ): ComponentType<any> => {
   if (props) {
-    const { username } = props.loggedInUser;
+    const username = props?.loggedInUser?.username;
 
     /*
      * In case the username is already registered
