78c2ffe86027115836c364c46a2d1be839fc2a9d
Raul Glogovetan <raul@glogovetan.com>
2020-09-14 17:48:46 +0300

    Remove: unused env variables

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,10 +3,8 @@ namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
     COLONY_NETWORK_ENS_NAME: string;
-    LOADER: string;
     NETWORK: string;
     PINNING_ROOM: string;
-    ETHPLORER_API_KEY: string;
     INFURA_ID: string;
     VERSION: string;
   }
