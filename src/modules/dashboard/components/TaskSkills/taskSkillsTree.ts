24e81faaf249e583e728d5c8b04bcf6ad4c7e7fd
Raul Glogovetan <raul@glogovetan.com>
2020-09-15 15:21:51 +0300

    Add: wire xdai task skills tree

--- src/modules/dashboard/components/TaskSkills/taskSkillsTree.ts
+++ src/modules/dashboard/components/TaskSkills/taskSkillsTree.ts
@@ -2,6 +2,7 @@ import { DEFAULT_NETWORK } from '~constants';
 
 import mainnetTaskSkillsTree from './taskSkillsTree.mainnet';
 import goerliTaskSkillsTree from './taskSkillsTree.goerli';
+import xdaiTaskSkillsTree from './taskSkillsTree.xdai';
 import localTaskSkillsTree from './taskSkillsTree.local';
 
 const getTaskSkillsTreeInUse = () => {
@@ -10,6 +11,8 @@ const getTaskSkillsTreeInUse = () => {
       return mainnetTaskSkillsTree;
     case 'goerli':
       return goerliTaskSkillsTree;
+    case 'xdai':
+      return xdaiTaskSkillsTree;
     default:
       return localTaskSkillsTree;
   }
