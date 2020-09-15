fd718082300089fa533328cdb8682fc0d5873b3c
Raul Glogovetan <raul@glogovetan.com>
2020-09-15 16:16:25 +0300

    Remove: un-needed network name props
    From link generator components: `TokenLink`, `TransactionLink` and `WalletLink`

--- src/modules/core/components/TransactionLink/TransactionLink.tsx
+++ src/modules/core/components/TransactionLink/TransactionLink.tsx
@@ -15,9 +15,6 @@ interface Props {
   /** Transaction hash */
   hash: string;
 
-  /** Optionally override current network */
-  network?: string;
-
   /** A string or a `messageDescriptor` that make up the link's text. Defaults to `hash`. */
   text?: MessageDescriptor | string;
 
