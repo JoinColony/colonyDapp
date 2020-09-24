d7ab30884e6da8f42959cd9e000c5676544f2e57
Raul Glogovetan <raul@glogovetan.com>
2020-09-24 13:01:07 +0300

    Add: `TokenInfo` constants for the various networks

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -2,8 +2,28 @@ export const DEFAULT_NETWORK = process.env.NETWORK || 'goerli';
 export const COLONY_TOTAL_BALANCE_DOMAIN_ID = 0;
 export const DEFAULT_TOKEN_DECIMALS = 18;
 
+export const NETWORK_XDAI = 'xdai';
+
 export enum ROLES_COMMUNITY {
   founder = 'role.founder',
   admin = 'role.admin',
   member = 'role.member',
 }
+
+type TokenInfo = {
+  name: string;
+  symbol: string;
+  decimals?: number;
+};
+
+export const XDAI_TOKEN: TokenInfo = {
+  name: 'XDAI Token',
+  symbol: 'XDAI',
+  decimals: 18,
+};
+
+export const ETHER_TOKEN: TokenInfo = {
+  name: 'Ether',
+  symbol: 'ETH',
+  decimals: 18,
+};
