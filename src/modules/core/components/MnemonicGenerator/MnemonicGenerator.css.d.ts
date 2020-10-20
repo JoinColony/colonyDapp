ead7878a43b4b069022c3e9f053ace7e1ad1a34a
Raul Glogovetan <raul@glogovetan.com>
2020-10-20 18:37:39 +0300

    Remove: `MnemonicGenerator` core component

--- src/modules/core/components/MnemonicGenerator/MnemonicGenerator.css.d.ts
+++
@@ -1,6 +0,0 @@
-export const main: string;
-export const generator: string;
-export const mnemonic: string;
-export const buttons: string;
-export const heading: string;
-export const customGhostButton: string;
