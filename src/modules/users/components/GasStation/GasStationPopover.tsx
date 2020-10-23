98256b4c5381a23eca91aca7b2369966a60d9428
Raul Glogovetan <raul@glogovetan.com>
2020-10-23 18:34:53 +0300

    Fix: `GasStationPopover` offset distances according to spec

--- src/modules/users/components/GasStation/GasStationPopover.tsx
+++ src/modules/users/components/GasStation/GasStationPopover.tsx
@@ -44,6 +44,32 @@ const GasStationPopover = ({
       showArrow={false}
       isOpen={isOpen}
       onClose={() => setOpen(false)}
+      popperProps={{
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              /*
+               * @NOTE Offset Calculations
+               * See: https://popper.js.org/docs/v2/modifiers/offset/
+               *
+               * Skidding:
+               * Half the width of the content + half the width of the reference.
+               * This will get you aligned to the right side (this just for this instance,
+               * where alignment is at the bottom).
+               * To this we add the required offset size in pixels.
+               * Note that all skidding, for bottom alignment, needs to be negative.
+               *
+               * Distace:
+               * This is just the required offset in pixels. Since we are aligned at
+               * the bottom of the screen, this will be added to the bottom of the
+               * reference element.
+               */
+              offset: [-167, 22],
+            },
+          },
+        ],
+      }}
     >
       {children}
     </Popover>
