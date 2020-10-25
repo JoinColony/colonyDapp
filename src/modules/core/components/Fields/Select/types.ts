6f71e0a64b5603aaf0c1aecbffe57984683dc8cf
Curtis Olson <curtis@colony.io>
2020-10-25 17:01:47 -0500

    Add content width `Select` option

--- src/modules/core/components/Fields/Select/types.ts
+++ src/modules/core/components/Fields/Select/types.ts
@@ -7,7 +7,7 @@ export interface Appearance {
   alignOptions?: 'left' | 'center' | 'right';
   borderedOptions?: 'true' | 'false';
   theme?: 'default' | 'alt' | 'grey';
-  width?: 'fluid' | 'strict';
+  width?: 'content' | 'fluid' | 'strict';
 }
 
 export interface SelectOption {
