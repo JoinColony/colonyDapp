737c6c4f56cf60f68295ee6400121c34fc17f909
Raul Glogovetan <raul@glogovetan.com>
2020-10-27 23:08:02 +0200

    Fix: `AvatarDropdown` popover offsets

--- src/modules/users/components/AvatarDropdown/AvatarDropdown.css.d.ts
+++ src/modules/users/components/AvatarDropdown/AvatarDropdown.css.d.ts
@@ -1,1 +1,4 @@
+export const refWidth: string;
+export const horizontalOffset: string;
+export const verticalOffset: string;
 export const avatarButton: string;
--- src/modules/users/components/AvatarDropdown/AvatarDropdown.tsx
+++ src/modules/users/components/AvatarDropdown/AvatarDropdown.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import Popover from '~core/Popover';
 import HookedUserAvatar from '~users/HookedUserAvatar';
 import { useLoggedInUser } from '~data/index';
+import { removeValueUnits } from '~utils/css';
 
 import AvatarDropdownPopover from './AvatarDropdownPopover';
 
-import styles from './AvatarDropdown.css';
+import styles, {
+  refWidth,
+  horizontalOffset,
+  verticalOffset,
+} from './AvatarDropdown.css';
 
 const UserAvatar = HookedUserAvatar();
 
@@ -14,6 +19,26 @@ const displayName = 'users.AvatarDropdown';
 
 const AvatarDropdown = () => {
   const { username, walletAddress, ethereal } = useLoggedInUser();
+
+  /*
+   * @NOTE Offset Calculations
+   * See: https://popper.js.org/docs/v2/modifiers/offset/
+   *
+   * Skidding:
+   * The Width of the reference element (width) plus the horizontal offset
+   * Note that all skidding, for bottom aligned elements, needs to be negative.
+   *
+   * Distace:
+   * This is just the required offset in pixels. Since we are aligned at
+   * the bottom of the screen, this will be added to the bottom of the
+   * reference element.
+   */
+  const popoverOffset = useMemo(() => {
+    const skid =
+      removeValueUnits(refWidth) + removeValueUnits(horizontalOffset);
+    return [-1 * skid, removeValueUnits(verticalOffset)];
+  }, []);
+
   return (
     <Popover
       content={({ close }) => (
@@ -24,6 +49,17 @@ const AvatarDropdown = () => {
         />
       )}
       trigger="click"
+      showArrow={false}
+      popperProps={{
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: popoverOffset,
+            },
+          },
+        ],
+      }}
     >
       <button
         className={styles.avatarButton}
