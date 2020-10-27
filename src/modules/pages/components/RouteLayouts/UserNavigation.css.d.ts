b860c69149c41d9cba7a6ed3a868561b83c877fe
Raul Glogovetan <raul@glogovetan.com>
2020-10-28 01:11:19 +0200

    Add: `UserNavigation` display not allowed network warning

--- src/modules/pages/components/RouteLayouts/UserNavigation.css.d.ts
+++ src/modules/pages/components/RouteLayouts/UserNavigation.css.d.ts
@@ -5,6 +5,7 @@ export const notificationsIconActive: string;
 export const connectWalletButton: string;
 export const connectWalletButtonActive: string;
 export const networkInfo: string;
+export const wrongNetwork: string;
 export const walletAddress: string;
 export const walletAddressActive: string;
 export const readyTransactionsCount: string;
