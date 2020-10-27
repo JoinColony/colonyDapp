bab6c43aaf71e67ec1a92e431101ed96478a272e
Raul Glogovetan <raul@glogovetan.com>
2020-10-27 22:33:34 +0200

    Fix: `ConnectWalletPopover` popover offsets

--- src/modules/users/components/ConnectWalletWizard/ConnectWalletPopover/ConnectWalletPopover.tsx
+++ src/modules/users/components/ConnectWalletWizard/ConnectWalletPopover/ConnectWalletPopover.tsx
@@ -1,7 +1,15 @@
-import React, { useState, ReactElement } from 'react';
+import React, { useState, ReactElement, useMemo } from 'react';
 
 import Popover, { PopoverChildFn } from '~core/Popover';
 import { ConnectWalletContent } from '../ConnectWalletWizard';
+
+import { removeValueUnits } from '~utils/css';
+
+import {
+  refWidth,
+  horizontalOffset,
+  verticalOffset,
+} from './ConnectWalletPopover.css';
 
 interface Props {
   children: ReactElement | PopoverChildFn;
@@ -9,6 +17,26 @@ interface Props {
 
 const ConnectWalletPopover = ({ children }: Props) => {
   const [isOpen, setOpen] = useState(false);
+
+  /*
+   * @NOTE Offset Calculations
+   * See: https://popper.js.org/docs/v2/modifiers/offset/
+   *
+   * Skidding:
+   * Half the width of the reference element plus the horizontal offset
+   * Note that all skidding, for bottom aligned elements, needs to be negative.
+   *
+   * Distace:
+   * This is just the required offset in pixels. Since we are aligned at
+   * the bottom of the screen, this will be added to the bottom of the
+   * reference element.
+   */
+  const popoverOffset = useMemo(() => {
+    const skid =
+      removeValueUnits(refWidth) + removeValueUnits(horizontalOffset);
+    return [-1 * skid, removeValueUnits(verticalOffset)];
+  }, []);
+
   return (
     <Popover
       appearance={{ theme: 'grey' }}
@@ -17,6 +45,16 @@ const ConnectWalletPopover = ({ children }: Props) => {
       showArrow={false}
       isOpen={isOpen}
       onClose={() => setOpen(false)}
+      popperProps={{
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: popoverOffset,
+            },
+          },
+        ],
+      }}
     >
       {children}
     </Popover>
