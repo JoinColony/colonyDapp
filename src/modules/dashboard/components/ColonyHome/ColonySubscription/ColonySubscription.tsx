90cae435a442bb461c27be16c945e653b1287f01
Raul Glogovetan <raul@glogovetan.com>
2020-10-28 18:33:33 +0200

    Add: `ColonySubscription` subscribe / unsubscribe logic

--- src/modules/dashboard/components/ColonyHome/ColonySubscription/ColonySubscription.tsx
+++ src/modules/dashboard/components/ColonyHome/ColonySubscription/ColonySubscription.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
+import { SpinnerLoader } from '~core/Preloaders';
 import { Address } from '~types/index';
+import {
+  useLoggedInUser,
+  useSubscribeToColonyMutation,
+  useUnsubscribeFromColonyMutation,
+  useUserColonyAddressesQuery,
+  cacheUpdates,
+} from '~data/index';
 
 import styles from './ColonySubscription.css';
 
@@ -21,11 +29,47 @@ interface Props {
 }
 
 const ColonySubscription = ({ colonyAddress }: Props) => {
+  const { username, walletAddress } = useLoggedInUser();
+
+  const { data } = useUserColonyAddressesQuery({
+    variables: { address: walletAddress },
+  });
+
+  const [
+    subscribe,
+    { loading: loadingSubscribe },
+  ] = useSubscribeToColonyMutation({
+    variables: { input: { colonyAddress } },
+    update: cacheUpdates.subscribeToColony(colonyAddress),
+  });
+  const [
+    unsubscribe,
+    { loading: loadingUnsubscribe },
+  ] = useUnsubscribeFromColonyMutation({
+    variables: { input: { colonyAddress } },
+    update: cacheUpdates.unsubscribeFromColony(colonyAddress),
+  });
+
+  if (!username || !data) return null;
+
+  const {
+    user: { colonyAddresses },
+  } = data;
+
+  const isSubscribed = (colonyAddresses || []).includes(colonyAddress);
+
   return (
     <div className={styles.main}>
-      <button type="button" className={styles.joinColony}>
-        <FormattedMessage {...MSG.joinColony} />
-      </button>
+      {!isSubscribed && (
+        <button type="button" className={styles.joinColony}>
+          <FormattedMessage {...MSG.joinColony} />
+        </button>
+      )}
+      {loadingSubscribe || loadingUnsubscribe && (
+        <div className={styles.spinnerContainer}>
+          <SpinnerLoader appearance={{ theme: 'primary', size: 'small' }} />
+        </div>
+      )}
     </div>
   );
 };
