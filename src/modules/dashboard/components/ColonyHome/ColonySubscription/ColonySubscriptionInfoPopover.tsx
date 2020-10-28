279aeab4d3e61fe086a9673e055f6a40e0c461d2
Raul Glogovetan <raul@glogovetan.com>
2020-10-28 21:39:28 +0200

    Add: `ColonySubscriptionInfoPopover` subcomponent

---
+++ src/modules/dashboard/components/ColonyHome/ColonySubscription/ColonySubscriptionInfoPopover.tsx
@@ -0,0 +1,85 @@
+import React, { ReactNode } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import { Address } from '~types/index';
+import ENS from '~lib/ENS';
+import Popover from '~core/Popover';
+import ColonyAvatar from '~core/ColonyAvatar';
+import MaskedAddress from '~core/MaskedAddress';
+
+import styles from './ColonySubscriptionInfoPopover.css';
+
+const MSG = defineMessages({
+  leaveColonyQuestion: {
+    id:
+      'dashboard.ColonyHome.ColonySubscriptionInfoPopover.leaveColonyQuestion',
+    defaultMessage: 'Leave?',
+  },
+  nativeTokenTitle: {
+    id:
+      'dashboard.ColonyHome.ColonySubscriptionInfoPopover.nativeTokenTitle',
+    defaultMessage: 'NativeTokenAddress',
+  },
+});
+
+interface Props {
+  colonyAddress: Address;
+  colonyDisplayName: string;
+  colonyName: string;
+  nativeTokenAddress: Address;
+  onUnsubscribe?: () => void;
+  children?: ReactNode;
+}
+
+const ColonySubscriptionInfoPopover = ({
+  children,
+  colonyAddress,
+  colonyDisplayName,
+  colonyName,
+  nativeTokenAddress,
+  onUnsubscribe = () => {},
+}: Props) => (
+  <Popover
+    content={
+      <div className={styles.main}>
+        <div className={styles.colonyDetails}>
+          <div className={styles.colonyAvatar}>
+            <ColonyAvatar colonyAddress={colonyAddress} size="s" />
+          </div>
+          <div className={styles.colonyInfo}>
+            <span className={styles.colonyInfoTitle}>{colonyDisplayName}</span>
+            <span className={styles.colonyInfoENS}>
+              {ENS.getFullDomain('colony', colonyName)}
+            </span>
+            <span className={styles.colonyInfoAddress}>
+              <MaskedAddress address={colonyAddress} full />
+            </span>
+          </div>
+          <button
+            type="button"
+            className={styles.unsubscribeFromColony}
+            onClick={onUnsubscribe}
+          >
+            <FormattedMessage {...MSG.leaveColonyQuestion} />
+          </button>
+        </div>
+        <span className={styles.nativeTokenTitle}>
+          <FormattedMessage {...MSG.nativeTokenTitle} />
+        </span>
+        <span className={styles.nativeTokenAddress}>
+          <MaskedAddress address={nativeTokenAddress} full />
+        </span>
+      </div>
+    }
+    trigger="click"
+    showArrow={false}
+    placement="right"
+  >
+    {children}
+  </Popover>
+);
+
+ColonySubscriptionInfoPopover.displayName =
+  'dashboard.ColonyHome.ColonySubscriptionInfoPopover';
+
+export default ColonySubscriptionInfoPopover;
