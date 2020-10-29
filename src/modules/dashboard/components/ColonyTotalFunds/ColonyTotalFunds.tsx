310b55fc97f64bb1b9c474494cabf907af08c077
Raul Glogovetan <raul@glogovetan.com>
2020-10-29 17:05:17 +0200

    Add: `ColonyTotalFunds` manage funds controls

--- src/modules/dashboard/components/ColonyTotalFunds/ColonyTotalFunds.tsx
+++ src/modules/dashboard/components/ColonyTotalFunds/ColonyTotalFunds.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
-import { Colony } from '~data/index';
+import Numeral from '~core/Numeral';
+import Button from '~core/Button';
+import { Colony, useTokenBalancesForDomainsQuery } from '~data/index';
+import { COLONY_TOTAL_BALANCE_DOMAIN_ID } from '~constants';
+import { getTokenDecimalsWithFallback } from '~utils/tokens';
 
 import styles from './ColonyTotalFunds.css';
 
@@ -9,6 +13,10 @@ const MSG = defineMessages({
   totalBalance: {
     id: 'dashboard.ColonyTotalFunds.totalBalance',
     defaultMessage: 'Colony total balance',
+  },
+  manageFundsLink: {
+    id: 'dashboard.ColonyTotalFunds.manageFundsLink',
+    defaultMessage: 'Manage Funds',
   },
 });
 
@@ -18,10 +26,61 @@ type Props = {
 
 const displayName = 'dashboard.ColonyTotalFunds';
 
-const ColonyTotalFunds = () => {
+const ColonyTotalFunds = ({
+  colony: { colonyAddress, tokens: colonyTokens, nativeTokenAddress },
+}: Props) => {
+  const handleManageFunds = useCallback(
+    // eslint-disable-next-line no-console
+    () => console.log('Clicked! This should open the UAC manage funds modal'),
+    [],
+  );
+
+  const {
+    data,
+    loading: isLoadingTokenBalances,
+  } = useTokenBalancesForDomainsQuery({
+    variables: {
+      colonyAddress,
+      domainIds: [COLONY_TOTAL_BALANCE_DOMAIN_ID],
+      tokenAddresses: colonyTokens.map(({ address }) => address),
+    },
+  });
+
+  const nativeColonyToken = useMemo(() => {
+    if (data && data.tokens) {
+      return data.tokens.find(
+        ({ address: tokenAddress }) => tokenAddress === nativeTokenAddress,
+      );
+    }
+    return undefined;
+  }, [data, nativeTokenAddress]);
+
+  if (!data || !nativeColonyToken || isLoadingTokenBalances) {
+    return null;
+  }
+
   return (
     <div className={styles.main}>
-      <FormattedMessage {...MSG.totalBalance} />
+      <div className={styles.selectedToken}>
+        <Numeral
+          className={styles.selectedTokenAmount}
+          unit={getTokenDecimalsWithFallback(nativeColonyToken.decimals)}
+          value={nativeColonyToken.balances[0].amount}
+        />
+        <span className={styles.selectedTokenSymbol}>
+          {nativeColonyToken.symbol}
+        </span>
+      </div>
+      <div className={styles.totalBalanceCopy}>
+        <FormattedMessage {...MSG.totalBalance} />
+        <Button
+          appearance={{ theme: 'blue', size: 'small' }}
+          onClick={handleManageFunds}
+        >
+          <span className={styles.rightArrowDisplay}>→</span>
+          <FormattedMessage {...MSG.manageFundsLink} />
+        </Button>
+      </div>
     </div>
   );
 };
