41ecf77dd71314d402d71a25f91c8a531f7708ea
Raul Glogovetan <raul@glogovetan.com>
2020-11-06 13:21:30 +0200

    Add: `ColonyActions` extract mock data into separate file

--- src/modules/dashboard/components/ColonyActions/ColonyActions.tsx
+++ src/modules/dashboard/components/ColonyActions/ColonyActions.tsx
@@ -6,59 +6,17 @@ import { Colony } from '~data/index';
 
 import styles from './ColonyActions.css';
 
+/*
+ * @TODO Replace with actual data (fetch from events most likely?)
+ */
+import { MOCK_ACTIONS } from './mockData';
+
 // const MSG = defineMessages({
 //   text: {
 //     id: 'dashboard.ColonyActions.text',
 //     defaultMessage: 'Text',
 //   },
 // });
-
-const MOCK_ACTIONS = [
-  {
-    id: 1,
-    title: 'Create new Domain #BDSM',
-    date: 1604399689594,
-    userAddress: '0xb77D57F4959eAfA0339424b83FcFaf9c15407461',
-    domain: {
-      name: 'First domain',
-      id: 2,
-    },
-    commentCount: 0,
-    status: 'needsAction',
-  },
-  {
-    id: 2,
-    title: 'Transfer 250000 xDai from #Dev to #Design by @storm',
-    date: 1604399844229,
-    userAddress: '0x9df24e73f40b2a911eb254a8825103723e13209c',
-    commentCount: 5,
-  },
-  {
-    id: 3,
-    title: 'Punish @a 500 #R&D Reputation',
-    date: 1604399689594,
-    userAddress: '0x27ff0c145e191c22c75cd123c679c3e1f58a4469',
-    domain: {
-      name: 'Third',
-      id: 4,
-    },
-    commentCount: 4,
-    status: 'needsAttention',
-  },
-  {
-    id: 4,
-    title:
-      // eslint-disable-next-line max-len
-      'A very very very long title, a very very very long title, a very very very long title, a very very very long title, a very very very long title, a very very very long title, a very very very long title, a very very very long title, a very very very long title',
-    date: 1604399689594,
-    userAddress: '0x27ff0c145e191c22c75cd123c679c3e1f58a4469',
-    domain: {
-      name: 'Third',
-      id: 4,
-    },
-    commentCount: 40,
-  },
-];
 
 type Props = {
   colony: Colony;
