409d6f16138320f41d406d3fd9c3f128a32867eb
Raul Glogovetan <raul@glogovetan.com>
2020-11-06 17:13:35 +0200

    Add: `ColonyActions` handle click items redirects

--- src/modules/dashboard/components/ColonyActions/ColonyActions.tsx
+++ src/modules/dashboard/components/ColonyActions/ColonyActions.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { defineMessages } from 'react-intl';
 
-import ActionsList from '~core/ActionsList';
+import ActionsList, {
+  ClickHandlerProps as RedirectHandlerProps,
+} from '~core/ActionsList';
 import { Colony } from '~data/index';
 
 import styles from './ColonyActions.css';
@@ -25,9 +27,30 @@ type Props = {
 const displayName = 'dashboard.ColonyActions';
 
 const ColonyActions = ({ colony }: Props) => {
+  /*
+   * @TODO This callback should handle what happends when clicking on an
+   * item in the actions list.
+   *
+   * It should, in theory, redirect to a route that will render the full page
+   * action
+   *
+   * This will only happen when UAC lands
+   */
+  const handleActionRedirect = useCallback(
+    ({ id }: RedirectHandlerProps) =>
+      // eslint-disable-next-line no-console
+      console.log(
+        'This will redirect to the specific action item route whn UAC lands',
+        id,
+      ),
+    [],
+  );
   return (
     <div className={styles.main}>
-      <ActionsList items={MOCK_ACTIONS} />
+      <ActionsList
+        items={MOCK_ACTIONS}
+        handleItemClick={handleActionRedirect}
+      />
     </div>
   );
 };
