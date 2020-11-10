5a6476e61f53a6877e3c4f4db80cf3dd8eb272ec
Alicja Kujawa <alicjak94.ak@gmail.com>
2020-11-10 15:05:10 +0100

    [DEV-10] Create new dashboard component

---
+++ src/modules/dashboard/components/ColonyEvents/ColonyEvents.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const displayName = 'dashboard.ColonyEvents';
+
+const ColonyEvents = () => {
+  return <div>Events</div>;
+};
+
+ColonyEvents.displayName = displayName;
+
+export default ColonyEvents;
