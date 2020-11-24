b93207228a0839960a1c2b2de5307ee8ef40bd78
Raul Glogovetan <raul@glogovetan.com>
2020-11-24 21:44:55 +0200

    Add: initial pages `LandingPage` component

---
+++ src/modules/pages/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './LandingPage.css';
+
+const displayName = 'pages.LandingPage';
+
+const LandingPage = () => <div className={styles.main} />;
+
+LandingPage.displayName = displayName;
+
+export default LandingPage;
