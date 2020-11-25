05ccb80e8c753a65e76338d0958cbf288732f0c7
Raul Glogovetan <raul@glogovetan.com>
2020-11-25 22:28:10 +0200

    Add: `LandingPage` styles and structure

--- src/modules/pages/components/LandingPage/LandingPage.css.d.ts
+++ src/modules/pages/components/LandingPage/LandingPage.css.d.ts
@@ -1,1 +1,7 @@
 export const main: string;
+export const coloniesList: string;
+export const contentWrapper: string;
+export const content: string;
+export const item: string;
+export const newColonyIcon: string;
+export const itemTitle: string;
--- src/modules/pages/components/LandingPage/LandingPage.tsx
+++ src/modules/pages/components/LandingPage/LandingPage.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import SubscribedColoniesList from '~dashboard/SubscribedColoniesList';
+import NavLink from '~core/NavLink';
+import Icon from '~core/Icon';
+
+import { CREATE_COLONY_ROUTE } from '~routes/index';
 
 import styles from './LandingPage.css';
 
+const MSG = defineMessages({
+  createColony: {
+    id: 'pages.LandingPage.createColony',
+    defaultMessage: 'Create a colony',
+  },
+});
+
 const displayName = 'pages.LandingPage';
 
-const LandingPage = () => <div className={styles.main} />;
+const LandingPage = () => (
+  <div className={styles.main}>
+    <div className={styles.coloniesList}>
+      <SubscribedColoniesList />
+    </div>
+    <div className={styles.contentWrapper}>
+      <div className={styles.content}>
+        <ul>
+          <li className={styles.item}>
+            <NavLink className={styles.itemLink} to={CREATE_COLONY_ROUTE}>
+              <Icon
+                className={styles.newColonyIcon}
+                name="circle-plus"
+                title={MSG.createColony}
+              />
+              <span className={styles.itemTitle}>
+                <FormattedMessage {...MSG.createColony} />
+              </span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
 
 LandingPage.displayName = displayName;
 
