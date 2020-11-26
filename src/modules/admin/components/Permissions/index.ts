fced26dfc4ebaec81f94d4329905a97c31ca4036
Alicja Kujawa <alicjak94.ak@gmail.com>
2020-11-26 13:43:05 +0100

    Remove permissions export

--- src/modules/admin/components/Permissions/index.ts
+++ src/modules/admin/components/Permissions/index.ts
@@ -1,5 +1,3 @@
 export { default as ColonyPermissionsEditDialog } from './ColonyPermissionsEditDialog';
 
 export { default as ColonyPermissionsAddDialog } from './ColonyPermissionsAddDialog';
-
-export { default } from './Permissions';
