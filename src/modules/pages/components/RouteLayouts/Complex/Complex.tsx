8f69fb1dbc63c62b377674eb49b4dcbc5b105d8a
Raul Glogovetan <raul@glogovetan.com>
2020-11-27 15:39:25 +0200

    Refactor: `Complex` route layout to support backlink

--- src/modules/pages/components/RouteLayouts/Complex/Complex.tsx
+++ src/modules/pages/components/RouteLayouts/Complex/Complex.tsx
@@ -1,8 +1,10 @@
 import React, { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { RouteComponentProps } from '~pages/RouteLayouts';
 import SubscribedColoniesList from '~dashboard/SubscribedColoniesList';
 import SimpleNav from '../SimpleNav';
+import HistoryNavigation from '../HistoryNavigation';
 
 import styles from './Complex.css';
 
@@ -13,16 +15,47 @@ interface Props {
 
 const displayName = 'pages.Complex';
 
-const Complex = ({ children }: Props) => (
-  <SimpleNav>
+const Complex = ({
+  children,
+  routeProps: {
+    hasBackLink = true,
+    backRoute,
+    backText,
+    backTextValues,
+    hasSubscribedColonies = true,
+  } = {},
+}: Props) => {
+  const location = useLocation<{ hasBackLink?: boolean }>();
+  const backLinkExists =
+    hasBackLink === undefined
+      ? location.state && location.state.hasBackLink
+      : hasBackLink;
+
+  return (
     <div className={styles.main}>
-      <div className={styles.coloniesList}>
-        <SubscribedColoniesList />
-      </div>
-      <div className={styles.content}>{children}</div>
+      <SimpleNav>
+        {backLinkExists && (
+          <HistoryNavigation
+            backRoute={backRoute}
+            backText={backText}
+            backTextValues={backTextValues}
+            className={
+              hasSubscribedColonies ? styles.history : styles.onlyHistory
+            }
+          />
+        )}
+        <div className={styles.content}>
+          {hasSubscribedColonies && (
+            <div className={styles.coloniesList}>
+              <SubscribedColoniesList />
+            </div>
+          )}
+          <div className={styles.children}>{children}</div>
+        </div>
+      </SimpleNav>
     </div>
-  </SimpleNav>
-);
+  );
+};
 
 Complex.displayName = displayName;
 
