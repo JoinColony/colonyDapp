d7f208529f4bc9214376168de2d52538fd82bebe
Raul Glogovetan <raul@glogovetan.com>
2020-11-28 18:01:25 +0200

    Add: `ActionsPage` page sections structure

--- src/modules/dashboard/components/ActionsPage/ActionsPage.css.d.ts
+++ src/modules/dashboard/components/ActionsPage/ActionsPage.css.d.ts
@@ -1,1 +1,3 @@
 export const main: string;
+export const content: string;
+export const details: string;
--- src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
+++ src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { defineMessages } from 'react-intl';
+import Heading from '~core/Heading';
 
 import styles from './ActionsPage.css';
+
+const MSG = defineMessages({
+  genericAction: {
+    id: 'dashboard.ActionsPage.genericAction',
+    /*
+     * @TODO The action title needs to be decorated with the user mention
+     */
+    defaultMessage: 'Generic Action',
+  },
+});
 
 const displayName = 'dashboard.ActionsPage';
 
@@ -9,7 +21,27 @@ const ActionsPage = () => {
   const { transactionHash } = useParams<{
     transactionHash?: string;
   }>();
-  return <div className={styles.main}>{transactionHash}</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.content}>
+        <Heading
+          text={MSG.genericAction}
+          appearance={{
+            size: 'medium',
+            weight: 'medium',
+            theme: 'dark',
+          }}
+        />
+        {transactionHash}
+      </div>
+      <div className={styles.details}>
+        {/*
+         * @TODO Add in DEV-45
+         */}
+        Details component goes here
+      </div>
+    </div>
+  );
 };
 
 ActionsPage.displayName = displayName;
