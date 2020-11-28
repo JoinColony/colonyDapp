707a0b2d9a26d8c1dd98f054e008026b8be98e47
Raul Glogovetan <raul@glogovetan.com>
2020-11-28 20:16:40 +0200

    Add: `TransactionMeta` subcomponent

---
+++ src/modules/dashboard/components/ActionsPage/TransactionMeta/TransactionMeta.css.d.ts
@@ -0,0 +1,3 @@
+export const main: string;
+export const items: string;
+export const blockscoutLink: string;
---
+++ src/modules/dashboard/components/ActionsPage/TransactionMeta/TransactionMeta.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import TimeRelative from '~core/TimeRelative';
+import TransactionLink from '~core/TransactionLink';
+
+import { DEFAULT_NETWORK_INFO } from '~constants';
+
+import styles from './TransactionMeta.css';
+
+const MSG = defineMessages({
+  blockscout: {
+    id: 'dashboard.ActionsPage.TransactionMeta.blockscout',
+    defaultMessage: 'Etherscan',
+  },
+  transactionStatus: {
+    id: 'dashboard.ColonyHome.ColonyTitle.transactionStatus',
+    defaultMessage: `Transaction {status, select,
+      failed {failed}
+      pending {is pending...}
+      other {status is unknown}
+    }`,
+  },
+});
+
+const displayName = 'dashboard.ActionsPage.TransactionMeta';
+
+interface Props {
+  createdAt?: Date;
+  transactionHash?: string;
+  /*
+   * @NOTE Maybe we need to create a general enum for tx states
+   *
+   * But we won't know until everything is done
+   * But if that's the case, we need to change this to that enum.
+   */
+  status?: 'failed' | 'pending';
+}
+
+const TransactionMeta = ({ createdAt, transactionHash, status }: Props) => (
+  <ul className={styles.main}>
+    {createdAt && (
+      <li className={styles.items}>
+        <TimeRelative value={createdAt} />
+      </li>
+    )}
+    {transactionHash && (
+      <li className={styles.items}>
+        <TransactionLink
+          className={styles.blockscoutLink}
+          hash={transactionHash}
+          text={MSG.blockscout}
+          textValues={{
+            blockExplorerName: DEFAULT_NETWORK_INFO.blockExplorerName,
+          }}
+        />
+      </li>
+    )}
+    {status && (
+      <li className={styles.items}>
+        <FormattedMessage {...MSG.transactionStatus} values={{ status }} />
+      </li>
+    )}
+  </ul>
+);
+
+TransactionMeta.displayName = displayName;
+
+export default TransactionMeta;
