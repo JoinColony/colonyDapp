b5edd8eef3cc21d3b4d162b8c881a4e0839eae2b
Raul Glogovetan <raul@glogovetan.com>
2020-11-28 22:33:47 +0200

    Add: `ActionsPage` hash, status, title

--- src/modules/dashboard/components/ActionsPage/ActionsPage.css.d.ts
+++ src/modules/dashboard/components/ActionsPage/ActionsPage.css.d.ts
@@ -1,3 +1,4 @@
 export const main: string;
+export const container: string;
 export const content: string;
 export const details: string;
--- src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
+++ src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { defineMessages } from 'react-intl';
+
 import Heading from '~core/Heading';
+import TransactionHash from './TransactionHash';
+import TextDecorator from '~lib/TextDecorator';
+import UserMention from '~core/UserMention';
+
+import { STATUS } from './types';
 
 import styles from './ActionsPage.css';
 
 const MSG = defineMessages({
   genericAction: {
     id: 'dashboard.ActionsPage.genericAction',
-    /*
-     * @TODO The action title needs to be decorated with the user mention
-     */
-    defaultMessage: 'Generic Action',
+    defaultMessage: 'Generic Action with {user} mention',
   },
 });
 
@@ -21,24 +24,39 @@ const ActionsPage = () => {
   const { transactionHash } = useParams<{
     transactionHash?: string;
   }>();
+
+  const { Decorate } = new TextDecorator({
+    username: (usernameWithAtSign) => (
+      <UserMention username={usernameWithAtSign.slice(1)} />
+    ),
+  });
+
   return (
     <div className={styles.main}>
-      <div className={styles.content}>
-        <Heading
-          text={MSG.genericAction}
-          appearance={{
-            size: 'medium',
-            weight: 'medium',
-            theme: 'dark',
-          }}
-        />
-        {transactionHash}
-      </div>
-      <div className={styles.details}>
-        {/*
-         * @TODO Add in DEV-45
-         */}
-        Details component goes here
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Heading
+            text={MSG.genericAction}
+            textValues={{ user: <Decorate>@username</Decorate> }}
+            appearance={{
+              size: 'medium',
+              weight: 'medium',
+              theme: 'dark',
+            }}
+          />
+          {transactionHash && (
+            <TransactionHash
+              transactionHash={transactionHash}
+              status={STATUS.Succeeded}
+            />
+          )}
+        </div>
+        <div className={styles.details}>
+          {/*
+           * @TODO Add in DEV-45
+           */}
+          Details component goes here
+        </div>
       </div>
     </div>
   );
