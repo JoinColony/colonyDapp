0f58af3a792e4841bdf4d76dbff34d97caa8d0c3
Raul Glogovetan <raul@glogovetan.com>
2020-11-30 20:29:09 +0200

    Add: initial `ActionsPageFeed` component

---
+++ src/modules/dashboard/components/ActionsPageFeed/ActionsPageFeed.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './ActionsPageFeed.css';
+
+const displayName = 'dashboard.ActionsPageFeed';
+
+const ActionsPageFeed = () => <div className={styles.main} />;
+
+ActionsPageFeed.displayName = displayName;
+
+export default ActionsPageFeed;
