dd940335a47e8263c98069dfb04b226f4d74063b
Raul Glogovetan <raul@glogovetan.com>
2020-12-01 14:28:17 +0200

    Rebase Fix: move token dialogs to `dashboard` module

--- src/modules/admin/components/Tokens/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ColonyTokenEditDialog } from './ColonyTokenEditDialog';
-export { default as TokenMintForm } from './TokenMintForm';
-export { default as TokenMintDialog } from './TokenMintDialog';
