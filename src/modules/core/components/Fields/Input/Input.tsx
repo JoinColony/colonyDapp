501ae75f5040248f4fcc11d64e21dde8df943be1
ArmandoGraterol <armandogrtrl@gmail.com>
2020-12-03 18:13:50 -0300

    Removed duplicated prop from Input component

--- src/modules/core/components/Fields/Input/Input.tsx
+++ src/modules/core/components/Fields/Input/Input.tsx
@@ -91,7 +91,6 @@ const Input = ({
   placeholderValues,
   status,
   statusValues,
-  disabled,
 }: Props) => {
   const [id] = useState(idProp || nanoid());
   const { formatMessage } = useIntl();
