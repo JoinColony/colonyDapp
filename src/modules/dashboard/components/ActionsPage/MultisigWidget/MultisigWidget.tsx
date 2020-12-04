effbe56ac1ed0f81cf0d1016b5c3a94c291ccc1a
Alicja Kujawa <alicjak94.ak@gmail.com>
2020-12-04 05:59:40 +0100

    Add MultisigWidget component

---
+++ src/modules/dashboard/components/ActionsPage/MultisigWidget/MultisigWidget.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const displayName = 'dashboard.ActionsPage.MultisigWidget';
+
+
+interface Props {
+
+}
+
+const MultisigWidget = ({  }: Props) => {
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+MultisigWidget.displayName = displayName;
+
+export default MultisigWidget;
