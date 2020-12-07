fc31d47f910681140ac04af712b8035310f53504
Alicja Kujawa <alicjak94.ak@gmail.com>
2020-12-07 14:03:07 +0100

    Add styles for MultisigWidget

---
+++ src/modules/dashboard/components/ActionsPage/MultisigWidget/MultisigWidget.css.d.ts
@@ -0,0 +1,7 @@
+export const wrapper: string;
+export const status: string;
+export const statusApproved: string;
+export const avatars: string;
+export const footer: string;
+export const title: string;
+export const progressBarLabel: string;
