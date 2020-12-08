d6d2c2c409c8ebde0cbe8ddf678ee9cf5dfd5037
Alicja Kujawa <alicjak94.ak@gmail.com>
2020-12-08 16:56:37 +0100

    Add CoreSelect skeleton component

---
+++ src/modules/core/components/ColorSelect/ColorSelect.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const displayName = 'ColorSelect';
+
+interface Props {}
+
+const ColorSelect = ({}: Props) => {
+  return <div />;
+};
+
+ColorSelect.displayName = displayName;
+
+export default ColorSelect;
