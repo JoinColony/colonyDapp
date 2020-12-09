3e34eeb484562812c376c8d5c0a3efae0af448e8
Raul Glogovetan <raul@glogovetan.com>
2020-12-09 15:54:57 +0200

    Add: `ActionsPage` external message descriptors

---
+++ src/modules/dashboard/components/ActionsPage/messages.ts
@@ -0,0 +1,15 @@
+import { defineMessages } from 'react-intl';
+
+import { ColonyActions } from '~types/index';
+
+const MSG = defineMessages({
+  actionTitle: {
+    id: 'dashboard.ActionsPage.actionTitle',
+    defaultMessage: `{actionType, select,
+      ${ColonyActions.Payment} {Pay {recipient} {amount} {tokenSymbol}}
+      other {Some other title}
+    }`,
+  },
+});
+
+export default MSG;
