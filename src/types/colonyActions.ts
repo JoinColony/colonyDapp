59a2b0261504475ece0873887f24772caaf5d0b5
Raul Glogovetan <raul@glogovetan.com>
2020-12-09 12:33:58 +0200

    Add: `colonyActions` types

---
+++ src/types/colonyActions.ts
@@ -0,0 +1,13 @@
+export enum ColonyActions {
+  Generic = 'Generic',
+  Payment = 'Payment',
+}
+
+export enum ColonyAndExtensionsEvents {
+  ColonyFundsMovedBetweenFundingPots = 'ColonyFundsMovedBetweenFundingPots',
+  FundingPotAdded = 'FundingPotAdded',
+  PaymentAdded = 'PaymentAdded',
+  PayoutClaimed = 'PayoutClaimed',
+  OneTxPaymentMade = 'OneTxPaymentMade',
+  Transfer = 'Transfer',
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -13,6 +13,7 @@ export * from './keyTypes';
 export * from './DefaultValues';
 export * from './RecordToJS';
 export * from './context';
+export * from './colonyActions';
 
 export type WithKey = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
