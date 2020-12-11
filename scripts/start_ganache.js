45dfe79cbdf2e42ce57b3b985f4334e69fa3c5af
Raul Glogovetan <raul@glogovetan.com>
2020-12-11 14:25:36 +0200

    Update: ganache start script to listen to all hosts

--- scripts/start_ganache.js
+++ scripts/start_ganache.js
@@ -10,10 +10,10 @@ const startGanache = async () => {
   const process = spawn(
     'ganache-cli',
     [
+      '--h', '0.0.0.0',
       '--acctKeys', './src/lib/colonyNetwork/ganache-accounts.json',
       '--noVMErrorsOnRPCResponse',
       '--gasLimit', '6721975',
-      '--networkId', '1',
       '--account', '0x0355596cdb5e5242ad082c4fe3f8bbe48c9dba843fe1f99dd8272f487e70efae,100000000000000000000',
       '--account', '0xe9aebe8791ad1ebd33211687e9c53f13fe8cca53b271a6529c7d7ba05eda5ce2,100000000000000000000',
       '--account', '0x6f36842c663f5afc0ef3ac986ec62af9d09caa1bbf59a50cdb7334c9cc880e65,100000000000000000000',
@@ -26,6 +26,12 @@ const startGanache = async () => {
       '--account', '0x5e383d2f98ac821c555333e5bb6109ca41ae89d613cb84887a2bdb933623c4e3,100000000000000000000',
       '--account', '0x33d2f6f6cc410c1d46d58f17efdd2b53a71527b27eaa7f2edcade351feb87425,100000000000000000000',
       '--account', '0x32400a48ff16119c134eef44e2627502ce6e367bc4810be07642275a9db47bf7,100000000000000000000',
+      '--account', '0x2a0f58ae46261b4ec4b635bde4bfabb680245c2a3abff7f54945ae44f7629b1d,100000000000000000000',
+      '--account', '0x94fe165ae1db4f7d24fa5506ecbf083dcb934823600cb56e2a191722f0b40903,100000000000000000000',
+      '--account', '0xc93aad16dd4aca2fa61316f83307362306ad6b2fc3e4a91801ce9010be7d9b63,100000000000000000000',
+      '--account', '0x27f8f0be23a027196c7b8f4c98502b113e3fa1474fc10eda21ef3b5473c1b773,100000000000000000000',
+      '--account', '0xb6245e0d2b64a92c0e6359500231087278f499de46fdfa351d4f1e09faf95a47,100000000000000000000',
+      '--account', '0xfe6066af949ec3c2c88ac10f47907c6d4e200c37b28b5af49e7d0ffd5c301c5c,100000000000000000000',
     ],
     {
       stdio,
