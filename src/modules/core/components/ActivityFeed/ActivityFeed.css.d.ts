ddc827cc53689c2fb1361dc09a46b365e23e5994
ArmandoGraterol <armandogrtrl@gmail.com>
2020-12-15 15:57:40 -0300

    Removed ActivityFeed core component

--- src/modules/core/components/ActivityFeed/ActivityFeed.css.d.ts
+++
@@ -1,2 +0,0 @@
-export const activityFeedHeaderCell: string;
-export const main: string;
--- src/modules/core/components/ActivityFeed/ActivityFeedItem.css.d.ts
+++
@@ -1,4 +0,0 @@
-export const activityFeedItemCell: string;
-export const placeholderItem: string;
-export const placeholderCoverUp: string;
-export const pipe: string;
