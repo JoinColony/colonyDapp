cefa17d75a15a0d3c315b5fb02662848bb892987
Raul Glogovetan <raul@glogovetan.com>
2020-12-19 14:13:58 +0200

    Refactor: `getActionsListData` initial list sorting

--- src/modules/dashboard/transformers.ts
+++ src/modules/dashboard/transformers.ts
@@ -75,7 +75,13 @@ export const getActionsListData = (
     );
     return null;
   });
-  return formattedActions.sort(
-    ({ createdAt: first }, { createdAt: second }) => second - first,
-  );
+  /*
+   * @NOTE Sort the list with new items first
+   *
+   * Since the query returns the actions list sorted chronologhical by default
+   * we just need to reverse it.
+   *
+   * This is much faster than sorting this by the `createdAt` prop
+   */
+  return formattedActions.reverse();
 };
