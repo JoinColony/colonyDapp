9002fc377132728b86a01a56c01e429361b9054b
jayaFeola <34057551+chinins@users.noreply.github.com>
2020-12-21 12:06:27 +0000

    Feat: add final functionality & desription to `ManageDomainsDialog`

--- src/modules/dashboard/components/ManageDomainsDialog/ManageDomainsDialog.tsx
+++ src/modules/dashboard/components/ManageDomainsDialog/ManageDomainsDialog.tsx
@@ -4,39 +4,63 @@ import { defineMessages } from 'react-intl';
 import { DialogProps } from '~core/Dialog';
 import IndexModal from '~core/IndexModal';
 
+import { WizardDialogType } from '~utils/hooks';
+
 const MSG = defineMessages({
-  title: {
-    id: 'dashboard.ManageDomainsDialog.title',
+  dialogHeader: {
+    id: 'dashboard.ManageDomainsDialog.dialogHeader',
     defaultMessage: 'Manage domains',
   },
-  creatNewDomain: {
-    id: 'dashboard.ManageDomainsDialog.creatNewDomain',
+  createNewDomainTitle: {
+    id: 'dashboard.ManageDomainsDialog.createNewDomainTitle',
     defaultMessage: 'Create new domain',
   },
-  editDomain: {
-    id: 'dashboard.ManageDomainsDialog.editDomain',
+  createNewDomainDescription: {
+    id: 'dashboard.ManageDomainsDialog.createNewDomainDescription',
+    defaultMessage:
+      'Teams, departments, circles: domains let you froup types of activity.',
+  },
+  editDomainTitle: {
+    id: 'dashboard.ManageDomainsDialog.editDomainTitle',
     defaultMessage: 'Edit domain',
+  },
+  editDomainDescription: {
+    id: 'dashboard.ManageDomainsDialog.editDomainDescription',
+    defaultMessage: `Need to repurpose domain? Here's the place to do it.`,
   },
 });
 
+interface CustomWizardDialogueProps {
+  nextStep: string;
+  prevStep: string;
+}
+
+type Props = DialogProps & WizardDialogType<object> & CustomWizardDialogueProps;
+
 const displayName = 'dashboard.ManageDomainsDialog';
 
-const ManageDomainsDialog = ({ cancel, close }: DialogProps) => {
+const ManageDomainsDialog = ({ cancel, close, callStep, prevStep }: Props) => {
   const items = [
     {
-      title: MSG.creatNewDomain,
-      description: MSG.creatNewDomain,
+      title: MSG.createNewDomainTitle,
+      description: MSG.createNewDomainDescription,
       icon: 'emoji-crane',
     },
     {
-      title: MSG.editDomain,
-      description: MSG.editDomain,
+      title: MSG.editDomainTitle,
+      description: MSG.editDomainDescription,
       icon: 'pencil-note',
     },
   ];
 
   return (
-    <IndexModal title={MSG.title} cancel={cancel} close={close} items={items} />
+    <IndexModal
+      title={MSG.dialogHeader}
+      cancel={cancel}
+      close={close}
+      items={items}
+      back={() => callStep(prevStep)}
+    />
   );
 };
 
