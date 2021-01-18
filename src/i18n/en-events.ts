a8d3f2f86d2de2e0c7ee7db9cfcbbadbea9c4f3e
Raul Glogovetan <raul@glogovetan.com>
2021-01-18 20:35:14 +0200

    Add: create domain action copy

--- src/i18n/en-events.ts
+++ src/i18n/en-events.ts
@@ -7,6 +7,7 @@ const eventsMessageDescriptors = {
       ${ColonyAndExtensionsEvents.OneTxPaymentMade} {{initiator} paid {amount} {tokenSymbol} from {fromDomain} to {recipient}}
       ${ColonyAndExtensionsEvents.ColonyFundsMovedBetweenFundingPots} {{initiator} transferred {amount} {tokenSymbol} from the {fromDomain} to the {toDomain}}
       ${ColonyAndExtensionsEvents.TokensMinted} {{initiator} minted {amount} {tokenSymbol} to {recipient}}
+      ${ColonyAndExtensionsEvents.DomainAdded} {{initiator} added Team: {fromDomain}}
       other {{eventNameDecorated} emmited by {clientOrExtensionType}}
     }`,
 };
