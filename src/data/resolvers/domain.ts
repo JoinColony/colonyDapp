4826d567b8048bf97fdec0875a12c941102336a3
Raul Glogovetan <raul@glogovetan.com>
2021-01-18 19:14:27 +0200

    Add: domain description graphql client resolver

--- src/data/resolvers/domain.ts
+++ src/data/resolvers/domain.ts
@@ -13,11 +13,12 @@ export const domainResolvers = ({ ipfs }: Required<Context>): Resolvers => ({
       const metadata = JSON.parse(metadataString || '{}');
       return metadata?.domainColor || Color.Pink;
     },
-    description: () => {
-      /*
-       * @TODO parse logs for description here
-       */
-      return null;
+    async description({ metadata: metadataHash, metadataHistory }) {
+      const lastMetadata = metadataHistory.slice(-1).pop();
+      const ipfsHash = metadataHash || lastMetadata?.metadata || '';
+      const metadataString = await ipfs.getString(ipfsHash as string);
+      const metadata = JSON.parse(metadataString || '{}');
+      return metadata?.domainPurpose || null;
     },
   },
 });
