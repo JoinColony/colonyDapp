096a71fa0138f631000f3b7f6e996a09786564dc
Raul Glogovetan <raul@glogovetan.com>
2021-01-28 19:36:48 +0200

    Add: Pinata client external lib

---
+++ src/lib/pinata/constants.ts
@@ -0,0 +1,7 @@
+export const PINATA_GATEWAY = 'https://gateway.pinata.cloud/ipfs';
+export const PINATA_ENDPOINT = 'https://api.pinata.cloud/pinning';
+export const PINATA_API_KEY: string | undefined =
+  process.env.PINATA_API_KEY || undefined;
+export const PINATA_API_SECRET: string | undefined =
+  process.env.PINATA_API_SECRET || undefined;
+export const JSON_MIME_TYPE = 'application/json';
