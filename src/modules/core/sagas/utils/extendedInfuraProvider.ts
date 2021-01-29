f97f51448355f3b17fcbd0d1ad9ea1feb92c30e5
Raul Glogovetan <raul@glogovetan.com>
2021-01-29 14:20:36 +0200

    Fix: extended providers constructor calls

--- src/modules/core/sagas/utils/extendedInfuraProvider.ts
+++ src/modules/core/sagas/utils/extendedInfuraProvider.ts
@@ -2,13 +2,20 @@ import { InfuraProvider, BlockTag } from 'ethers/providers';
 import { poll } from 'ethers/utils';
 
 export class ExtendedInfuraProvider extends InfuraProvider {
+  _parentGetCode: any;
+
+  constructor(...args) {
+    super(...args);
+    this._parentGetCode = super.getCode;
+  }
+
   getCode(
     addressOrName: string | Promise<string>,
     blockTag?: BlockTag | Promise<BlockTag>,
   ): Promise<string> {
     return poll(
       async () => {
-        const result = await super.getCode(addressOrName, blockTag);
+        const result = await this._parentGetCode(addressOrName, blockTag);
         if (result === '0x') {
           return undefined;
         }
--- src/modules/core/sagas/utils/extendedJsonRpcProvider.ts
+++ src/modules/core/sagas/utils/extendedJsonRpcProvider.ts
@@ -2,13 +2,20 @@ import { JsonRpcProvider, BlockTag } from 'ethers/providers';
 import { poll } from 'ethers/utils';
 
 export class ExtendedJsonRpcProvider extends JsonRpcProvider {
+  _parentGetCode: any;
+
+  constructor(...args) {
+    super(...args);
+    this._parentGetCode = super.getCode;
+  }
+
   getCode(
     addressOrName: string | Promise<string>,
     blockTag?: BlockTag | Promise<BlockTag>,
   ): Promise<string> {
     return poll(
       async () => {
-        const result = await super.getCode(addressOrName, blockTag);
+        const result = await this._parentGetCode(addressOrName, blockTag);
         if (result === '0x') {
           return undefined;
         }
