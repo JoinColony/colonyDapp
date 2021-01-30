3fb5702827676a200f675a1f62f2889252d488ab
Raul Glogovetan <raul@glogovetan.com>
2021-01-30 22:50:13 +0200

    Optimize: colony edit / manage tokens tx wait time

--- src/redux/types/actions/colonyActions.ts
+++ src/redux/types/actions/colonyActions.ts
@@ -84,6 +84,8 @@ export type ColonyActionsActionTypes =
         colonyName: string;
         colonyDisplayName: string;
         colonyAvatarImage?: string;
+        colonyAvatarHash?: string;
+        hasAvatarChanged?: boolean;
         colonyTokens?: Address[];
         annotationMessage?: string;
         /*
