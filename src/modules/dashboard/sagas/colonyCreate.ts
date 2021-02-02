d146f0ca06459c75ddf3dd4d4706baf5a2d1d3e4
Raul Glogovetan <raul@glogovetan.com>
2021-02-02 19:20:47 +0200

    Refactor: `colonyCreate` saga to only handle creating

--- src/modules/dashboard/sagas/colonyCreate.ts
+++ src/modules/dashboard/sagas/colonyCreate.ts
@@ -7,11 +7,7 @@ import {
   ClientType,
   ROOT_DOMAIN_ID,
   ColonyVersion,
-  ColonyClient,
-  ColonyRole,
-  TokenClientType,
 } from '@colony/colony-js';
-import { AddressZero } from 'ethers/constants';
 import { poll } from 'ethers/utils';
 
 import { ContextModule, TEMP_getContext } from '~context/index';
@@ -19,9 +15,6 @@ import { DEFAULT_TOKEN_DECIMALS } from '~constants';
 import {
   getLoggedInUser,
   refetchUserNotifications,
-  ColonyProfileDocument,
-  ColonyProfileQuery,
-  ColonyProfileQueryVariables,
   CreateUserMutation,
   CreateUserDocument,
   CreateUserMutationVariables,
@@ -52,106 +45,11 @@ interface ChannelDefinition {
   id: string;
 }
 
-function* getRecoveryInfo(recoveryAddress: string) {
-  const { username, walletAddress } = yield getLoggedInUser();
-  const apolloClient = TEMP_getContext(ContextModule.ApolloClient);
-  const { networkClient } = TEMP_getContext(ContextModule.ColonyManager);
-  const ens = TEMP_getContext(ContextModule.ENS);
-
-  const colonyInfo = {
-    isProfileCreated: false,
-    isNativeTokenColonyToken: false,
-    isTokenAuthoritySetUp: false,
-    isOneTxExtensionDeployed: false,
-    hasOneTxAdminRole: false,
-    hasOneTxFundingRole: false,
-    colonyAddress: '',
-    colonyName: '',
-    tokenAddress: '',
-    tokenName: '',
-    tokenSymbol: '',
-  };
-
-  if (!username) {
-    throw new Error('User does not have a username associated.');
-  }
-
-  const colonyClient: ColonyClient = yield networkClient.getColonyClient(
-    recoveryAddress,
-  );
-
-  const domain = yield ens.getDomain(colonyClient.address, networkClient);
-  colonyInfo.colonyName = ENS.stripDomainParts('colony', domain);
-
-  const hasRecoveryRole = yield colonyClient.hasUserRole(
-    walletAddress,
-    ROOT_DOMAIN_ID,
-    ColonyRole.Recovery,
-  );
-
-  if (!hasRecoveryRole) {
-    throw new Error('User does not have the permission to recovery colony');
-  }
-
-  try {
-    yield apolloClient.query<ColonyProfileQuery, ColonyProfileQueryVariables>({
-      query: ColonyProfileDocument,
-      variables: {
-        address: colonyClient.address,
-      },
-    });
-    colonyInfo.isProfileCreated = true;
-  } catch {
-    colonyInfo.isProfileCreated = false;
-  }
-
-  const { tokenClient } = colonyClient;
-
-  const tokenInfo = yield tokenClient.getTokenInfo();
-
-  colonyInfo.tokenName = tokenInfo.name;
-  colonyInfo.tokenSymbol = tokenInfo.symbol;
-
-  colonyInfo.colonyAddress = colonyClient.address;
-  colonyInfo.tokenAddress = tokenClient.address;
-
-  if (tokenClient.tokenClientType === TokenClientType.Colony) {
-    colonyInfo.isNativeTokenColonyToken = true;
-    const tokenAuthority = yield tokenClient.authority();
-    if (tokenAuthority !== AddressZero) {
-      colonyInfo.isTokenAuthoritySetUp = true;
-    }
-  }
-
-  // eslint-disable-next-line max-len
-  const oneTxExtensionAddress = yield networkClient.getExtensionInstallation(
-    recoveryAddress,
-    getExtensionHash(Extension.OneTxPayment),
-  );
-
-  if (oneTxExtensionAddress !== AddressZero) {
-    colonyInfo.isOneTxExtensionDeployed = true;
-    colonyInfo.hasOneTxAdminRole = yield colonyClient.hasUserRole(
-      oneTxExtensionAddress,
-      ROOT_DOMAIN_ID,
-      ColonyRole.Administration,
-    );
-    colonyInfo.hasOneTxFundingRole = yield colonyClient.hasUserRole(
-      oneTxExtensionAddress,
-      ROOT_DOMAIN_ID,
-      ColonyRole.Funding,
-    );
-  }
-
-  return colonyInfo;
-}
-
 function* colonyCreate({
   meta,
   payload: {
     colonyName: givenColonyName,
     displayName,
-    recover: recoveryAddress,
     tokenAddress: givenTokenAddress,
     tokenChoice,
     tokenName: givenTokenName,
@@ -166,48 +64,30 @@ function* colonyCreate({
 
   const channelNames: string[] = [];
 
-  let recoveryInfo;
-
-  if (recoveryAddress) {
-    recoveryInfo = yield getRecoveryInfo(recoveryAddress);
-  }
-
   /*
    * If the user did not claim a profile yet, define a tx to create the user.
    */
-  if (!currentUsername && !recoveryAddress) channelNames.push('createUser');
+  if (!currentUsername) {
+    channelNames.push('createUser');
+  }
   /*
    * If the user opted to create a token, define a tx to create the token.
    */
-  if (tokenChoice === 'create' && !recoveryAddress) {
+  if (tokenChoice === 'create') {
     channelNames.push('createToken');
   }
-  if (!recoveryAddress) channelNames.push('createColony');
+  channelNames.push('createColony');
   /*
    * If the user opted to create a token,  define txs to manage the token.
    */
-  if (
-    tokenChoice === 'create' &&
-    (!recoveryAddress || (recoveryInfo && !recoveryInfo.isTokenAuthoritySetUp))
-  ) {
+  if (tokenChoice === 'create') {
     channelNames.push('deployTokenAuthority');
     channelNames.push('setTokenAuthority');
   }
 
-  if (
-    !recoveryAddress ||
-    (recoveryInfo && !recoveryInfo.isOneTxExtensionDeployed)
-  ) {
-    channelNames.push('deployOneTx');
-  }
-
-  if (!recoveryAddress || (recoveryInfo && !recoveryInfo.hasOneTxAdminRole)) {
-    channelNames.push('setOneTxRoleAdministration');
-  }
-
-  if (!recoveryAddress || (recoveryInfo && !recoveryInfo.hasOneTxFundingRole)) {
-    channelNames.push('setOneTxRoleFunding');
-  }
+  channelNames.push('deployOneTx');
+  channelNames.push('setOneTxRoleAdministration');
+  channelNames.push('setOneTxRoleFunding');
 
   /*
    * Define a manifest of transaction ids and their respective channels.
@@ -243,15 +123,11 @@ function* colonyCreate({
    * Create all transactions for the group.
    */
   try {
-    const colonyName = ENS.normalize(
-      (recoveryInfo && recoveryInfo.colonyName) || givenColonyName,
-    );
+    const colonyName = ENS.normalize(givenColonyName);
     const username = ENS.normalize(givenUsername);
 
-    const tokenName =
-      (recoveryInfo && recoveryInfo.tokenName) || givenTokenName;
-    const tokenSymbol =
-      (recoveryInfo && recoveryInfo.tokenSymbol) || givenTokenSymbol;
+    const tokenName = givenTokenName;
+    const tokenSymbol = givenTokenSymbol;
 
     if (createUser) {
       yield createGroupedTransaction(createUser, {
@@ -377,8 +253,6 @@ function* colonyCreate({
         ActionTypes.TRANSACTION_SUCCEEDED,
       );
       tokenAddress = createAddress(deployedContractAddress);
-    } else if (recoveryInfo && recoveryInfo.tokenAddress) {
-      tokenAddress = recoveryInfo.tokenAddress;
     } else {
       if (!givenTokenAddress) {
         throw new Error('Token address not provided');
@@ -389,8 +263,7 @@ function* colonyCreate({
     /**
      * If we're not recovering this will be overwritten
      */
-    let colonyAddress = recoveryInfo && recoveryInfo.colonyAddress;
-
+    let colonyAddress;
     if (createColony) {
       const colonyMetadataIpfsHash = yield call(
         ipfsUpload,
@@ -434,10 +307,8 @@ function* colonyCreate({
       yield put(transactionLoadRelated(createColony.id, true));
     }
 
-    if (!recoveryAddress || (recoveryInfo && !recoveryInfo.isProfileCreated)) {
-      if (createColony) {
-        yield put(transactionLoadRelated(createColony.id, false));
-      }
+    if (createColony) {
+      yield put(transactionLoadRelated(createColony.id, false));
     }
     /*
      * Add a colonyAddress identifier to all pending transactions.
@@ -555,39 +426,40 @@ function* colonyCreate({
         setOneTxRoleFunding.channel,
         ActionTypes.TRANSACTION_SUCCEEDED,
       );
-
-      /*
-       * Manually subscribe the user to the colony
-       *
-       * @NOTE That this just subscribes the user to a particular address, as we
-       * don't have the capability any more, to check if that address is a valid
-       * colony, on the server side
-       *
-       * However, we do know that his colony actually exists, since we just
-       * created it, but be **WARNED** that his is race condition!!
-       *
-       * We just skirt around it by calling this mutation after the whole batch
-       * of transactions have been sent, assuming that by that time, the subgraph
-       * had time to ingest the new block in which the colony was created.
-       *
-       * However, due to various network conditions, this might not be case, and
-       * the colony might not exist still.
-       *
-       * It's not a super-huge deal breaker, as a page refresh will solve it,
-       * and the colony is still usable, just that it doesn't provide _that_
-       * nice of a user experience.
-       */
-      yield apolloClient.mutate<
-        SubscribeToColonyMutation,
-        SubscribeToColonyMutationVariables
-      >({
-        mutation: SubscribeToColonyDocument,
-        variables: {
-          input: { colonyAddress },
-        },
-        update: cacheUpdates.subscribeToColony(colonyAddress),
-      });
     }
+
+    /*
+     * Manually subscribe the user to the colony
+     *
+     * @NOTE That this just subscribes the user to a particular address, as we
+     * don't have the capability any more, to check if that address is a valid
+     * colony, on the server side
+     *
+     * However, we do know that his colony actually exists, since we just
+     * created it, but be **WARNED** that his is race condition!!
+     *
+     * We just skirt around it by calling this mutation after the whole batch
+     * of transactions have been sent, assuming that by that time, the subgraph
+     * had time to ingest the new block in which the colony was created.
+     *
+     * However, due to various network conditions, this might not be case, and
+     * the colony might not exist still.
+     *
+     * It's not a super-huge deal breaker, as a page refresh will solve it,
+     * and the colony is still usable, just that it doesn't provide _that_
+     * nice of a user experience.
+     */
+    yield apolloClient.mutate<
+      SubscribeToColonyMutation,
+      SubscribeToColonyMutationVariables
+    >({
+      mutation: SubscribeToColonyDocument,
+      variables: {
+        input: { colonyAddress },
+      },
+      update: cacheUpdates.subscribeToColony(colonyAddress),
+    });
+
     return null;
   } catch (error) {
     yield putError(ActionTypes.COLONY_CREATE_ERROR, error, meta);
