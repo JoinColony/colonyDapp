a52b89e32947a36cea52e4e07c7e9b4ca2ec99c5
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-02-09 17:26:00 +0000

    Feat: add onhover lin to `BetaCautionAlert`

--- src/modules/core/components/BetaCautionAlert/BetaCautionAlert.tsx
+++ src/modules/core/components/BetaCautionAlert/BetaCautionAlert.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import styles from './BetaCautionAlert.css';
@@ -12,20 +12,46 @@ const MSG = {
     id: 'BetaCautionAlert.cautionText',
     defaultMessage: 'Use with caution!',
   },
+  learnMore: {
+    id: 'BetaCautionAlert.learnMore',
+    defaultMessage: 'Learn more',
+  },
 };
 
 const BetaCautionAlert = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const toggleHover = (hasHover) => setIsHovered(hasHover);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.pinkStripe} />
-      <div>
-        <div className={styles.betaText}>
-          <FormattedMessage {...MSG.beta} />
-        </div>
-        <div className={styles.cautionText}>
-          <FormattedMessage {...MSG.cautionText} />
-        </div>
-      </div>
+    <div
+      className={styles.container}
+      onMouseEnter={() => toggleHover(true)}
+      onMouseLeave={() => toggleHover(false)}
+    >
+      {isHovered ? (
+        <a
+          // need to provide a proper link (probably to notion page)
+          href="https://colony.io"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          <FormattedMessage {...MSG.learnMore} />
+        </a>
+      ) : (
+        <>
+          <div className={styles.pinkStripe} />
+          <div>
+            <div className={styles.betaText}>
+              <FormattedMessage {...MSG.beta} />
+            </div>
+            <div className={styles.cautionText}>
+              <FormattedMessage {...MSG.cautionText} />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
