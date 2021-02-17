92dac623bb223a123bfd55fd107884a10160a6d5
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-02-17 14:02:58 +0100

    Add token activation button to UserNavigation

--- src/modules/pages/components/RouteLayouts/UserNavigation.tsx
+++ src/modules/pages/components/RouteLayouts/UserNavigation.tsx
@@ -112,6 +112,13 @@ const UserNavigation = () => {
         </ConnectWalletPopover>
       )}
       {userCanNavigate && (
+        <>
+          <button type="button" className={styles.tokens}>
+            TEST
+          </button>
+        </>
+      )}
+      {userCanNavigate && (
         <GasStationPopover
           transactionAndMessageGroups={transactionAndMessageGroups}
         >
