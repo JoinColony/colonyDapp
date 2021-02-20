fc4ea6ece1b03a00e5fd729fb4c7bc3cb52e0b81
Raul Glogovetan <raul@glogovetan.com>
2021-02-20 12:13:13 +0200

    Move: `colonyClaimToken` saga to dashboard

--- src/modules/dashboard/sagas/colony.ts
+++ src/modules/dashboard/sagas/colony.ts
@@ -1,12 +1,18 @@
 import { call, fork, put, takeEvery } from 'redux-saga/effects';
 import { ClientType } from '@colony/colony-js';
 
-import { Action, ActionTypes } from '~redux/index';
+import { Action, ActionTypes, AllActions } from '~redux/index';
 import { putError, takeFrom } from '~utils/saga/effects';
 import {
   ProcessedColonyDocument,
   ProcessedColonyQuery,
   ProcessedColonyQueryVariables,
+  ColonyTransfersDocument,
+  ColonyTransfersQueryVariables,
+  ColonyTransfersQuery,
+  TokenBalancesForDomainsQuery,
+  TokenBalancesForDomainsQueryVariables,
+  TokenBalancesForDomainsDocument,
 } from '~data/index';
 import { ContextModule, TEMP_getContext } from '~context/index';
 
@@ -58,9 +64,62 @@ function* colonyRecoveryModeEnter({
   return null;
 }
 
+function* colonyClaimToken({
+  payload: { colonyAddress, tokenAddress },
+  meta,
+}: Action<ActionTypes.COLONY_CLAIM_TOKEN>) {
+  let txChannel;
+  try {
+    const apolloClient = TEMP_getContext(ContextModule.ApolloClient);
+
+    txChannel = yield call(getTxChannel, meta.id);
+    yield fork(createTransaction, meta.id, {
+      context: ClientType.ColonyClient,
+      methodName: 'claimColonyFunds',
+      identifier: colonyAddress,
+      params: [tokenAddress],
+    });
+
+    const { payload } = yield takeFrom(
+      txChannel,
+      ActionTypes.TRANSACTION_SUCCEEDED,
+    );
+
+    yield put<AllActions>({
+      type: ActionTypes.COLONY_CLAIM_TOKEN_SUCCESS,
+      payload,
+      meta,
+    });
+
+    // Refresh relevant values
+    yield apolloClient.query<
+      ColonyTransfersQuery,
+      ColonyTransfersQueryVariables
+    >({
+      query: ColonyTransfersDocument,
+      variables: { address: colonyAddress },
+      fetchPolicy: 'network-only',
+    });
+    yield apolloClient.query<
+      TokenBalancesForDomainsQuery,
+      TokenBalancesForDomainsQueryVariables
+    >({
+      query: TokenBalancesForDomainsDocument,
+      variables: { colonyAddress, tokenAddresses: [tokenAddress] },
+      fetchPolicy: 'network-only',
+    });
+  } catch (error) {
+    return yield putError(ActionTypes.COLONY_CLAIM_TOKEN_ERROR, error, meta);
+  } finally {
+    if (txChannel) txChannel.close();
+  }
+  return null;
+}
+
 export default function* colonySagas() {
   yield takeEvery(
     ActionTypes.COLONY_RECOVERY_MODE_ENTER,
     colonyRecoveryModeEnter,
   );
+  yield takeEvery(ActionTypes.COLONY_CLAIM_TOKEN, colonyClaimToken);
 }
