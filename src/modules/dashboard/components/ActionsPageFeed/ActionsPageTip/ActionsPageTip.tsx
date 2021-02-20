b12b8426cebfa45e44e8b05387b08a4e2cd08f95
Raul Glogovetan <raul@glogovetan.com>
2021-02-20 14:56:58 +0200

    Add: `ActionsPageTip` component

---
+++ src/modules/dashboard/components/ActionsPageFeed/ActionsPageTip/ActionsPageTip.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MessageDescriptor, FormattedMessage } from 'react-intl';
+
+import { getMainClasses } from '~utils/css';
+import { UniversalMessageValues } from '~types/index';
+
+import styles from './ActionsPageTip.css';
+
+const displayName = 'dashboard.ActionsPageFeed.ActionsPageTip';
+
+interface Appearance {
+  theme?: 'default' | 'recovery';
+}
+
+interface Props {
+  appearance?: Appearance;
+  tip?: MessageDescriptor | string;
+  tipValues?: UniversalMessageValues;
+}
+
+const ActionsPageTip = ({ tip, tipValues, appearance }: Props) => (
+  <div className={getMainClasses(appearance, styles)}>
+    <div className={styles.content}>
+      {tip && tip === 'string' ? (
+        tip
+      ) : (
+        <FormattedMessage {...tip} values={tipValues} />
+      )}
+    </div>
+  </div>
+);
+
+ActionsPageTip.displayName = displayName;
+
+export default ActionsPageTip;
