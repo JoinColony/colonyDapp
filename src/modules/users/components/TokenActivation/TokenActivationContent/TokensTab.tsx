f8e71766cad83c24a02ed0ad1c2ec0c290316835
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-02-20 17:54:18 +0000

    WIP: add `TokensTab` styles & structure

--- src/modules/users/components/TokenActivation/TokenActivationContent/TokensTab.tsx
+++ src/modules/users/components/TokenActivation/TokenActivationContent/TokensTab.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage, defineMessages } from 'react-intl';
+
+import Button from '~core/Button';
+import Icon from '~core/Icon';
+import TokenIcon from '~dashboard/HookedTokenIcon';
 
 import styles from './TokenActivationContent.css';
 
@@ -16,6 +20,14 @@ const MSG = defineMessages({
     id: 'users.TokenActivation.TokenActivationContent.TokensTab.changeState',
     defaultMessage: 'Change token state',
   },
+  activate: {
+    id: 'users.TokenActivation.TokenActivationContent.TokensTab.activate',
+    defaultMessage: 'Activate',
+  },
+  withdraw: {
+    id: 'users.TokenActivation.TokenActivationContent.TokensTab.withdraw',
+    defaultMessage: 'Withdraw',
+  },
 });
 
 export interface TokensTabProps {
@@ -29,25 +41,68 @@ const TokensTab = ({
   inactiveTokens,
   tokenSymbol,
 }: TokensTabProps) => {
+  const [isActivate, setIsActivate] = useState(true);
+
   return (
     <>
-      <div className={styles.totalTokens}>{`SYMBOL plchd${
-        activeTokens + inactiveTokens
-      } ${tokenSymbol}`}</div>
-      <div className={styles.tokensDetails}>
-        <div>
-          <FormattedMessage {...MSG.active} />
-        </div>
-        <div>{`${activeTokens} ${tokenSymbol}`}</div>
-        <div>Totoal tokens placeholder?</div>
-        <div>
-          <FormattedMessage {...MSG.inactive} />
-        </div>
-        <div>{`${inactiveTokens} ${tokenSymbol}`}</div>
+      <div className={styles.totalTokensContainer}>
+        {/* @ts-ignore */}
+        <TokenIcon token={{}} size="xs" />
+        <p className={styles.totalTokens}>
+          {activeTokens + inactiveTokens} <span>{tokenSymbol}</span>
+        </p>
+      </div>
+      <div className={styles.tokensDetailsContainer}>
+        <ul>
+          <li>
+            <div className={styles.listItem}>
+              <div className={styles.greenDisc} />
+              <p className={styles.listItemTitle}>
+                <FormattedMessage {...MSG.active} />
+              </p>
+            </div>
+            <div
+              className={styles.tokenNumbers}
+            >{`${activeTokens} ${tokenSymbol}`}</div>
+            <div className={styles.previousTotalAmount}>
+              <Icon
+                title="padlock"
+                appearance={{ size: 'extraTiny' }}
+                name="emoji-padlock-closed"
+              />
+              <p>{`18,000 ${tokenSymbol} !!!!! PLACEHOLDER`}</p>
+            </div>
+          </li>
+          <li>
+            <div className={styles.listItem}>
+              <div className={styles.redDisc} />
+              <p className={styles.listItemTitle}>
+                <FormattedMessage {...MSG.inactive} />
+              </p>
+            </div>
+            <div
+              className={styles.tokenNumbers}
+            >{`${inactiveTokens} ${tokenSymbol}`}</div>
+          </li>
+        </ul>
       </div>
       <div className={styles.changeTokensState}>
-        <div>
+        <div className={styles.changeStateTitle}>
           <FormattedMessage {...MSG.changeState} />
+        </div>
+        <div className={styles.changeStateButtonsContainer}>
+          <Button
+            appearance={{ theme: isActivate ? 'primary' : 'secondary' }}
+            className={!isActivate ? styles.leftBorders : ''}
+            onClick={() => setIsActivate(true)}
+            text={MSG.activate}
+          />
+          <Button
+            appearance={{ theme: !isActivate ? 'primary' : 'secondary' }}
+            className={isActivate ? styles.rightBorders : ''}
+            onClick={() => setIsActivate(false)}
+            text={MSG.withdraw}
+          />
         </div>
       </div>
     </>
