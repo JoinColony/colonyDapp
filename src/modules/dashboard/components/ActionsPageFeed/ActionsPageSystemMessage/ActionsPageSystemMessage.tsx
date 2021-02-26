0c2423b576cf63de80675ac5088b024ce18fa974
Raul Glogovetan <raul@glogovetan.com>
2021-02-26 22:33:13 +0200

    Add: `ActionsPageSystemMessage` component

---
+++ src/modules/dashboard/components/ActionsPageFeed/ActionsPageSystemMessage/ActionsPageSystemMessage.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import { TransactionMeta, TransactionStatus } from '~dashboard/ActionsPage';
+
+import { STATUS } from '../../ActionsPage/types';
+import { SystemMessage } from '../types';
+
+import styles from './ActionsPageSystemMessage.css';
+
+const displayName = 'dashboard.ActionsPageFeed.ActionsPageSystemMessage';
+
+interface Props {
+  systemMessage: SystemMessage;
+}
+
+const ActionsPageSystemMessage = ({
+  systemMessage: { name, createdAt },
+}: Props) => (
+  <div className={styles.main}>
+    <div className={styles.wrapper}>
+      <div className={styles.status}>
+        <TransactionStatus status={STATUS.SystemMessage} showTooltip={false} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <FormattedMessage id="systemMessage.title" values={{ name }} />
+        </div>
+        <div className={styles.details}>
+          <div className={styles.meta}>
+            <TransactionMeta createdAt={createdAt} />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+ActionsPageSystemMessage.displayName = displayName;
+
+export default ActionsPageSystemMessage;
