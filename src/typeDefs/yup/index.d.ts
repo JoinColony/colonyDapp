b0fd93f1423db35b6408070c1cb29766f4815ee2
Raul Glogovetan <raul@glogovetan.com>
2021-02-27 12:58:04 +0200

    Add: `hexString`, `hasHexPrefix` yup string validators

--- src/typeDefs/yup/index.d.ts
+++ src/typeDefs/yup/index.d.ts
@@ -4,5 +4,7 @@ declare module 'yup' {
   interface StringSchema<T> {
     address(message?: TestOptionsMessage): StringSchema<T>;
     ensAddress(message?: TestOptionsMessage): StringSchema<T>;
+    hexString(message?: TestOptionsMessage): StringSchema<T>;
+    hasHexPrefix(message?: TestOptionsMessage): StringSchema<T>;
   }
 }
