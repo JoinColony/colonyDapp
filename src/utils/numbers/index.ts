8743a772fe779757ef2936f9bae09ec936684575
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-03-02 15:37:50 +0000

    Refactor: update css in token activation files

--- src/utils/numbers/index.ts
+++ src/utils/numbers/index.ts
@@ -1,5 +1,7 @@
 import { BigNumberish, bigNumberify } from 'ethers/utils';
 
+export { formatTokenValue } from './formatTokenValue';
+
 /**
  * Return whether `a` is less than `b` where each are number-like values.
  */
@@ -7,4 +9,3 @@ export const bnLessThan = (a: BigNumberish, b: BigNumberish) =>
   bigNumberify(a).lt(bigNumberify(b));
 
 export const halfPlusOne = (count: number) => Math.floor(count / 2) + 1;
-export { formatTokenValue } from './formatTokenValue';
