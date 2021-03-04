63c9bbbd7b67b111f9c4e9d0a135bbf8423df4de
Raul Glogovetan <raul@glogovetan.com>
2021-03-05 00:25:07 +0200

    Add: `ColonyUpgrade` show optional upgrade banner

--- src/modules/dashboard/components/ColonyHome/ColonyUpgrade/ColonyUpgrade.tsx
+++ src/modules/dashboard/components/ColonyHome/ColonyUpgrade/ColonyUpgrade.tsx
@@ -1,15 +1,18 @@
 import React, { useCallback } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
+import { ColonyVersion } from '@colony/colony-js';
 
 import { useDialog } from '~core/Dialog';
 import NetworkContractUpgradeDialog from '~dashboard/NetworkContractUpgradeDialog';
 import Alert from '~core/Alert';
 import Button from '~core/Button';
+import ExternalLink from '~core/ExternalLink';
 
 import { Colony, useNetworkContracts } from '~data/index';
 import { useLoggedInUser } from '~data/helpers';
 import { useTransformer } from '~utils/hooks';
-import { canBeUpgraded } from '../../../checks';
+import { getNetworkRelaseLink } from '~utils/external';
+import { mustBeUpgraded, shouldBeUpgraded } from '../../../checks';
 import { hasRoot } from '../../../../users/checks';
 import { getAllUserRoles } from '../../../../transformers';
 
@@ -20,6 +23,14 @@ const MSG = defineMessages({
     id: `dashboard.ColonyHome.ColonyUpgrade.upgradeRequired`,
     defaultMessage: `This colony uses a version of the network that is no
       longer supported. You must upgrade to continue using this application.`,
+  },
+  upgradeSuggested: {
+    id: `dashboard.ColonyHome.ColonyUpgrade.upgradeSuggested`,
+    defaultMessage: `A new version of the Colony Network is available! {linkToRelease}`,
+  },
+  learnMore: {
+    id: `dashboard.ColonyHome.ColonyUpgrade.learnMore`,
+    defaultMessage: `Learn More`,
   },
 });
 
@@ -47,29 +58,83 @@ const ColonyUpgrade = ({ colony }: Props) => {
   const hasRegisteredProfile = !!username && !ethereal;
   const canUpgradeColony = hasRegisteredProfile && hasRoot(allUserRoles);
 
-  const canUpgrade = canBeUpgraded(colony, networkVersion as string);
+  const mustUpgrade = mustBeUpgraded(colony, networkVersion as string);
+  const shouldUpdgrade = shouldBeUpgraded(colony, networkVersion as string);
 
-  return canUpgrade ? (
-    <div className={styles.upgradeBannerContainer}>
-      <Alert
-        appearance={{
-          theme: 'danger',
-          margin: 'none',
-          borderRadius: 'none',
-        }}
-      >
-        <div className={styles.upgradeBanner}>
-          <FormattedMessage {...MSG.upgradeRequired} />
-        </div>
-        <Button
-          appearance={{ theme: 'primary', size: 'medium' }}
-          text={{ id: 'button.upgrade' }}
-          onClick={handleUpgradeColony}
-          disabled={!canUpgradeColony}
-        />
-      </Alert>
-    </div>
-  ) : null;
+  if (mustUpgrade) {
+    return (
+      <div className={styles.upgradeBannerContainer}>
+        <Alert
+          appearance={{
+            theme: 'danger',
+            margin: 'none',
+            borderRadius: 'none',
+          }}
+        >
+          <div className={styles.upgradeBanner}>
+            <FormattedMessage {...MSG.upgradeRequired} />
+          </div>
+          <div className={styles.controls}>
+            <Button
+              appearance={{ theme: 'primary', size: 'medium' }}
+              text={{ id: 'button.upgrade' }}
+              onClick={handleUpgradeColony}
+              disabled={!canUpgradeColony}
+            />
+          </div>
+        </Alert>
+      </div>
+    );
+  }
+
+  if (shouldUpdgrade) {
+    return (
+      <div className={styles.upgradeBannerContainer}>
+        <Alert
+          appearance={{
+            theme: 'info',
+            margin: 'none',
+            borderRadius: 'none',
+          }}
+        >
+          {(handleDismissed) => (
+            <>
+              <div className={styles.upgradeBanner}>
+                <FormattedMessage
+                  {...MSG.upgradeSuggested}
+                  values={{
+                    linkToRelease: (
+                      <ExternalLink
+                        text={MSG.learnMore}
+                        href={getNetworkRelaseLink(
+                          (colony.version as unknown) as ColonyVersion,
+                        )}
+                      />
+                    ),
+                  }}
+                />
+              </div>
+              <div className={styles.controls}>
+                <Button
+                  appearance={{ theme: 'danger', size: 'medium' }}
+                  text={{ id: 'button.dismiss' }}
+                  onClick={handleDismissed}
+                />
+                <Button
+                  appearance={{ theme: 'primary', size: 'medium' }}
+                  text={{ id: 'button.upgrade' }}
+                  onClick={handleUpgradeColony}
+                  disabled={!canUpgradeColony}
+                />
+              </div>
+            </>
+          )}
+        </Alert>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 ColonyUpgrade.displayName = displayName;
