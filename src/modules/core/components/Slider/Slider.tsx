12e11169073842315c281c215d222db8aff422f6
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-03-05 16:30:27 +0100

    Create limit logic

--- src/modules/core/components/Slider/Slider.tsx
+++ src/modules/core/components/Slider/Slider.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import ReactSlider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
-import styles from './Slider.css';
-
 type Appearance = {
   theme?: 'primary' | 'danger';
 };
@@ -19,44 +17,54 @@ interface Props {
 const displayName = 'Slider';
 
 const Slider = ({ value, max = 100, onChange, limit = null }: Props) => {
+  const [sliderValue, setSliderValue] = useState<number>(value);
+
   const marks = {};
 
   if (limit) {
-    // Mark will be displayed by calculating currentValue + limit
-    const markValue = value + limit;
-    if (markValue >= max) return;
+    if (limit >= max) return;
   
-    marks[markValue] = {};
+    marks[limit] = {};
+  }
+
+  const onSliderChange = (val) => {
+    if ((limit && sliderValue < limit) || val < sliderValue || !limit) {
+      setSliderValue(val);
+      onChange(val);
+    }
+    if (limit && sliderValue > limit) {
+      setSliderValue(limit);
+      onChange(limit);
+    }
   }
 
   return (
-    <div className={styles.main}>
-      <ReactSlider
-        min={0}
-        defaultValue={value}
-        onChange={onChange}
-        marks={marks}
-        max={max}
-        trackStyle={{ backgroundColor: '#68D2D2', height: 1 }}
-        handleStyle={{
-          borderColor: '#68D2D2',
-          borderWidth: 6,
-          height: 15,
-          width: 15,
-          marginTop: -7,
-          backgroundColor: 'transparent',
-        }}
-        dotStyle={{
-          height: 5,
-          width: 1,
-          backgroundColor: '#76748B',
-          border: 0,
-          borderRadius: 0,
-          top: -2,
-        }}
-        railStyle={{ backgroundColor: '#C2CCCC', height: 1 }}
-      />
-    </div>
+    <ReactSlider
+      min={0}
+      step={1}
+      value={sliderValue}
+      onChange={onSliderChange}
+      marks={marks}
+      max={max}
+      trackStyle={{ backgroundColor: '#68D2D2', height: 1 }}
+      handleStyle={{
+        borderColor: '#68D2D2',
+        borderWidth: 6,
+        height: 15,
+        width: 15,
+        marginTop: -7,
+        backgroundColor: 'transparent',
+      }}
+      dotStyle={{
+        height: 5,
+        width: 1,
+        backgroundColor: '#76748B',
+        border: 0,
+        borderRadius: 0,
+        top: -2,
+      }}
+      railStyle={{ backgroundColor: '#C2CCCC', height: 1 }}
+    />
   );
 };
 
