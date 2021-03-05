64f474f0fa4365671e831b6a48b6e86e0b0cf9f0
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-03-05 15:58:27 +0100

    Apply basic styles

--- src/modules/core/components/Slider/Slider.tsx
+++ src/modules/core/components/Slider/Slider.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactSlider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 import styles from './Slider.css';
 
@@ -7,18 +9,53 @@ type Appearance = {
 };
 
 interface Props {
-  value?: number;
+  value: number;
   max?: number;
   limit?: number;
   appearance?: Appearance;
+  onChange: (val: any) => void;
 }
 
 const displayName = 'Slider';
 
-const Slider = ({ value = 0, max = 100 }: Props) => {
+const Slider = ({ value, max = 100, onChange, limit = null }: Props) => {
+  const marks = {};
+
+  if (limit) {
+    // Mark will be displayed by calculating currentValue + limit
+    const markValue = value + limit;
+    if (markValue >= max) return;
+  
+    marks[markValue] = {};
+  }
+
   return (
     <div className={styles.main}>
-      {value} {max}
+      <ReactSlider
+        min={0}
+        defaultValue={value}
+        onChange={onChange}
+        marks={marks}
+        max={max}
+        trackStyle={{ backgroundColor: '#68D2D2', height: 1 }}
+        handleStyle={{
+          borderColor: '#68D2D2',
+          borderWidth: 6,
+          height: 15,
+          width: 15,
+          marginTop: -7,
+          backgroundColor: 'transparent',
+        }}
+        dotStyle={{
+          height: 5,
+          width: 1,
+          backgroundColor: '#76748B',
+          border: 0,
+          borderRadius: 0,
+          top: -2,
+        }}
+        railStyle={{ backgroundColor: '#C2CCCC', height: 1 }}
+      />
     </div>
   );
 };
