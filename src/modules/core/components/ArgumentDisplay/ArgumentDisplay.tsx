c544d25b10198b50375dd618ab4239dde902c4c1
ArmandoGraterol <armandogrtrl@gmail.com>
2021-03-11 16:52:38 -0300

    Added WIP baseline for motion and objection displays

---
+++ src/modules/core/components/ArgumentDisplay/ArgumentDisplay.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import HookedUserAvatar from '~users/HookedUserAvatar';
+import TimeRelative from '~core/TimeRelative';
+import { useUser } from '~data/index';
+import { getMainClasses } from '~utils/css';
+
+import styles from './ArgumentDisplay.css';
+
+interface Appearance {
+  theme?: 'motion' | 'objection';
+}
+
+interface Argument {
+  author: string;
+  comment: string;
+  timestamp: number;
+}
+
+interface Props {
+  appearance?: Appearance;
+  argument: Argument;
+}
+
+const displayName = 'ArgumentDisplay';
+
+const ArgumentDisplay = ({
+  appearance = { theme: 'motion' },
+  argument: { author, timestamp, comment },
+}: Props) => {
+  const user = useUser(author);
+  // const tagTheme = appearance.theme === 'motion' ? 'primary' : 'pink';
+  // const tagText = appearance.theme === 'motion' ? 'Motion' : 'Objection';
+  const UserAvatar = HookedUserAvatar({ fetchUser: false });
+
+  return (
+    <div
+      className={`${styles.container} ${getMainClasses(appearance, styles)}`}
+    >
+      <div className={styles.avatar}>
+        <UserAvatar address={author} user={user} size="xs" notSet={false} />
+      </div>
+      <div className={styles.main}>
+        <div>
+          {/* <Tag text={tagText} appearance={{ theme: tagTheme colorSchema: 'plain', fontSize: 'tiny' }} /> */}
+          <TimeRelative
+            className={styles.timestamp}
+            value={new Date(timestamp)}
+          />
+        </div>
+        <p>{comment}</p>
+      </div>
+    </div>
+  );
+};
+
+ArgumentDisplay.displayName = displayName;
+
+export default ArgumentDisplay;
