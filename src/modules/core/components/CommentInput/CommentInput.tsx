4f5569b0b83558050280f33db644c1300a563e47
ArmandoGraterol <armandogrtrl@gmail.com>
2021-03-15 16:41:26 -0300

    Added ActionsPageComment as a core component

--- src/modules/core/components/CommentInput/CommentInput.tsx
+++ src/modules/core/components/CommentInput/CommentInput.tsx
@@ -19,6 +19,7 @@ import {
 } from '~data/index';
 import { Address, ENTER } from '~types/index';
 
+<<<<<<< HEAD:src/modules/core/components/CommentInput/CommentInput.tsx
 import styles from './CommentInput.css';
 
 const displayName = 'CommentInput';
@@ -34,13 +35,34 @@ const MSG = defineMessages({
   },
   sendCombo: {
     id: 'CommentInput.sendCombo',
+=======
+import styles from './Comment.css';
+
+const displayName = 'Comment';
+
+const MSG = defineMessages({
+  commentInputPlaceholder: {
+    id: 'Comment.commentInputPlaceholder',
+    defaultMessage: 'What would you like to say?',
+  },
+  commentInstuctions: {
+    id: 'Comment.commentInstuctions',
+    defaultMessage: `{sendCombo} to send {newLineCombo} for a new line`,
+  },
+  sendCombo: {
+    id: 'Comment.sendCombo',
+>>>>>>> Added ActionsPageComment as a core component:src/modules/core/components/Comment/Comment.tsx
     defaultMessage: `{isMac, select,
       true {⌘}
       other {Ctrl}
     }+Return`,
   },
   newLineCombo: {
+<<<<<<< HEAD:src/modules/core/components/CommentInput/CommentInput.tsx
     id: 'CommentInput.newLineCombo',
+=======
+    id: 'Comment.newLineCombo',
+>>>>>>> Added ActionsPageComment as a core component:src/modules/core/components/Comment/Comment.tsx
     defaultMessage: 'Return',
   },
 });
@@ -82,7 +104,11 @@ const handleKeyboardSubmit = (
   return false;
 };
 
+<<<<<<< HEAD:src/modules/core/components/CommentInput/CommentInput.tsx
 const CommentInput = ({ transactionHash, colonyAddress, callback }: Props) => {
+=======
+const Comment = ({ transactionHash, colonyAddress }: Props) => {
+>>>>>>> Added ActionsPageComment as a core component:src/modules/core/components/Comment/Comment.tsx
   const commentBoxRef = useRef<HTMLInputElement>(null);
   const [
     commentBoxInputRef,
@@ -194,6 +220,12 @@ const CommentInput = ({ transactionHash, colonyAddress, callback }: Props) => {
   );
 };
 
+<<<<<<< HEAD:src/modules/core/components/CommentInput/CommentInput.tsx
 CommentInput.displayName = displayName;
 
 export default CommentInput;
+=======
+Comment.displayName = displayName;
+
+export default Comment;
+>>>>>>> Added ActionsPageComment as a core component:src/modules/core/components/Comment/Comment.tsx
