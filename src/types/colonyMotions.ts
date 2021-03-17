8ea721aa3d214764aaceb55329852451a5e52633
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-03-17 16:50:08 +0100

    Create ColonyMotion enum

---
+++ src/types/colonyMotions.ts
@@ -0,0 +1,8 @@
+
+export enum ColonyMotions {
+  /*
+    I dont like Motion postfitx, but if we would use actionType for 
+    both motions and actions we need to have something distinct
+  */
+  MintTokensMotion = 'MintTokensMotion',
+}
