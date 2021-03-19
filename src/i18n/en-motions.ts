31888dae594a9c022af4c76e6b8757f8750633d8
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-03-19 09:45:36 +0100

    CHange action to motion in translations

--- src/i18n/en-motions.ts
+++ src/i18n/en-motions.ts
@@ -3,7 +3,7 @@ import { ColonyMotions } from '~types/index';
 const motionsMessageDescriptors = {
   'motion.title': `{actionType, select,
       ${ColonyMotions.MintTokensMotion} {Mint {amount} {tokenSymbol}}
-      other {Generic action we don't have information about}
+      other {Generic motion we don't have information about}
     }`,
   'motion.type': `{actionType, select,
       ${ColonyMotions.MintTokensMotion} {Mint Tokens}
