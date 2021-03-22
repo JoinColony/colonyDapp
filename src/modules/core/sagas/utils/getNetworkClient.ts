f32ef5c51048e58022394889b9693b158f64848b
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-03-22 18:51:13 +0000

    Refactor: put `reputation` back to `reputationOracleEndpoint`

--- src/modules/core/sagas/utils/getNetworkClient.ts
+++ src/modules/core/sagas/utils/getNetworkClient.ts
@@ -51,7 +51,7 @@ export default function* getNetworkClient() {
   ) {
     return yield call(getColonyNetworkClient, network, signer, {
       networkAddress: getLocalContractAddress('EtherRouter'),
-      reputationOracleEndpoint: 'http://localhost:3001',
+      reputationOracleEndpoint: 'http://localhost:3001/reputation',
     });
   }
 
