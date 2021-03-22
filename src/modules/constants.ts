36d86fc491fdab80a5e42ddad2dad586ccfa9b81
Raul Glogovetan <raul@glogovetan.com>
2021-03-22 22:43:00 +0200

    Fix: `TOKEN_LOGOS_REPO_URL` constant to allow network

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -138,8 +138,6 @@ export const ALLDOMAINS_DOMAIN_SELECTION = {
   description: '',
 };
 
-export const TOKEN_LOGOS_REPO_URL =
-  'https://raw.githubusercontent.com/trustwallet' +
-  '/assets/master/blockchains/ethereum/';
+export const TOKEN_LOGOS_REPO_URL = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains`;
 
 export const NETWORK_RELEASES_URL = `https://github.com/JoinColony/colonyNetwork/releases/tag`;
