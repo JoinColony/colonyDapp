6db6c1fc4e91bbb25f6b99c548f1b526f953b5b1
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-03-26 15:11:21 +0000

    Feat: add `MiniSpinnerLoader` core component

---
+++ src/modules/core/components/Preloaders/MiniSpinnerLoader.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FormattedMessage, MessageDescriptor } from 'react-intl';
+
+import Heading, { Appearance as HeadingAppearance } from '~core/Heading';
+import { SimpleMessageValues } from '~types/index';
+
+import SpinnerLoader, { Appearance } from './SpinnerLoader';
+
+interface Props {
+  appearance?: Appearance;
+  titleAppearance?: HeadingAppearance;
+  title?: string | MessageDescriptor;
+  /** Values for text (react-intl interpolation) */
+  titleTextValues?: SimpleMessageValues;
+  loadingText?: string | MessageDescriptor;
+  className?: string;
+  loadingTextClassName?: string;
+}
+
+const MiniSpinnerLoader = ({
+  appearance,
+  titleAppearance = { size: 'normal', weight: 'bold' },
+  title,
+  titleTextValues,
+  loadingText,
+  className,
+  loadingTextClassName,
+}: Props) => {
+  return (
+    <div className={className}>
+      {title && (
+        <Heading
+          text={title}
+          textValues={titleTextValues}
+          appearance={titleAppearance}
+        />
+      )}
+      <SpinnerLoader appearance={appearance} />
+      {loadingText && (
+        <span className={loadingTextClassName}>
+          <FormattedMessage {...loadingText} />
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default MiniSpinnerLoader;
