7dd4c1f2625ef2db39ae9c9f886f5eeeaff213cc
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-04-02 15:54:17 +0100

    Refactor: generalise & add edge case to `calculateTimeLeft`

--- src/modules/core/components/TimeLeftToStake/calculateTimeLeft.ts
+++ src/modules/core/components/TimeLeftToStake/calculateTimeLeft.ts
@@ -1,11 +1,18 @@
-export const calculateTimeLeft = (createdAt: number, stakePeriod: number) => {
+export const calculateTimeLeft = (createdAt: number, actionPeriod?: number) => {
+  if (!actionPeriod) {
+    return undefined;
+  }
   // 1617362399585
-  const difference = createdAt + stakePeriod * 1000 - Date.now();
+  const difference = createdAt + actionPeriod * 1000 - Date.now();
 
-  return {
-    days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-    minutes: Math.floor((difference / 1000 / 60) % 60),
-    seconds: Math.floor((difference / 1000) % 60),
-  };
+  if (difference > 0) {
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  return undefined;
 };
