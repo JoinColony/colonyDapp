715368ff9e5aa7b4228f15aa2273c7d8b73ad94d
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-04-02 15:57:05 +0100

    Feat: add conditional strings to `TimeLeftToStake`, remove `TimeLeft`

--- src/modules/core/components/TimeLeftToStake/TimeLeftToStake.tsx
+++ src/modules/core/components/TimeLeftToStake/TimeLeftToStake.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormattedMessage, defineMessage } from 'react-intl';
 
 // import TimeAgo from 'react-timeago';
@@ -6,32 +6,100 @@ import { FormattedMessage, defineMessage } from 'react-intl';
 import { useVotingExtensionParamsQuery } from '~data/index';
 import { Address } from '~types/index';
 
-import TimeLeft from './TimeLeft';
 import styles from './TimeLeftToStake.css';
+import { calculateTimeLeft } from './calculateTimeLeft';
 
 const MSG = defineMessage({
-  difference: {
-    id: 'TimeLeftToStake.TimeLeftToStake.difference',
-    defaultMessage: 'Time left to stake ',
+  stake: {
+    id: 'TimeLeftToStake.TimeLeftToStake.stake',
+    defaultMessage: 'Time left to stake',
+  },
+  motionPass: {
+    id: 'TimeLeftToStake.TimeLeftToStake.motionPass',
+    defaultMessage: 'Motion will pass in',
+  },
+  motionFail: {
+    id: 'TimeLeftToStake.TimeLeftToStake.motionFail',
+    defaultMessage: 'Motion will fail in',
+  },
+  reveal: {
+    id: 'TimeLeftToStake.TimeLeftToStake.reveal',
+    defaultMessage: 'Reveal ends in',
+  },
+  voting: {
+    id: 'TimeLeftToStake.TimeLeftToStake.voting',
+    defaultMessage: 'Voting ends in',
+  },
+  days: {
+    id: 'TimeLeftToStake.days',
+    defaultMessage: ' {days}d',
+  },
+  hours: {
+    id: 'TimeLeftToStake.hours',
+    defaultMessage: ' {hours}h',
+  },
+  minutes: {
+    id: 'TimeLeftToStake.minutes',
+    defaultMessage: ' {minutes}m',
+  },
+  seconds: {
+    id: 'TimeLeftToStake.seconds',
+    defaultMessage: ' {seconds}s',
   },
 });
 
 interface Props {
+  copyOption: 'stake' | 'motionPass' | 'motionFail' | 'reveal' | 'voting';
   createdAt: number;
   colonyAddress: Address;
 }
 
-const TimeLeftToStake = ({ colonyAddress, createdAt }: Props) => {
-  const { data } = useVotingExtensionParamsQuery({
+const TimeLeftToStake = ({ copyOption, colonyAddress, createdAt }: Props) => {
+  const { data, error } = useVotingExtensionParamsQuery({
     variables: { colonyAddress },
   });
 
-  const stakePeriod = data?.votingExtensionParams.stakePeriod || 0;
+  const stakePeriod = data?.votingExtensionParams.stakePeriod;
+
+  const [timeLeft, setTimeLeft] = useState(
+    calculateTimeLeft(createdAt, stakePeriod),
+  );
+
+  useEffect(() => {
+    if (stakePeriod !== undefined) {
+      const timer = setInterval(() => {
+        setTimeLeft(calculateTimeLeft(createdAt, stakePeriod));
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+    return undefined;
+  }, [createdAt, stakePeriod]);
+
+  if (data === undefined || error || timeLeft === undefined) {
+    return null;
+  }
 
   return (
     <div className={styles.container}>
-      <FormattedMessage {...MSG.difference} />
-      <TimeLeft createdAt={createdAt} stakePeriod={stakePeriod} />
+      <FormattedMessage {...MSG[copyOption]} />
+      <span className={styles.time}>
+        {timeLeft.days > 0 && (
+          <FormattedMessage {...MSG.days} values={{ days: timeLeft.days }} />
+        )}
+        {timeLeft.days > 0 && timeLeft.hours > 0 && (
+          <FormattedMessage {...MSG.hours} values={{ hours: timeLeft.hours }} />
+        )}
+        {timeLeft.days > 0 && timeLeft.hours > 0 && timeLeft.minutes > 0 && (
+          <FormattedMessage
+            {...MSG.minutes}
+            values={{ minutes: timeLeft.minutes }}
+          />
+        )}
+        <FormattedMessage
+          {...MSG.seconds}
+          values={{ seconds: timeLeft.seconds }}
+        />
+      </span>
     </div>
   );
 };
