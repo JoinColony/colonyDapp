c313339cd6a40d08cb66c9a78fe5ade31e8c20c3
Raul Glogovetan <raul@glogovetan.com>
2021-04-18 02:18:39 +0300

    Refactor: coin machine `Chat` to actually use comments

--- src/modules/dashboard/components/CoinMachine/Chat/Chat.css.d.ts
+++ src/modules/dashboard/components/CoinMachine/Chat/Chat.css.d.ts
@@ -1,3 +1,6 @@
 export const main: string;
 export const messages: string;
 export const inputBox: string;
+export const loading: string;
+export const empty: string;
+export const loaderMessage: string;
--- src/modules/dashboard/components/CoinMachine/Chat/Chat.tsx
+++ src/modules/dashboard/components/CoinMachine/Chat/Chat.tsx
@@ -1,69 +1,113 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
 import Comment from '~core/Comment';
 import CommentInput from '~core/CommentInput';
+import { MiniSpinnerLoader } from '~core/Preloaders';
 
 import {
   useLoggedInUser,
-  TransactionMessageFragment,
   Colony,
+  useTransactionMessagesQuery,
 } from '~data/index';
 
 import styles from './Chat.css';
 
 const MSG = defineMessages({
   emptyText: {
-    id: 'Chat.emptyText',
+    id: 'dashboard.CoinMachine.Chat.emptyText',
     defaultMessage: 'Nobody’s said anything yet... 😢',
   },
   labelLeaveComment: {
-    id: 'Chat.labelLeaveComment',
+    id: 'dashboard.CoinMachine.Chat.labelLeaveComment',
     defaultMessage: 'Leave a comment',
   },
   loginToComment: {
-    id: 'Chat.loginToComment',
+    id: 'dashboard.CoinMachine.Chat.loginToComment',
     defaultMessage: 'Login to comment',
+  },
+  loading: {
+    id: 'dashboard.CoinMachine.Chat.loading',
+    defaultMessage: 'Loading messages',
   },
 });
 
 interface Props {
-  comments: Array<TransactionMessageFragment>;
   colony: Colony;
   transactionHash: string;
 }
 
 const displayName = 'dashboard.CoinMachine.Chat';
 
-const Chat = ({
-  comments,
-  colony: { colonyAddress },
-  transactionHash,
-}: Props) => {
+const Chat = ({ colony: { colonyAddress }, transactionHash }: Props) => {
   const { username, ethereal } = useLoggedInUser();
   const scrollElmRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
+  /*
+   * @NOTE This is needed in order to make the scroll effect trigger after
+   * each new comment was made, otherwise it will use React's internal cache
+   * and will always be one comment behind
+   */
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const scrollComments = () => {
     if (scrollElmRef.current) {
       scrollElmRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-  }, [comments]);
+  };
+
+  /*
+   * This is used for the first render, so we have to (fake) wait for the
+   * compoenent to mount, and for this we make the user of setTimeout
+   */
+  useLayoutEffect(() => {
+    setTimeout(scrollComments, 0);
+  }, [scrollComments]);
+
+  const { data, loading } = useTransactionMessagesQuery({
+    variables: { transactionHash },
+  });
+
+  /*
+   * This triggers after every new comment was made
+   */
+  useLayoutEffect(scrollComments, [scrollComments]);
+
+  if (loading) {
+    return (
+      <div className={styles.main}>
+        <div className={styles.messages}>
+          <MiniSpinnerLoader
+            className={styles.loading}
+            loadingTextClassName={styles.loaderMessage}
+            loadingText={MSG.loading}
+          />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.main}>
       <div className={styles.messages}>
-        {comments && comments.length ? (
-          comments.map(({ createdAt, initiator, context }) => (
-            <Comment
-              key={`${initiator}.${createdAt}`}
-              createdAt={createdAt}
-              comment={context.message}
-              user={initiator}
-            />
-          ))
-        ) : (
-          <FormattedMessage {...MSG.emptyText} />
-        )}
+        <div>
+          {data?.transactionMessages?.messages &&
+          data?.transactionMessages?.messages?.length ? (
+            data?.transactionMessages?.messages.map(
+              ({ createdAt, initiator, context }) => (
+                <Comment
+                  key={`${initiator}.${createdAt}`}
+                  createdAt={createdAt}
+                  comment={context.message}
+                  user={initiator}
+                />
+              ),
+            )
+          ) : (
+            <div className={styles.empty}>
+              <FormattedMessage {...MSG.emptyText} />
+            </div>
+          )}
+        </div>
         <div ref={scrollElmRef} />
       </div>
       <div className={styles.inputBox}>
@@ -71,6 +115,7 @@ const Chat = ({
           <CommentInput
             colonyAddress={colonyAddress}
             transactionHash={transactionHash}
+            callback={scrollComments}
           />
         )}
       </div>
