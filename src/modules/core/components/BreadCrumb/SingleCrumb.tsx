0f30105fccd250d0a956fbb6fd5b510322e8d0a6
Raul Glogovetan <raul@glogovetan.com>
2021-04-18 00:00:06 +0300

    Refactor: core `BreadCrumb` to support react components

--- src/modules/core/components/BreadCrumb/SingleCrumb.tsx
+++ src/modules/core/components/BreadCrumb/SingleCrumb.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 import NavLink from '~core/NavLink';
 
+import { CrumbText } from './BreadCrumb';
+
 import styles from './BreadCrumb.css';
 
 interface Props {
-  crumbText: string;
+  crumbText: CrumbText;
   crumbLink?: string;
   lastCrumb?: boolean;
 }
 
 const SingleCrumb = ({ crumbText, crumbLink, lastCrumb }: Props) => {
+  const crumbTitle = typeof crumbText === 'string' ? crumbText : '';
   if (lastCrumb) {
     return (
-      <div className={styles.elementLast} title={crumbText}>
+      <div className={styles.elementLast} title={crumbTitle}>
         <span className={styles.breadCrumble}>
           {crumbLink ? (
             <NavLink className={styles.invisibleLink} to={crumbLink}>
@@ -27,7 +30,7 @@ const SingleCrumb = ({ crumbText, crumbLink, lastCrumb }: Props) => {
     );
   }
   return (
-    <div className={styles.element} title={crumbText}>
+    <div className={styles.element} title={crumbTitle}>
       <span className={styles.breadCrumble}>
         {crumbLink ? (
           <NavLink className={styles.invisibleLink} to={crumbLink}>
