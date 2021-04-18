8a43f226b2d87a71080540dd8e84fca14b8cc040
Raul Glogovetan <raul@glogovetan.com>
2021-04-18 20:01:29 +0300

    Add: `BuyToken` sale price section

--- src/modules/dashboard/components/CoinMachine/BuyTokens/BuyTokens.css.d.ts
+++ src/modules/dashboard/components/CoinMachine/BuyTokens/BuyTokens.css.d.ts
@@ -6,3 +6,8 @@ export const inputContainer: string;
 export const inputComponent: string;
 export const nativeToken: string;
 export const userBalance: string;
+export const amountsContainer: string;
+export const amounts: string;
+export const amountsLabel: string;
+export const amountsValues: string;
+export const symbols: string;
