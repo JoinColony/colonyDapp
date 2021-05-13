d01f0074b0c1e351cca870baddc0c744d815a9d7
ArmandoGraterol <armandogrtrl@gmail.com>
2021-05-13 10:15:55 -0300

    Added SetUserRolesMotion type

--- src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
+++ src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
@@ -268,6 +268,7 @@ const ActionsPage = () => {
           initiator={initiatorProfileWithFallback}
         />
       );
+    case ColonyMotions.SetUserRolesMotion:
     case ColonyMotions.CreateDomainMotion:
     case ColonyMotions.MintTokensMotion:
       return (
