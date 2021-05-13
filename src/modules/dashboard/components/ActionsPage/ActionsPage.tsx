47d63ca37f5a7f4f1d2a548f6726df9ca8c6451b
ArmandoGraterol <armandogrtrl@gmail.com>
2021-05-13 11:24:31 -0300

    Incorporate PaymentMotion type

--- src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
+++ src/modules/dashboard/components/ActionsPage/ActionsPage.tsx
@@ -268,6 +268,7 @@ const ActionsPage = () => {
           initiator={initiatorProfileWithFallback}
         />
       );
+    case ColonyMotions.PaymentMotion:
     case ColonyMotions.ColonyEditMotion:
     case ColonyMotions.MoveFundsMotion:
     case ColonyMotions.SetUserRolesMotion:
