9490fc3a12ac99da2c15feb655f993570714c1f2
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-05-21 12:26:01 +0300

    Refactor: update `StakingValidationError` component

--- src/modules/dashboard/components/ActionsPage/StakingValidationError/StakingValidationError.tsx
+++ src/modules/dashboard/components/ActionsPage/StakingValidationError/StakingValidationError.tsx
@@ -1,50 +1,30 @@
 import React from 'react';
 import { defineMessage, FormattedMessage } from 'react-intl';
-import { Decimal } from 'decimal.js';
-import { BigNumber } from 'ethers/utils';
 
 import styles from './StakingValidationError.css';
 
 interface Props {
-  userInactivatedTokens: BigNumber;
-  userActivatedTokens: Decimal;
-  decimalAmount: Decimal;
+  stakeType: 'tokens' | 'reputation';
 }
 
 const stakeValidationMSG = defineMessage({
-  hasInactiveTokens: {
-    id: 'dashboard.ActionsPage.StakingValidationError.hasInactiveTokens',
-    defaultMessage: `You have inactive tokens which cannot be staked, please activate them.`,
-  },
-  notEnoughTokens: {
+  tokens: {
     id: 'dashboard.ActionsPage.StakingValidationError.notEnoughTokens',
-    defaultMessage: 'You do not have enough activated tokens to stake.',
+    defaultMessage: 'You do not have enough active tokens to stake.',
+  },
+  reputation: {
+    id: 'dashboard.ActionsPage.StakingValidationError.notEnoughTokens',
+    defaultMessage: 'You do not have enough reputation to stake.',
   },
 });
 
 const displayName = 'StakingValidationError';
 
-const StakingValidationError = ({
-  userInactivatedTokens,
-  userActivatedTokens,
-  decimalAmount,
-}: Props) => {
-  const notEnoughActiveTokens = userActivatedTokens.lt(decimalAmount);
-  return (
-    <>
-      {notEnoughActiveTokens && !userInactivatedTokens.isZero() && (
-        <div className={styles.validationError}>
-          <FormattedMessage {...stakeValidationMSG.hasInactiveTokens} />
-        </div>
-      )}
-      {notEnoughActiveTokens && userInactivatedTokens.isZero() && (
-        <div className={styles.validationError}>
-          <FormattedMessage {...stakeValidationMSG.notEnoughTokens} />
-        </div>
-      )}
-    </>
-  );
-};
+const StakingValidationError = ({ stakeType }: Props) => (
+  <div className={styles.validationError}>
+    <FormattedMessage {...stakeValidationMSG[stakeType]} />
+  </div>
+);
 
 StakingValidationError.displayName = displayName;
 
