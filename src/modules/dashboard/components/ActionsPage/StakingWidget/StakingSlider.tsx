744d1b832f70ac1490319eea4dbb42f2925708c4
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-06-01 11:07:38 +0300

    Refactor: increase decimal limit for the slider

--- src/modules/dashboard/components/ActionsPage/StakingWidget/StakingSlider.tsx
+++ src/modules/dashboard/components/ActionsPage/StakingWidget/StakingSlider.tsx
@@ -121,7 +121,7 @@ const StakingSlider = ({
         <Slider
           name="amount"
           value={values.amount}
-          limit={parseFloat(userStakeLimitPercentage.toFixed(5))}
+          limit={parseFloat(userStakeLimitPercentage.toFixed(6))}
           step={0.01}
           min={0}
           max={100}
