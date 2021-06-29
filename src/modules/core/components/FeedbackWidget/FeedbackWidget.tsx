11e858aae0cd623370d08c2aad84b8100979cee1
Raul Glogovetan <raul@glogovetan.com>
2021-06-29 16:13:20 +0300

    Refactor: `FeedbackWidget` as external link

--- src/modules/core/components/FeedbackWidget/FeedbackWidget.tsx
+++ src/modules/core/components/FeedbackWidget/FeedbackWidget.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { FeedbackFish } from '@feedback-fish/react';
 
-import Button from '~core/Button';
-import { useLoggedInUser } from '~data/index';
+import ExternalLink from '~core/ExternalLink';
 
 import styles from './FeedbackWidget.css';
-
-/* can add real project id from personal account if need to test.
-I've tested the feedback.fish process - works very well */
-const PROJECT_ID = '';
 
 const MSG = {
   loveFeedback: {
@@ -18,36 +12,23 @@ const MSG = {
   },
 };
 
-const FeedbackWidget = () => {
-  const { username } = useLoggedInUser();
+const FEEDBACK_LINK = `https://colony.canny.io`;
 
-  const isDevelopment = process.env.NODE_ENV === 'development';
-
-  return (
-    <FeedbackFish
-      projectId={
-        /* the second check is for the types
-        if the projectId is an empty string the feedback won't be sent */
-        isDevelopment || process.env.FEEDBACK_FISH_PROJECT_ID === undefined
-          ? PROJECT_ID
-          : process.env.FEEDBACK_FISH_PROJECT_ID
-      }
-      userId={username === null ? undefined : username}
-    >
-      <Button appearance={{ theme: 'no-style' }} className={styles.button}>
-        <FormattedMessage
-          {...MSG.loveFeedback}
-          values={{
-            heart: (
-              <span role="img" className={styles.heart} aria-label="">
-                ♥️
-              </span>
-            ),
-          }}
-        />
-      </Button>
-    </FeedbackFish>
-  );
-};
+const FeedbackWidget = () => (
+  <div className={styles.main}>
+    <ExternalLink className={styles.link} href={FEEDBACK_LINK}>
+      <FormattedMessage
+        {...MSG.loveFeedback}
+        values={{
+          heart: (
+            <span role="img" className={styles.heart} aria-label="">
+              ♥️
+            </span>
+          ),
+        }}
+      />
+    </ExternalLink>
+  </div>
+);
 
 export default FeedbackWidget;
