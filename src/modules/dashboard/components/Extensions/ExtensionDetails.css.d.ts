506a9639fd133fbe342700a00d3e0ce73cc3bba8
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-07-03 12:28:06 +0200

    Display info text in extension details

--- src/modules/dashboard/components/Extensions/ExtensionDetails.css.d.ts
+++ src/modules/dashboard/components/Extensions/ExtensionDetails.css.d.ts
@@ -1,6 +1,7 @@
 export const main: string;
 export const headerLine: string;
 export const extensionText: string;
+export const extensionSubtext: string;
 export const extensionDetails: string;
 export const buttonWrapper: string;
 export const cellLabel: string;
