133997b32f9e90229bdc93376fb2911dc93f4f46
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-07-14 14:22:36 +0300

    Feat: add proper logic to `whitelistAgreement` resolver

--- src/data/resolvers/whitelist.ts
+++ src/data/resolvers/whitelist.ts
@@ -1,28 +1,38 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { ClientType, ColonyClientV6 } from '@colony/colony-js';
+import { ClientType } from '@colony/colony-js';
 import { Resolvers } from '@apollo/client';
 
 import { Context } from '~context/index';
 
+import { log } from '~utils/debug';
+
 export const whitelistResolvers = ({
   colonyManager,
-  apolloClient,
+  ipfsWithFallback,
 }: Required<Context>): Resolvers => ({
   Query: {
     async whitelistAgreement(_, { colonyAddress }) {
       try {
-        const colonyClient = (await colonyManager.getClient(
-          ClientType.ColonyClient,
+        const whitelistClient = await colonyManager.getClient(
+          ClientType.WhitelistClient,
           colonyAddress,
-        )) as ColonyClientV6;
-        // const whitelistClient = await colonyManager.getClient(
-        //   ClientType.WhitelistClient,
-        //   colonyAddress,
-        // );
+        );
 
-        return {
-          whitelistAgreement: '',
-        };
+        const agreementHash = await whitelistClient.getAgreementHash();
+
+        if (agreementHash) {
+          try {
+            const agreement = await ipfsWithFallback.getString(agreementHash);
+
+            return JSON.parse(agreement).agreement;
+          } catch (error) {
+            log.verbose(
+              `Could not fetch whitelist agreement from IPFS with hash: `,
+              agreementHash,
+            );
+          }
+        }
+
+        return null;
       } catch (error) {
         console.error(error);
         return null;
