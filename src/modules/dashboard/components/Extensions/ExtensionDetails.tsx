d7ea44073bcd515181428b2d2b950dca42f59e63
Raul Glogovetan <raul@glogovetan.com>
2021-07-19 15:59:58 +0300

    Fix: `ExtensionDetails` determine if can be installed

--- src/modules/dashboard/components/Extensions/ExtensionDetails.tsx
+++ src/modules/dashboard/components/Extensions/ExtensionDetails.tsx
@@ -313,10 +313,6 @@ const ExtensionDetails = ({
       heading: ExtensionsMSG.headingVotingUninstall,
       children: <Warning text={ExtensionsMSG.textVotingUninstall} />,
     },
-    [Extension.OneTxPayment]: {
-      heading: ExtensionsMSG.headingDefaultUninstall,
-      children: <FormattedMessage {...ExtensionsMSG.textDefaultUninstall} />,
-    },
     DEFAULT: {
       heading: ExtensionsMSG.headingDefaultUninstall,
       children: <FormattedMessage {...ExtensionsMSG.textDefaultUninstall} />,
