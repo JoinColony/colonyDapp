ec1f8438c5712b1e94ceffa6c19fcf70ab33f60a
Raul Glogovetan <raul@glogovetan.com>
2021-07-20 13:03:41 +0300

    Update: `CoinMachine` extension init params

--- src/modules/dashboard/components/Extensions/ExtensionDetails.tsx
+++ src/modules/dashboard/components/Extensions/ExtensionDetails.tsx
@@ -142,7 +142,7 @@ interface Props {
 }
 
 const ExtensionDetails = ({
-  colony: { colonyAddress, version: colonyVersion },
+  colony: { colonyAddress, version: colonyVersion, nativeTokenAddress },
   colony,
 }: Props) => {
   const { colonyName, extensionId } = useParams<{
@@ -402,6 +402,7 @@ const ExtensionDetails = ({
                     extension={extension}
                     installedExtension={installedExtension}
                     colonyAddress={colonyAddress}
+                    nativeTokenAddress={nativeTokenAddress}
                   />
                 ) : (
                   <Redirect to={extensionUrl} />
