109000b0d1f6f28dc4b010a6f7999a7e26ddf7eb
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-07-26 22:09:22 +0300

    Feat: add batching whitelist addreses to the saga & increase csv file limit

--- src/modules/dashboard/sagas/extensions/updateWhitelist.ts
+++ src/modules/dashboard/sagas/extensions/updateWhitelist.ts
@@ -1,5 +1,6 @@
-import { call, fork, put, takeEvery } from 'redux-saga/effects';
+import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
 import { ClientType } from '@colony/colony-js';
+import { $Values } from 'utility-types';
 
 import { Action, ActionTypes, AllActions } from '~redux/index';
 import {
@@ -9,36 +10,91 @@ import {
 } from '~data/index';
 import { ContextModule, TEMP_getContext } from '~context/index';
 import { putError, takeFrom } from '~utils/saga/effects';
+import { TxConfig } from '~types/index';
 
 import {
+  ChannelDefinition,
   createTransaction,
-  getTxChannel,
-  waitForTxResult,
+  createTransactionChannels,
 } from '../../../core/sagas';
 
 export function* updateWhitelist({
   meta,
   payload: { userAddresses, colonyAddress, status },
 }: Action<ActionTypes.WHITELIST_UPDATE>) {
-  const txChannel = yield call(getTxChannel, meta.id);
   const apolloClient = TEMP_getContext(ContextModule.ApolloClient);
-  try {
-    yield fork(createTransaction, meta.id, {
-      context: ClientType.WhitelistClient,
-      methodName: 'approveUsers',
-      identifier: colonyAddress,
-      params: [userAddresses, status],
-    });
+  const requireTransactions = Math.ceil(userAddresses.length / 100);
+  const channelNames: string[] = [];
 
-    yield takeFrom(txChannel, ActionTypes.TRANSACTION_CREATED);
+  for (let index = 0; index < requireTransactions; index += 1) {
+    channelNames.push(String(index));
+  }
+
+  /*
+   * Define a manifest of transaction ids and their respective channels.
+   */
+  const channels: { [id: string]: ChannelDefinition } = yield call(
+    createTransactionChannels,
+    meta.id,
+    channelNames,
+  );
+  try {
+    const createGroupedTransaction = (
+      { id, index }: $Values<typeof channels>,
+      config: TxConfig,
+    ) =>
+      fork(createTransaction, id, {
+        ...config,
+        group: {
+          key: 'approveUsers',
+          id: meta.id,
+          index,
+        },
+      });
+
+    /*
+     * Create all transactions for the group.
+     */
+    yield all(
+      Object.keys(channels).map((id) =>
+        createGroupedTransaction(channels[id], {
+          context: ClientType.WhitelistClient,
+          methodName: 'approveUsers',
+          identifier: colonyAddress,
+          params: [
+            userAddresses.slice(
+              channels[id].index * 100,
+              channels[id].index * 100 + 100,
+            ),
+            status,
+          ],
+        }),
+      ),
+    );
+
+    /*
+     * Wait until all transactions are created.
+     */
+    yield all(
+      Object.keys(channels).map((id) =>
+        takeFrom(channels[id].channel, ActionTypes.TRANSACTION_CREATED),
+      ),
+    );
+
+    /*
+     * Wait until all transactions are succeeded.
+     */
+    yield all(
+      Object.keys(channels).map((id) =>
+        takeFrom(channels[id].channel, ActionTypes.TRANSACTION_SUCCEEDED),
+      ),
+    );
 
     yield put<AllActions>({
       type: ActionTypes.WHITELIST_UPDATE_SUCCESS,
       payload: {},
       meta,
     });
-
-    yield waitForTxResult(txChannel);
   } catch (error) {
     return yield putError(ActionTypes.WHITELIST_UPDATE_ERROR, error, meta);
   } finally {
@@ -52,7 +108,14 @@ export function* updateWhitelist({
       },
       fetchPolicy: 'network-only',
     });
-    txChannel.close();
+    /*
+     * Close all transaction channels.
+     */
+    yield all(
+      Object.keys(channels).map((id) =>
+        call([channels[id].channel, channels[id].channel.close]),
+      ),
+    );
   }
   return null;
 }
