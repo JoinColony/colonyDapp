c34e3bbe59aca94b6c0c6a0dffcb8ac016314e45
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-08-02 16:45:42 +0300

    Feat: extend `AgreementDialog` for signing functionality

--- src/modules/dashboard/components/Whitelist/AgreementDialog/AgreementDialog.tsx
+++ src/modules/dashboard/components/Whitelist/AgreementDialog/AgreementDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
 import Dialog, { DialogSection } from '~core/Dialog';
@@ -15,13 +15,29 @@ const MSG = defineMessages({
     id: 'dashboard.Extensions.WhitelisExtension.AgreementDialog.title',
     defaultMessage: 'Sale agreement',
   },
+  agreementExplanation: {
+    id: `dashboard.Extensions.WhitelisExtension.AgreementDialog.agreementExplanation`,
+    defaultMessage: `Add to the whitelist wallet addresses which should be participating in the token sale. `,
+  },
+  readCarefully: {
+    id: 'dashboard.Extensions.WhitelisExtension.AgreementDialog.readCarefully',
+    defaultMessage: 'Read carefully',
+  },
   gotItButton: {
     id: 'dashboard.Extensions.WhitelisExtension.AgreementDialog.gotItButton',
     defaultMessage: 'Got it',
   },
+  iAgreeButton: {
+    id: 'dashboard.Extensions.WhitelisExtension.AgreementDialog.iAgreeButton',
+    defaultMessage: 'I agree',
+  },
   ipfsError: {
     id: 'dashboard.Extensions.WhitelisExtension.AgreementDialog.ipfsError',
     defaultMessage: `There is a problem loading the agreement. Please, try again or contact colony admins.`,
+  },
+  agreeToTerms: {
+    id: 'dashboard.Extensions.WhitelisExtension.AgreementDialog.agreeToTerms',
+    defaultMessage: 'By Signing you agree to the terms and conditions',
   },
 });
 
@@ -29,13 +45,43 @@ interface Props {
   cancel: () => void;
   close: () => void;
   agreementHash: string;
+  back?: () => void;
+  isSignable?: boolean;
 }
 
-const AgreementDialog = ({ cancel, close, agreementHash }: Props) => {
+const AgreementDialog = ({
+  cancel,
+  close,
+  agreementHash,
+  isSignable,
+  back,
+}: Props) => {
+  const [hasBeenScrolled, setHasBeenScrolled] = useState(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+
   const { data, loading } = useWhitelistAgreementQuery({
     variables: { agreementHash },
     fetchPolicy: 'network-only',
   });
+
+  const handleScroll = (e) => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop <= e.target.clientHeight;
+
+    if (bottom) {
+      setHasBeenScrolled(true);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      ref.current !== null &&
+      ref.current.clientHeight === ref.current.scrollHeight
+    ) {
+      setHasBeenScrolled(true);
+    }
+  }, [data]);
 
   return (
     <Dialog cancel={cancel}>
@@ -46,11 +92,27 @@ const AgreementDialog = ({ cancel, close, agreementHash }: Props) => {
           className={styles.title}
         />
       </DialogSection>
+      {isSignable && (
+        <div className={styles.description}>
+          <DialogSection appearance={{ theme: 'sidePadding' }}>
+            <FormattedMessage {...MSG.agreementExplanation} />
+          </DialogSection>
+        </div>
+      )}
       <DialogSection>
+        {isSignable && (
+          <div className={styles.readCarefullyText}>
+            <FormattedMessage {...MSG.readCarefully} />
+          </div>
+        )}
         {loading ? (
           <SpinnerLoader appearance={{ size: 'huge', theme: 'primary' }} />
         ) : (
-          <div className={styles.agreementContainer}>
+          <div
+            className={styles.agreementContainer}
+            ref={ref}
+            onScroll={handleScroll}
+          >
             {data?.whitelistAgreement || (
               <div className={styles.error}>
                 <FormattedMessage {...MSG.ipfsError} />
@@ -58,12 +120,27 @@ const AgreementDialog = ({ cancel, close, agreementHash }: Props) => {
             )}
           </div>
         )}
+        {isSignable && (
+          <div className={styles.agreeToTermsText}>
+            <FormattedMessage {...MSG.agreeToTerms} />
+          </div>
+        )}
       </DialogSection>
       <DialogSection appearance={{ align: 'right', theme: 'footer' }}>
+        {back && (
+          <Button
+            appearance={{ theme: 'secondary', size: 'large' }}
+            onClick={back}
+            text={{ id: 'button.back' }}
+          />
+        )}
         <Button
           appearance={{ theme: 'primary', size: 'large' }}
           onClick={close}
-          text={MSG.gotItButton}
+          text={isSignable ? MSG.iAgreeButton : MSG.gotItButton}
+          {...(isSignable
+            ? { disabled: !data?.whitelistAgreement || !hasBeenScrolled }
+            : {})}
         />
       </DialogSection>
     </Dialog>
