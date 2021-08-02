9895025cbdf29d45a682ab8574b376e7c494e8b6
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-08-02 16:45:42 +0300

    Feat: extend `AgreementDialog` for signing functionality

--- src/modules/dashboard/components/Whitelist/AgreementDialog/AgreementDialog.css.d.ts
+++ src/modules/dashboard/components/Whitelist/AgreementDialog/AgreementDialog.css.d.ts
@@ -1,3 +1,6 @@
 export const title: string;
 export const agreementContainer: string;
 export const error: string;
+export const description: string;
+export const readCarefullyText: string;
+export const agreeToTermsText: string;
