de8ad3bf7f52fcef6389c07f893ab9e54b9288e8
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-08-02 16:45:42 +0300

    Feat: extend `AgreementDialog` for signing functionality

--- src/modules/dashboard/components/Whitelist/AgreementDialog/AgreementContainer.tsx
+++ src/modules/dashboard/components/Whitelist/AgreementDialog/AgreementContainer.tsx
@@ -7,15 +7,6 @@ import { SpinnerLoader } from '~core/Preloaders';
 import styles from './AgreementDialog.css';
 
 const MSG = defineMessages({
-  title: {
-    id:
-      'dashboard.Extensions.Whitelist.AgreementDialog.AgreementContainer.title',
-    defaultMessage: 'Sale agreement',
-  },
-  signedButton: {
-    id: `dashboard.Extensions.Whitelist.AgreementDialog.AgreementContainer.gotItButton`,
-    defaultMessage: 'Signed',
-  },
   ipfsError: {
     id: `dashboard.Extensions.Whitelist.AgreementDialog.AgreementContainer.ipfsError`,
     defaultMessage: `Failed to retrieve data from IPFS. Please try again.`,
@@ -25,13 +16,24 @@ const MSG = defineMessages({
 interface Props {
   loading: boolean;
   text?: string;
+  handleScroll: (e: any) => void;
+  containerRef: React.Ref<HTMLDivElement>;
 }
 
-const AgreementContainer = ({ loading, text }: Props) => {
+const AgreementContainer = ({
+  loading,
+  text,
+  handleScroll,
+  containerRef,
+}: Props) => {
   return loading ? (
     <SpinnerLoader appearance={{ size: 'huge', theme: 'primary' }} />
   ) : (
-    <div className={styles.agreementContainer}>
+    <div
+      className={styles.agreementContainer}
+      ref={containerRef}
+      onScroll={handleScroll}
+    >
       {text || (
         <div className={styles.error}>
           <FormattedMessage {...MSG.ipfsError} />
