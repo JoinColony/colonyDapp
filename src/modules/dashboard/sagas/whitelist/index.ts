8773bc84a799090cae7b2e38bc980b0a22631267
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-08-05 14:46:55 +0200

    Add sign agreement saga

---
+++ src/modules/dashboard/sagas/whitelist/index.ts
@@ -0,0 +1,9 @@
+import { all, call } from 'redux-saga/effects';
+
+import signAgreement from './signAgreement';
+
+export default function* whitelistSagas() {
+  yield all([
+    call(signAgreement),
+  ]);
+}
