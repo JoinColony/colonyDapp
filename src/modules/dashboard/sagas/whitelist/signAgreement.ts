eb60802b209d9b50a3b151829472a3a0d0ac5669
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-08-05 15:59:55 +0200

    Clean up files from unused variables

--- src/modules/dashboard/sagas/whitelist/signAgreement.ts
+++ src/modules/dashboard/sagas/whitelist/signAgreement.ts
@@ -1,26 +1,12 @@
 import { call, fork, put, takeEvery } from 'redux-saga/effects';
 import { ClientType } from '@colony/colony-js';
-import { bigNumberify } from 'ethers/utils';
-import { AddressZero } from 'ethers/constants';
-import moveDecimal from 'move-decimal-point';
 
-import { Action, ActionTypes, AllActions } from '~redux/index';
+import { Action, ActionTypes } from '~redux/index';
 import { putError, takeFrom } from '~utils/saga/effects';
-import { getTokenDecimalsWithFallback } from '~utils/tokens';
-import { createAddress } from '~utils/web3';
-import { ContextModule, TEMP_getContext } from '~context/index';
-import { getToken } from '~data/resolvers/token';
-import { TxConfig } from '~types/index';
-import {
-  createTransaction,
-  createTransactionChannels,
-  getTxChannel,
-} from '../../../core/sagas';
-import { transactionReady } from '../../../core/actionCreators';
+import { createTransaction, getTxChannel } from '../../../core/sagas';
 
 function* signAgreement({
   payload: { agreementHash, colonyAddress },
-  meta: { id: metaId },
   meta,
 }: Action<ActionTypes.WHITELIST_SIGN_AGREEMENT>) {
   const txChannel = yield call(getTxChannel, meta.id);
@@ -28,9 +14,6 @@ function* signAgreement({
     if (!agreementHash) {
       throw new Error('Agreement hash needs to be provided');
     }
-
-    const colonyManager = TEMP_getContext(ContextModule.ColonyManager);
-    const apolloClient = TEMP_getContext(ContextModule.ApolloClient);
 
     yield fork(createTransaction, meta.id, {
       context: ClientType.WhitelistClient,
@@ -45,7 +28,6 @@ function* signAgreement({
       type: ActionTypes.WHITELIST_SIGN_AGREEMENT_SUCCESS,
       meta,
     });
-
   } catch (caughtError) {
     putError(ActionTypes.WHITELIST_SIGN_AGREEMENT_ERROR, caughtError, meta);
   } finally {
