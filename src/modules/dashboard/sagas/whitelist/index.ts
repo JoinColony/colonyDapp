084a671cf1d0bd19283cae468d3ec6d1b4971103
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-08-05 15:59:55 +0200

    Clean up files from unused variables

--- src/modules/dashboard/sagas/whitelist/index.ts
+++ src/modules/dashboard/sagas/whitelist/index.ts
@@ -3,7 +3,5 @@ import { all, call } from 'redux-saga/effects';
 import signAgreement from './signAgreement';
 
 export default function* whitelistSagas() {
-  yield all([
-    call(signAgreement),
-  ]);
+  yield all([call(signAgreement)]);
 }
