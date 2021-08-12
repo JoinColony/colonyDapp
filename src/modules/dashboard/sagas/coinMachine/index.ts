fb917aac12e800b174ac462e68881bb5701ac12e
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-08-12 23:23:23 +0300

    Feat: add `updatePeriod` saga to `coinMachine` sagas

--- src/modules/dashboard/sagas/coinMachine/index.ts
+++ src/modules/dashboard/sagas/coinMachine/index.ts
@@ -1,7 +1,9 @@
 import { all, call } from 'redux-saga/effects';
 
 import buyTokensSaga from './buyTokens';
+import updatePeriodSaga from './updatePeriod';
 
 export default function* coinMachineSagas() {
   yield all([call(buyTokensSaga)]);
+  yield all([call(updatePeriodSaga)]);
 }
