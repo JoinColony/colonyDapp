fa2d35f41d1191cffef94337e0abc36b924b01d2
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-08-18 22:51:01 +0300

    Feat: add token selector to `ExtensionSetup`

--- src/modules/dashboard/components/Extensions/ExtensionDetails.tsx
+++ src/modules/dashboard/components/Extensions/ExtensionDetails.tsx
@@ -399,7 +399,7 @@ const ExtensionDetails = ({
                   <ExtensionSetup
                     extension={extension}
                     installedExtension={installedExtension}
-                    colonyAddress={colonyAddress}
+                    colony={colony}
                     nativeTokenAddress={nativeTokenAddress}
                   />
                 ) : (
