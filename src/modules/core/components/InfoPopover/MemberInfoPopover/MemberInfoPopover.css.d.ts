7192fa5992a075c954412dbc06ff40e2339d24bb
ArmandoGraterol <armandogrtrl@gmail.com>
2021-08-20 20:19:11 -0300

    Refactored MemberInfoPopover

---
+++ src/modules/core/components/InfoPopover/MemberInfoPopover/MemberInfoPopover.css.d.ts
@@ -0,0 +1,6 @@
+export const main: string;
+export const section: string;
+export const reputation: string;
+export const reputationHeading: string;
+export const reputationError: string;
+export const loadingSpinnerContainer: string;
