bdc0be92a34395d18fa2259516d75551466ed190
Raul Glogovetan <raul@glogovetan.com>
2021-08-29 18:07:41 +0300

    Refactor: allow for multiple graphql queries files

--- src/data/index.ts
+++ src/data/index.ts
@@ -39,6 +39,9 @@ import { eventsResolvers } from './resolvers/events';
 import { recoveryModeResolvers } from './resolvers/recovery';
 import { extensionsResolvers } from './resolvers/extensions';
 import { motionsResolvers } from './resolvers/motions';
+/*
+ * @TODO This needs to be merged with the motions resolvers
+ */
 import { stakesResolvers } from './resolvers/stakes';
 import { whitelistResolvers } from './resolvers/whitelist';
 
