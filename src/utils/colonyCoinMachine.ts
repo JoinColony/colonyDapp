1a47e9cb7d54e5b8ba81e53f6f4d2a800fd63bf8
ArmandoGraterol <armandogrtrl@gmail.com>
2021-08-30 19:42:37 -0300

    Refactored getFormattedTokensRemaning into a component

--- src/utils/colonyCoinMachine.ts
+++ src/utils/colonyCoinMachine.ts
@@ -1,18 +1,7 @@
-import { FormattedMessage, defineMessages } from 'react-intl';
-import React from 'react';
 import { bigNumberify, BigNumberish } from 'ethers/utils';
 
 import { PeriodTokensType } from '~dashboard/CoinMachine/RemainingDisplayWidget';
 import { TokenPriceStatuses } from '~dashboard/CoinMachine/TokenPriceStatusIcon';
-
-import { getFormattedTokenValue } from './tokens';
-
-const MSG = defineMessages({
-  soldOut: {
-    id: 'dashboard.CoinMachine.soldOut',
-    defaultMessage: 'SOLD OUT',
-  },
-});
 
 export const getPriceStatus = (
   periodTokens: PeriodTokensType,
@@ -37,20 +26,4 @@ export const getPriceStatus = (
   }
 
   return undefined;
-};
-
-export const getFormattedTokensRemaining = (
-  periodTokens: PeriodTokensType,
-  tokensBought: BigNumberish,
-) => {
-  const { maxPeriodTokens, decimals } = periodTokens;
-
-  if (bigNumberify(tokensBought).gte(maxPeriodTokens)) {
-    return <FormattedMessage {...MSG.soldOut} />;
-  }
-
-  return `${getFormattedTokenValue(
-    maxPeriodTokens.sub(tokensBought),
-    decimals,
-  )}/${getFormattedTokenValue(maxPeriodTokens, decimals)}`;
 };
