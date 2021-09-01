25d9ed06faec5cd180e182ad03c69fe521168652
ArmandoGraterol <armandogrtrl@gmail.com>
2021-09-01 15:19:10 -0300

    Added UserPermissions component to MemberInfoPopover

--- src/modules/core/components/InfoPopover/MemberInfoPopover/MemberInfoPopover.css.d.ts
+++ src/modules/core/components/InfoPopover/MemberInfoPopover/MemberInfoPopover.css.d.ts
@@ -4,3 +4,6 @@ export const reputation: string;
 export const reputationHeading: string;
 export const reputationError: string;
 export const loadingSpinnerContainer: string;
+export const permissionsContainer: string;
+export const roleList: string;
+export const roleListItem: string;
