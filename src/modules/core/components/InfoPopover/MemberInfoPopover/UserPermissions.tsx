885057bd3da383b8af332f4c08b139b5b119926a
ArmandoGraterol <armandogrtrl@gmail.com>
2021-09-01 15:18:17 -0300

    Added UserPermissions component with styles

---
+++ src/modules/core/components/InfoPopover/MemberInfoPopover/UserPermissions.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ColonyRole } from '@colony/colony-js';
+import { defineMessages } from 'react-intl';
+
+import PermissionsLabel from '~core/PermissionsLabel';
+
+import styles from './MemberInfoPopover.css';
+import Heading from '~core/Heading';
+
+const displayName = `InfoPopover.MemberInfoPopover.UserPermissions`;
+
+interface Props {
+  roles: ColonyRole[];
+}
+
+const MSG = defineMessages({
+  labelText: {
+    id: 'InfoPopover.MemberInfoPopover.UserPermissions.labelText',
+    defaultMessage: 'Permissions',
+  },
+});
+
+const UserPermissions = ({ roles }: Props) => {
+  return (
+    <div className={styles.permissionsContainer}>
+      <Heading
+        appearance={{
+          size: 'normal',
+          margin: 'none',
+          theme: 'grey',
+          weight: 'bold',
+        }}
+        text={MSG.labelText}
+      />
+      <ul className={styles.roleList}>
+        {roles.map((role) => (
+          <li key={role} className={styles.roleListItem}>
+            <PermissionsLabel permission={role} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+UserPermissions.displayName = displayName;
+
+export default UserPermissions;
