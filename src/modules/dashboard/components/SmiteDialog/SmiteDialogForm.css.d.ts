acb17ff2a9d570582005a01fe87daacbfca38c20
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-09-03 09:20:59 +0200

    Add smite dialog

---
+++ src/modules/dashboard/components/SmiteDialog/SmiteDialogForm.css.d.ts
@@ -0,0 +1,7 @@
+export const wideButton: string;
+export const inputContainer: string;
+export const domainSelects: string;
+export const singleUserContainer: string;
+export const noPermissionFromMessage: string;
+export const modalHeading: string;
+export const headingContainer: string;
