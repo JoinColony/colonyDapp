c58fefbc25b360503dcef4f077972941360748ed
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-09-07 22:17:01 +0200

    Feat: add `updateUserReputation` function to saga utils

--- src/modules/dashboard/sagas/utils/index.ts
+++ src/modules/dashboard/sagas/utils/index.ts
@@ -2,3 +2,4 @@ export * from './updateColonyDisplayCache';
 export { updateMotionValues } from './updateMotionValues';
 export { refreshExtension } from './refreshExtension';
 export { uploadIfpsAnnotation } from './uploadIfpsAnnotation';
+export { updateUserReputation } from './updateUserReputation';
