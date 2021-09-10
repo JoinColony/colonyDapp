ed181db90a21e6ab0678b71b428dacbd0d42a74a
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-09-10 21:25:45 +0200

    Feat: add `transform` function to `SmiteDialog`

--- src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
+++ src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
@@ -85,6 +85,7 @@ const ColonyHomeActions = ({ colony, ethDomainId }: Props) => {
         prevStep: 'dashboard.ColonyActionsDialog',
         colony,
         isVotingExtensionEnabled,
+        ethDomainId,
       },
     },
     {
