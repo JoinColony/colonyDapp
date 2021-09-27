a42e9ac5ec3683399331187ff517b7f888b855dc
ArmandoGraterol <armandogrtrl@gmail.com>
2021-09-27 19:28:46 -0300

    Updated DefaultAction and DetailsWidget according to new reputationPenalty field

--- src/modules/dashboard/components/ActionsPage/DetailsWidget/DetailsWidget.css.d.ts
+++ src/modules/dashboard/components/ActionsPage/DetailsWidget/DetailsWidget.css.d.ts
@@ -1,6 +1,7 @@
 export const item: string;
 export const value: string;
 export const label: string;
+export const penaltyValue: string;
 export const descriptionValue: string;
 export const transactionHashLink: string;
 export const domainDescriptionItem: string;
