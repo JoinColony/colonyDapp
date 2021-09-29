872f3e489d84e354c13f0fefc671ad01f8bb84ea
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-09-29 17:22:00 +0200

    Remove unused variables

--- src/modules/users/components/UserProfileEdit/UserMainSettings.tsx
+++ src/modules/users/components/UserProfileEdit/UserMainSettings.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
 import { defineMessages } from 'react-intl';
 import * as yup from 'yup';
-import { Redirect } from 'react-router-dom';
 
 import CopyableAddress from '~core/CopyableAddress';
 import UserMention from '~core/UserMention';
@@ -71,7 +70,6 @@ const validationSchema = yup.object({
 });
 
 const UserMainSettings = ({ user }: Props) => {
-
   const [editUser] = useEditUserMutation();
   const onSubmit = useCallback(
     (profile: FormValues) => editUser({ variables: { input: profile } }),
@@ -80,72 +78,72 @@ const UserMainSettings = ({ user }: Props) => {
 
   return (
     <>
-    <Heading
-      appearance={{ theme: 'dark', size: 'medium' }}
-      text={MSG.heading}
-    />
-    <Form<FormValues>
-      initialValues={{
-        displayName: user.profile.displayName || undefined,
-        bio: user.profile.bio || undefined,
-        website: user.profile.website || undefined,
-        location: user.profile.location || undefined,
-      }}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}
-    >
-      {({ status, isSubmitting }) => (
-        <div className={styles.main}>
-          <FieldSet>
-            <InputLabel label={MSG.labelWallet} />
-            <CopyableAddress appearance={{ theme: 'big' }} full>
-              {user.profile.walletAddress}
-            </CopyableAddress>
-          </FieldSet>
-          <div className={styles.usernameContainer}>
-            <InputLabel label={MSG.labelUsername} />
-            <UserMention
-              username={user.profile.username || user.profile.walletAddress}
-              title={user.profile.username || user.profile.walletAddress}
-              hasLink={false}
-              data-test="userProfileUsername"
-            />
+      <Heading
+        appearance={{ theme: 'dark', size: 'medium' }}
+        text={MSG.heading}
+      />
+      <Form<FormValues>
+        initialValues={{
+          displayName: user.profile.displayName || undefined,
+          bio: user.profile.bio || undefined,
+          website: user.profile.website || undefined,
+          location: user.profile.location || undefined,
+        }}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ status, isSubmitting }) => (
+          <div className={styles.main}>
+            <FieldSet>
+              <InputLabel label={MSG.labelWallet} />
+              <CopyableAddress appearance={{ theme: 'big' }} full>
+                {user.profile.walletAddress}
+              </CopyableAddress>
+            </FieldSet>
+            <div className={styles.usernameContainer}>
+              <InputLabel label={MSG.labelUsername} />
+              <UserMention
+                username={user.profile.username || user.profile.walletAddress}
+                title={user.profile.username || user.profile.walletAddress}
+                hasLink={false}
+                data-test="userProfileUsername"
+              />
+            </div>
+            <FieldSet className={styles.inputFieldSet}>
+              <Input
+                label={MSG.labelName}
+                name="displayName"
+                data-test="userSettingsName"
+              />
+              <Textarea
+                label={MSG.labelBio}
+                name="bio"
+                maxLength={160}
+                data-test="userSettingsBio"
+              />
+              <Input
+                label={MSG.labelWebsite}
+                name="website"
+                data-test="userSettingsWebsite"
+              />
+              <Input
+                label={MSG.labelLocation}
+                name="location"
+                data-test="userSettingsLocation"
+              />
+            </FieldSet>
+            <FieldSet>
+              <Button
+                type="submit"
+                text={{ id: 'button.save' }}
+                loading={isSubmitting}
+                data-test="userSettingsSubmit"
+              />
+            </FieldSet>
+            <FormStatus status={status} />
           </div>
-          <FieldSet className={styles.inputFieldSet}>
-            <Input
-              label={MSG.labelName}
-              name="displayName"
-              data-test="userSettingsName"
-            />
-            <Textarea
-              label={MSG.labelBio}
-              name="bio"
-              maxLength={160}
-              data-test="userSettingsBio"
-            />
-            <Input
-              label={MSG.labelWebsite}
-              name="website"
-              data-test="userSettingsWebsite"
-            />
-            <Input
-              label={MSG.labelLocation}
-              name="location"
-              data-test="userSettingsLocation"
-            />
-          </FieldSet>
-          <FieldSet>
-            <Button
-              type="submit"
-              text={{ id: 'button.save' }}
-              loading={isSubmitting}
-              data-test="userSettingsSubmit"
-            />
-          </FieldSet>
-          <FormStatus status={status} />
-        </div>
-      )}
-    </Form>
+        )}
+      </Form>
     </>
   );
 };
