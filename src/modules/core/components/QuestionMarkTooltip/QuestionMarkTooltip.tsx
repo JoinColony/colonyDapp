42d42720ad1925794b7c9c4fc303e318e9e05a53
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-09-30 14:34:55 +0200

    Update QuestionMarkTooltip component to use ComplexMessageValues for context text values

--- src/modules/core/components/QuestionMarkTooltip/QuestionMarkTooltip.tsx
+++ src/modules/core/components/QuestionMarkTooltip/QuestionMarkTooltip.tsx
@@ -4,13 +4,13 @@ import { MessageDescriptor, FormattedMessage } from 'react-intl';
 
 import Icon from '~core/Icon';
 import { Tooltip } from '~core/Popover';
-import { SimpleMessageValues } from '~types/index';
+import { ComplexMessageValues } from '~types/index';
 
 const displayName = 'QuestionMarkTooltip';
 
 interface Props {
   tooltipText: string | MessageDescriptor;
-  tooltipTextValues?: SimpleMessageValues;
+  tooltipTextValues?: ComplexMessageValues;
   /** Options to pass to the underlying PopperJS element. See here for more: https://popper.js.org/docs/v2/constructors/#options. */
   tooltipPopperOptions?: PopperOptions;
   className?: string;
