42bd45ed10cb78d71a7ff0a340a990db90284580
ArmandoGraterol <armandogrtrl@gmail.com>
2021-10-04 17:58:11 -0300

    Added schemas and types shared by award and smite to reusable file

---
+++ src/modules/dashboard/components/AwardAndSmiteDialogForm/types.ts
@@ -0,0 +1,18 @@
+import { WizardDialogType } from '~utils/hooks';
+import { DialogProps, ActionDialogProps } from '~core/Dialog';
+import { Address } from '~types/index';
+
+export interface AwardAndSmiteDialogFormValues {
+  forceAction: boolean;
+  domainId: string;
+  user: { profile: { walletAddress: Address } };
+  amount: number;
+  annotation: string;
+  motionDomainId: string;
+}
+
+export type AwardAndSmiteDialogProps = Required<DialogProps> &
+  WizardDialogType<object> &
+  ActionDialogProps & {
+    ethDomainId?: number;
+  };
