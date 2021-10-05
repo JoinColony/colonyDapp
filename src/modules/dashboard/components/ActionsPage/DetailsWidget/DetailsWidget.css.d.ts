00904ad28bc07b368116faba96ca8e4a6585e9d6
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-10-05 17:19:02 +0200

    Refactor: remove stale css class from `DetailsWidget.css`

--- src/modules/dashboard/components/ActionsPage/DetailsWidget/DetailsWidget.css.d.ts
+++ src/modules/dashboard/components/ActionsPage/DetailsWidget/DetailsWidget.css.d.ts
@@ -1,7 +1,6 @@
 export const item: string;
 export const value: string;
 export const label: string;
-export const penaltyValue: string;
 export const descriptionValue: string;
 export const transactionHashLink: string;
 export const domainDescriptionItem: string;
