1bc50b05097e73d2645e959b19b0de646454de07
Raul Glogovetan <raul@glogovetan.com>
2021-10-06 18:51:13 +0300

    Add: `getAverageBlockPeriod` web3 util

--- src/utils/web3/index.ts
+++ src/utils/web3/index.ts
@@ -1,5 +1,6 @@
 import BN from 'bn.js';
 import { getAddress, padZeros, hexlify } from 'ethers/utils';
+import { Provider } from 'ethers/providers';
 
 import { Address } from '~types/index';
 
@@ -62,3 +63,24 @@ export function intArrayToBytes32(arr: Array<number>) {
     arr.map((num) => new BN(1).shln(num)).reduce((a, b) => a.or(b), new BN(0)),
   ).toString(16, 64)}`;
 }
+
+export const getAverageBlockPeriod = async (
+  provider: Provider,
+  endBlock: number | 'latest' = 'latest',
+  startBlock = 1,
+): Promise<number | null> => {
+  if (!provider) {
+    return null;
+  }
+  const latestBlock = await provider.getBlock(endBlock);
+  const middleishBlock = await provider.getBlock(
+    Math.ceil(latestBlock.number / 2),
+  );
+  const firstBlock = await provider.getBlock(startBlock);
+  const fullTimespanAvg =
+    (latestBlock.timestamp - firstBlock.timestamp) / latestBlock.number;
+  const halfTimespanAvg =
+    (middleishBlock.timestamp - firstBlock.timestamp) / middleishBlock.number;
+  const approximateAvg = (fullTimespanAvg + halfTimespanAvg) / 2;
+  return Math.ceil(approximateAvg) * 1000;
+};
