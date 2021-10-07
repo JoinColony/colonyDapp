455b8d5e13bba6b7e1a9ceac80dcbb6dc1b5ad05
Raul Glogovetan <raul@glogovetan.com>
2021-10-07 23:10:57 +0300

    Refactor: subgraph events query type definitions

--- src/data/graphql/typeDefsSubscriptions.ts
+++ src/data/graphql/typeDefsSubscriptions.ts
@@ -5,12 +5,6 @@ export default gql`
     payment_contains: String
   }
 
-  input EventsFilter {
-    associatedColony_contains: String
-    associatedColony: String
-    name_in: [String!]
-  }
-
   input MotionsFilter {
     associatedColony: String
     extensionAddress: String
@@ -21,36 +15,6 @@ export default gql`
     agent: String!
     transaction: SubgraphTransaction!
     payment: SubgraphPayment!
-  }
-
-  type EventProcessedValues {
-    agent: String!
-    who: String!
-    fromPot: String!
-    fromDomain: String!
-    toPot: String!
-    toDomain: String!
-    domainId: String!
-    amount: String!
-    token: String!
-    metadata: String!
-    user: String!
-    role: String!
-    setTo: String!
-    oldVersion: String!
-    newVersion: String!
-    storageSlot: String!
-    storageSlotValue: String!
-  }
-
-  type SubscriptionEvent {
-    id: String!
-    transaction: SubgraphTransaction!
-    address: String!
-    name: String!
-    args: String!
-    associatedColony: SubgraphColony!
-    processedValues: EventProcessedValues!
   }
 
   type SubscriptionMotion {
@@ -94,7 +58,7 @@ export default gql`
       first: Int!
       where: ActionsFilter!
     ): [OneTxPayment!]!
-    events(skip: Int!, first: Int!, where: EventsFilter!): [SubscriptionEvent!]!
+    events(skip: Int, first: Int, where: EventsFilter): [SubgraphEvent!]!
     motions(
       skip: Int!
       first: Int!
