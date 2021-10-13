8a75b835d9176f8d2b4f5650bd9a48cf3d80fe60
Raul Glogovetan <raul@glogovetan.com>
2021-10-13 20:50:11 +0300

    Remove: unused `shell` scripts util

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,47 +1,10 @@
-const exec = require('child_process').exec;
-const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
 const PATHS = require('./paths');
 
-const { readdirSync, existsSync } = fs;
+const { readdirSync } = fs;
 const { DAPP_MODULES, COMPONENTS_FOLDER } = PATHS;
 
-
-/**
- * Wrapper method for node's `child_process` `exec` to get the live output of a shell command.
- * This is an updated version from the one
- *
- * As with anything that touches the (priviledged) shell, BE CAREFUL, you are responsible for the commands you run!
- *
- * @method shell
- *
- * @param {string} command Shell command to execute
- * @param {object} options Optional options to pass down to the `exec()` method
- * @param {function} callback Optional callback fuction to pass the successfull output string to
- */
-const shell = (
-  command,
-  options = {},
-  callback,
-) => {
-  const runner = exec(command, options, (error, stdout) => {
-    if (error) {
-      console.log(chalk.red(`There was an error executing: '${command}'`));
-      console.log(error);
-      console.log('Exitting ...');
-      process.exit(1);
-    }
-  });
-  runner.stdout.on('data', output => process.stdout.write(output));
-  runner.on('exit', () => {
-    if (callback && typeof callback === 'function') {
-      return callback();
-    }
-    return undefined;
-  });
-  return runner;
-};
 
 /**
  * Helper method to generate a specific webpack alias format entry object
@@ -150,7 +113,6 @@ const getStaticDevResource = (processName) => {
 };
 
 module.exports = {
-  shell,
   generateWebpackAlias,
   getDappModules,
   getStaticDevResource,
