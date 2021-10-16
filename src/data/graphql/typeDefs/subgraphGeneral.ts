357b521d14072ab667103932719f02b0350f4895
Raul Glogovetan <raul@glogovetan.com>
2021-10-17 01:07:19 +0300

    Add: subgraph roles query

---
+++ src/data/graphql/typeDefs/subgraphGeneral.ts
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  extend type Query {
+    events(
+      skip: Int
+      first: Int
+      where: EventsFilter
+      orderDirection: String
+    ): [SubgraphEvent!]!
+  }
+`;
