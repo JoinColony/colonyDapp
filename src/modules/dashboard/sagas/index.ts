f70c4c78021a4f8a391b7efe40678ebfaf9ced9d
Raul Glogovetan <raul@glogovetan.com>
2021-10-25 21:38:59 +0300

    Add: initial `createMintTokensMetaAction` saga

--- src/modules/dashboard/sagas/index.ts
+++ src/modules/dashboard/sagas/index.ts
@@ -1,6 +1,7 @@
 import { all, call } from 'redux-saga/effects';
 
 import actionsSagas from './actions';
+import mteaActionsSagas from './actions/meta';
 import colonySagas from './colony';
 import colonyCreateSaga from './colonyCreate';
 import colonyDeploymentSaga from './colonyFinishDeployment';
@@ -10,6 +11,7 @@ import motionSagas from './motions';
 export default function* setupDashboardSagas() {
   yield all([
     call(actionsSagas),
+    call(mteaActionsSagas),
     call(colonySagas),
     call(colonyCreateSaga),
     call(colonyDeploymentSaga),
