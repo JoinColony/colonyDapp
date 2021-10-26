6c7f60e1b5257454ffb9330d6a8fbc25a5414040
Raul Glogovetan <raul@glogovetan.com>
2021-10-26 21:17:22 +0300

    Add: initial mint tokens metaTx saga

--- src/modules/dashboard/sagas/actions/meta/mintTokens.ts
+++ src/modules/dashboard/sagas/actions/meta/mintTokens.ts
@@ -1,17 +1,23 @@
 import { put, takeEvery } from 'redux-saga/effects';
-import { ClientType, ColonyClient } from '@colony/colony-js';
+import { ClientType, ColonyClient, Network } from '@colony/colony-js';
 import { soliditySha3 } from 'web3-utils';
+import { splitSignature } from 'ethers/utils';
+import { hexSequenceNormalizer } from '@purser/core';
 
 import { getLoggedInUser } from '~data/index';
 import { ContextModule, TEMP_getContext } from '~context/index';
 import { Action, ActionTypes, AllActions } from '~redux/index';
 import { putError } from '~utils/saga/effects';
+import { DEFAULT_NETWORK } from '~constants';
+import { signMessage } from '../../../../core/sagas/messages';
 
 /*
  * @TODO This is not a real action saga... yet
  * It's just being used as an example to be able generate a basic metatransactions
+ *
  * The rest of the content for this saga will be added by
- * https://github.com/JoinColony/colonyDapp/pull/2779
+ * https://github.com/JoinColony/colonyDapp/pull/2779 which will also add a bunch
+ * of abstractions over some of this boilerplate code
  */
 function* createMintTokensMetaAction({
   payload: { colonyAddress, amount },
@@ -24,6 +30,7 @@ function* createMintTokensMetaAction({
     const { walletAddress: userAddress } = yield getLoggedInUser();
 
     const colonyManager = TEMP_getContext(ContextModule.ColonyManager);
+    const { provider } = colonyManager;
 
     const colonyClient: ColonyClient = yield colonyManager.getClient(
       ClientType.ColonyClient,
@@ -41,16 +48,90 @@ function* createMintTokensMetaAction({
       [amount],
     );
 
-    console.log(colonyClient.provider);
+    const { chainId: currentNetworkChainId } = yield provider.getNetwork();
+    let chainId = currentNetworkChainId;
+    if (DEFAULT_NETWORK === Network.Local) {
+      /*
+       * Due to ganache internals shannanigans, when on the local ganache network
+       * we must use chainId 1, otherwise the broadcaster (and the underlying contracts)
+       * wont't be able to verify the signature (due to a chainId miss-match)
+       *
+       * This issue is only valid for ganache networks, as in production the chain id
+       * is returned properly
+       */
+      chainId = 1;
+    }
 
-    const metatransactionMessage = soliditySha3(
-      { t: 'uint256', v: currentNonce.toString() },
-      { t: 'address', v: colonyClient.address },
-      { t: 'uint256', v: 1337 },
-      { t: 'bytes', v: encodedTransaction },
+    const metatransactionMessage = hexSequenceNormalizer(
+      soliditySha3(
+        { t: 'uint256', v: currentNonce.toString() },
+        { t: 'address', v: colonyClient.address },
+        { t: 'uint256', v: chainId },
+        { t: 'bytes', v: encodedTransaction },
+      ) as string,
+      false,
     );
 
-    console.log('message', metatransactionMessage);
+    // eslint-disable-next-line no-console
+    console.log('Transaction message', metatransactionMessage);
+
+    const metatransactionMessageBuffer = Buffer.from(
+      metatransactionMessage,
+      'hex',
+    );
+
+    const convertedBufferMetatransactionMessage = Array.from(
+      metatransactionMessageBuffer,
+    );
+
+    // eslint-disable-next-line no-console
+    console.log(
+      'Actual signature converted Buffer',
+      convertedBufferMetatransactionMessage,
+    );
+
+    const metatransactionSignature = yield signMessage(
+      'generic',
+      convertedBufferMetatransactionMessage,
+    );
+
+    // eslint-disable-next-line no-console
+    console.log('Signature', metatransactionSignature);
+
+    const { r, s, v } = splitSignature(metatransactionSignature);
+
+    const broadcastData = JSON.stringify({
+      target: colonyClient.address,
+      payload: `encodedTransaction`,
+      userAddress,
+      r,
+      s,
+      v,
+    });
+
+    // eslint-disable-next-line no-console
+    console.log('Broadcast data', broadcastData);
+
+    const response = yield fetch('http://127.0.0.1:3004/broadcast', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: broadcastData,
+    });
+    const responseData = yield response.json();
+
+    // eslint-disable-next-line no-console
+    console.log('Response data', responseData);
+
+    if (responseData.status !== 'success') {
+      yield putError(
+        ActionTypes.META_MINT_TOKENS_ERROR,
+        new Error(responseData.message.reason),
+        meta,
+      );
+      return;
+    }
 
     yield put<AllActions>({
       type: ActionTypes.META_MINT_TOKENS_SUCCESS,
