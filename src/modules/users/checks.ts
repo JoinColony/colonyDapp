4f2dad65a2ea42c1a334e7f8fd6b3a3b186773f6
Raul Glogovetan <raul@glogovetan.com>
2021-10-28 21:20:40 +0300

    Add: `canUseMetatransactions` user module check

--- src/modules/users/checks.ts
+++ src/modules/users/checks.ts
@@ -1,4 +1,7 @@
-import { ColonyRole } from '@colony/colony-js';
+import { ColonyRole, Network } from '@colony/colony-js';
+
+import { DEFAULT_NETWORK, ETHEREUM_NETWORK } from '~constants';
+import { isDev } from '~utils/debug';
 
 export const userHasRole = (userRoles: ColonyRole[], role: ColonyRole) =>
   userRoles.includes(role);
@@ -17,3 +20,12 @@ export const hasRoot = (userRoles: ColonyRole[]) =>
 
 export const canArchitect = (userRoles: ColonyRole[]) =>
   userHasRole(userRoles, ColonyRole.Architecture);
+
+export const canUseMetatransactions = (userNetworkId: number): boolean => {
+  const isDeployedToUnsupportedNetwork =
+    DEFAULT_NETWORK === Network.Xdai ||
+    DEFAULT_NETWORK === Network.XdaiFork ||
+    (isDev && DEFAULT_NETWORK === Network.Local);
+  const isUserWalletOnMainenet = userNetworkId === ETHEREUM_NETWORK.chainId;
+  return isDeployedToUnsupportedNetwork && isUserWalletOnMainenet;
+};
