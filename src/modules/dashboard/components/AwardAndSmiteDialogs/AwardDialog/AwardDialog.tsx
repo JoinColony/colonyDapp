00a3956fed50c03a52421f3f1d1bf113ced91e7a
ArmandoGraterol <armandogrtrl@gmail.com>
2021-11-08 19:48:00 -0300

    Moved manage reputation form msgs to ManageReputationDialogForm

--- src/modules/dashboard/components/AwardAndSmiteDialogs/AwardDialog/AwardDialog.tsx
+++ src/modules/dashboard/components/AwardAndSmiteDialogs/AwardDialog/AwardDialog.tsx
@@ -1,48 +1,12 @@
 import React from 'react';
-import { defineMessages } from 'react-intl';
 
 import { AwardAndSmiteDialogProps } from '../types';
 import ManageReputationContainer from '../ManageReputationContainer';
 
 const displayName = 'dashboard.AwardDialog';
 
-const MSG = defineMessages({
-  title: {
-    id: 'dashboard.AwardDialog.title',
-    defaultMessage: 'Award',
-  },
-  team: {
-    id: 'dashboard.AwardDialog.team',
-    defaultMessage: 'Team in which Reputation should be awarded',
-  },
-  recipient: {
-    id: 'dashboard.AwardDialog.recipient',
-    defaultMessage: 'Recipient',
-  },
-  amount: {
-    id: 'dashboard.AwardDialog.amount',
-    defaultMessage: 'Amount of reputation points to award',
-  },
-  annotation: {
-    id: 'dashboard.AwardDialog.annotation',
-    defaultMessage: "Explain why you're awarding the user (optional)",
-  },
-  userPickerPlaceholder: {
-    id: 'dashboard.AwardDialog.userPickerPlaceholder',
-    defaultMessage: 'Search for a user or paste wallet address',
-  },
-  noPermission: {
-    id: 'dashboard.AwardDialog.noPermission',
-    defaultMessage: `You need the {roleRequired} permission in {domain} to take this action.`,
-  },
-  maxReputation: {
-    id: 'dashboard.AwardDialog.maxReputation',
-    defaultMessage: '{userReputationAmount} pts ({userPercentageReputation}%)',
-  },
-});
-
 const AwardDialog = (props: AwardAndSmiteDialogProps) => (
-  <ManageReputationContainer {...props} formMSG={MSG} />
+  <ManageReputationContainer {...props} />
 );
 
 AwardDialog.displayName = displayName;
--- src/modules/dashboard/components/AwardAndSmiteDialogs/SmiteDialog/SmiteDialog.tsx
+++ src/modules/dashboard/components/AwardAndSmiteDialogs/SmiteDialog/SmiteDialog.tsx
@@ -1,50 +1,12 @@
 import React from 'react';
-import { defineMessages } from 'react-intl';
 
 import { AwardAndSmiteDialogProps } from '../types';
 import ManageReputationContainer from '../ManageReputationContainer';
 
-
-const MSG = defineMessages({
-  title: {
-    id: 'dashboard.SmiteDialog.title',
-    defaultMessage: 'Smite',
-  },
-  team: {
-    id: 'dashboard.SmiteDialog.team',
-    defaultMessage: 'Team in which Reputation should be deducted',
-  },
-  recipient: {
-    id: 'dashboard.SmiteDialog.recipient',
-    defaultMessage: 'Recipient',
-  },
-  amount: {
-    id: 'dashboard.SmiteDialog.amount',
-    defaultMessage: 'Amount of reputation points to deduct',
-  },
-  annotation: {
-    id: 'dashboard.SmiteDialog.annotation',
-    defaultMessage: "Explain why you're smiting the user (optional)",
-  },
-  userPickerPlaceholder: {
-    id: 'dashboard.SmiteDialog.userPickerPlaceholder',
-    defaultMessage: 'Search for a user or paste wallet address',
-  },
-  noPermission: {
-    id: 'dashboard.SmiteDialog.noPermission',
-    defaultMessage: `You need the {roleRequired} permission in {domain} to take this action.`,
-  },
-  maxReputation: {
-    id: 'dashboard.SmiteDialog.maxReputation',
-    defaultMessage:
-      'max: {userReputationAmount} pts ({userPercentageReputation}%)',
-  },
-});
-
 const displayName = 'dashboard.SmiteDialog';
 
 const SmiteDialog = (props: AwardAndSmiteDialogProps) => (
-  <ManageReputationContainer {...props} isSmitingReputation formMSG={MSG} />
+  <ManageReputationContainer {...props} isSmiteAction />
 );
 
 SmiteDialog.displayName = displayName;
