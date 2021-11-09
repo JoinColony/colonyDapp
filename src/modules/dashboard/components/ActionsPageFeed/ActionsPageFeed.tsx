e4876c1e039e62c69ed9d52d67613be7ef1818c0
ArmandoGraterol <armandogrtrl@gmail.com>
2021-11-09 18:54:28 -0300

    Adapted DefaultAction to include award action

--- src/modules/dashboard/components/ActionsPageFeed/ActionsPageFeed.tsx
+++ src/modules/dashboard/components/ActionsPageFeed/ActionsPageFeed.tsx
@@ -68,7 +68,8 @@ export interface EventValues {
   voter?: Address;
   motionTag?: ReactElement;
   objectionTag?: ReactElement;
-  reputationPenalty?: string;
+  reputationChange?: string;
+  isSmiteAction?: boolean;
 }
 
 export type FeedItemWithId<T> = T & { uniqueId: string };
