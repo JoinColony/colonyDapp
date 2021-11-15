487a54bbfd4cdec785dd8700637955e8feaca6b9
jayaFeola <34057551+chinins@users.noreply.github.com>
2021-11-15 21:13:23 +0100

    Refactor: remove unnecessary css classes

--- src/modules/dashboard/components/ColonyMembers/ColonyMembers.css.d.ts
+++ src/modules/dashboard/components/ColonyMembers/ColonyMembers.css.d.ts
@@ -2,6 +2,5 @@ export const main: string;
 export const mainContentGrid: string;
 export const mainContent: string;
 export const rightAside: string;
-export const actionButtons: string;
 export const loadingWrapper: string;
 export const controls: string;
