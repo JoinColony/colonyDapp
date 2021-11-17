8cf914ce92c2c8827be3bbafb884b245ccffec2f
Agusti Fernandez Pardo <agusti@colony.io>
2021-11-17 16:25:45 +0100

    Use react-intl in title too

--- src/modules/core/components/BannedTag/BannedTag.tsx
+++ src/modules/core/components/BannedTag/BannedTag.tsx
@@ -1,5 +1,5 @@
 import React, { HTMLAttributes } from 'react';
-import { MessageDescriptor } from 'react-intl';
+import { FormattedMessage, MessageDescriptor } from 'react-intl';
 
 import Icon from '~core/Icon';
 import Tag from '~core/Tag';
@@ -10,18 +10,31 @@ const displayName = 'BannedTag';
 
 interface Props extends HTMLAttributes<HTMLSpanElement> {
   text?: MessageDescriptor | string;
+  textValues?: { [key: string]: string };
 }
 
-const BannedTag = ({ text = 'Banned' }: Props) => {
+const BannedTag = ({ children, text, textValues, ...rest }: Props) => {
   return (
     <Tag appearance={{ fontSize: 'small', theme: 'banned', margin: 'none' }}>
       <Icon
-        title="banned"
+        title={{ id: 'label.banned' }}
         name="emoji-goblin"
         appearance={{ size: 'normal' }}
         className={styles.icon}
       />
-      <div className={styles.textSyles}>{text}</div>
+      <div className={styles.textSyles} {...rest}>
+        {text ? (
+          <>
+            {typeof text === 'string' ? (
+              text
+            ) : (
+              <FormattedMessage {...text} values={textValues} />
+            )}
+          </>
+        ) : (
+          children
+        )}
+      </div>
     </Tag>
   );
 };
