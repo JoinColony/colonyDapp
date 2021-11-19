201c4d81740d8a68cba31817d4d2a18949b8f558
Raul Glogovetan <raul@glogovetan.com>
2021-11-20 01:46:00 +0200

    Refactor: whitelist saga folder structure

--- src/modules/dashboard/sagas/whitelist/index.ts
+++ src/modules/dashboard/sagas/whitelist/index.ts
@@ -1,7 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 
 import signAgreement from './signAgreement';
+import updateWhitelistSaga from './updateWhitelist';
 
 export default function* whitelistSagas() {
-  yield all([call(signAgreement)]);
+  yield all([call(signAgreement), call(updateWhitelistSaga)]);
 }
