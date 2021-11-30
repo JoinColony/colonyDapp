c4ff5c8da2735a73621a415dff9575195d894da8
ArmandoGraterol <armandogrtrl@gmail.com>
2021-11-30 16:49:53 -0300

    Added UnwrapTokensPage and ClaimTokensPage component

---
+++ src/modules/dashboard/components/ClaimTokensPage/ClaimTokensPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const displayName = 'dashboard.ClaimTokensPage';
+
+const ClaimTokensPage = () => {
+  // @TODO: Add actual content for the claim tokens page
+  return <span>Claim tokens now!! Limited time offer</span>;
+};
+
+ClaimTokensPage.displayName = displayName;
+
+export default ClaimTokensPage;
