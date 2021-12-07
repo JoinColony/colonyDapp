17d8a43d748fdf3c45e12a119eb727cae948fa1a
Raul Glogovetan <raul@glogovetan.com>
2021-12-07 17:52:13 +0200

    Fix: `AvatarDropdown` if no colony loaded

--- src/modules/users/components/AvatarDropdown/AvatarDropdownPopover.tsx
+++ src/modules/users/components/AvatarDropdown/AvatarDropdownPopover.tsx
@@ -80,7 +80,7 @@ const AvatarDropdownPopover = ({
             <NavLink
               to={{
                 pathname: CREATE_USER_ROUTE,
-                state: { colonyURL: `/colony/${colony.colonyName}` },
+                state: { colonyURL: `/colony/${colony?.colonyName}` },
               }}
               text={MSG.buttonGetStarted}
             />
