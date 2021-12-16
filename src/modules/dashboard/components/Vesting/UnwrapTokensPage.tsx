94ef402978e567f9b9b8660f470fc388732a861d
Raul Glogovetan <raul@glogovetan.com>
2021-12-16 12:39:16 +0200

    Add: `UnwrapTokensPage` load colony data

--- src/modules/dashboard/components/Vesting/UnwrapTokensPage.tsx
+++ src/modules/dashboard/components/Vesting/UnwrapTokensPage.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { FormikProps } from 'formik';
 import { defineMessage, FormattedMessage } from 'react-intl';
+import { Redirect, RouteChildrenProps, useParams } from 'react-router-dom';
 
 import { ActionForm } from '~core/Fields';
 import Heading from '~core/Heading';
+
+import LoadingTemplate from '~pages/LoadingTemplate';
+
+import { useColonyFromNameQuery, useMetaColonyQuery } from '~data/index';
 import { ActionTypes } from '~redux/actionTypes';
+import { NOT_FOUND_ROUTE } from '~routes/index';
 
 import VestingPageLayout from './VestingPageLayout';
+
+import styles from './Vesting.css';
 
 const MSG = defineMessage({
   title: {
@@ -25,11 +33,37 @@ const MSG = defineMessage({
     id: 'dashboard.Vesting.UnwrapTokensPage.buttonUnwrap',
     defaultMessage: 'Unwrap {tokenSymbol}',
   },
+  loadingText: {
+    id: 'dashboard.Vesting.ClaimTokensPage.loadingText',
+    defaultMessage: 'Loading Colony',
+  },
 });
 
 const displayName = 'dashboard.Vesting.UnwrapTokensPage';
 
-const UnwrapTokensPage = () => {
+type Props = RouteChildrenProps<{ colonyName: string }>;
+
+const UnwrapTokensPage = ({ match }: Props) => {
+  if (!match) {
+    throw new Error(
+      `No match found for route in ${displayName} Please check route setup.`,
+    );
+  }
+
+  const { colonyName } = useParams<{
+    colonyName: string;
+  }>();
+
+  const { data, error, loading } = useColonyFromNameQuery({
+    // We have to define an empty address here for type safety, will be replaced by the query
+    variables: { name: colonyName, address: '' },
+    pollInterval: 5000,
+  });
+
+  if (error) console.error(error);
+
+  const { data: metaColonyData } = useMetaColonyQuery();
+
   // replace with a query later
   const token = {
     symbol: 'CLNY',
@@ -37,6 +71,37 @@ const UnwrapTokensPage = () => {
     balance: '300000000000000000000',
     decimals: 18,
   };
+
+  if (
+    loading ||
+    (data?.processedColony && data.processedColony.colonyName !== colonyName) ||
+    (data?.colonyAddress &&
+      !data?.processedColony &&
+      !((data.colonyAddress as any) instanceof Error))
+  ) {
+    return (
+      <div className={styles.loadingWrapper}>
+        <LoadingTemplate loadingText={MSG.loadingText} />
+      </div>
+    );
+  }
+
+  if (
+    !colonyName ||
+    error ||
+    !data?.processedColony ||
+    (data?.colonyAddress as any) instanceof Error
+  ) {
+    return <Redirect to={NOT_FOUND_ROUTE} />;
+  }
+
+  if (
+    !process.env.META_WRAPPED_TOKEN_ADDRESS ||
+    metaColonyData?.processedMetaColony?.colonyAddress !==
+      data?.processedColony?.colonyAddress
+  ) {
+    return <Redirect to={`/colony/${colonyName}`} />;
+  }
 
   return (
     <ActionForm
