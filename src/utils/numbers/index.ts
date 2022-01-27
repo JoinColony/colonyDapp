ebd651cb97aec59c28816f515390f04dd94a2b21
danbr <danatcern@hotmail.com>
2022-01-27 16:43:16 -0600

    refactor: replace number formatter

--- src/utils/numbers/index.ts
+++ src/utils/numbers/index.ts
@@ -1,6 +1,6 @@
 import { BigNumberish, bigNumberify } from 'ethers/utils';
 
-export { formatTokenValue } from './formatTokenValue';
+export { stdNumberFormatter } from './stdNumberFormatter';
 
 /**
  * Return whether `a` is less than `b` where each are number-like values.
