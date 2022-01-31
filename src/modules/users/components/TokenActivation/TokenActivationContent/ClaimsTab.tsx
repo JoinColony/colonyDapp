8923cd5bae9907f80246cbbe2d2995fcd8b31c5a
arrenv <33682027+arrenv@users.noreply.github.com>
2022-01-31 13:21:03 +0100

    Add: TokenActivation tab for claiming stakes

--- src/modules/users/components/TokenActivation/TokenActivationContent/ClaimsTab.tsx
+++ src/modules/users/components/TokenActivation/TokenActivationContent/ClaimsTab.tsx
@@ -6,14 +6,16 @@ import styles from './TokenActivationContent.css';
 const MSG = defineMessages({
   noClaims: {
     id: 'users.TokenActivation.TokenActivationContent.ClaimsTab.noClaims',
-    defaultMessage: 'There are currently no claims',
+    defaultMessage: 'There are no stakes to claim.',
   },
 });
 
 const ClaimsTab = () => {
   return (
     <div className={styles.claimsContainer}>
-      <FormattedMessage {...MSG.noClaims} />
+      <div className={styles.noClaims}>
+        <FormattedMessage {...MSG.noClaims} />
+      </div>
     </div>
   );
 };
