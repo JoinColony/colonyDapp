5a8498cc75b87beb473f8ada64377229e07dcc60
Alicja Kujawa <alicjak94.ak@gmail.com>
2022-02-11 14:39:02 +0100

    Add ManageWhitelist modal

---
+++ src/modules/dashboard/components/Dialogs/ManageWhitelistDialog/ManageWhitelistDialogForm.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { FormattedMessage, defineMessages } from 'react-intl';
+import { ColonyRole } from '@colony/colony-js';
+import { FormikProps } from 'formik';
+
+import Button from '~core/Button';
+import DialogSection from '~core/Dialog/DialogSection';
+import { Annotations } from '~core/Fields';
+import Heading from '~core/Heading';
+import PermissionsLabel from '~core/PermissionsLabel';
+import PermissionRequiredInfo from '~core/PermissionRequiredInfo';
+
+import { useLoggedInUser, Colony } from '~data/index';
+import { useTransformer } from '~utils/hooks';
+import { getAllUserRoles } from '~modules/transformers';
+import { hasRoot } from '~modules/users/checks';
+
+import { FormValues } from './ManageWhitelistDialog';
+import styles from './ManageWhitelistDialogForm.css';
+
+const MSG = defineMessages({
+  title: {
+    id: 'dashboard.ManageWhitelistDialog.ManageWhitelistDialogForm.title',
+    defaultMessage: 'Manage whitelist',
+  },
+  annotation: {
+    id: `dashboard.ManageWhitelistDialog.ManageWhitelistDialogForm.annotation`,
+    defaultMessage: 'Explain why you’re making these changes (optional)',
+  },
+  noPermission: {
+    id:
+      'dashboard.ManageWhitelistDialog.ManageWhitelistDialogForm.noPermission',
+    defaultMessage: `You do not have the {roleRequired} permission required
+      to take this action.`,
+  },
+});
+
+interface Props {
+  back: () => void;
+  colony: Colony;
+}
+
+const ManageWhitelistDialogForm = ({
+  back,
+  colony,
+}: Props & FormikProps<FormValues>) => {
+  const { walletAddress, username, ethereal } = useLoggedInUser();
+
+  const allUserRoles = useTransformer(getAllUserRoles, [colony, walletAddress]);
+
+  const hasRegisteredProfile = !!username && !ethereal;
+
+  const userHasPermission = hasRegisteredProfile && hasRoot(allUserRoles);
+
+  return (
+    <>
+      <DialogSection appearance={{ theme: 'heading' }}>
+        <Heading
+          appearance={{ size: 'medium', margin: 'none', theme: 'dark' }}
+          text={MSG.title}
+        />
+      </DialogSection>
+      {!userHasPermission && (
+        <DialogSection>
+          <PermissionRequiredInfo requiredRoles={[ColonyRole.Root]} />
+        </DialogSection>
+      )}
+      <DialogSection>
+        <Annotations
+          label={MSG.annotation}
+          name="annotation"
+          disabled={!userHasPermission}
+        />
+      </DialogSection>
+      {!userHasPermission && (
+        <DialogSection appearance={{ theme: 'sidePadding' }}>
+          <div className={styles.noPermissionMessage}>
+            <FormattedMessage
+              {...MSG.noPermission}
+              values={{
+                roleRequired: (
+                  <PermissionsLabel
+                    permission={ColonyRole.Root}
+                    name={{
+                      id: `role.${ColonyRole.Root}`,
+                    }}
+                  />
+                ),
+              }}
+            />
+          </div>
+        </DialogSection>
+      )}
+      <DialogSection appearance={{ align: 'right', theme: 'footer' }}>
+        <Button
+          appearance={{ theme: 'secondary', size: 'large' }}
+          onClick={back}
+          text={{ id: 'button.back' }}
+        />
+        <Button
+          appearance={{ theme: 'primary', size: 'large' }}
+          text={{ id: 'button.confirm' }}
+        />
+      </DialogSection>
+    </>
+  );
+};
+
+export default ManageWhitelistDialogForm;
