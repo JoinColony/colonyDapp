75e1b80cc92b9d855a356ebd3872b4b8d4e3ba60
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-02-17 19:18:59 +0300

    Feat: add custom cypress commands

---
+++ cypress/support/commands.js
@@ -0,0 +1,41 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/landing')
+    .contains(/connect wallet/i)
+    .click();
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.contains(/ganache/i)
+    .click()
+    .get('.Button_themePrimary_3aiuKGRF')
+    .click()
+    .wait(3000);
+});
+
+Cypress.Commands.add('assertHome', () => {
+  cy.url().should('eq', `${Cypress.config().baseUrl}/landing`);
+});
