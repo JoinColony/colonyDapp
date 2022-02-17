34096aaf895bed1c141a51002b0be45ca4bffbd1
danbr <danatcern@hotmail.com>
2022-02-17 20:31:00 +0700

    feat: update jsx & css in `StakesTab`

--- src/modules/users/components/TokenActivation/StakesTab/StakesTab.css.d.ts
+++ src/modules/users/components/TokenActivation/StakesTab/StakesTab.css.d.ts
@@ -1,6 +1,5 @@
 export const stakesContainer: string;
 export const mainDivider: string;
-export const claimStakesContainer: string;
 export const stakesListItem: string;
 export const link: string;
 export const noClaims: string;
