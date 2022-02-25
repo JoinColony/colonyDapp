f719e96b68628e5acc0fe9b698392d2f13cc7d34
Alicja Kujawa <alicjak94.ak@gmail.com>
2022-02-25 12:22:30 +0100

    Add additional method to checkbox

--- src/modules/core/components/Fields/Checkbox/Checkbox.tsx
+++ src/modules/core/components/Fields/Checkbox/Checkbox.tsx
@@ -4,6 +4,7 @@ import React, {
   useState,
   useCallback,
   useMemo,
+  useEffect,
 } from 'react';
 import { MessageDescriptor } from 'react-intl';
 import { nanoid } from 'nanoid';
@@ -45,6 +46,8 @@ interface Props {
   name: string;
   /** Standard input field property */
   onChange?: Function;
+  /** Just to check what is a default value of checkbox */
+  getDefaultValue?: Function;
   /** Input field value */
   value: string;
   /**  Text for the checkbox tooltip */
@@ -79,6 +82,7 @@ const Checkbox = ({
   value,
   tooltipText,
   tooltipPopperProps,
+  getDefaultValue,
 }: Props) => {
   const [inputId] = useState<string>(nanoid());
 
@@ -91,7 +95,7 @@ const Checkbox = ({
         push(value);
       }
       if (onChange) {
-        onChange(e);
+        onChange({...e, isChecked: !(idx >= 0)});
       }
     },
     [name, onChange, push, remove, value, values],
@@ -124,6 +128,12 @@ const Checkbox = ({
     ),
     [disabled, handleOnChange, inputId, isChecked, name],
   );
+
+  useEffect(() => {
+    if (getDefaultValue) {
+      getDefaultValue(isChecked);
+    }
+  }, []);
 
   return (
     <label className={classNames} htmlFor={elementOnly ? inputId : undefined}>
