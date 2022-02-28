d9e4f1f5b313f33d024fe8974218998ed69588cc
Alicja Kujawa <alicjak94.ak@gmail.com>
2022-02-28 15:35:54 +0100

    Add search input

--- src/modules/dashboard/components/Dialogs/ManageWhitelistDialog/WhitelistedAddresses/WhitelistedAddresses.tsx
+++ src/modules/dashboard/components/Dialogs/ManageWhitelistDialog/WhitelistedAddresses/WhitelistedAddresses.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
 
 import UserCheckbox from '~core/UserCheckbox';
 import { Colony } from '~data/index';
 import { Address } from '~types/index';
+import Icon from '~core/Icon';
 
 import styles from './WhitelistedAddresses.css';
 
@@ -11,14 +13,33 @@ interface Props {
   whitelistedAddresses: Address[];
 }
 
+const MSG = defineMessages({
+  search: {
+    id: 'dashboard.ManageWhitelistDialog.WhitelistedAddresses.search',
+    defaultMessage: 'Search...',
+  },
+});
+
 const displayName = 'dashboard.ManageWhitelistDialog.WhitelistedAddresses';
 
 const WhitelistedAddresses = ({ colony, whitelistedAddresses }: Props) => {
-  const [addresses] = useState<Address[]>(
-    whitelistedAddresses,
-  );
+  const [addresses] = useState<Address[]>(whitelistedAddresses);
+  const { formatMessage } = useIntl();
+
+  const onChange = () => {
+    // @TODO, implement searching
+  };
   return (
     <div className={styles.main}>
+      <div className={styles.searchContainer}>
+        <input
+          name="warning"
+          className={styles.input}
+          onChange={onChange}
+          placeholder={formatMessage(MSG.search)}
+        />
+        <Icon className={styles.icon} name="search" title={MSG.search} />
+      </div>
       <div className={styles.container}>
         {(addresses || []).map((address) => {
           return (
