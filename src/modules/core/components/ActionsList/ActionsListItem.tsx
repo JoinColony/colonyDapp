bbe62116737afe3e649f8869c4cc523ee89ef261
ArmandoGraterol <armandogrtrl@gmail.com>
2022-03-03 13:14:51 -0300

    Removed dynamic dataCy from Comment as they may be unnecesary

--- src/modules/core/components/ActionsList/ActionsListItem.tsx
+++ src/modules/core/components/ActionsList/ActionsListItem.tsx
@@ -76,7 +76,6 @@ interface Props {
   item: FormattedAction;
   colony: Colony;
   handleOnClick?: (handlerProps: ClickHandlerProps) => void;
-  dataCy: string;
 }
 
 const ActionsListItem = ({
@@ -107,7 +106,6 @@ const ActionsListItem = ({
   },
   colony,
   handleOnClick,
-  dataCy,
 }: Props) => {
   const { formatMessage, formatNumber } = useIntl();
   const { data: metadataJSON } = useDataFetcher(
@@ -204,7 +202,7 @@ const ActionsListItem = ({
 
   const stopPropagation = (event) => event.stopPropagation();
   return (
-    <li data-cy={dataCy}>
+    <li data-cy="action-item">
       <div
         /*
          * @NOTE This is non-interactive element to appease the DOM Nesting Validator
