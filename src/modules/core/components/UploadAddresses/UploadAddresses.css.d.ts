519663f09d8956fdeb67775711f45ecc796fe328
danbr <danatcern@hotmail.com>
2022-03-07 08:18:10 +0700

    refactor:remove unused selector

--- src/modules/core/components/UploadAddresses/UploadAddresses.css.d.ts
+++ src/modules/core/components/UploadAddresses/UploadAddresses.css.d.ts
@@ -3,5 +3,4 @@ export const inputContainer: string;
 export const actionsContainer: string;
 export const actionsSubContainer: string;
 export const buttonsContainer: string;
-export const agreeemntButton: string;
 export const uploaderContainer: string;
