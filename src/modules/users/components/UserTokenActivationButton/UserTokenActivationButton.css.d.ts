98121a351006ae3241203c75f595b01d510c15c4
danbr <danatcern@hotmail.com>
2022-03-15 18:08:14 +0700

    feat:add tooltip to `TokenActivationButton`

--- src/modules/users/components/UserTokenActivationButton/UserTokenActivationButton.css.d.ts
+++ src/modules/users/components/UserTokenActivationButton/UserTokenActivationButton.css.d.ts
@@ -2,3 +2,4 @@ export const tokens: string;
 export const tokensNumber: string;
 export const dot: string;
 export const dotInactive: string;
+export const tooltip: string;
--- src/modules/users/components/UserTokenActivationButton/UserTokenActivationButton.tsx
+++ src/modules/users/components/UserTokenActivationButton/UserTokenActivationButton.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
 import { bigNumberify } from 'ethers/utils';
 
 import { TokenActivationPopover } from '~users/TokenActivation';
-
 import { getFormattedTokenValue } from '~utils/tokens';
+import { Tooltip } from '~core/Popover';
 import Numeral from '~core/Numeral';
-
 import { FullColonyFragment, UserLock, UserToken } from '~data/index';
 import { Address } from '~types/index';
 
 import styles from './UserTokenActivationButton.css';
 
 const displayName = 'users.UserTokenActivationButton';
+
+const MSG = defineMessages({
+  tooltip: {
+    id: 'pages.NavigationWrapper.UserNavigation.tooltip',
+    defaultMessage:
+      'View and activate tokens for staking or claim any unclaimed stakes.',
+  },
+});
 
 interface Props {
   userLock: UserLock;
@@ -51,7 +59,7 @@ const UserTokenActivationButton = ({
       walletAddress={walletAddress}
       isPendingBalanceZero={isPendingBalanceZero}
     >
-      {({ toggle, ref }) => (
+      {({ isOpen, toggle, ref }) => (
         <>
           <button
             type="button"
@@ -59,17 +67,30 @@ const UserTokenActivationButton = ({
             onClick={toggle}
             ref={ref}
           >
-            <span
-              className={`${styles.dot} ${
-                (inactiveBalance.gt(0) || totalBalance.isZero()) &&
-                styles.dotInactive
-              }`}
-            />
-            <Numeral
-              className={styles.tokensNumber}
-              suffix={` ${nativeToken?.symbol} `}
-              value={formattedTotalBalance}
-            />
+            <Tooltip
+              appearance={{ theme: 'dark', size: 'medium' }}
+              placement="bottom"
+              trigger={!isOpen ? 'hover' : 'disabled'}
+              showArrow
+              content={
+                <div className={styles.tooltip}>
+                  <FormattedMessage {...MSG.tooltip} />
+                </div>
+              }
+            >
+              <div>
+                <span
+                  className={`${styles.dot} ${
+                    (inactiveBalance.gt(0) || totalBalance.isZero()) &&
+                    styles.dotInactive
+                  }`}
+                />
+                <Numeral
+                  suffix={` ${nativeToken?.symbol} `}
+                  value={formattedTotalBalance}
+                />
+              </div>
+            </Tooltip>
           </button>
         </>
       )}
