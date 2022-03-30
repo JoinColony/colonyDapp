7cd57fb98342a015ead96f2f5280f948e961ebff
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-03-30 19:52:38 +0300

    Feat: add `makePayment`, `createTeam`, `updateTeam` motion tests

--- cypress/e2e/3-actions.js
+++ cypress/e2e/3-actions.js
@@ -1,8 +1,6 @@
-import Decimal from 'decimal.js';
 import { bigNumberify } from 'ethers/utils';
 
 import ganacheAccounts from '~lib/colonyNetwork/ganache-accounts.json';
-import { splitAddress } from '~utils/strings';
 import { createAddress } from '~utils/web3';
 
 describe('User can create actions via UAC', () => {
@@ -26,92 +24,31 @@ describe('User can create actions via UAC', () => {
 
   it('Can make payment', () => {
     const amountToPay = 10;
-    const annotationText = 'Test annotation';
     const accounts = Object.entries(ganacheAccounts.private_keys).map(
       ([address]) => address,
     );
-    const cutAddress = splitAddress(createAddress(accounts[1]));
-    const paidAmount = new Decimal(amountToPay).sub(
-      new Decimal(1.0001).div(100).mul(amountToPay),
-    );
-    let prevColonyFunds;
 
-    cy.login();
-
-    cy.visit(`/colony/${Cypress.config().colony.name}`);
-
-    cy.getBySel('colonyTotalFunds', { timeout: 30000 }).then(($text) => {
-      const amount = $text.text().split(',').join('');
-      prevColonyFunds = amount;
-    });
-
-    cy.getBySel('newActionButton', { timeout: 60000 }).click();
-    cy.getBySel('expenditureDialogIndexItem').click();
-    cy.getBySel('paymentDialogIndexItem').click();
-
-    cy.getBySel('paymentRecipientPicker').click().type(accounts[1]);
-    cy.getBySel('paymentRecipientItem').first().click();
-
-    cy.getBySel('paymentAmountInput').click().type(amountToPay);
-
-    cy.getBySel('paymentAnnotation').click().type(annotationText);
-    cy.getBySel('paymentConfirmButton').click();
-
-    cy.getBySel('actionHeading', { timeout: 70000 }).should(
-      'have.text',
-      `Pay ${cutAddress.header}${cutAddress.start}...${
-        cutAddress.end
-      } ${paidAmount.toString()} ${Cypress.config().colony.nativeToken}`,
-    );
-
-    cy.url().should(
-      'contains',
-      `${Cypress.config().baseUrl}/colony/${
-        Cypress.config().colony.name
-      }/tx/0x`,
-    );
-
-    cy.getBySel('comment').should('have.text', annotationText);
+    cy.makePayment(amountToPay, createAddress(accounts[1]), true);
 
     cy.getBySel('backButton').click();
 
-    cy.getBySel('colonyTotalFunds', { timeout: 15000 }).then(($text) => {
-      const amount = $text.text().split(',').join('');
-      expect(amount).to.eq((prevColonyFunds - amountToPay).toString());
+    cy.get('@totalFunds').then(($totalFunds) => {
+      const totalFunds = bigNumberify($totalFunds.split(',').join(''))
+        .sub(amountToPay)
+        .toString();
+
+      cy.getBySel('colonyTotalFunds', { timeout: 15000 }).then(($text) => {
+        const text = $text.text().split(',').join('');
+        expect(text).to.eq(totalFunds);
+      });
     });
   });
+
   it('Can create teams', () => {
-    const annotationText = 'Test annotation';
     const domainName = 'Cats';
     const domainPurpose = 'Only cats allowed';
 
-    cy.login();
-
-    cy.visit(`/colony/${Cypress.config().colony.name}`);
-
-    cy.getBySel('newActionButton', { timeout: 60000 }).click();
-    cy.getBySel('domainsDialogIndexItem').click();
-    cy.getBySel('createDomainDialogIndexItem').click();
-
-    cy.getBySel('domainNameInput').click().type(domainName);
-    cy.getBySel('domainPurposeInput').click().type(domainPurpose);
-    cy.getBySel('createDomainAnnotation').click().type(annotationText);
-
-    cy.getBySel('createDomainConfirmButton').click();
-
-    cy.getBySel('actionHeading', { timeout: 70000 }).should(
-      'have.text',
-      `New team: ${domainName}`,
-    );
-
-    cy.url().should(
-      'contains',
-      `${Cypress.config().baseUrl}/colony/${
-        Cypress.config().colony.name
-      }/tx/0x`,
-    );
-
-    cy.getBySel('comment').should('have.text', annotationText);
+    cy.createTeam(domainName, domainPurpose, false);
 
     cy.getBySel('backButton').click();
 
@@ -123,42 +60,12 @@ describe('User can create actions via UAC', () => {
       .last()
       .should('have.text', domainPurpose);
   });
+
   it('Can edit teams', () => {
-    const annotationText = 'Test annotation';
     const domainName = 'Dolphins';
     const domainPurpose = 'This team has been taken over by dolphins';
 
-    cy.login();
-
-    cy.visit(`/colony/${Cypress.config().colony.name}`);
-
-    cy.getBySel('newActionButton', { timeout: 90000 }).click();
-    cy.getBySel('domainsDialogIndexItem').click();
-    cy.getBySel('editDomainDialogIndexItem').click();
-
-    cy.getBySel('domainIdSelector').click();
-    cy.getBySel('domainIdItem').last().click();
-    cy.getBySel('domainNameInput').clear();
-    cy.getBySel('domainNameInput').click().type(domainName);
-    cy.getBySel('domainPurposeInput').clear();
-    cy.getBySel('domainPurposeInput').click().type(domainPurpose);
-    cy.getBySel('editDomainAnnotation').click().type(annotationText);
-
-    cy.getBySel('editDomainConfirmButton').click();
-
-    cy.getBySel('actionHeading', { timeout: 100000 }).should(
-      'have.text',
-      `${domainName} team details edited`,
-    );
-
-    cy.url().should(
-      'contains',
-      `${Cypress.config().baseUrl}/colony/${
-        Cypress.config().colony.name
-      }/tx/0x`,
-    );
-
-    cy.getBySel('comment').should('have.text', annotationText);
+    cy.updateTeam(domainName, domainPurpose, false);
 
     cy.getBySel('backButton').click();
 
@@ -170,6 +77,7 @@ describe('User can create actions via UAC', () => {
       .last()
       .should('have.text', domainPurpose);
   });
+
   it('Can award users', () => {
     const amountToAward = 10;
     const annotationText = 'You have been a good boy, time for your reward';
@@ -209,6 +117,7 @@ describe('User can create actions via UAC', () => {
 
     cy.getBySel('comment').should('have.text', annotationText);
   });
+
   it('Can smite users', () => {
     const amountToSmite = 10;
     const annotationText =
@@ -249,6 +158,7 @@ describe('User can create actions via UAC', () => {
 
     cy.getBySel('comment').should('have.text', annotationText);
   });
+
   it('Can transfer funds', () => {
     const amountToTransfer = 2;
     const annotationText = 'I want to transfer these funds just because';
