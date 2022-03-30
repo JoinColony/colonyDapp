296ad4210cd20fc8ba8c0c9959519be7cba083d7
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-03-30 19:52:38 +0300

    Feat: add `makePayment`, `createTeam`, `updateTeam` motion tests

--- cypress/e2e/8-motions.js
+++ cypress/e2e/8-motions.js
@@ -1,11 +1,9 @@
-import Decimal from 'decimal.js';
 import { Extension } from '@colony/colony-js';
 
 import ganacheAccounts from '~lib/colonyNetwork/ganache-accounts.json';
-import { splitAddress } from '~utils/strings';
 import { createAddress } from '~utils/web3';
 
-describe.only('User can create actions via UAC', () => {
+describe('User can create actions via UAC', () => {
   it('Installs & enables voting extensions', () => {
     cy.login();
     cy.visit(`/colony/${Cypress.config().colony.name}`);
@@ -20,7 +18,7 @@ describe.only('User can create actions via UAC', () => {
     cy.enableExtension(Extension.VotingReputation);
   });
 
-  it.only('Can mint native tokens', () => {
+  it('Can mint native tokens', () => {
     const amountToMint = 10;
     cy.mintTokens(amountToMint, true);
 
@@ -29,150 +27,32 @@ describe.only('User can create actions via UAC', () => {
 
   it('Can make payment', () => {
     const amountToPay = 10;
-    const annotationText = 'Test annotation';
     const accounts = Object.entries(ganacheAccounts.private_keys).map(
       ([address]) => address,
     );
-    const cutAddress = splitAddress(createAddress(accounts[1]));
-    const paidAmount = new Decimal(amountToPay).sub(
-      new Decimal(1.0001).div(100).mul(amountToPay),
-    );
-    let prevColonyFunds;
 
-    cy.login();
-
-    cy.visit(`/colony/${Cypress.config().colony.name}`);
-
-    cy.getBySel('colonyTotalFunds', { timeout: 30000 }).then(($text) => {
-      const amount = $text.text().split(',').join('');
-      prevColonyFunds = amount;
-    });
-
-    cy.getBySel('newActionButton', { timeout: 60000 }).click();
-    cy.getBySel('indexModalItem').eq(0).click();
-    cy.getBySel('indexModalItem').eq(0).click();
-
-    cy.getBySel('paymentRecipientPicker').click().type(accounts[1]);
-    cy.getBySel('paymentRecipientItem').first().click();
-
-    cy.getBySel('paymentAmountInput').click().type(amountToPay);
-
-    cy.getBySel('paymentAnnotation').click().type(annotationText);
-    cy.getBySel('paymentConfirmButton').click();
-
-    cy.getBySel('actionHeading', { timeout: 70000 }).should(
-      'have.text',
-      `Pay ${cutAddress.header}${cutAddress.start}...${
-        cutAddress.end
-      } ${paidAmount.toString()} ${Cypress.config().colony.nativeToken}`,
-    );
-
-    cy.url().should(
-      'contains',
-      `${Cypress.config().baseUrl}/colony/${
-        Cypress.config().colony.name
-      }/tx/0x`,
-    );
-
-    cy.getBySel('comment').should('have.text', annotationText);
-
-    cy.getBySel('backButton').click();
-
-    cy.getBySel('colonyTotalFunds', { timeout: 15000 }).then(($text) => {
-      const amount = $text.text().split(',').join('');
-      expect(amount).to.eq((prevColonyFunds - amountToPay).toString());
-    });
+    cy.makePayment(amountToPay, createAddress(accounts[1]), true);
+    cy.checkMotion();
   });
+
   it('Can create teams', () => {
-    const annotationText = 'Test annotation';
     const domainName = 'Cats';
     const domainPurpose = 'Only cats allowed';
 
-    cy.login();
+    cy.createTeam(domainName, domainPurpose, true);
 
-    cy.visit(`/colony/${Cypress.config().colony.name}`);
-
-    cy.getBySel('newActionButton', { timeout: 60000 }).click();
-    cy.getBySel('indexModalItem').eq(2).click();
-    cy.getBySel('indexModalItem').eq(0).click();
-
-    cy.getBySel('domainNameInput').click().type(domainName);
-    cy.getBySel('domainPurposeInput').click().type(domainPurpose);
-    cy.getBySel('createDomainAnnotation').click().type(annotationText);
-
-    cy.getBySel('createDomainConfirmButton').click();
-
-    cy.getBySel('actionHeading', { timeout: 70000 }).should(
-      'have.text',
-      `New team: ${domainName}`,
-    );
-
-    cy.url().should(
-      'contains',
-      `${Cypress.config().baseUrl}/colony/${
-        Cypress.config().colony.name
-      }/tx/0x`,
-    );
-
-    cy.getBySel('comment').should('have.text', annotationText);
-
-    cy.getBySel('backButton').click();
-
-    cy.getBySel('colonyDomainSelector', { timeout: 15000 }).click();
-    cy.getBySel('domainDropdownItemName')
-      .last()
-      .should('have.text', domainName);
-    cy.getBySel('domainDropdownItemPurpose')
-      .last()
-      .should('have.text', domainPurpose);
+    cy.checkMotion();
   });
-  it('Can edit teams', () => {
-    const annotationText = 'Test annotation';
+
+  it.only('Can edit teams', () => {
     const domainName = 'Dolphins';
     const domainPurpose = 'This team has been taken over by dolphins';
 
-    cy.login();
+    cy.updateTeam(domainName, domainPurpose, true);
 
-    cy.visit(`/colony/${Cypress.config().colony.name}`);
-
-    cy.getBySel('newActionButton', { timeout: 90000 }).click();
-    cy.getBySel('indexModalItem').eq(2).click();
-    cy.getBySel('indexModalItem').eq(1).click();
-
-    cy.getBySel('domainIdSelector').click();
-    cy.getBySel('domainIdItem').last().click();
-    cy.getBySel('domainNameInput').clear();
-    cy.getBySel('domainNameInput').click().type(domainName);
-    cy.getBySel('domainPurposeInput').clear();
-    cy.getBySel('domainPurposeInput').click().type(domainPurpose);
-    cy.getBySel('editDomainAnnotation').click().type(annotationText);
-
-    cy.getBySel('editDomainConfirmButton').click();
-
-    cy.getBySel('actionHeading', { timeout: 100000 }).should(
-      'have.text',
-      `${domainName} team details edited`,
-    );
-
-    cy.url().should(
-      'contains',
-      `${Cypress.config().baseUrl}/colony/${
-        Cypress.config().colony.name
-      }/tx/0x`,
-    );
-
-    cy.getBySel('comment').should('have.text', annotationText);
-
-    cy.getBySel('backButton').click();
-
-    cy.getBySel('colonyDomainSelector', { timeout: 15000 }).click();
-    cy.getBySel('domainDropdownItemName')
-      .last()
-      .should('have.text', domainName);
-    cy.getBySel('domainDropdownItemPurpose')
-      .last()
-      .should('have.text', domainPurpose);
+    cy.checkMotion();
   });
+
   it('Can award users', () => {
     const amountToAward = 10;
     const annotationText = 'You have been a good boy, time for your reward';
