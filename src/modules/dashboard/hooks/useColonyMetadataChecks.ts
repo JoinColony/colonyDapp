b5785adf17ffd9f1ca24b1e0c8c524c14ddc0d3a
ArmandoGraterol <armandogrtrl@gmail.com>
2022-04-28 21:11:19 -0300

    Fixed infinite loop and improved code in useColonyMetadataChecks

--- src/modules/dashboard/hooks/useColonyMetadataChecks.ts
+++ src/modules/dashboard/hooks/useColonyMetadataChecks.ts
@@ -1,5 +1,6 @@
+import isEqual from 'lodash/isEqual';
 import findLastIndex from 'lodash/findLastIndex';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import {
   useSubgraphColonyMetadataQuery,
@@ -7,7 +8,7 @@ import {
   ColonyAction,
 } from '~data/index';
 import { ipfsDataFetcher } from '~modules/core/fetchers';
-import { ColonyAndExtensionsEvents } from '~types/colonyActions';
+import { ColonyAndExtensionsEvents, ColonyActions } from '~types/colonyActions';
 import {
   getSpecificActionValuesCheck,
   sortMetadataHistory,
@@ -27,6 +28,17 @@ export const useColonyMetadataChecks = (
 ) => {
   let metadataJSON: string | null = null;
   const [metadataIpfsHash, setMetadataIpfsHash] = useState<string>('');
+  /*
+   * Default fallback, just use the current colony's values
+   */
+  const [metadataChecks, setMetadataChecks] = useState<{
+    [key: string]: boolean;
+  }>({
+    nameChanged: false,
+    logoChanged: false,
+    tokensChanged: false,
+    verifiedAddressesChanged: false,
+  });
 
   const colonyMetadataHistory = useSubgraphColonyMetadataQuery({
     variables: {
@@ -45,76 +57,82 @@ export const useColonyMetadataChecks = (
   } catch (error) {
     // silent error
   }
+  useEffect(() => {
+    if (
+      (eventName === ColonyAndExtensionsEvents.ColonyMetadata ||
+        eventName === ColonyActions.ColonyEdit) &&
+      !!colonyMetadataHistory?.data?.colony
+    ) {
+      const {
+        data: {
+          colony: { metadataHistory },
+        },
+      } = colonyMetadataHistory;
+      const sortedMetadataHistory = sortMetadataHistory(metadataHistory);
+      const currentMetadataIndex = findLastIndex(
+        sortedMetadataHistory,
+        ({ transaction: { id: hash } }) => hash === transactionHash,
+      );
+      /*
+       * We have a previous metadata entry
+       */
+      if (currentMetadataIndex > 0) {
+        const prevMetadata = sortedMetadataHistory[currentMetadataIndex - 1];
+        if (prevMetadata) {
+          if (prevMetadata.metadata !== metadataIpfsHash) {
+            setMetadataIpfsHash(prevMetadata.metadata);
+          }
 
-  if (
-    eventName === ColonyAndExtensionsEvents.ColonyMetadata &&
-    !!colonyMetadataHistory?.data?.colony
-  ) {
-    const {
-      data: {
-        colony: { metadataHistory },
-      },
-    } = colonyMetadataHistory;
-    const sortedMetadataHistory = sortMetadataHistory(metadataHistory);
-    const currentMetadataIndex = findLastIndex(
-      sortedMetadataHistory,
-      ({ transaction: { id: hash } }) => hash === transactionHash,
-    );
-    /*
-     * We have a previous metadata entry
-     */
-    if (currentMetadataIndex > 0) {
-      const prevMetadata = sortedMetadataHistory[currentMetadataIndex - 1];
-      if (prevMetadata) {
-        setMetadataIpfsHash(prevMetadata.metadata);
-        if (metadataJSON) {
-          const prevColonyMetadata = parseColonyMetadata(metadataJSON);
-          /*
-           * If we have a metadata json, parse into the expected values and then
-           * compare them agains the ones from the current action
-           *
-           * This should be the default case for a colony with metadata history
-           */
-          return getSpecificActionValuesCheck(
-            eventName as ColonyAndExtensionsEvents,
-            actionData,
-            prevColonyMetadata,
-          );
+          if (metadataJSON) {
+            const prevColonyMetadata = parseColonyMetadata(metadataJSON);
+            /*
+             * If we have a metadata json, parse into the expected values and then
+             * compare them agains the ones from the current action
+             *
+             * This should be the default case for a colony with metadata history
+             */
+            const newMetadataChecks = getSpecificActionValuesCheck(
+              ColonyAndExtensionsEvents.ColonyMetadata,
+              actionData,
+              prevColonyMetadata,
+            );
+
+            if (!isEqual(newMetadataChecks, metadataChecks)) {
+              setMetadataChecks(newMetadataChecks);
+            }
+          }
+        }
+      } else if (actionData) {
+        /*
+         * We don't have a previous metadata entry, so fall back to the current
+         * action's values if we can
+         */
+        const {
+          colonyDisplayName: actionColonyDisplayName,
+          colonyAvatarHash: actionColonyAvatarHash,
+          colonyTokens: actionColonyTokens,
+          verifiedAddresses: actionVerifiedAddresses,
+        } = actionData;
+        const newMetadataValues = {
+          nameChanged: !!actionColonyDisplayName,
+          logoChanged: !!actionColonyAvatarHash,
+          tokensChanged: !!actionColonyTokens?.length,
+          verifiedAddressesChanged: !!actionVerifiedAddresses?.length,
+        };
+
+        if (!isEqual(newMetadataValues, metadataChecks)) {
+          setMetadataChecks(newMetadataValues);
         }
       }
     }
-    /*
-     * We don't have a previous metadata entry, so fall back to the current
-     * action's values if we can
-     */
-    if (actionData) {
-      const {
-        colonyDisplayName,
-        colonyAvatarHash,
-        colonyTokens,
-        verifiedAddresses,
-      } = actionData;
-      return {
-        nameChanged: !!colonyDisplayName,
-        logoChanged: !!colonyAvatarHash,
-        tokensChanged: !!colonyTokens?.length,
-        verifiedAddresses: !!verifiedAddresses?.length,
-      };
-    }
-  }
-  /*
-   * Default fallback, just use the current colony's values
-   */
-  const {
-    displayName: colonyDisplayName,
-    avatarHash,
-    tokenAddresses,
-    whitelistedAddresses,
-  } = colony;
-  return {
-    nameChanged: !!colonyDisplayName,
-    logoChanged: !!avatarHash,
-    tokensChanged: !!tokenAddresses?.length,
-    verifiedAddresses: !!whitelistedAddresses?.length,
-  };
+  }, [
+    eventName,
+    colonyMetadataHistory,
+    transactionHash,
+    setMetadataIpfsHash,
+    metadataJSON,
+    actionData,
+  ]);
+
+  return metadataChecks;
 };
