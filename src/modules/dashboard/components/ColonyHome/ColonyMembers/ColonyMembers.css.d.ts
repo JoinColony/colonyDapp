6dfebc8e0233bd8ad9e68353abfc32553afd18f0
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-05-12 18:10:46 +0200

    Feat: reorganise ColonyMembers & add MembersSubsection

--- src/modules/dashboard/components/ColonyHome/ColonyMembers/ColonyMembers.css.d.ts
+++ src/modules/dashboard/components/ColonyHome/ColonyMembers/ColonyMembers.css.d.ts
@@ -1,6 +1,6 @@
 export const main: string;
 export const userAvatars: string;
 export const userAvatar: string;
-export const remaningAvatars: string;
+export const caretIconLink: string;
 export const loadingText: string;
 export const userBanned: string;
