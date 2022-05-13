2ff20663c2046fc259435a822b1ce1ce28b9e7d4
ArmandoGraterol <armandogrtrl@gmail.com>
2022-05-13 15:03:34 -0300

    Integrated new total team rep points section with sidebar in ColonyMembers

--- src/modules/dashboard/components/ColonyMembers/ColonyMembers.css.d.ts
+++ src/modules/dashboard/components/ColonyMembers/ColonyMembers.css.d.ts
@@ -4,3 +4,5 @@ export const mainContent: string;
 export const rightAside: string;
 export const loadingWrapper: string;
 export const controls: string;
+export const teamReputationPointsContainer: string;
+export const reputationPoints: string;
