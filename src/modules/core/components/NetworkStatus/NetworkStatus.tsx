c4f4c03087ca4255a6a89c20f1967245a30d59dc
Mostly Void <7rat13@gmail.com>
2022-05-23 17:04:01 +0530

    Fix: rename to popperOptions in NetworkStatus

--- src/modules/core/components/NetworkStatus/NetworkStatus.tsx
+++ src/modules/core/components/NetworkStatus/NetworkStatus.tsx
@@ -142,7 +142,7 @@ const NetworkStatus = () => {
                 />
               </div>
             )}
-            popperProps={{
+            popperOptions={{
               modifiers: [
                 {
                   name: 'offset',
