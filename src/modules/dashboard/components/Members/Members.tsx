efb0196a78218d393c13be4993a551d72a4b4c8b
danbr <danatcern@hotmail.com>
2022-05-31 21:30:22 +0100

    feat:refactor to incorp updates from rebase

--- src/modules/dashboard/components/Members/Members.tsx
+++ src/modules/dashboard/components/Members/Members.tsx
@@ -1,11 +1,9 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 import { ROOT_DOMAIN_ID } from '@colony/colony-js';
 import sortBy from 'lodash/sortBy';
 
-import MembersList from '~core/MembersList';
 import { SpinnerLoader } from '~core/Preloaders';
-import LoadMoreButton from '~core/LoadMoreButton';
 import UserPermissions from '~dashboard/UserPermissions';
 import { useTransformer } from '~utils/hooks';
 import {
@@ -51,7 +49,7 @@ interface Props {
   selectedDomain: DomainFieldsFragment | undefined;
   handleDomainChange: React.Dispatch<React.SetStateAction<number>>;
 }
-const ITEMS_PER_PAGE = 1;
+
 const displayName = 'dashboard.Members';
 
 const Members = ({
@@ -67,8 +65,6 @@ const Members = ({
     ethereal,
   } = useLoggedInUser();
   const hasRegisteredProfile = !!username && !ethereal;
-  const [watchersConsidered, setWatchersConsidered] = useState<boolean>(true);
-  const [dataPage, setDataPage] = useState<number>(10);
   const [contributors, setContributors] = useState<ColonyContributor[]>([]);
   const [watchers, setWatchers] = useState<ColonyWatcher[]>([]);
 
@@ -83,13 +79,6 @@ const Members = ({
    */
   const { ethDomainId: currentDomainId = COLONY_TOTAL_BALANCE_DOMAIN_ID } =
     selectedDomain || {};
-
-  useEffect(() => {
-    setWatchersConsidered(
-      currentDomainId === ROOT_DOMAIN_ID ||
-        currentDomainId === COLONY_TOTAL_BALANCE_DOMAIN_ID,
-    );
-  }, [currentDomainId]);
 
   const {
     data: members,
@@ -142,10 +131,6 @@ const Members = ({
     [members],
   );
 
-  const handleDataPagination = useCallback(() => {
-    setDataPage(dataPage + 1);
-  }, [dataPage]);
-
   // handles search values & close button
   const handleSearch = useCallback(
     (event) => {
@@ -156,21 +141,48 @@ const Members = ({
     [filterContributorsAndWatchers, setSearchValue],
   );
 
-  const zeroResultsMsg = (isSearchResult: boolean) => {
-    const resultMsg = isSearchResult ? MSG.noMemebersFound : MSG.failedToFetch;
-    return (
-      <div className={styles.noResults}>
-        <FormattedMessage {...resultMsg} />
-      </div>
+  const membersContent = useMemo(() => {
+    const contributorsContent = (
+      <MembersSection<ColonyContributor>
+        isContributorsSection
+        colony={colony}
+        currentDomainId={currentDomainId}
+        members={contributors as ColonyContributor[]}
+        canAdministerComments={canAdministerComments}
+        extraItemContent={({ roles, directRoles, banned }) => {
+          return (
+            <UserPermissions
+              roles={roles}
+              directRoles={directRoles}
+              banned={banned}
+            />
+          );
+        }}
+      />
     );
-  };
 
-  const hasNoMembers = useCallback(() => {
-    if (!contributors?.length && !watchers?.length) {
-      return zeroResultsMsg(!!searchValue);
-    }
-    return null;
-  }, [contributors, watchers, searchValue]);
+    const watchersContent =
+      currentDomainId === ROOT_DOMAIN_ID ||
+      currentDomainId === COLONY_TOTAL_BALANCE_DOMAIN_ID ? (
+        <MembersSection<ColonyWatcher>
+          isContributorsSection={false}
+          colony={colony}
+          currentDomainId={currentDomainId}
+          members={watchers as ColonyWatcher[]}
+          canAdministerComments={canAdministerComments}
+          extraItemContent={({ banned }) => (
+            <UserPermissions roles={[]} directRoles={[]} banned={banned} />
+          )}
+        />
+      ) : null;
+
+    return (
+      <>
+        {contributorsContent}
+        {watchersContent}
+      </>
+    );
+  }, [canAdministerComments, colony, contributors, currentDomainId, watchers]);
 
   if (loadingMembers) {
     return (
@@ -183,22 +195,6 @@ const Members = ({
     );
   }
 
-  /*
-   * @NOTE Poor man's pagination
-   *
-   * We're not really doing proper pagination like we do for say... `ColonyEvents`,
-   * because our metadata server doesn't support skip, first, more, next, etc...
-   * query filters
-   *
-   * A proper solution for this would be to teach the server to return just part
-   * of the results, with the next waiting in line for a callback, but the current
-   * timeframe doesn't allow for this, so I guess this is the "best" we can do for now
-   */
-  const paginatedContributors =
-    contributors?.slice(0, ITEMS_PER_PAGE * dataPage) || [];
-
-  const paginatedWatchers = watchers?.slice(0, ITEMS_PER_PAGE * dataPage) || [];
-
   return (
     <div className={styles.main}>
       <MembersTitle
@@ -209,104 +205,12 @@ const Members = ({
         setSearchValue={setSearchValue}
         handleSearch={handleSearch}
       />
-      <div>
-        {/* This needs attention!!!! - ready to be refactored in another PR
-        Either display no member message OR show contributors & watchers
-        components. - dan */}
-        {hasNoMembers()}
-        {/* PLACEHOLDER */}
-        <div>CONTRIBUTORS</div>
-        {contributors?.length ? (
-          <MembersList<ColonyContributor>
-            colony={colony}
-            extraItemContent={({ roles, directRoles, banned }) => (
-              <UserPermissions
-                roles={roles}
-                directRoles={directRoles}
-                banned={banned}
-              />
-            )}
-            domainId={currentDomainId}
-            users={paginatedContributors}
-            canAdministerComments={canAdministerComments}
-          />
-        ) : (
-          watchers?.length && zeroResultsMsg(!!searchValue)
-        )}
-
-        {ITEMS_PER_PAGE * dataPage < contributors?.length && (
-          <LoadMoreButton
-            onClick={handleDataPagination}
-            isLoadingData={loadingMembers}
-          />
-        )}
-        {watchersConsidered && (
-          <>
-            {/* PLACEHOLDER */}
-            <div>WATCHERS</div>
-            {watchers?.length ? (
-              <MembersList<ColonyWatcher>
-                colony={colony}
-                domainId={currentDomainId}
-                users={paginatedWatchers}
-                canAdministerComments={canAdministerComments}
-                extraItemContent={({ banned }) => (
-                  <UserPermissions
-                    roles={[]}
-                    directRoles={[]}
-                    banned={banned}
-                  />
-                )}
-              />
-            ) : (
-              contributors?.length && zeroResultsMsg(!!searchValue)
-            )}
-
-            {ITEMS_PER_PAGE * dataPage < watchers?.length && (
-              <LoadMoreButton
-                onClick={handleDataPagination}
-                isLoadingData={loadingMembers}
-              />
-            )}
-          </>
-        )}
-      </div>
-      {contributors.length ? (
-        <MembersSection<ColonyContributor>
-          isContributorsSection
-          colony={colony}
-          currentDomainId={currentDomainId}
-          members={contributors as ColonyContributor[]}
-          canAdministerComments={canAdministerComments}
-          extraItemContent={({ roles, directRoles, banned }) => {
-            return (
-              <UserPermissions
-                roles={roles}
-                directRoles={directRoles}
-                banned={banned}
-              />
-            );
-          }}
-        />
+      {!contributors?.length && !watchers?.length ? (
+        <div className={styles.noResults}>
+          <FormattedMessage {...MSG.noMemebersFound} />
+        </div>
       ) : (
-        <FormattedMessage {...MSG.failedToFetch} />
-      )}
-      {(currentDomainId === ROOT_DOMAIN_ID ||
-        currentDomainId === COLONY_TOTAL_BALANCE_DOMAIN_ID) && (
-        <>
-          {watchers?.length && (
-            <MembersSection<ColonyWatcher>
-              isContributorsSection={false}
-              colony={colony}
-              currentDomainId={currentDomainId}
-              members={watchers as ColonyWatcher[]}
-              canAdministerComments={canAdministerComments}
-              extraItemContent={({ banned }) => (
-                <UserPermissions roles={[]} directRoles={[]} banned={banned} />
-              )}
-            />
-          )}
-        </>
+        membersContent
       )}
     </div>
   );
