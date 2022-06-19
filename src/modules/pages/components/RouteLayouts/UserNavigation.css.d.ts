ce49f01fe12cf084ce6da395c04148d7ec011394
willm30 <williamajmartin@gmail.com>
2022-06-19 16:50:58 +0100

    Remove: fade div for colonies list
    As the spec changed, there is no longer a need for a fade effect when the colonies list
    overflows its container.
    
    f

--- src/modules/pages/components/RouteLayouts/UserNavigation.css.d.ts
+++ src/modules/pages/components/RouteLayouts/UserNavigation.css.d.ts
@@ -16,4 +16,3 @@ export const walletWrapper: string;
 export const readyTransactionsCount: string;
 export const reputation: string;
 export const walletAutoLogin: string;
-export const fade: string;
