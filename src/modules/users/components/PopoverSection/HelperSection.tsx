5af42fdf154729df7830b9128eb2cd9e2120d2da
willm30 <williamajmartin@gmail.com>
2022-06-20 19:43:03 +0100

    Refactor: Extracting Menu sections into reusable components
    Extract menu sections into resuable components for reuse in
    HamburgerDropdownPopover.

---
+++ src/modules/users/components/PopoverSection/HelperSection.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { defineMessages } from 'react-intl';
+import { DropdownMenuItem, DropdownMenuSection } from '~core/DropdownMenu';
+import ExternalLink from '~core/ExternalLink';
+import { FEEDBACK, HELP } from '~externalUrls';
+
+import styles from './HelperSection.css';
+
+const MSG = defineMessages({
+  reportBugs: {
+    id: 'users.PopoverSection.HelperSection.link.reportBugs',
+    defaultMessage: 'Report Bugs',
+  },
+  helpCenter: {
+    id: 'users.PopoverSection.HelperSection.link.helpCenter',
+    defaultMessage: 'Help Center',
+  },
+});
+
+const displayName = 'users.PopoverSection.HelperSection';
+
+const HelperSection = () => (
+  <DropdownMenuSection separator>
+    <DropdownMenuItem>
+      <ExternalLink
+        href={FEEDBACK}
+        text={MSG.reportBugs}
+        className={styles.externalLink}
+      />
+    </DropdownMenuItem>
+    <DropdownMenuItem>
+      <ExternalLink
+        href={HELP}
+        text={MSG.helpCenter}
+        className={styles.externalLink}
+      />
+    </DropdownMenuItem>
+  </DropdownMenuSection>
+);
+
+HelperSection.displayName = displayName;
+
+export default HelperSection;
