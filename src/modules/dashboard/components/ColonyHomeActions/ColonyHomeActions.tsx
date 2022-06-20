1d20fc2319a5e22fdbe4523fb1d8bc64350ca7cb
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-06-20 22:49:50 +0300

    Feat: add `GnosisControlSafeForm` to the dialog

--- src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
+++ src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
@@ -211,7 +211,6 @@ const ColonyHomeActions = ({ colony, ethDomainId }: Props) => {
         nextStepControlSafe: 'dashboard.GnosisControlSafeDialog',
         prevStep: 'dashboard.AdvancedDialog',
         colony,
-        isVotingExtensionEnabled,
       },
     },
     // @todo - ready to be implemented in another PR.
