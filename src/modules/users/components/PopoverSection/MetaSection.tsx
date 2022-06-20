81b92685364d226874d909428a4c09354efc30e8
willm30 <williamajmartin@gmail.com>
2022-06-20 19:43:03 +0100

    Refactor: Extracting Menu sections into reusable components
    Extract menu sections into resuable components for reuse in
    HamburgerDropdownPopover.

---
+++ src/modules/users/components/PopoverSection/MetaSection.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { defineMessages } from 'react-intl';
+import { ActionButton } from '~core/Button';
+import { DropdownMenuItem, DropdownMenuSection } from '~core/DropdownMenu';
+import { ActionTypes } from '~redux/actionTypes';
+
+const MSG = defineMessages({
+  signOut: {
+    id: 'users.PopoverSection.MetaSection.link.signOut',
+    defaultMessage: 'Sign Out',
+  },
+});
+
+const displayName = 'users.PopoverSection.MetaSection';
+
+const MetaSection = () => (
+  <DropdownMenuSection separator>
+    <DropdownMenuItem>
+      <ActionButton
+        appearance={{ theme: 'no-style' }}
+        text={MSG.signOut}
+        submit={ActionTypes.USER_LOGOUT}
+        error={ActionTypes.USER_LOGOUT_ERROR}
+        success={ActionTypes.USER_LOGOUT_SUCCESS}
+      />
+    </DropdownMenuItem>
+  </DropdownMenuSection>
+);
+
+MetaSection.displayName = displayName;
+
+export default MetaSection;
---
+++ src/modules/users/components/PopoverSection/UserSection.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { defineMessages } from 'react-intl';
+import { DropdownMenuItem, DropdownMenuSection } from '~core/DropdownMenu';
+import NavLink from '~core/NavLink';
+import { Colony, Maybe } from '~data/index';
+import { CREATE_USER_ROUTE, USER_EDIT_ROUTE } from '~routes/routeConstants';
+
+const MSG = defineMessages({
+  buttonGetStarted: {
+    id: 'users.PopoverSection.UserSection.buttonGetStarted',
+    defaultMessage: 'Get started',
+  },
+  myProfile: {
+    id: 'users.PopoverSection.UserSection.link.myProfile',
+    defaultMessage: 'My Profile',
+  },
+  settings: {
+    id: 'users.PopoverSection.UserSection.link.colonySettings',
+    defaultMessage: 'Settings',
+  },
+});
+
+interface Props {
+  colony?: Colony;
+  username?: Maybe<string>;
+  itemClassName?: string;
+}
+
+const displayName = 'users.PopoverSection.UserSection';
+
+const UserSection = ({ colony, username }: Props) => (
+  <DropdownMenuSection separator>
+    {!username && (
+      <DropdownMenuItem>
+        <NavLink
+          to={{
+            pathname: CREATE_USER_ROUTE,
+            state: colony?.colonyName
+              ? { colonyURL: `/colony/${colony?.colonyName}` }
+              : {},
+          }}
+          text={MSG.buttonGetStarted}
+        />
+      </DropdownMenuItem>
+    )}
+    {username && (
+      <DropdownMenuItem>
+        <NavLink
+          to={`/user/${username}`}
+          text={MSG.myProfile}
+          data-test="userProfile"
+        />
+      </DropdownMenuItem>
+    )}
+    {username && (
+      <DropdownMenuItem>
+        <NavLink
+          to={USER_EDIT_ROUTE}
+          text={MSG.settings}
+          data-test="userProfileSettings"
+        />
+      </DropdownMenuItem>
+    )}
+  </DropdownMenuSection>
+);
+
+UserSection.displayName = displayName;
+
+export default UserSection;
