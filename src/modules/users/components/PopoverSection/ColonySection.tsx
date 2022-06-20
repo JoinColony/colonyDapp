c17d2e3bc64669f125f14032cead31b27feebe16
willm30 <williamajmartin@gmail.com>
2022-06-20 19:43:03 +0100

    Refactor: Extracting Menu sections into reusable components
    Extract menu sections into resuable components for reuse in
    HamburgerDropdownPopover.

---
+++ src/modules/users/components/PopoverSection/ColonySection.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { defineMessages } from 'react-intl';
+import { DropdownMenuItem, DropdownMenuSection } from '~core/DropdownMenu';
+import NavLink from '~core/NavLink';
+import { CREATE_COLONY_ROUTE } from '~routes/routeConstants';
+
+const MSG = defineMessages({
+  createColony: {
+    id: 'users.PopoverSection.ColonySection.link.createColony',
+    defaultMessage: 'Create a Colony',
+  },
+});
+
+const displayName = 'users.PopoverSection.ColonySection';
+
+const ColonySection = () => (
+  <DropdownMenuSection separator>
+    <DropdownMenuItem>
+      <NavLink to={CREATE_COLONY_ROUTE} text={MSG.createColony} />
+    </DropdownMenuItem>
+  </DropdownMenuSection>
+);
+
+ColonySection.displayName = displayName;
+
+export default ColonySection;
---
+++ src/modules/users/components/PopoverSection/HelperSection.css.d.ts
@@ -0,0 +1,3 @@
+export const queries: string;
+export const query700: string;
+export const externalLink: string;
