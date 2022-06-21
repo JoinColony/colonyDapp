84f26d36eb95402bc76fe8e381cb2c7dacdffd7e
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-06-21 17:12:20 +0300

    Feat: update `AddItemButton`

--- src/modules/core/components/Button/AddItemButton.tsx
+++ src/modules/core/components/Button/AddItemButton.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import { MessageDescriptor } from 'react-intl';
+import { FormattedMessage, MessageDescriptor } from 'react-intl';
 
-import { SimpleMessageValues } from '~types/index';
-
-import IconButton from './IconButton';
+import Icon from '../Icon';
 import styles from './AddItemButton.css';
 
 interface Props {
-  /** Name of the icon to display */
-  icon?: string;
   /** A string or a `messageDescriptor` that make up the button's text label */
   text: MessageDescriptor;
-  /** Values for message descriptors */
-  textValues?: SimpleMessageValues;
+
+  disabled?: boolean;
+
+  handleClick?: () => void;
 }
 
 const displayName = 'dashboard.DomainDropdown.AddItemButton';
 
-const AddItemButton = ({ text, icon, textValues }: Props) => {
+const AddItemButton = ({ text, handleClick, disabled }: Props) => {
   return (
-    <div className={styles.button}>
-      <IconButton icon={icon} text={text} textValues={textValues} />
-    </div>
+    <button
+      className={styles.main}
+      onClick={handleClick}
+      type="button"
+      disabled={disabled}
+    >
+      <Icon name="circle-plus" title={text} appearance={{ size: 'medium' }} />
+      <span>
+        <FormattedMessage {...text} />
+      </span>
+    </button>
   );
 };
 
