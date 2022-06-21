90fdac12f03c964a87f5a4487378ad60d6efb446
danbr <danatcern@hotmail.com>
2022-06-21 14:56:19 +0300

    feat:add `Remove Safe Dialog`, form, & types

--- src/modules/dashboard/components/Dialogs/RemoveSafeDialog/RemoveSafeDialog.tsx
+++ src/modules/dashboard/components/Dialogs/RemoveSafeDialog/RemoveSafeDialog.tsx
@@ -1,20 +1,18 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { FormikProps } from 'formik';
-import * as yup from 'yup';
-import { useHistory } from 'react-router-dom';
 
 import Dialog, { DialogProps } from '~core/Dialog';
 import { ActionForm } from '~core/Fields';
 
-import { Colony, useLoggedInUser } from '~data/index';
+import { Colony } from '~data/index';
 import { ActionTypes } from '~redux/index';
 import { WizardDialogType } from '~utils/hooks';
-import { pipe, withMeta, mapPayload } from '~utils/actions';
 
+import { Safe } from './types';
 import DialogForm from './RemoveSafeDialogForm';
 
 export interface FormValues {
-  annotation: string;
+  safeList: string[];
 }
 
 interface CustomWizardDialogProps {
@@ -26,57 +24,64 @@ type Props = DialogProps & WizardDialogType<object> & CustomWizardDialogProps;
 
 const displayName = 'dashboard.RemoveSafeDialog';
 
+const safes: Safe[] = [
+  {
+    name: 'All Saints (Gnosis Chain)',
+    address: '0x3a157280ca91bB49dAe3D1619C55Da7F9D4438c2',
+  },
+  {
+    name: '(Mainnet)',
+    address: '0x4a157280ca91bB49dAe3D1619C55Da7F9D4438c3',
+  },
+  {
+    name: 'Big safe',
+    address: '0x5a157280ca91bB49dAe3D1619C55Da7F9D4438c2',
+  },
+  {
+    name: `Just a test with a very very long name,
+    hang on its even longer!!!!!!!!`,
+    address: '0x6a157280ca91bB49dAe3D1619C55Da7F9D4438c3',
+  },
+  {
+    name: 'blalabalabal',
+    address: '0x7a157280ca91bB49dAe3D1619C55Da7F9D4438c2',
+  },
+  {
+    name: 'final test',
+    address: '0x8a157280ca91bB49dAe3D1619C55Da7F9D4438c3',
+  },
+];
+
 const RemoveSafeDialog = ({
   cancel,
   close,
   callStep,
   prevStep,
-  colony: { colonyName, colonyAddress },
   colony,
 }: Props) => {
-  const { walletAddress } = useLoggedInUser();
-  const history = useHistory();
-
-  const validationSchema = yup.object().shape({
-    annotation: yup.string().max(4000),
-  });
-
-  const transform = useCallback(
-    pipe(
-      mapPayload(({ annotation: annotationMessage }) => {
-        return {
-          colonyName,
-          colonyAddress,
-          walletAddress,
-          annotationMessage,
-        };
-      }),
-      withMeta({ history }),
-    ),
-    [],
-  );
-
   return (
     <ActionForm
       initialValues={{
-        annotation: undefined,
+        safeList: [],
       }}
+      // @TODO need to update action, in another PR
       submit={ActionTypes.COLONY_ACTION_RECOVERY}
       error={ActionTypes.COLONY_ACTION_RECOVERY_ERROR}
       success={ActionTypes.COLONY_ACTION_RECOVERY_SUCCESS}
-      validationSchema={validationSchema}
       onSuccess={close}
-      transform={transform}
     >
-      {(formValues: FormikProps<FormValues>) => (
-        <Dialog cancel={cancel}>
-          <DialogForm
-            {...formValues}
-            colony={colony}
-            back={() => callStep(prevStep)}
-          />
-        </Dialog>
-      )}
+      {(formValues: FormikProps<FormValues>) => {
+        return (
+          <Dialog cancel={cancel}>
+            <DialogForm
+              {...formValues}
+              colony={colony}
+              back={() => callStep(prevStep)}
+              safeList={safes}
+            />
+          </Dialog>
+        );
+      }}
     </ActionForm>
   );
 };
---
+++ src/modules/dashboard/components/Dialogs/RemoveSafeDialog/SafeListItem.css.d.ts
@@ -0,0 +1,7 @@
+export const main: string;
+export const avatar: string;
+export const checked: string;
+export const address: string;
+export const checkbox: string;
+export const label: string;
+export const selectedLabel: string;
