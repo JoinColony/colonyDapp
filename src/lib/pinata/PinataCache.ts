88b35c8cbe4b69a7e2b566914f9e20f03da4152f
Raul Glogovetan <raul@glogovetan.com>
2022-06-21 22:08:01 +0300

    Fix: `PinataCache` better comments

--- src/lib/pinata/PinataCache.ts
+++ src/lib/pinata/PinataCache.ts
@@ -29,9 +29,16 @@ class PinataCache {
        * realize this as it can't properly deal with async constructor assignments
        */
       let response = await this.cache?.match(new Request(HASH_URL));
+      /*
+       * @NOTE If we don't find the object in the local cache, attempt to get it
+       * from the Gateway
+       */
       if (!response) {
         response = (await this.setCacheObject(hash)) as Response;
         log.verbose(`Could not fetch Pinata Cache entry for hash '${hash}'`);
+        /*
+         * @NOTE Only if we can't find it via the Gateway that we'll throw
+         */
         if (!response) {
           throw new Error(
             `Could not fetch Pinata IPFS entry (both Cache or Gateway for hash ${hash}`,
@@ -62,8 +69,18 @@ class PinataCache {
          * @NOTE That this.cache exists at this point in time, just that TS doesn't
          * realize this as it can't properly deal with async constructor assignments
          */
+        /*
+         * @NOTE If we don't have a network response already, it means that we didn't
+         * upload the IPFS blob ourself, in which case, we'll have to request it from
+         * the Gateway
+         */
         await this.cache?.add(new Request(HASH_URL));
       } else {
+        /*
+         * @NOTE If we have a network response, that means that we've uploaded to IPFS
+         * ourselfs, so there's no point in fetching it again from the Gateway as we
+         * already have this data locally
+         */
         await this.cache?.put(HASH_URL, networkResponse);
       }
       return this.getCacheObject(hash);
