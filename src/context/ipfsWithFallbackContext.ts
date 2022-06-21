93d2657aa0c0b0ab8ac8b77566b58463700a5a71
Raul Glogovetan <raul@glogovetan.com>
2022-06-21 18:40:58 +0300

    Refactor: `IPFS` context

--- src/context/ipfsWithFallbackContext.ts
+++ src/context/ipfsWithFallbackContext.ts
@@ -1,8 +1,19 @@
-import pinataClient from './pinataClient';
-import ipfsNode from './ipfsNodeContext';
+import IPFSNode from '../lib/ipfs';
 
+import pinataClient from './pinataClient';
 import getIPFSWithFallback from './getIPFSWithFallback';
 
-const ipfsWithFallback = getIPFSWithFallback(ipfsNode, pinataClient);
+const getIPFSContext = () => {
+  if (
+    process.env.NODE_ENV === 'development' ||
+    !(process.env.PINATA_API_KEY && process.env.PINATA_API_SECRET)
+  ) {
+    const ipfsNode = new IPFSNode();
+    return getIPFSWithFallback(ipfsNode);
+  }
+  return getIPFSWithFallback(undefined, pinataClient);
+};
+
+const ipfsWithFallback = getIPFSContext();
 
 export default ipfsWithFallback;
