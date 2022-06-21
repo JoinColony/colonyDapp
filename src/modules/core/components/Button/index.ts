4faeb11025e31ab451ce2be6dad4d9d1a37fc524
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-06-21 15:59:25 +0300

    Feat: add `AddItemButton` core component

--- src/modules/core/components/Button/index.ts
+++ src/modules/core/components/Button/index.ts
@@ -5,3 +5,4 @@ export { default as DottedAddButton } from './DottedAddButton';
 export { default as DialogActionButton } from './DialogActionButton';
 export { default as IconButton } from './IconButton';
 export { default as ThreeDotsButton } from './ThreeDotsButton';
+export { default as AddItemButton } from './AddItemButton';
