62f1f4821d7a3b36b761bd51b28925130e67b955
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-06-22 13:27:58 +0200

    type fix

--- src/modules/dashboard/components/ExpenditurePage/Payments/types.d.ts
+++ src/modules/dashboard/components/ExpenditurePage/Payments/types.d.ts
@@ -1,7 +1,7 @@
 export interface Recipient {
   id?: string;
   recipient?: AnyUser;
-  value?: { amount?: number; tokenAddress?: number }[];
+  value?: { amount?: number; tokenAddress?: number; id: string }[];
   delay?: {
     amount: string;
     time: string;
