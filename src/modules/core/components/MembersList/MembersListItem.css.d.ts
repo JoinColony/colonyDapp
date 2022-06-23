a198f45b1c3a8f4b5e5d6930767a0997ba926f7b
willm30 <williamajmartin@gmail.com>
2022-06-23 15:59:59 +0100

    Add: Mobile Responsive Members Component
    Configuring the Members Component and relevant children to be mobile responsive.

--- src/modules/core/components/MembersList/MembersListItem.css.d.ts
+++ src/modules/core/components/MembersList/MembersListItem.css.d.ts
@@ -1,3 +1,4 @@
+export const query700: string;
 export const main: string;
 export const section: string;
 export const usernameSection: string;
--- src/modules/dashboard/components/ColonyHome/ColonyHomeInfo.tsx
+++ src/modules/dashboard/components/ColonyHome/ColonyHomeInfo.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Colony } from '~data/index';
+
 import ColonyTitle from './ColonyTitle';
 import ColonyNavigation from './ColonyNavigation';
 
@@ -7,7 +9,13 @@ import styles from './ColonyHomeLayout.css';
 
 const displayName = 'dashboard.ColonyHome.ColonyHomeInfo';
 
-const ColonyHomeInfo = ({ colony, isMobile, showNavigation }) => (
+interface Props {
+  colony: Colony;
+  isMobile: boolean;
+  showNavigation: boolean;
+}
+
+const ColonyHomeInfo = ({ colony, isMobile, showNavigation }: Props) => (
   <aside className={styles.leftAside}>
     <ColonyTitle colony={colony} />
     {!isMobile && showNavigation && <ColonyNavigation colony={colony} />}
