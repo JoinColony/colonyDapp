e2e9c3cf4715182c6a5f5c9ea3da45d8ac032a95
danbr <danatcern@hotmail.com>
2022-06-23 10:16:10 +0300

    fix:remove extra props

--- src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
+++ src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
@@ -251,8 +251,6 @@ const ColonyHomeActions = ({ colony, ethDomainId }: Props) => {
       props: {
         prevStep: 'dashboard.ManageGnosisSafeDialog',
         colony,
-        isVotingExtensionEnabled,
-        ethDomainId,
       },
     },
     // {
