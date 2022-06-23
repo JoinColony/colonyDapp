8c900ce227c26fe2e5103e3a96e4b20abca50a68
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-06-23 10:07:43 +0300

    Refactor: remove unnecessary props in `ColonyHomeAction`

--- src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
+++ src/modules/dashboard/components/ColonyHomeActions/ColonyHomeActions.tsx
@@ -250,8 +250,6 @@ const ColonyHomeActions = ({ colony, ethDomainId }: Props) => {
       props: {
         prevStep: 'dashboard.ManageGnosisSafeDialog',
         colony,
-        isVotingExtensionEnabled,
-        ethDomainId,
       },
     },
 
