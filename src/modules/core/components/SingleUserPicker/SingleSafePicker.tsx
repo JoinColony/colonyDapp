2eefa5969ff2638e08e0eebc812913a4d41d4c23
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-06-24 18:33:36 +0300

    Refactor: update `SingleSafePicker` component

--- src/modules/core/components/SingleUserPicker/SingleSafePicker.tsx
+++ src/modules/core/components/SingleUserPicker/SingleSafePicker.tsx
@@ -4,6 +4,7 @@ import { GnosisSafe } from '~dashboard/Dialogs/GnosisControlSafeDialog';
 
 import SingleUserPicker from './SingleUserPicker';
 
+/* SingleSafePicker is a wrapper around SingleUserPicker component */
 interface Props extends ComponentProps<typeof SingleUserPicker> {
   data: GnosisSafe[];
 }
@@ -14,6 +15,7 @@ const SingleSafePicker = ({ data, ...props }: Props) => {
   const formattedData = useMemo(
     () =>
       data.map((item) => ({
+        id: item.address,
         profile: {
           displayName: `${item.name} (${item.chain})`,
           walletAddress: item.address,
@@ -22,7 +24,13 @@ const SingleSafePicker = ({ data, ...props }: Props) => {
     [data],
   );
 
-  return <SingleUserPicker {...props} data={formattedData} />;
+  return (
+    <SingleUserPicker
+      {...props}
+      data={formattedData}
+      placholderIconName="gnosis-logo"
+    />
+  );
 };
 
 SingleSafePicker.displayName = displayName;
