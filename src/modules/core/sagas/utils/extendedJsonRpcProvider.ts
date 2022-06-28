3dbbe50b5744ae68d912c66f3705c089b964f3db
Alex Rea <git@area.io>
2022-06-28 10:10:15 +0100

    Add rudimentary call caching to provider

--- src/modules/core/sagas/utils/extendedJsonRpcProvider.ts
+++ src/modules/core/sagas/utils/extendedJsonRpcProvider.ts
@@ -1,5 +1,6 @@
 import { JsonRpcProvider, BlockTag } from 'ethers/providers';
-import { poll } from 'ethers/utils';
+import { poll, serializeTransaction, resolveProperties } from 'ethers/utils';
+import { TransactionRequest } from 'ethers';
 
 export class ExtendedJsonRpcProvider extends JsonRpcProvider {
   _parentGetCode: (
@@ -10,6 +11,33 @@ export class ExtendedJsonRpcProvider extends JsonRpcProvider {
   constructor(...args) {
     super(...args);
     this._parentGetCode = super.getCode;
+    this._parentCall = super.call;
+    this.cachedData = {};
+  }
+
+  async call(
+    transaction: TransactionRequest,
+    blockTag?: BlockTag | Promise<BlockTag>,
+  ): Promise<string> {
+    const tx = await resolveProperties(transaction);
+
+    const { from } = tx;
+    delete tx.from;
+    const key = `${serializeTransaction(tx)}:${from || ''}`;
+
+    if (
+      this.cachedData[key] &&
+      this.cachedData[key].timestamp > Date.now() - 60000
+    ) {
+      return this.cachedData[key].value;
+    }
+
+    this.cachedData[key] = {
+      timestamp: Date.now(),
+      value: this._parentCall(transaction, blockTag),
+    };
+
+    return this.cachedData[key].value;
   }
 
   getCode(
