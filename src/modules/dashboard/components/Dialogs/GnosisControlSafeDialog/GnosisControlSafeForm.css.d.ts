dac518651c59bb42bf0a294d0f0f470030714218
ArmandoGraterol <armandogrtrl@gmail.com>
2022-06-28 14:29:45 -0300

    Added permission validation to GnosisControlSafeDialog

--- src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/GnosisControlSafeForm.css.d.ts
+++ src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/GnosisControlSafeForm.css.d.ts
@@ -2,4 +2,5 @@ export const wideButton: string;
 export const width: string;
 export const heading: string;
 export const safePicker: string;
+export const transactionTypeSelectContainer: string;
 export const addTransaction: string;
