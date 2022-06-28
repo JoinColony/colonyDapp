8ad8074e2560862363c7606e73ba5c596326071b
Alex Rea <git@area.io>
2022-06-28 16:56:25 +0100

    Fix up types

--- src/modules/core/sagas/utils/extendedJsonRpcProvider.ts
+++ src/modules/core/sagas/utils/extendedJsonRpcProvider.ts
@@ -1,12 +1,27 @@
-import { JsonRpcProvider, BlockTag } from 'ethers/providers';
+import {
+  JsonRpcProvider,
+  BlockTag,
+  TransactionRequest,
+} from 'ethers/providers';
 import { poll, serializeTransaction, resolveProperties } from 'ethers/utils';
-import { TransactionRequest } from 'ethers';
+
+type CachedRPCResponse = {
+  response: Promise<string>;
+  timestamp: number;
+};
 
 export class ExtendedJsonRpcProvider extends JsonRpcProvider {
   _parentGetCode: (
     addressOrName: string | Promise<string>,
     blockTag?: BlockTag | Promise<BlockTag>,
   ) => Promise<string>;
+
+  _parentCall: (
+    transaction: TransactionRequest,
+    blockTag?: BlockTag | Promise<BlockTag>,
+  ) => Promise<string>;
+
+  cachedData: { [key: string]: CachedRPCResponse } = {};
 
   constructor(...args) {
     super(...args);
@@ -29,15 +44,15 @@ export class ExtendedJsonRpcProvider extends JsonRpcProvider {
       this.cachedData[key] &&
       this.cachedData[key].timestamp > Date.now() - 60000
     ) {
-      return this.cachedData[key].value;
+      return this.cachedData[key].response;
     }
 
     this.cachedData[key] = {
       timestamp: Date.now(),
-      value: this._parentCall(transaction, blockTag),
+      response: this._parentCall(transaction, blockTag),
     };
 
-    return this.cachedData[key].value;
+    return this.cachedData[key].response;
   }
 
   getCode(
