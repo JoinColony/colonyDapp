8ac28b336097a3b8af2c760aff225de7e5bac663
ArmandoGraterol <armandogrtrl@gmail.com>
2022-06-27 22:01:57 -0300

    Integrated AmountTokens with CreatePaymentDialogForm

--- src/modules/core/components/Fields/AmountTokens/AmountTokens.tsx
+++ src/modules/core/components/Fields/AmountTokens/AmountTokens.tsx
@@ -82,10 +82,9 @@ const AmountTokens = ({
           dataTest="paymentAmountInput"
         />
         {networkFeeInverse &&
+          customAmountError === undefined &&
           values.amount &&
-          values.amount !== '0' &&
-          values.amount !== '0.' &&
-          values.amount !== '.' && (
+          Number(values.amount) > 0 && (
             <div className={styles.networkFee}>
               <FormattedMessage
                 {...MSG.fee}
