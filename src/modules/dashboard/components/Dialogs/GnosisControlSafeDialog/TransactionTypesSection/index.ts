6e36b1136fc10676433d430292e74fb9ba9286d7
ArmandoGraterol <armandogrtrl@gmail.com>
2022-06-30 18:39:06 -0300

    Implemented ContractInteractionSection component

--- src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/TransactionTypesSection/index.ts
+++ src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/TransactionTypesSection/index.ts
@@ -1,2 +1,3 @@
 export { default as TransferFundsSection } from './TransferFundsSection';
 export { default as RawTransactionSection } from './RawTransactionSection';
+export { default as ContractInteractionSection } from './ContractInteractionSection';
