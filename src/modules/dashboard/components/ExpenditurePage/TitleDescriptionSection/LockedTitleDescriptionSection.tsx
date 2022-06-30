a6853b9ba5567563c863da791bfa3ed3a9ad4be7
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-06-30 07:09:15 +0200

    fixed styles

--- src/modules/dashboard/components/ExpenditurePage/TitleDescriptionSection/LockedTitleDescriptionSection.tsx
+++ src/modules/dashboard/components/ExpenditurePage/TitleDescriptionSection/LockedTitleDescriptionSection.tsx
@@ -13,8 +13,10 @@ const LockedTitleDescriptionSection = ({ title, description }: Props) => {
     <div className={styles.lockedSectionContainer}>
       {/* "Exp - 25" is temporary value, needs to be changed to fetched value, id? */}
       <div className={styles.number}>Exp - 25</div>
-      <div className={classNames(styles.titleContainer)}>
-        <div className={styles.title}>{title}</div>
+      <div className={styles.titleContainer}>
+        <div className={classNames(styles.title, styles.lockedTitle)}>
+          {title}
+        </div>
       </div>
       <div className={classNames(styles.descriptionContainer)}>
         <div className={styles.description}>{description}</div>
