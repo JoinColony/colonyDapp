0f67a4967e95aa7c7001a5caafc27ae7061d92d0
ArmandoGraterol <armandogrtrl@gmail.com>
2022-06-30 18:39:06 -0300

    Implemented ContractInteractionSection component

---
+++ src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/TransactionTypesSection/ContractInteractionSection.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { defineMessages } from 'react-intl';
+
+import { AnyUser } from '~data/index';
+import { Address } from '~types/index';
+import { Select, Textarea } from '~core/Fields';
+import UserAvatar from '~core/UserAvatar';
+import SingleUserPicker, { filterUserSelection } from '~core/SingleUserPicker';
+import { DialogSection } from '~core/Dialog';
+
+import styles from './TransactionTypesSection.css';
+
+const MSG = defineMessages({
+  abiLabel: {
+    id: `dashboard.GnosisControlSafeDialog.GnosisControlSafeForm.ContractInteractionSection.abiLabel`,
+    defaultMessage: 'ABI/JSON',
+  },
+  functionLabel: {
+    id: `dashboard.GnosisControlSafeDialog.GnosisControlSafeForm.ContractInteractionSection.functionLabel`,
+    defaultMessage: 'Select function to interact with',
+  },
+  functionPlaceholder: {
+    id: `dashboard.GnosisControlSafeDialog.GnosisControlSafeForm.ContractInteractionSection.functionPlaceholder`,
+    defaultMessage: 'Select function',
+  },
+  contractLabel: {
+    id: `dashboard.GnosisControlSafeDialog.GnosisControlSafeForm.ContractInteractionSection.contractLabel`,
+    defaultMessage: 'Target contract address',
+  },
+  userPickerPlaceholder: {
+    id: `dashboard.GnosisControlSafeDialog.GnosisControlSafeForm.ContractInteractionSection.userPickerPlaceholder`,
+    defaultMessage: 'Select or paste a contract address',
+  },
+});
+
+const displayName = `dashboard.GnosisControlSafeDialog.GnosisControlSafeForm.ContractInteractionSection`;
+
+interface Props {
+  disabledInput: boolean;
+}
+
+const renderAvatar = (address: Address, item: AnyUser) => (
+  <UserAvatar address={address} user={item} size="xs" notSet={false} />
+);
+
+const ContractInteractionSection = ({ disabledInput }: Props) => {
+  return (
+    <>
+      <DialogSection>
+        <div className={styles.singleUserPickerContainer}>
+          {/* @TODO: Connect available contract data with picker */}
+          <SingleUserPicker
+            data={[]}
+            label={MSG.contractLabel}
+            name="contract"
+            filter={filterUserSelection}
+            renderAvatar={renderAvatar}
+            disabled={disabledInput}
+            placeholder={MSG.userPickerPlaceholder}
+          />
+        </div>
+      </DialogSection>
+      <DialogSection>
+        <Textarea
+          label={MSG.abiLabel}
+          name="abi"
+          appearance={{ colorSchema: 'grey', resizable: 'vertical' }}
+          disabled={disabledInput}
+        />
+      </DialogSection>
+      <DialogSection appearance={{ theme: 'sidePadding' }}>
+        <div className={styles.contractFunctionSelectorContainer}>
+          {/* @TODO: Connect available contract functions data with picker */}
+          <Select
+            label={MSG.functionLabel}
+            name="contractFunction"
+            appearance={{ theme: 'grey', width: 'fluid' }}
+            placeholder={MSG.functionPlaceholder}
+            disabled={disabledInput}
+          />
+        </div>
+      </DialogSection>
+    </>
+  );
+};
+
+ContractInteractionSection.displayName = displayName;
+
+export default ContractInteractionSection;
