80dcb861e85a65d8cb39e1899a9fd56aba6200ea
danbr <danatcern@hotmail.com>
2022-06-30 09:36:24 +0300

    Feat:q:using `@colony/colony-event-metadata-parser`

---
+++ src/modules/dashboard/sagas/utils/uploadIfpsAnnotation.ts
@@ -0,0 +1,24 @@
+import { call } from 'redux-saga/effects';
+import { getStringForMetadataAnnotation } from '@colony/colony-event-metadata-parser';
+import { ipfsUpload } from '../../../core/sagas/ipfs';
+
+export function* uploadIfpsAnnotation(annotationMessage: string) {
+  const annotationMetadata = getStringForMetadataAnnotation({
+    annotationMsg: annotationMessage,
+  });
+  console.log(`🚀 ~ annotationMetadata`, annotationMetadata);
+
+  let ipfsHash: string | null = null;
+  ipfsHash = yield call(ipfsUpload, annotationMetadata);
+
+  /* If the ipfs upload failed we try again, then if it fails again we just assign
+      an empty string so that the `transactionAddParams` won't fail */
+  if (!ipfsHash) {
+    ipfsHash = yield call(ipfsUpload, annotationMetadata);
+    if (!ipfsHash) {
+      ipfsHash = '';
+    }
+  }
+
+  return ipfsHash;
+}
