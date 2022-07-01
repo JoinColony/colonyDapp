f6a9852abcbb937e83a8973d45e882ee1a70cdf7
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-07-01 13:43:16 +0300

    Feat: add checkbox filters logic to `filterMembers` helper

--- src/modules/dashboard/components/Members/filterMembers.ts
+++ src/modules/dashboard/components/Members/filterMembers.ts
@@ -1,21 +1,64 @@
+import { MEMEBERS_FILTERS } from '~dashboard/ColonyMembers/MembersFilter';
+
 import { ColonyContributor, ColonyWatcher } from '~data/index';
 
 export const filterMembers = <M extends ColonyContributor | ColonyWatcher>(
   data: M[],
-  filterValue: string,
+  filterValue?: string,
+  filtersList?: MEMEBERS_FILTERS[],
 ): M[] => {
-  if (!filterValue) {
+  const excludeBanned = !filtersList?.includes(MEMEBERS_FILTERS.BANNED);
+  const excludeVerified = !filtersList?.includes(MEMEBERS_FILTERS.VERIFIED);
+
+  /* No filters */
+  if (!filterValue && !excludeBanned && !excludeVerified) {
     return data;
   }
 
-  return data.filter(
-    (member) =>
-      member?.profile?.username
+  /* Only text filter */
+  if (filterValue && !excludeBanned && !excludeVerified) {
+    return data.filter(
+      ({ profile, id }) =>
+        profile?.username?.toLowerCase().includes(filterValue.toLowerCase()) ||
+        profile?.walletAddress
+          ?.toLowerCase()
+          .includes(filterValue.toLowerCase()) ||
+        id?.toLowerCase().includes(filterValue.toLowerCase()),
+    );
+  }
+
+  /* Only checkbox filters */
+  if (!filterValue) {
+    return data.filter(({ banned, isWhitelisted }) => {
+      if (excludeBanned && excludeVerified) {
+        return !banned && !isWhitelisted;
+      }
+
+      if (excludeBanned) {
+        return !banned;
+      }
+
+      return !isWhitelisted;
+    });
+  }
+
+  /* All the filters together */
+  return data.filter(({ banned, isWhitelisted, profile, id }) => {
+    const textFilter =
+      profile?.username?.toLowerCase().includes(filterValue?.toLowerCase()) ||
+      profile?.walletAddress
         ?.toLowerCase()
-        .includes(filterValue.toLowerCase()) ||
-      member?.profile?.walletAddress
-        ?.toLowerCase()
-        .includes(filterValue.toLowerCase()) ||
-      member?.id?.toLowerCase().includes(filterValue.toLowerCase()),
-  );
+        .includes(filterValue?.toLowerCase()) ||
+      id?.toLowerCase().includes(filterValue?.toLowerCase());
+
+    if (excludeBanned && excludeVerified) {
+      return !banned && !isWhitelisted && textFilter;
+    }
+
+    if (excludeBanned) {
+      return !banned && textFilter;
+    }
+
+    return !isWhitelisted && textFilter;
+  });
 };
