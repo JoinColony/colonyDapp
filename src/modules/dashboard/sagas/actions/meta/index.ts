a5d7d5586e8c75fdf14e0f579b068932d61526c7
Raul Glogovetan <raul@glogovetan.com>
2022-07-05 14:55:33 +0300

    Remove: unused, initial metaTx mint tokens implementation

--- src/modules/dashboard/sagas/actions/meta/index.ts
+++
@@ -1,7 +0,0 @@
-import { all, call } from 'redux-saga/effects';
-
-import mintTokensMetaActionSaga from './mintTokens';
-
-export default function* metaActionsSagas() {
-  yield all([call(mintTokensMetaActionSaga)]);
-}
