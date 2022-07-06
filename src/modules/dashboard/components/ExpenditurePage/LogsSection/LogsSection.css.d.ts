70e5f261b8bc86658701f50ef0c33d1e876511ca
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-07-06 10:34:33 +0200

    removed unnecessary code

--- src/modules/dashboard/components/ExpenditurePage/LogsSection/LogsSection.css.d.ts
+++ src/modules/dashboard/components/ExpenditurePage/LogsSection/LogsSection.css.d.ts
@@ -2,7 +2,6 @@ export const inputBackground: string;
 export const shadow: string;
 export const container: string;
 export const commentInput: string;
-export const disabledComment: string;
 export const logContainer: string;
 export const dot: string;
 export const dotContainer: string;
