37ddb5638b075708c28e25208966c7ff3b15fea4
Raul Glogovetan <raul@glogovetan.com>
2022-07-06 13:57:51 +0300

    Add: `web3` metatransaction error related utils

--- src/utils/web3/index.ts
+++ src/utils/web3/index.ts
@@ -1,7 +1,9 @@
 import BN from 'bn.js';
 import { getAddress, padZeros, hexlify } from 'ethers/utils';
+import { ClientType, ContractClient, TokenClientType } from '@colony/colony-js';
 
 import { Address } from '~types/index';
+import { TransactionError } from '~immutable/Transaction';
 
 export const createAddress = (address: string): Address => getAddress(address);
 
@@ -57,8 +59,55 @@ export const isTransactionFormat = (
   return !!potentialTransactionHash.match(hexStringRegex);
 };
 
+export const generateMetatransactionErrorMessage = (
+  emmitentClient: ContractClient,
+) =>
+  `Contract does not support MetaTransactions. ${emmitentClient.clientType}${
+    emmitentClient.clientType === ClientType.TokenClient
+      ? ` of type ${emmitentClient.tokenClientType}`
+      : ''
+  } at ${emmitentClient.address}`;
+
 export function intArrayToBytes32(arr: Array<number>) {
   return `0x${new BN(
     arr.map((num) => new BN(1).shln(num)).reduce((a, b) => a.or(b), new BN(0)),
   ).toString(16, 64)}`;
 }
+
+export const isGasStationMetatransactionError = (
+  error?: TransactionError,
+): boolean =>
+  !!error?.message.includes('Contract does not support MetaTransactions');
+
+/*
+ * Attempt to detect, poorly, if the error originated from our own contracts
+ * because we know they support Metatransactions, hence they're out of date.
+ *
+ * If the error originated from other contracts, it means they most likely
+ * don't support metatransactions
+ *
+ * Also, as a side note, this can only happen, currently, for the TokenClient
+ * contracts
+ */
+export const isMetatransactionErrorFromColonyContract = (
+  error?: TransactionError,
+): boolean => {
+  if (!error?.message) {
+    return false;
+  }
+  const CLIENT_ADDRESS_REGEX = /\.\s(.*Client)\s/;
+  const TOKEN_TYPE_REGEX = /of\stype\s(.*)\sat/;
+  let tokenType: string = TokenClientType.Erc20;
+  const [, clientType] = error.message.match(CLIENT_ADDRESS_REGEX) || [];
+  if (clientType === ClientType.TokenClient) {
+    [, tokenType] = error.message.match(TOKEN_TYPE_REGEX) || [];
+  }
+  const isCorrectClient = Object.values(ClientType).includes(
+    clientType as ClientType,
+  );
+  const isCorrectToken = tokenType === TokenClientType.Colony;
+  if (clientType === ClientType.TokenClient) {
+    return isCorrectClient && isCorrectToken;
+  }
+  return isCorrectClient;
+};
