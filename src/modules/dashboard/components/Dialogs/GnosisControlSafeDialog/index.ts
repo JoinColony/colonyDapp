352141bc791fce51d37b3a4322d25ec56c59f403
danbr <danatcern@hotmail.com>
2022-07-06 21:29:21 +0300

    feat:add separate UI component for NFT transfers

--- src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/index.ts
+++ src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/index.ts
@@ -1,2 +1,3 @@
 export { default } from './GnosisControlSafeDialog';
-export { GnosisSafe } from './GnosisControlSafeForm';
+export { default as GnosisNFTTransfer } from './GnosisNFTTransfer';
+export { GnosisSafe, NFT } from './GnosisControlSafeForm';
