3f98f591ebfd6687b062cca59a22fd7e50e2869d
willm30 <williamajmartin@gmail.com>
2022-07-08 17:22:04 +0100

    Add: mobile navigation for wallet, inbox, and coin machine

--- src/modules/pages/components/RouteLayouts/UserNavigation.css.d.ts
+++ src/modules/pages/components/RouteLayouts/UserNavigation.css.d.ts
@@ -15,5 +15,4 @@ export const walletWrapper: string;
 export const readyTransactionsCount: string;
 export const reputation: string;
 export const walletAutoLogin: string;
-export const fade: string;
 export const gasStationReference: string;
