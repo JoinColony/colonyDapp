c56b3977bce52890c16d0b2c4df424f3555a2632
Raul Glogovetan <raul@glogovetan.com>
2022-07-11 13:53:38 +0300

    Revert: re-enable correct network version check
    Including for when user has metatransactions enabled

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -1,9 +1,5 @@
 import { Network } from '@colony/colony-js';
 
-import { ContextModule, TEMP_getContext } from '~context/index';
-import { SlotKey } from '~context/userSettings';
-import { canUseMetatransactions } from '../modules/users/checks';
-
 import { DEFAULT_NETWORK, NETWORK_DATA, SUPPORTED_NETWORKS } from '~constants';
 
 export const checkIfNetworkIsAllowed = (
@@ -14,29 +10,9 @@ export const checkIfNetworkIsAllowed = (
   const currentNetworkData =
     NETWORK_DATA[process.env.NETWORK || DEFAULT_NETWORK];
 
-  let userSettings;
-  let userHasMetatransactionEnabled;
-  try {
-    userSettings = TEMP_getContext(ContextModule.UserSettings);
-    userHasMetatransactionEnabled = userSettings.getSlotStorageAtKey(
-      SlotKey.Metatransactions,
-    );
-  } catch (error) {
-    /*
-     * This is just needed when developing locally, since the Hot Module Reloader
-     * gets tripped up by the context not existing for a brief moment.
-     * It's basically a race condition
-     */
-    // silent error
-  }
-
-  const willSendMetaTransactions =
-    userHasMetatransactionEnabled && canUseMetatransactions();
-
   return (
-    willSendMetaTransactions ||
-    (walletSupportedNetwork &&
-      (walletSupportedNetwork.chainId === currentNetworkData.chainId ||
-        onLocalDevEnvironment))
+    walletSupportedNetwork &&
+    (walletSupportedNetwork.chainId === currentNetworkData.chainId ||
+      onLocalDevEnvironment)
   );
 };
