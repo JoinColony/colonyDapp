ea61e4a6985b0c62474796708365430c4a1abb90
Raul Glogovetan <raul@glogovetan.com>
2022-07-14 15:57:12 +0300

    Update: gnosis chain rpc url

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -79,7 +79,7 @@ export const XDAI_NETWORK: NetworkInfo = {
   blockExplorerUrl: 'https://blockscout.com/poa/xdai',
   tokenExplorerLink: 'https://blockscout.com/poa/xdai/tokens',
   contractAddressLink: 'https://blockscout.com/poa/xdai/address',
-  rpcUrl: 'https://rpc.xdaichain.com',
+  rpcUrl: 'https://rpc.gnosischain.com',
 };
 
 export const ETHEREUM_NETWORK: NetworkInfo = {
