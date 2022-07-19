d25d705c80ec52ca387ccc24b30280036112b4c8
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-07-19 09:23:09 +0200

    fixed types

--- src/modules/pages/components/ExpenditurePage/ExpenditureForm.tsx
+++ src/modules/pages/components/ExpenditurePage/ExpenditureForm.tsx
@@ -5,34 +5,28 @@ import { ExpenditureSettings } from '~dashboard/ExpenditurePage';
 import Payments from '~dashboard/ExpenditurePage/Payments';
 import Split from '~dashboard/ExpenditurePage/Split';
 import { Colony } from '~data/index';
+import { ValuesType } from './ExpenditurePage';
 
 interface Props {
   colony: Colony;
   sidebarRef: HTMLElement | null;
 }
 
-const hasExpenditureKey = (obj: any): obj is { expenditure: string } => {
-  return Object.prototype.hasOwnProperty.call(obj, 'expenditure');
-};
-
 const ExpenditureForm = ({ sidebarRef, colony }: Props) => {
-  const { values } = useFormikContext() || {};
+  const { values } = useFormikContext<ValuesType>() || {};
 
   const secondFormSection = useMemo(() => {
-    if (hasExpenditureKey(values)) {
-      const expenditureType = values.expenditure;
-      switch (expenditureType) {
-        case 'advanced': {
-          return <Payments sidebarRef={sidebarRef} colony={colony} />;
-        }
-        case 'split': {
-          return <Split />;
-        }
-        default:
-          return null;
+    const expenditureType = values.expenditure;
+    switch (expenditureType) {
+      case 'advanced': {
+        return <Payments sidebarRef={sidebarRef} colony={colony} />;
       }
+      case 'split': {
+        return <Split />;
+      }
+      default:
+        return null;
     }
-    return null;
   }, [colony, sidebarRef, values]);
 
   return (
