d17d1f83ef99a098d29080171f71cc7614f508ec
ArmandoGraterol <armandogrtrl@gmail.com>
2022-07-21 19:50:11 -0300

    Connected Remove Safes dialog

--- src/modules/dashboard/components/Dialogs/RemoveSafeDialog/RemoveSafeDialog.tsx
+++ src/modules/dashboard/components/Dialogs/RemoveSafeDialog/RemoveSafeDialog.tsx
@@ -1,53 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FormikProps } from 'formik';
+import { useHistory } from 'react-router';
 
 import Dialog, { DialogProps, ActionDialogProps } from '~core/Dialog';
 import { ActionForm } from '~core/Fields';
 import { Address } from '~types/index';
 
-// import { Colony } from '~data/index';
 import { ActionTypes } from '~redux/index';
 import { WizardDialogType } from '~utils/hooks';
+import { pipe, withMeta, mapPayload } from '~utils/actions';
 
-import { Safe } from './types';
 import DialogForm from './RemoveSafeDialogForm';
 
 const displayName = 'dashboard.RemoveSafeDialog';
-
-// Mock data for testing
-const safes: Safe[] = [
-  {
-    name: 'All Saints',
-    chain: `Gnosis Chain`,
-    address: '0x3a157280ca91bB49dAe3D1619C55Da7F9D4438c2',
-  },
-  // {
-  //   name: '',
-  //   chain: `Mainnet`
-  //   address: '0x4a157280ca91bB49dAe3D1619C55Da7F9D4438c3',
-  // },
-  // {
-  //   name: 'Big safe',
-  //  chain: `Mainnet`
-  //   address: '0x5a157280ca91bB49dAe3D1619C55Da7F9D4438c2',
-  // },
-  // {
-  //   name: `Just a test with a very very long name,
-  //   hang on its even longer!!!!!!!!`,
-  //  chain: `Mainnet`
-  //   address: '0x6a157280ca91bB49dAe3D1619C55Da7F9D4438c3',
-  // },
-  // {
-  //   name: 'blalabalabal',
-  //  chain: `Mainnet`
-  //   address: '0x7a157280ca91bB49dAe3D1619C55Da7F9D4438c2',
-  // },
-  // {
-  //   name: 'final test',
-  //   chain: `Mainnet`
-  //   address: '0x8a157280ca91bB49dAe3D1619C55Da7F9D4438c3',
-  // },
-];
 
 export interface FormValues {
   safeList: Address[];
@@ -64,17 +29,35 @@ const RemoveSafeDialog = ({
   prevStep,
   colony,
 }: Props) => {
+  const history = useHistory();
+  const { safes } = colony;
+
+  const transform = useCallback(
+    pipe(
+      mapPayload(({ safeList, annotation: annotationMessage }) => {
+        return {
+          colonyName: colony.colonyName,
+          colonyAddress: colony.colonyAddress,
+          safeAddresses: safeList,
+          annotationMessage,
+        };
+      }),
+      withMeta({ history }),
+    ),
+    [],
+  );
+
   return (
     <ActionForm
       initialValues={{
         // if there's only 1 safe then that safe is already checked.
-        safeList: safes.length === 1 ? [safes[0].address] : [],
+        safeList: safes.length === 1 ? [safes[0].contractAddress] : [],
       }}
-      // @TODO need to update action, in another PR
-      submit={ActionTypes.COLONY_ACTION_GENERIC}
-      error={ActionTypes.COLONY_ACTION_GENERIC_ERROR}
-      success={ActionTypes.COLONY_ACTION_GENERIC_SUCCESS}
+      submit={ActionTypes.COLONY_ACTION_REMOVE_EXISTING_SAFES}
+      error={ActionTypes.COLONY_ACTION_REMOVE_EXISTING_SAFES_ERROR}
+      success={ActionTypes.COLONY_ACTION_REMOVE_EXISTING_SAFES_SUCCESS}
       onSuccess={close}
+      transform={transform}
     >
       {(formValues: FormikProps<FormValues>) => {
         return (
