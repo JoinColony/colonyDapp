47e0f9a8bfecdc4b02464a27312a5fa192c3a482
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-07-21 15:04:18 +0300

    Refactor: extract Preview transaction and streamline code

---
+++ src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/constants.ts
@@ -0,0 +1,6 @@
+export enum TransactionTypes {
+  TRANSFER_FUNDS = 'transferFunds',
+  TRANSFER_NFT = 'transferNft',
+  CONTRACT_INTERACTION = 'contractInteraction',
+  RAW_TRANSACTION = 'rawTransaction',
+}
