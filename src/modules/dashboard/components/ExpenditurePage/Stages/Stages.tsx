78e55af29551632fdc0a3e8190b40d4616036ac5
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-07-22 09:14:28 +0200

    changed formik validation to trigger on change before first submit

--- src/modules/dashboard/components/ExpenditurePage/Stages/Stages.tsx
+++ src/modules/dashboard/components/ExpenditurePage/Stages/Stages.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 import copyToClipboard from 'copy-to-clipboard';
 import classNames from 'classnames';
+import { isEmpty } from 'lodash';
 
 import Button from '~core/Button';
 import Icon from '~core/Icon';
@@ -171,7 +172,11 @@ const Stages = ({
                   </div>
                 </Tooltip>
               </Button>
-              <Button onClick={handleSaveDraft} style={buttonStyles}>
+              <Button
+                onClick={handleSaveDraft}
+                style={buttonStyles}
+                disabled={!isEmpty(errors)}
+              >
                 <FormattedMessage {...MSG.submitDraft} />
               </Button>
             </>
