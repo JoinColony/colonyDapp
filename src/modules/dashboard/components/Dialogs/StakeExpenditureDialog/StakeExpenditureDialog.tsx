5b2849639a2903728571090319074b6e86b13770
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-07-25 13:27:10 +0200

    removed StakeExpenditureDialogForm

--- src/modules/dashboard/components/Dialogs/StakeExpenditureDialog/StakeExpenditureDialog.tsx
+++ src/modules/dashboard/components/Dialogs/StakeExpenditureDialog/StakeExpenditureDialog.tsx
@@ -1,4 +1,6 @@
 import React, { useCallback, useState } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { FormikProps } from 'formik';
 
 import { COLONY_TOTAL_BALANCE_DOMAIN_ID } from '~constants';
 import Dialog, { DialogSection } from '~core/Dialog';
@@ -65,6 +67,10 @@ const MSG = defineMessages({
 });
 
 const displayName = 'dashboard.StakeExpenditureDialog';
+
+interface FormValues {
+  forceAction: boolean;
+}
 
 type Props = {
   onClick: () => void;
