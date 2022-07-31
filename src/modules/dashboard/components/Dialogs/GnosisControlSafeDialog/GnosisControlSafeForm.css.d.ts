3bcf15bcd788b28571a99f5e35c6a2ac8d0de13e
Julianahlk <julianayasnog@gmail.com>
2022-08-01 00:07:34 +0200

    Feat: add support for multiple transaction preview

--- src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/GnosisControlSafeForm.css.d.ts
+++ src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/GnosisControlSafeForm.css.d.ts
@@ -1,6 +1,7 @@
 export const wideButton: string;
 export const width: string;
 export const heading: string;
+export const footer: string;
 export const safePicker: string;
 export const transactionTypeSelectContainer: string;
 export const addTransaction: string;
