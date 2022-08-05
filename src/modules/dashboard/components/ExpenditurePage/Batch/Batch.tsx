5ea4ba24343056225652888b7028cc02bf7b0aad
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-08-05 08:48:08 +0200

    added CSVUploader

--- src/modules/dashboard/components/ExpenditurePage/Batch/Batch.tsx
+++ src/modules/dashboard/components/ExpenditurePage/Batch/Batch.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import { useField } from 'formik';
 import Button from '~core/Button';
-import CSVUploader from '~core/CSVUploader';
 import { FormSection } from '~core/Fields';
-
 import { Colony } from '~data/index';
 
-import styles from './Batch.css';
-import DownloadTemplate from './DownloadTemplate/DownloadTemplate';
 import { Batch as BatchType } from './types';
+import styles from './Batch.css';
+import { SpinnerLoader } from '~core/Preloaders';
+import UploadAddressesWidget from '~dashboard/Whitelist/UploadAddressesWidget';
+import CSVUploader from './CSVUploader/CSVUploader';
 
 export const MSG = defineMessages({
   batch: {
@@ -31,48 +32,75 @@ export const MSG = defineMessages({
     id: 'dashboard.ExpenditurePage.Batch.hide',
     defaultMessage: 'Hide',
   },
+  viewAll: {
+    id: 'dashboard.ExpenditurePage.Batch.viewAll',
+    defaultMessage: 'View all',
+  },
 });
 
 const displayName = 'dashboard.ExpenditurePage.Batch';
 
 interface Props {
-  batch: BatchType;
+  batch?: BatchType;
   colony: Colony;
 }
 
-const Batch = () => {
+const Batch = ({ colony }: Props) => {
   const [processingCSVData, setProcessingCSVData] = useState<boolean>(false);
   const [isOpen, setOpen] = useState<boolean>(false);
   const { formatMessage } = useIntl();
+  const [, { value: batch }] = useField('batch');
+
+  useEffect(() => {
+    if (processingCSVData) {
+      setOpen(false);
+    }
+  }, [processingCSVData]);
 
   return (
     <div className={styles.batchContainer}>
       <FormSection appearance={{ border: 'bottom' }}>
         <div className={styles.wrapper}>
           <FormattedMessage {...MSG.batch} />
-          <DownloadTemplate />
+          <Button appearance={{ theme: 'blue' }} type="button">
+            {formatMessage(MSG.downloadTemplate)}
+          </Button>
         </div>
       </FormSection>
       <FormSection appearance={{ border: 'bottom' }}>
         <div className={styles.data}>
           <FormattedMessage {...MSG.data} />
-          <Button
-            appearance={{ theme: 'blue' }}
-            type="button"
-            onClick={() => setOpen((open) => !open)}
-          >
-            {formatMessage(isOpen ? MSG.hide : MSG.upload)}
-          </Button>
+          <div>
+            {processingCSVData && <SpinnerLoader />}
+            {!isOpen && (
+              <Button
+                appearance={{ theme: 'blue' }}
+                type="button"
+                onClick={() => setOpen((open) => !open)}
+              >
+                {formatMessage(MSG.upload)}
+              </Button>
+            )}
+            {batch?.dataCSVUploader?.uploaded && (
+              <Button
+                appearance={{ theme: 'blue' }}
+                type="button"
+                onClick={() => {}}
+              >
+                {formatMessage(MSG.upload)}
+              </Button>
+            )}
+          </div>
         </div>
-        {isOpen && (
-          <CSVUploader
-            name="whitelistCSVUploader"
-            error={undefined}
-            processingData={processingCSVData}
-            setProcessingData={setProcessingCSVData}
-          />
-        )}
+        <CSVUploader
+          name="batch.dataCSVUploader"
+          error={undefined}
+          processingData={processingCSVData}
+          setProcessingData={setProcessingCSVData}
+          isOpen={isOpen}
+        />
       </FormSection>
+      <UploadAddressesWidget colony={colony} />
     </div>
   );
 };
