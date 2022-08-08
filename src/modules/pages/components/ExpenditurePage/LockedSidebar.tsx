11caedc0c21a651488fd9e797d45f6fc09f25e24
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-08-08 09:26:48 +0200

    added displaying data

--- src/modules/pages/components/ExpenditurePage/LockedSidebar.tsx
+++ src/modules/pages/components/ExpenditurePage/LockedSidebar.tsx
@@ -24,7 +24,7 @@ const LockedSidebar = ({ colony, formValues }: Props) => {
         return <LockedPayments recipients={recipients} colony={colony} />;
       }
       case ExpenditureTypes.Batch: {
-        return <Batch />;
+        return <Batch colony={colony} />;
       }
       default:
         return null;
