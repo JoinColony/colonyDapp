e4a477b65a5f4d7e1cc1d492e4348a5492bb5259
Julianahlk <julianayasnog@gmail.com>
2022-08-10 11:38:10 +0200

    Feat: create details widget for decisions

---
+++ src/modules/dashboard/components/ColonyDecisions/DetailsWidget/DetailsWidget.css.d.ts
@@ -0,0 +1,6 @@
+export const item: string;
+export const value: string;
+export const label: string;
+export const text: string;
+export const domainDescriptionItem: string;
+export const domainDescription: string;
