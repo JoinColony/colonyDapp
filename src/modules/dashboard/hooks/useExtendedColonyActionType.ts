c41ad103d7d39f676adc84b7c7dada3b95c18fda
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-08-11 12:29:43 +0300

    Feat: add `useExtendedColonyActionType` hook

---
+++ src/modules/dashboard/hooks/useExtendedColonyActionType.ts
@@ -0,0 +1,45 @@
+import { useMemo } from 'react';
+
+import { Colony } from '~data/index';
+import { ColonyActionQuery } from '~data/generated';
+
+import { ColonyActions, ColonyExtendedActions } from '~types/index';
+
+import useColonyMetadataChecks from './useColonyMetadataChecks';
+
+const useExtendedColonyActionType = (
+  actionType: string,
+  colony: Colony,
+  transactionHash: string,
+  colonyAction: ColonyActionQuery['colonyAction'],
+) => {
+  const {
+    verifiedAddressesChanged,
+    tokensChanged,
+    removedSafes,
+  } = useColonyMetadataChecks(
+    actionType,
+    colony,
+    transactionHash,
+    colonyAction,
+  );
+
+  return useMemo(() => {
+    if (actionType === ColonyActions.ColonyEdit) {
+      if (verifiedAddressesChanged) {
+        return ColonyExtendedActions.AddressBookUpdated;
+      }
+
+      if (tokensChanged) {
+        return ColonyExtendedActions.TokensUpdated;
+      }
+
+      if ((removedSafes || []).length > 0) {
+        return ColonyExtendedActions.SafeRemoved;
+      }
+    }
+    return actionType;
+  }, [actionType, verifiedAddressesChanged, tokensChanged, removedSafes]);
+};
+
+export default useExtendedColonyActionType;
