2931e8eac2f407b4c66cc83ddab0e8672ab0286f
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-08-11 12:29:43 +0300

    Feat: add `useExtendedColonyActionType` hook

---
+++ src/modules/dashboard/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useColonyMembersSorting } from './useColonyMembersSorting';
+export { default as useColonyMetadataChecks } from './useColonyMetadataChecks';
+export { default as useReputationEarned } from './useReputationEarned';
+export { default as useExtendedColonyActionType } from './useExtendedColonyActionType';
