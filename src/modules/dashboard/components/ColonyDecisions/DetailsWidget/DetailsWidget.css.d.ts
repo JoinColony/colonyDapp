8e46447c6f030ca8704707b45bfe0964acac88f8
Julianahlk <julianayasnog@gmail.com>
2022-08-13 18:22:48 +0200

    Feat: add mock data

--- src/modules/dashboard/components/ColonyDecisions/DetailsWidget/DetailsWidget.css.d.ts
+++ src/modules/dashboard/components/ColonyDecisions/DetailsWidget/DetailsWidget.css.d.ts
@@ -2,5 +2,3 @@ export const item: string;
 export const value: string;
 export const label: string;
 export const text: string;
-export const domainDescriptionItem: string;
-export const domainDescription: string;
