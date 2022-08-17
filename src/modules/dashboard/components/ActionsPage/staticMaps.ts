0cf4e1c48150efd9d657fcafabb66ce487b95f0b
Julianahlk <julianayasnog@gmail.com>
2022-08-17 22:56:09 +0200

    Refactor: add missing type for icons map

--- src/modules/dashboard/components/ActionsPage/staticMaps.ts
+++ src/modules/dashboard/components/ActionsPage/staticMaps.ts
@@ -98,6 +98,7 @@ export const ACTION_TYPES_ICONS_MAP: {
   [ColonyMotions.EmitDomainReputationPenaltyMotion]: 'emoji-firebolt',
   [ColonyMotions.EmitDomainReputationRewardMotion]: 'emoji-shooting-star',
   [ColonyMotions.UnlockTokenMotion]: 'emoji-padlock',
+  [ColonyActions.Decision]: 'circle-check-primary',
   [ColonyActions.Generic]: 'circle-check-primary',
 };
 
