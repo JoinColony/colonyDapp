b9971cf36f50e81ba0bfe1625a689151636bdf29
ArmandoGraterol <armandogrtrl@gmail.com>
2022-08-17 22:55:56 -0300

    Updated styles of ContractInteractionSection

--- src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/TransactionTypesSection.css.d.ts
+++ src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/TransactionTypesSection.css.d.ts
@@ -1,3 +1,4 @@
 export const singleUserPickerContainer: string;
 export const contractFunctionSelectorContainer: string;
 export const labelDescription: string;
+export const inputParamContainer: string;
