96c877eb82de4fe733b5c76fdf7b8692fdde6ec1
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-08-19 16:29:30 +0200

    added data validation and message when all data are invalid

--- src/modules/dashboard/components/ExpenditurePage/Batch/Batch.tsx
+++ src/modules/dashboard/components/ExpenditurePage/Batch/Batch.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 import { useField } from 'formik';
 import classNames from 'classnames';
