84cf957e70bc653544afdfeb1242b66cff026272
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-08-19 16:29:30 +0200

    added data validation and message when all data are invalid

---
+++ src/modules/dashboard/components/ExpenditurePage/Batch/hooks.ts
@@ -0,0 +1,77 @@
+import { useMemo } from 'react';
+import { Colony, useMembersSubscription } from '~data/index';
+import { BatchDataItem } from './types';
+
+export const useCalculateBatchPayment = (
+  colony: Colony,
+  data?: BatchDataItem[],
+) => {
+  const { data: colonyMembers } = useMembersSubscription({
+    variables: { colonyAddress: colony.colonyAddress || '' },
+  });
+
+  return useMemo(() => {
+    if (!data) {
+      return null;
+    }
+    const { tokens: colonyTokens } = colony || {};
+
+    const validatedData = data.map(({ recipient, amount, token }) => {
+      const correctRecipient = colonyMembers?.subscribedUsers.find(
+        (user) => user.id === recipient,
+      );
+      const correctToken = colonyTokens?.find(
+        (tokenItem) => tokenItem.id === token,
+      );
+      return {
+        recipient: correctRecipient ? recipient : undefined,
+        amount,
+        token: correctToken || undefined,
+      };
+    });
+
+    const filteredData = data.filter((item) => {
+      const correctRecipient = colonyMembers?.subscribedUsers.find(
+        (user) => user.id === item.recipient,
+      );
+      const correctToken = colonyTokens?.find(
+        (tokenItem) => tokenItem.id === item.token,
+      );
+      return !!item.amount && correctRecipient && correctToken;
+    });
+
+    const recipientsCount = data.filter(({ recipient }) =>
+      colonyMembers?.subscribedUsers.find((user) => user.id === recipient),
+    ).length;
+
+    const amount = filteredData.reduce((acc, item) => {
+      if (item.token in acc) {
+        return {
+          ...acc,
+          [item.token]: acc[item.token] + Number(item.amount),
+        };
+      }
+      return { ...acc, [item.token]: Number(item.amount) };
+    }, {});
+
+    const tokens = Object.entries(amount || {})?.map(
+      ([tokenId, tokenValue]) => {
+        const token = colonyTokens?.find(
+          (tokenItem) => tokenItem.id === tokenId,
+        );
+
+        return {
+          value: Number(tokenValue),
+          token,
+        };
+      },
+    );
+
+    return {
+      tokens,
+      recipientsCount,
+      invalidRows: data.length !== filteredData.length,
+      validatedData,
+    };
+  }, [data, colony, colonyMembers]);
+};
