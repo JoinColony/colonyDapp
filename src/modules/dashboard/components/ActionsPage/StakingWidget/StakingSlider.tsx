4d5cd8eec87a20e15ecf8401c3bdb44dd2f4c897
Julianahlk <julianayasnog@gmail.com>
2022-08-31 21:02:33 +0200

    Refactor: fix StakingSlider ids and displayName

--- src/modules/dashboard/components/ActionsPage/StakingWidget/StakingSlider.tsx
+++ src/modules/dashboard/components/ActionsPage/StakingWidget/StakingSlider.tsx
@@ -33,35 +33,35 @@ interface Props extends StakingAmounts {
   isObjection: boolean;
 }
 
-const displayName = 'StakingSlider';
+const displayName = 'dashboard.ActionsPage.StakingWidget.StakingSlider';
 
 const MSG = defineMessages({
   titleStake: {
-    id: 'dashboard.ActionsPage.StakingSlider.title',
+    id: 'dashboard.ActionsPage.StakingWidget.StakingSlider.title',
     defaultMessage: `Select the amount to back the motion`,
   },
   titleObject: {
-    id: 'dashboard.ActionsPage.StakingSlider.title',
+    id: 'dashboard.ActionsPage.StakingWidget.StakingSlider.title',
     defaultMessage: `Select the amount to stake the objection`,
   },
   descriptionStake: {
-    id: 'dashboard.ActionsPage.StakingSlider.description',
+    id: 'dashboard.ActionsPage.StakingWidget.StakingSlider.description',
     defaultMessage: `Stake is returned if the motion passes. If there is a dispute, and the motion loses, part or all of your stake will be lost.`,
   },
   descriptionObject: {
-    id: 'dashboard.ActionsPage.StakingSlider.description',
+    id: 'dashboard.ActionsPage.StakingWidget.StakingSlider.description',
     defaultMessage: `Stake will be returned if the objection succeeds. If the objection fails, part or all of your stake will be lost.`,
   },
   loading: {
-    id: 'dashboard.ActionsPage.StakingSlider.loading',
+    id: 'dashboard.ActionsPage.StakingWidget.StakingSlider.loading',
     defaultMessage: 'Loading staking values ...',
   },
   minimumAmount: {
-    id: 'dashboard.ActionsPage.StakingSlider.minimumAmount',
+    id: 'dashboard.ActionsPage.StakingWidget.StakingSlider.minimumAmount',
     defaultMessage: 'at least {minStake}',
   },
   stakingToolTip: {
-    id: 'dashboard.ActionsPage.StakingSlider.stakingToolTip',
+    id: 'dashboard.ActionsPage.StakingWidget.StakingSlider.stakingToolTip',
     defaultMessage: `Staking involves committing an amount of tokens as collateral to support your decision.`,
   },
 });
