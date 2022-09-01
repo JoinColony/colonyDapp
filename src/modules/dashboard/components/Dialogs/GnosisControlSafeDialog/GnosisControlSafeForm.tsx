ba19cdb12023c88e6e0f940f4df6c1b8666a99ce
willm30 <williamajmartin@gmail.com>
2022-09-01 14:51:19 +0200

    Fix: select safe width

--- src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/GnosisControlSafeForm.tsx
+++ src/modules/dashboard/components/Dialogs/GnosisControlSafeDialog/GnosisControlSafeForm.tsx
@@ -272,7 +272,6 @@ const GnosisControlSafeForm = ({
           <DialogSection>
             <div className={styles.safePicker}>
               <SingleSafePicker
-                appearance={{ width: 'wide' }}
                 label={MSG.selectSafe}
                 name="safe"
                 filter={filterUserSelection}
