6f9419c7992d9533c8698822571e483a3daf0b84
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-09-01 12:09:58 +0300

    Feat: add try catch block & extra types to `uploadIfsWithFallback`

--- src/modules/dashboard/sagas/utils/uploadIfsWithFallback.ts
+++ src/modules/dashboard/sagas/utils/uploadIfsWithFallback.ts
@@ -1,14 +1,22 @@
 import { call } from 'redux-saga/effects';
+
+import { log } from '~utils/debug';
+
 import { ipfsUpload } from '../../../core/sagas/ipfs';
 
-export function* uploadIfsWithFallback(annotationMessage: string) {
+export function* uploadIfsWithFallback(annotationMessage: string | object) {
   let ipfsHash: string | null = null;
-  ipfsHash = yield call(
-    ipfsUpload,
-    JSON.stringify({
-      annotationMessage,
-    }),
-  );
+  try {
+    ipfsHash = yield call(
+      ipfsUpload,
+      JSON.stringify({
+        annotationMessage,
+      }),
+    );
+  } catch (error) {
+    log.verbose('Could not upload the colony metadata IPFS. Retrying...');
+    log.verbose(error);
+  }
 
   /* If the ipfs upload failed we try again, then if it fails again we just assign
       an empty string so that the `transactionAddParams` won't fail */
