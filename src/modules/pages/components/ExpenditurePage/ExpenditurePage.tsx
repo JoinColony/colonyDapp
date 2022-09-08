c290d6b2bd1135c20320c49f1e0fe3054c9a302c
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-09-08 09:16:00 +0200

    added limit

--- src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
+++ src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
@@ -49,6 +49,7 @@ import LockedSidebar from './LockedSidebar';
 import { ExpenditureTypes, ValuesType } from './types';
 import { initialValues, validationSchema } from './constants';
 import styles from './ExpenditurePage.css';
+import { newFundingSource } from '~dashboard/ExpenditurePage/ExpenditureSettings/Streaming/constants';
 
 const displayName = 'pages.ExpenditurePage';
 
