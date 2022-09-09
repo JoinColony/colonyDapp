e99b64f6e8f83f124af567eb0dcb4365c94c5a9f
joanna <joanna.waszkiewicz@pagepro.pl>
2022-09-09 14:11:19 +0200

    fix: empty lines between imports

--- src/modules/dashboard/components/ExpenditurePage/ExpenditureSettings/ExpenditureStreamingForm.tsx
+++ src/modules/dashboard/components/ExpenditurePage/ExpenditureSettings/ExpenditureStreamingForm.tsx
@@ -11,6 +11,7 @@ import { ExpenditureEndDateTypes } from '~pages/ExpenditurePage/types';
 
 import { supRenderAvatar } from '../Recipient/Recipient';
 
+import { Props } from './ExpenditureSettings';
 import styles from './ExpenditureSettings.css';
 
 export const MSG = defineMessages({
