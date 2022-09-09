1f7a4bfc27892e2f1f36679e6cc7d872fbcc0b8d
joanna <joanna.waszkiewicz@pagepro.pl>
2022-09-09 13:30:46 +0200

    fix: lint

--- src/modules/dashboard/components/ExpenditurePage/ExpenditureSettings/ExpenditureSettings.tsx
+++ src/modules/dashboard/components/ExpenditurePage/ExpenditureSettings/ExpenditureSettings.tsx
@@ -16,6 +16,18 @@ export const MSG = defineMessages({
     id: 'dashboard.ExpenditurePage.ExpenditureSettings.type',
     defaultMessage: 'Expenditure type',
   },
+  team: {
+    id: 'dashboard.ExpenditurePage.ExpenditureSettings.team',
+    defaultMessage: 'Team',
+  },
+  balance: {
+    id: 'dashboard.ExpenditurePage.ExpenditureSettings.balance',
+    defaultMessage: 'Balance',
+  },
+  owner: {
+    id: 'dashboard.ExpenditurePage.ExpenditureSettings.owner',
+    defaultMessage: 'Owner',
+  },
   advancedPayment: {
     id: 'dashboard.ExpenditurePage.ExpenditureSettings.advancedPayment',
     defaultMessage: 'Advanced payment',
