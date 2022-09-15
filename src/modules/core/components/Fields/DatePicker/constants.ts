62283d75d6075e1ddc8fbcc80c7f79edcffd691e
jakubcolony <jakub@colony.io>
2022-09-15 19:27:28 +0100

    Feat: Basic TimePicker in DatePicker

---
+++ src/modules/core/components/Fields/DatePicker/constants.ts
@@ -0,0 +1,16 @@
+export const MONTH_NAMES = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const TIME_FORMAT = 'h:mm aaa';
