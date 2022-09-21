9f62e28057b3bfd1372a209d261016ca52d210c1
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-09-21 11:26:36 +0200

    moved 'Streaming' folder to 'Expenditures', renamed fundingSource to fundingSources

--- src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
+++ src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
@@ -214,19 +214,15 @@ const ExpenditurePage = ({ match }: Props) => {
         setActiveStageId(Stage.Draft);
       }
 
-      if (values.expenditure === ExpenditureTypes.Split) {
-        const recipientsCount =
-          values.split.recipients?.filter(
-            (recipient) => recipient?.user?.id !== undefined,
-          ).length || 0;
+      if (values.expenditure === ExpenditureTypes.Streaming) {
+        lockValues();
+        setMotion({
+          type: MotionType.StartStream,
+          status: MotionStatus.Pending,
+        });
+        setFormValues(values);
 
-        setTimeout(() => {
-          setMotion({
-            type: MotionType.StartStream,
-            status: MotionStatus.Failed,
-          });
-        }, 5000);
-
+        // it's temporary timeout
         setTimeout(() => {
           setMotion({
             type: MotionType.StartStream,
