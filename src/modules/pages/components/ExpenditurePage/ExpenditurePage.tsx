e0ca3cedf7d95bfe1a3b9e094c1eb10059fc2529
jakubcolony <jakub@colony.io>
2022-09-23 20:18:46 +0100

    Delete old DatePicker component

--- src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
+++ src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
@@ -188,6 +188,7 @@ const ExpenditurePage = ({ match }: Props) => {
           },
         },
         streaming: {
+          ...initialValues.streaming,
           fundingSources: [
             {
               ...newFundingSource,
