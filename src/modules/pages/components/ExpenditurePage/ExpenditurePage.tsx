b39a8bfc0f7c1a0aef5bdf15fdbd145bbe51ba0f
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-09-26 11:58:41 +0200

    fixed LockedSidebar

--- src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
+++ src/modules/pages/components/ExpenditurePage/ExpenditurePage.tsx
@@ -576,6 +576,7 @@ const ExpenditurePage = ({ match }: Props) => {
             pendingMotion={motion?.status === MotionStatus.Pending}
             activeStage={stages.find((stage) => stage.id === activeStageId)}
             handleReleaseMilestone={handleReleaseMilestone}
+            states={states}
           />
         )}
       </aside>
