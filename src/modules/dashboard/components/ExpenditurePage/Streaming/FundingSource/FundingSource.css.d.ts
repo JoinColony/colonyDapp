3d0998a145e2bfb78059cc28d0682efab5f1350f
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-09-28 13:33:40 +0200

    fixed styles

--- src/modules/dashboard/components/ExpenditurePage/Streaming/FundingSource/FundingSource.css.d.ts
+++ src/modules/dashboard/components/ExpenditurePage/Streaming/FundingSource/FundingSource.css.d.ts
@@ -1,7 +1,6 @@
 export const activeItem: string;
 export const activeItemLabel: string;
 export const formContainer: string;
-export const marginBottom: string;
 export const settingsRow: string;
 export const inputRateAmount: string;
 export const tokenWrapper: string;
@@ -11,3 +10,4 @@ export const selectWrapper: string;
 export const slash: string;
 export const inputWrapper: string;
 export const addTokenLabel: string;
+export const paddingTopZero: string;
