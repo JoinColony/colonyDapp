f5a4764593a8e7ca8ccc290598f3b9d8c9cb724b
ArmandoGraterol <armandogrtrl@gmail.com>
2022-09-29 20:19:14 -0300

    Removed balanceLoading classname in favor of reusing spinner in TransactionTypesSection

--- src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/TransactionTypesSection.css.d.ts
+++ src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/TransactionTypesSection.css.d.ts
@@ -2,5 +2,5 @@ export const singleUserPickerContainer: string;
 export const contractFunctionSelectorContainer: string;
 export const labelDescription: string;
 export const inputParamContainer: string;
-export const balanceLoading: string;
 export const balanceError: string;
+export const spinner: string;
