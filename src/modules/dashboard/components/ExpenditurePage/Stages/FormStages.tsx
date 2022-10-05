e88eb5c67abf3a455f76c2c08949ca0e9328df89
joanna <joanna.waszkiewicz@pagepro.pl>
2022-10-05 09:19:18 +0200

    feat: base of the message info on submit

--- src/modules/dashboard/components/ExpenditurePage/Stages/FormStages.tsx
+++ src/modules/dashboard/components/ExpenditurePage/Stages/FormStages.tsx
@@ -1,5 +1,6 @@
 import { useFormikContext, setNestedObjectValues, FormikTouched } from 'formik';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
 
 import { useDialog } from '~core/Dialog';
 import DeleteDraftDialog from '~dashboard/Dialogs/DeleteDraftDialog/DeleteDraftDialog';
@@ -15,7 +16,20 @@ import { Stage } from './constants';
 import Stages from './Stages';
 import StreamingStages from './StreamingStages';
 
+import styles from './FormStages.css';
+
 const displayName = 'dashboard.ExpenditurePage.Stages.FormStages';
+
+const MSG = defineMessages({
+  errorMessage: {
+    id: 'dashboard.ExpenditurePage.Stages.FormStages.errorMessage',
+    defaultMessage: '{number} required field has an error.',
+  },
+  errorMessageAction: {
+    id: 'dashboard.ExpenditurePage.Stages.FormStages.errorMessageAction',
+    defaultMessage: 'Fix it!',
+  },
+});
 
 interface Props {
   states: State[];
@@ -38,24 +52,24 @@ const FormStages = ({
     useFormikContext<ValuesType>() || {};
   const openDeleteDraftDialog = useDialog(DeleteDraftDialog);
   const openDraftConfirmDialog = useDialog(StakeExpenditureDialog);
+  const [fieldErrors, setFieldErrors] = useState<number>(0);
 
   const handleSaveDraft = useCallback(async () => {
     const errors = await validateForm(values);
     const hasErrors = Object.keys(errors)?.length;
     setTouched(setNestedObjectValues<FormikTouched<ValuesType>>(errors, true));
+    setFieldErrors(errorsLength);
 
-    return (
-      !hasErrors &&
-      colony &&
-      openDraftConfirmDialog({
-        onClick: () => {
-          handleSubmit(values as any);
-          setActiveStateId?.(Stage.Draft);
-        },
-        isVotingExtensionEnabled: true,
-        colony,
-      })
-    );
+    return !errorsLength && colony
+      ? openDraftConfirmDialog({
+          onClick: () => {
+            handleSubmit(values as any);
+            setActiveStateId?.(Stage.Draft);
+          },
+          isVotingExtensionEnabled: true, // 'true' is temporary value
+          colony,
+        })
+      : errorsLength;
   }, [
     colony,
     handleSubmit,
@@ -75,6 +89,7 @@ const FormStages = ({
         setFormValues?.(undefined);
       },
     });
+
   const activeState = states.find((state) => state.id === activeStateId);
 
   const handleButtonClick = useCallback(async () => {
@@ -88,23 +103,51 @@ const FormStages = ({
     }
   }, [activeState, handleSubmit, setTouched, validateForm, values]);
 
-  if (values.expenditure === ExpenditureTypes.Streaming) {
-    return <StreamingStages handleSaveDraft={handleSaveDraft} />;
-  }
+  const handleSubmitButtonClick = () => {
+    const firstInvalidEl = document
+      .getElementById('expenditureForm')
+      ?.querySelectorAll('[aria-invalid="true"]')[0];
+
+    firstInvalidEl.style.border = '1px solid deeppink';
+    firstInvalidEl?.focus();
+  };
 
   return (
-    <Stages
-      {...{
-        states,
-        activeStateId,
-        setActiveStateId,
-        handleButtonClick,
-        handleDeleteDraft,
-        handleSaveDraft,
-        handleCancelExpenditure,
-        colony,
-      }}
-    />
+    <div className={styles.formStages}>
+      {!!fieldErrors && (
+        <div className={styles.formStagesMsg}>
+          <p className={styles.formStagesMsgText}>
+            <FormattedMessage
+              {...MSG.errorMessage}
+              values={{ number: fieldErrors }}
+            />
+          </p>
+          <button
+            type="button"
+            onClick={handleSubmitButtonClick}
+            className={styles.formStagesMsgAction}
+          >
+            <FormattedMessage {...MSG.errorMessageAction} />
+          </button>
+        </div>
+      )}
+      {values.expenditure === ExpenditureTypes.Streaming ? (
+        <StreamingStages handleSaveDraft={handleSaveDraft} />
+      ) : (
+        <Stages
+          {...{
+            states,
+            activeStateId,
+            setActiveStateId,
+            handleButtonClick,
+            handleDeleteDraft,
+            handleSaveDraft,
+            handleCancelExpenditure,
+            colony,
+          }}
+        />
+      )}
+    </div>
   );
 };
 
