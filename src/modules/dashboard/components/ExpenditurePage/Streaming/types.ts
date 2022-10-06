574f61e7474e0a77653260f0daab5f2a80195b52
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-10-06 14:31:10 +0200

    added Limit component

--- src/modules/dashboard/components/ExpenditurePage/Streaming/types.ts
+++ src/modules/dashboard/components/ExpenditurePage/Streaming/types.ts
@@ -1,18 +1,12 @@
-import { DatePickerFieldValue } from '~core/Fields/DatePicker/DatePicker';
-import { AnyUser } from '~data/index';
-import { ExpenditureEndDateTypes } from '~pages/ExpenditurePage/types';
-
-export interface Rate {
-  amount?: number;
-  token?: string;
-  time?: string;
-  limit?: number;
-  id: string;
-}
-
 export interface FundingSource {
   team: string;
-  rate: Rate[];
+  rate: {
+    amount?: number;
+    token?: string;
+    time?: string;
+    limit?: number;
+    id: string;
+  }[];
   isExpanded: boolean;
   id: string;
 }
