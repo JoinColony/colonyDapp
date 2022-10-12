e8c92b58a4622ba43e0d72d2e6d09bd8c79ee3bc
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-10-12 15:08:11 +0200

    ChangedStaged refactoring

--- src/modules/dashboard/components/Dialogs/EditExpenditureDialog/ChangedStaged/ChangedStaged.css.d.ts
+++ src/modules/dashboard/components/Dialogs/EditExpenditureDialog/ChangedStaged/ChangedStaged.css.d.ts
@@ -2,6 +2,3 @@ export const buttonWrappper: string;
 export const discard: string;
 export const changeContainer: string;
 export const subheader: string;
-export const row: string;
-export const arrowIcon: string;
-export const right: string;
