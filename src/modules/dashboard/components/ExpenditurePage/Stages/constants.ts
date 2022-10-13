686536298850cb4f546911748aa204931ee399b9
joanna <joanna.waszkiewicz@pagepro.pl>
2022-10-13 15:31:14 +0200

    fix: dynamical error message

--- src/modules/dashboard/components/ExpenditurePage/Stages/constants.ts
+++ src/modules/dashboard/components/ExpenditurePage/Stages/constants.ts
@@ -30,3 +30,21 @@ export interface Motion {
   type: MotionType;
   status: MotionStatus;
 }
+
+export const useObserver = (
+  setFieldErrorsAmount: React.Dispatch<React.SetStateAction<number>>,
+) => {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === 'attributes') {
+        const nodeListLength = document
+          .getElementById('expenditurePage')
+          ?.querySelectorAll('[aria-invalid="true"]').length;
+
+        setFieldErrorsAmount(nodeListLength || 0);
+      }
+    });
+  });
+
+  return { observer };
+};
