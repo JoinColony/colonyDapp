db7e2e7d59a03eb6dda75a65a9cab95964c65b3e
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-10-13 09:04:00 +0200

    ChangedSplit refactoring

--- src/modules/dashboard/components/Dialogs/EditExpenditureDialog/ChangedSplit/ChangedSplit.tsx
+++ src/modules/dashboard/components/Dialogs/EditExpenditureDialog/ChangedSplit/ChangedSplit.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { defineMessages, FormattedMessage } from 'react-intl';
+import React, { Fragment, useMemo } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
 
 import Button from '~core/Button';
 import { Colony, LoggedInUser } from '~data/index';
 import { ValuesType } from '~pages/ExpenditurePage/types';
 import { Staged } from '~dashboard/ExpenditurePage/Staged/types';
 import { Batch } from '~dashboard/ExpenditurePage/Batch/types';
-import { FormSection } from '~core/Fields';
-import Icon from '~core/Icon';
 
 import ChangeItem from '../ChangedMultiple/ChangeItem';
-import ChangeHeader from '../ChangedMultiple/ChangeHeader';
-import NewRecipient from '../NewRecipient';
+import ChangeHeader from '../ChangeHeader';
 
 import { isRecipientType } from './utils';
+import ChangedRecipient from './ChangedRecipient';
 import styles from './ChangedSplit.css';
 
 export const MSG = defineMessages({
@@ -23,24 +21,27 @@ export const MSG = defineMessages({
   },
   change: {
     id: 'dashboard.EditExpenditureDialog.ChangedSplit.change',
-    defaultMessage: 'Change Split',
-  },
-  from: {
-    id: 'dashboard.EditExpenditureDialog.ChangedSplit.from',
-    defaultMessage: 'From',
-  },
-  to: {
-    id: 'dashboard.EditExpenditureDialog.ChangedSplit.to',
-    defaultMessage: 'To',
+    defaultMessage: 'Split',
   },
   removed: {
     id: 'dashboard.EditExpenditureDialog.ChangedSplit.removed',
     defaultMessage: 'Recipient has been deleted',
   },
+  changeRecipient: {
+    id: 'dashboard.EditExpenditureDialog.ChangedSplit.changeRecipient',
+    defaultMessage: 'Change Recipient',
+  },
 });
 
 const displayName = 'dashboard.EditExpenditureDialog.ChangedSplit';
-const skip = ['id', 'claimDate', 'isExpanded', 'created', 'percent', 'unequal'];
+export const skip = [
+  'id',
+  'claimDate',
+  'isExpanded',
+  'created',
+  'percent',
+  'unequal',
+];
 
 export type NewValueType = {
   id: string;
@@ -69,111 +70,83 @@ const ChangedSplit = ({
   colony,
   discardChange,
 }: Props) => {
+  const { formatMessage } = useIntl();
+
+  const changedSplit = useMemo(
+    () =>
+      typeof newValues?.value === 'object'
+        ? Object.entries(newValues.value).filter(([key, value]) => {
+            return !(skip.includes(key) || Array.isArray(value));
+          })
+        : [],
+    [newValues],
+  );
+
+  const changedRecipients = useMemo(
+    () =>
+      typeof newValues?.value === 'object'
+        ? Object.entries(newValues.value).filter(
+            ([key, value]) => !skip.includes(key) && Array.isArray(value),
+          )
+        : [],
+    [newValues],
+  );
+
   if (!newValues) {
     return null;
   }
 
   return (
     <>
-      {typeof newValues?.value === 'object' &&
-        Object.entries(newValues.value)
-          .filter(([key, value]) => {
-            return !(skip.includes(key) || Array.isArray(value));
-          })
-          .map(([key, value]) => {
-            const oldValue = oldValues[newValues.key]?.[key]; // oldValue is a split.amount or split.recipients
+      {changedSplit.map(([key, value], index) => {
+        const oldValue = oldValues[newValues.key]?.[key];
 
-            return (
-              <>
-                <FormSection appearance={{ border: 'bottom' }}>
-                  <div className={styles.changeContainer}>
-                    <FormattedMessage
-                      {...MSG.change}
-                      values={{
-                        name: key,
-                      }}
-                    />
-                  </div>
-                  <div className={styles.subheader}>
-                    <span>
-                      <FormattedMessage {...MSG.from} />
-                    </span>
-                    <span>
-                      <FormattedMessage {...MSG.to} />
-                    </span>
-                  </div>
-                </FormSection>
-                <ChangeItem
-                  newValue={value}
-                  oldValue={oldValue}
-                  key={value.id}
-                  colony={colony}
-                  name={key}
-                />
-              </>
-            );
-          })}
-      {typeof newValues?.value === 'object' &&
-        Object.entries(newValues.value)
-          .filter(([key, value]) => {
-            return !skip.includes(key) && Array.isArray(value);
-          })
-          .map(([key, value]) => {
-            const oldValue = oldValues[newValues.key]?.[key]; // oldValue is a split.amount or split.recipients
+        return (
+          // eslint-disable-next-line react/no-array-index-key
+          <Fragment key={index}>
+            <ChangeHeader name={formatMessage(MSG.change)} />
+            <ChangeItem
+              newValue={value}
+              oldValue={oldValue}
+              key={value.id}
+              colony={colony}
+              name={key}
+            />
+          </Fragment>
+        );
+      })}
 
-            return (
-              <>
-                {value.map((changeItem, idx) => {
-                  // changeItem is split.recipient
-                  if (!isRecipientType(changeItem)) {
-                    return null;
-                  }
-                  const oldRecipienet = oldValue.find(
-                    (rec) => rec.id === changeItem.id,
-                  );
+      {changedRecipients.map(([key, value]) => {
+        const oldRecipients = oldValues[newValues.key]?.[key];
 
-                  return (
-                    <>
-                      <ChangeHeader name={key} index={idx} />
-                      {Object.entries(changeItem).map(
-                        ([recipientKey, recipientValue], index) => {
-                          // recipientKey - 'user', 'amount', 'id'
-                          if (skip.includes(recipientKey)) {
-                            return null;
-                          }
+        return (
+          <Fragment key={key}>
+            {value.map((recipient, idx) => {
+              if (!isRecipientType(recipient)) {
+                return null;
+              }
+              const oldRecipienet = oldRecipients.find(
+                (rec) => rec.id === recipient.id,
+              );
 
-                          if (recipientKey === 'removed') {
-                            return (
-                              <div className={styles.row}>
-                                <NewRecipient newValue={oldRecipienet.user} />
-                                <Icon
-                                  name="arrow-right"
-                                  className={styles.arrowIcon}
-                                />
-                                <span className={styles.right}>
-                                  <FormattedMessage {...MSG.removed} />
-                                </span>
-                              </div>
-                            );
-                          }
-
-                          return (
-                            <ChangeItem
-                              newValue={recipientValue}
-                              oldValue={oldRecipienet?.[recipientKey]}
-                              key={recipientValue.id || index}
-                              colony={colony}
-                              name={recipientKey}
-                            />
-                          );
-                        },
-                      )}
-                    </>
-                  );
-                })}
-              </>
-            );
-          })}
+              return (
+                <Fragment key={recipient.id}>
+                  <ChangeHeader
+                    name={formatMessage(MSG.changeRecipient)}
+                    count={idx + 1}
+                    withCounter
+                  />
+                  <ChangedRecipient
+                    recipient={recipient}
+                    oldRecipient={oldRecipienet}
+                    colony={colony}
+                  />
+                </Fragment>
+              );
+            })}
+          </Fragment>
+        );
+      })}
       <div className={styles.buttonWrappper}>
         <Button
           className={styles.discard}
