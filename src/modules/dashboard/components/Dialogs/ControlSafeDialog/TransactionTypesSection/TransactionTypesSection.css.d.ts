cbedafa1795d7ff7b5b553da23239164b66a66a9
willm30 <williamajmartin@gmail.com>
2022-10-13 16:34:10 +0200

    Fix: transfer funds loading when no safe selected

--- src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/TransactionTypesSection.css.d.ts
+++ src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/TransactionTypesSection.css.d.ts
@@ -3,5 +3,4 @@ export const contractFunctionSelectorContainer: string;
 export const labelDescription: string;
 export const inputParamContainer: string;
 export const spinner: string;
-export const balanceError: string;
 export const error: string;
