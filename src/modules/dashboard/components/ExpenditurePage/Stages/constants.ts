d683c29ed4076aacddfe4e807b4be680d29e0671
joanna <joanna.waszkiewicz@pagepro.pl>
2022-10-14 09:08:03 +0200

    fix: move useObserver to hooks

--- src/modules/dashboard/components/ExpenditurePage/Stages/constants.ts
+++ src/modules/dashboard/components/ExpenditurePage/Stages/constants.ts
@@ -30,21 +30,3 @@ export interface Motion {
   type: MotionType;
   status: MotionStatus;
 }
-
-export const useObserver = (
-  setFieldErrorsAmount: React.Dispatch<React.SetStateAction<number>>,
-) => {
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.type === 'attributes') {
-        const nodeListLength = document
-          .getElementById('expenditurePage')
-          ?.querySelectorAll('[aria-invalid="true"]').length;
-
-        setFieldErrorsAmount(nodeListLength || 0);
-      }
-    });
-  });
-
-  return { observer };
-};
