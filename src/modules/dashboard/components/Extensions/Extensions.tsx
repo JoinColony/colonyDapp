3a213e4308712cee95fb869949d8c4ef2adcfd22
jayaFeola <34057551+chinins@users.noreply.github.com>
2022-10-18 15:51:50 +0300

    Refactor: update version destructuring in `Extensions`

--- src/modules/dashboard/components/Extensions/Extensions.tsx
+++ src/modules/dashboard/components/Extensions/Extensions.tsx
@@ -51,23 +51,35 @@ const Extensions = ({ colonyAddress }: Props) => {
 
   const { data: networkExtensionData } = useNetworkExtensionVersionQuery();
 
+  console.log('GOT HERE 1');
+  console.log(
+    '🚀 ~ file: Extensions.tsx ~ line 58 ~ installedExtensionsData ~ data?.processedColony?.installedExtensions',
+    data?.processedColony?.installedExtensions,
+  );
+
   const installedExtensionsData = useMemo(() => {
     if (data?.processedColony?.installedExtensions) {
       const { installedExtensions } = data.processedColony;
-      return installedExtensions.map(
-        ({ extensionId, address, details: { version } }) => ({
-          ...extensionData[extensionId],
-          address,
-          currentVersion: version,
-        }),
-      );
+      return installedExtensions.map(({ extensionId, address, details }) => ({
+        ...extensionData[extensionId],
+        address,
+        currentVersion: details?.version || 0,
+      }));
     }
     return [];
   }, [data]);
+  console.log(
+    '🚀 ~ file: Extensions.tsx ~ line 67 ~ installedExtensionsData ~ installedExtensionsData',
+    installedExtensionsData,
+  );
 
   const availableExtensionsData = useMemo(() => {
     if (data?.processedColony?.installedExtensions) {
       const { installedExtensions } = data.processedColony;
+      console.log(
+        '🚀 ~ file: Extensions.tsx ~ line 73 ~ availableExtensionsData ~ installedExtensions',
+        installedExtensions,
+      );
 
       return allAllowedExtensions.reduce(
         (availableExtensions, extensionName) => {
@@ -82,6 +94,10 @@ const Extensions = ({ colonyAddress }: Props) => {
             const networkExtension = networkExtensionVersion?.find(
               (extension) =>
                 extension?.extensionHash === getExtensionHash(extensionName),
+            );
+            console.log(
+              '🚀 ~ file: Extensions.tsx ~ line 98 ~ availableExtensionsData ~ networkExtension',
+              networkExtension,
             );
             return [
               ...availableExtensions,
@@ -98,6 +114,10 @@ const Extensions = ({ colonyAddress }: Props) => {
     }
     return [];
   }, [data, networkExtensionData]);
+  console.log(
+    '🚀 ~ file: Extensions.tsx ~ line 103 ~ availableExtensionsData',
+    availableExtensionsData,
+  );
 
   if (loading) {
     return (
