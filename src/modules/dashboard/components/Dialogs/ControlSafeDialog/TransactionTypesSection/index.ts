4695eadf9cb0c940facf1b2d328348b1b4cb3efc
willm30 <williamajmartin@gmail.com>
2022-10-19 15:44:28 +0200

    Refactor invalid safe error

--- src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/index.ts
+++ src/modules/dashboard/components/Dialogs/ControlSafeDialog/TransactionTypesSection/index.ts
@@ -2,3 +2,4 @@ export { default as TransferFundsSection } from './TransferFundsSection';
 export { default as RawTransactionSection } from './RawTransactionSection';
 export { default as ContractInteractionSection } from './ContractInteractionSection';
 export { default as TransferNFTSection } from './TransferNFTSection';
+export * from './shared';
