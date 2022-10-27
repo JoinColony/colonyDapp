e61f67acbb4702c577110afbbc515431aa83ed80
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-10-27 13:45:04 +0200

    fixed types errors

--- src/modules/dashboard/components/ExpenditurePage/Streaming/types.ts
+++ src/modules/dashboard/components/ExpenditurePage/Streaming/types.ts
@@ -1,12 +1,18 @@
+import { DatePickerFieldValue } from '~core/Fields/DatePicker/DatePicker';
+import { AnyUser } from '~data/index';
+import { ExpenditureEndDateTypes } from '~pages/ExpenditurePage/types';
+
+export interface Rate {
+  amount?: number;
+  token?: string;
+  time?: string;
+  limit?: number;
+  id: string;
+}
+
 export interface FundingSource {
   team: string;
-  rate: {
-    amount?: number;
-    token?: string;
-    time?: string;
-    limit?: number;
-    id: string;
-  }[];
+  rate: Rate[];
   isExpanded: boolean;
   id: string;
 }
