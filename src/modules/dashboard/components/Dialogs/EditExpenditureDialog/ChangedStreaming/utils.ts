ca452227eac12bafc182afd4d6698c4c4e4179bb
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-11-10 12:37:24 +0100

    fixed messages in edit modal

--- src/modules/dashboard/components/Dialogs/EditExpenditureDialog/ChangedStreaming/utils.ts
+++ src/modules/dashboard/components/Dialogs/EditExpenditureDialog/ChangedStreaming/utils.ts
@@ -1,23 +1,36 @@
-import { Streaming } from '~dashboard/ExpenditurePage/Streaming/types';
+import {
+  FundingSource,
+  Streaming,
+} from '~dashboard/ExpenditurePage/Streaming/types';
 import { ExpenditureEndDateTypes } from '~pages/ExpenditurePage/types';
 
 import { checkIfRateIsChanged } from './ChangedRate/utils';
 
-export const hasChanges = ({ newValue, oldValue, endDate }) => {
+interface Params {
+  newValue: Partial<FundingSource>;
+  oldValue?: FundingSource;
+  endDate: ExpenditureEndDateTypes;
+}
+
+export const fundingSourceWasChanged = ({
+  newValue,
+  oldValue,
+  endDate,
+}: Params) => {
   if (!oldValue) {
-    return true;
+    return true; // funding shource has been added
   }
 
   if ('removed' in newValue) {
-    return true;
+    return true; // removed funding source
   }
 
   if (newValue.team && newValue.team !== oldValue.team) {
-    return true;
+    return true; // changed team property
   }
 
   if (endDate === ExpenditureEndDateTypes.LimitIsReached) {
-    return true;
+    return true; // if limit is required, we want to show every change
   }
 
   // check if rates array has been changed
@@ -41,4 +54,25 @@ export const hasChanges = ({ newValue, oldValue, endDate }) => {
 
 export const isStreamingPaymentType = (obj: any): obj is Streaming => {
   return Object.prototype.hasOwnProperty.call(obj, 'fundingSources');
+};
+
+interface RemovedRatesParams {
+  newFundingSource: Partial<FundingSource & { removed?: boolean }>;
+  oldFundingSource?: FundingSource;
+}
+
+export const findRemovedRates = ({
+  newFundingSource,
+  oldFundingSource,
+}: RemovedRatesParams) => {
+  if (newFundingSource.removed) {
+    return undefined;
+  }
+  if (!newFundingSource.rates) {
+    return undefined;
+  }
+  return oldFundingSource?.rates.filter(
+    (oldItem) =>
+      !newFundingSource?.rates?.find((rate) => rate.id === oldItem.id),
+  );
 };
