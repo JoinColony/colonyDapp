5518b032d4775a92e3e567cac3b9a96182b25586
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-11-18 23:38:23 +0100

    added token balance validation

--- src/modules/dashboard/components/ExpenditurePage/Stages/StreamingStages/StreamingStagesLocked/hooks.ts
+++ src/modules/dashboard/components/ExpenditurePage/Stages/StreamingStages/StreamingStagesLocked/hooks.ts
@@ -1,5 +1,18 @@
 import { useState } from 'react';
-import { Rate } from '~dashboard/ExpenditurePage/Streaming/types';
+import moveDecimal from 'move-decimal-point';
+import { isEmpty, uniq } from 'lodash';
+import { BigNumber, bigNumberify } from 'ethers/utils';
+
+import {
+  FundingSource,
+  Rate,
+} from '~dashboard/ExpenditurePage/Streaming/types';
+import { Colony } from '~data/index';
+import { useTokenBalancesForDomainsQuery } from '~data/generated';
+import {
+  getBalanceFromToken,
+  getTokenDecimalsWithFallback,
+} from '~utils/tokens';
 
 export const useClaimStreamingPayment = (
   initialAvailableToClaim?: Rate[],
@@ -53,4 +66,97 @@ export const useClaimStreamingPayment = (
     claimed,
     setAvailableToClaim,
   };
+};
+
+export const useAvailableFundsInTeam = ({
+  fundingSources,
+  colony,
+}: {
+  fundingSources?: FundingSource[];
+  colony?: Colony;
+}) => {
+  const { colonyAddress, domains, tokens: colonyTokens } = colony || {};
+  const domainIds = fundingSources?.map((fundingSource) => {
+    const domain = domains?.find(
+      ({ ethDomainId }) => Number(fundingSource.team) === ethDomainId,
+    );
+    return domain?.ethDomainId || 0;
+  });
+
+  const { data } = useTokenBalancesForDomainsQuery({
+    variables: {
+      colonyAddress: colonyAddress || '',
+      domainIds,
+      tokenAddresses: colonyTokens?.map((token) => token.address || '') || [''],
+    },
+    fetchPolicy: 'network-only',
+  });
+
+  // checking each funding source - if team has enough balance
+  const notFundedTeams = fundingSources?.reduce<{
+    tokens: string[];
+    teams: string[];
+  }>(
+    (accumulator, fundingSource) => {
+      // calculate tokens, create an array with rates summed by token id
+      const tokensSum = fundingSource.rates.reduce<
+        { token: string; amount: BigNumber }[]
+      >((acc, curr) => {
+        if (!curr.token) return acc;
+
+        const tokenObj = colonyTokens?.find(
+          (tokenItem) => tokenItem.id === curr.token,
+        );
+        const convertedAmount = bigNumberify(
+          moveDecimal(
+            curr.amount,
+            getTokenDecimalsWithFallback(tokenObj?.decimals),
+          ),
+        );
+        // if token has already been added to the tokensSum array, then add to the exisitng object
+        if (acc?.find((accItem) => accItem?.token === curr.token)) {
+          return acc.map((accItem) =>
+            accItem?.token === curr.token
+              ? { ...accItem, amount: accItem.amount.add(convertedAmount) }
+              : accItem,
+          );
+        }
+        // if not added, then add a new object with token
+        return [...acc, { token: curr.token, amount: convertedAmount }];
+      }, []);
+      const domain = domains?.find(
+        ({ ethDomainId }) => Number(fundingSource.team) === ethDomainId,
+      );
+
+      // if team from funding source hasn't got enough balance,
+      // then add the tokenId to the notEnoughBalances array
+      const notEnoughBalances = tokensSum.reduce<string[]>((acc, curr) => {
+        const tokenItem = data?.tokens.find((token) => token.id === curr.token);
+        const tokenBalance = getBalanceFromToken(
+          tokenItem,
+          domain?.ethDomainId,
+        );
+
+        if (tokenBalance.lt(curr.amount)) {
+          return [...acc, curr.token];
+        }
+        return acc;
+      }, []);
+
+      return isEmpty(notEnoughBalances)
+        ? accumulator
+        : {
+            teams: [...accumulator.teams, fundingSource.team],
+            tokens: [...accumulator.tokens, ...notEnoughBalances],
+          };
+    },
+    { teams: [], tokens: [] },
+  );
+  const { teams, tokens } = notFundedTeams || {};
+  const teamsWithError = {
+    teams: uniq(teams),
+    tokens: uniq(tokens),
+  };
+
+  return teamsWithError;
 };
