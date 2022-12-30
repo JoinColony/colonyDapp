651243706734dbd04420cf02bb0e794734893a28
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2022-12-30 16:15:35 +0100

    added Stages section

--- src/modules/pages/components/IncorporationPage/IncorporationPage.tsx
+++ src/modules/pages/components/IncorporationPage/IncorporationPage.tsx
@@ -1,13 +1,18 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { RouteChildrenProps, useParams } from 'react-router';
 import { Formik } from 'formik';
 
 import { useColonyFromNameQuery } from '~data/generated';
 import { getMainClasses } from '~utils/css';
 import { SpinnerLoader } from '~core/Preloaders';
-import IncorporationForm from '~dashboard/Incorporation/IncorporationForm';
+import IncorporationForm from '~dashboard/DAOIncorporation/IncorporationForm';
+import Stages from '~dashboard/DAOIncorporation/Stages';
 
-import { initialValues } from './constants';
+import {
+  initialValues,
+  stages,
+  Stages as StagesEnum,
+} from './constants';
 import styles from './IncorporationPage.css';
 
 const displayName = 'pages.IncorporationPage';
@@ -20,7 +25,39 @@ const IncorporationPage = () => {
   const { colonyName } = useParams<{
     colonyName: string;
   }>();
+  const [activeStageId, setActiveStageId] = useState(StagesEnum.Draft);
   const sidebarRef = useRef<HTMLElement>(null);
+
+  const handleSubmit = useCallback(() => {
+    setActiveStageId(StagesEnum.Created);
+  }, []);
+
+  const handleProceed = useCallback(() => {
+    setActiveStageId(StagesEnum.Payment);
+  }, []);
+
+  const handlePay = useCallback(() => {
+    setActiveStageId(StagesEnum.Processing);
+  }, []);
+
+  const buttonAction = useMemo(() => {
+    switch (activeStageId) {
+      case StagesEnum.Draft: {
+        return handleSubmit;
+      }
+      case StagesEnum.Created: {
+        return handleProceed;
+      }
+      case StagesEnum.Payment: {
+        return handlePay;
+      }
+      default: {
+        return () => {};
+      }
+    }
+  }, [activeStageId, handlePay, handleProceed, handleSubmit]);
+
+
 
   const { data: colonyData, loading } = useColonyFromNameQuery({
     variables: { name: colonyName, address: '' },
@@ -44,7 +81,16 @@ const IncorporationPage = () => {
               )
             )}
           </aside>
-          <div />
+          <div className={styles.mainContainer}>
+            <main className={styles.mainContent}>
+              <div />
+              <Stages
+                activeStageId={activeStageId}
+                stages={stages}
+                buttonAction={buttonAction}
+              />
+            </main>
+          </div>
         </div>
       )}
     </Formik>
