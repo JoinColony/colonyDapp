5a5dc84bc100de7256e945ba46dfee17b0f2c846
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2023-01-16 13:37:24 +0100

    fixed types

--- src/modules/dashboard/components/Incorporation/Stages/FormStages.tsx
+++ src/modules/dashboard/components/Incorporation/Stages/FormStages.tsx
@@ -98,6 +98,7 @@ const FormStages = ({ stages, activeStageId }: Props) => {
         stages={stages}
         activeStageId={activeStageId}
         buttonAction={handleSaveDraft}
+        handleCancelIncorporation={() => {}}
       />
     </div>
   );
