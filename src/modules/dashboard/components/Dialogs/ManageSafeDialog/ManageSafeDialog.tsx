136362408e20ca728b5625fefde3fb83da08f513
ArmandoGraterol <armandogrtrl@gmail.com>
2023-01-26 15:18:15 -0300

    Update name and logo of Safe Control in the UAC

--- src/modules/dashboard/components/Dialogs/ManageSafeDialog/ManageSafeDialog.tsx
+++ src/modules/dashboard/components/Dialogs/ManageSafeDialog/ManageSafeDialog.tsx
@@ -12,7 +12,7 @@ import { useTransformer, WizardDialogType } from '~utils/hooks';
 const MSG = defineMessages({
   dialogHeader: {
     id: 'dashboard.ManageSafeDialog.dialogHeader',
-    defaultMessage: 'Safe Control',
+    defaultMessage: 'Safe (multi-sig) Control',
   },
   addExistingSafeTitle: {
     id: 'dashboard.ManageSafeDialog.addExistingSafeTitle',
