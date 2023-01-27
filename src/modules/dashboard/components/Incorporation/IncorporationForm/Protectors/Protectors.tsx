4430b8f1f099ffa746d4f420b6a9d137207dc545
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2023-01-27 14:17:10 +0100

    removed duplicated import

--- src/modules/dashboard/components/Incorporation/IncorporationForm/Protectors/Protectors.tsx
+++ src/modules/dashboard/components/Incorporation/IncorporationForm/Protectors/Protectors.tsx
@@ -18,7 +18,6 @@ import Button from '~core/Button';
 import Radio from '../Radio';
 import { SignOption } from '../types';
 import SingleUserPicker from '../SingleUserPicker';
-import { SignOption } from '../constants';
 
 import styles from './Protectors.css';
 
