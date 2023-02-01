19483da96b58f4b4d7a876184b2b20b0c32e66d8
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2023-02-01 14:52:16 +0100

    added info banner

---
+++ src/modules/dashboard/components/Incorporation/InfoBanner/InfoBanner.css.d.ts
@@ -0,0 +1,6 @@
+export const wrapper: string;
+export const buttonsWrapper: string;
+export const title: string;
+export const description: string;
+export const removeButton: string;
+export const verifyButton: string;
