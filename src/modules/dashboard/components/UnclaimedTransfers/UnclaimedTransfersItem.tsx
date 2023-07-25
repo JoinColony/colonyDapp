f95ab5b9bc0e1d108edab59c1aa7d3b2c0fe956d
Raul Glogovetan <raul@glogovetan.com>
2023-07-25 12:15:53 +0300

    Fix: `UnclaimedTransferItem` handle non ERC20 tokens

--- src/modules/dashboard/components/UnclaimedTransfers/UnclaimedTransfersItem.tsx
+++ src/modules/dashboard/components/UnclaimedTransfers/UnclaimedTransfersItem.tsx
@@ -53,7 +53,7 @@ const UnclaimedTransfersItem = ({
 
   const isNetworkAllowed = checkIfNetworkIsAllowed(networkId);
 
-  if (!tokenData) return null;
+  if (!tokenData || !tokenData.token) return null;
 
   const { token } = tokenData;
 
